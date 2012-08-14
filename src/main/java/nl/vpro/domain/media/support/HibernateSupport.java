806f89fab6a007359c3dd7cfa436494102797ce3
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2012-08-14 09:22:59 +0000

    started on a straight-forward project to collect shared classes between user/media/image projects
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=47339

---
+++ src/main/java/nl/vpro/domain/media/support/HibernateSupport.java
@@ -0,0 +1,43 @@
+package nl.vpro.domain.media.support;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.search.FullTextSession;
+import org.hibernate.search.Search;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+
+public class HibernateSupport {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+//    private TransactionTemplate transactionTemplate;
+
+    protected Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    protected FullTextSession getFullTextSession() {
+        return Search.getFullTextSession(getSession());
+    }
+
+    protected TransactionTemplate getReadOnlyTransactionTemplate() {
+        final TransactionTemplate transactionTemplate = getTransactionTemplate();
+        transactionTemplate.setReadOnly(true);
+        return transactionTemplate;
+    }
+
+    protected TransactionTemplate getTransactionTemplate() {
+//        if(transactionTemplate == null) {
+//            this.transactionTemplate = new TransactionTemplate(transactionManager);
+//        }
+
+        return new TransactionTemplate(transactionManager);
+    }
+}
