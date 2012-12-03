cdb22118da6ab6b35a2a936631d542b505a6bc4e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2012-12-03 13:01:09 +0000

    MSE-1743
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=51937

--- poms-shared/src/main/java/nl/vpro/util/CommandExecutorImpl.java
+++ poms-shared/src/main/java/nl/vpro/util/CommandExecutorImpl.java
@@ -3,9 +3,7 @@ package nl.vpro.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -29,6 +27,11 @@ public class CommandExecutorImpl implements CommandExecutor {
 
     private final String binary;
 
+
+    private long processTimeout = -1l;
+    private static final Timer PROCESS_MONITOR = new Timer(true); // create as daemon so that it shuts down at program exit
+
+
     public CommandExecutorImpl(String c) {
         binary = c;
     }
@@ -40,7 +43,7 @@ public class CommandExecutorImpl implements CommandExecutor {
     }
 
     @Override
-    public int execute(OutputStream out, OutputStream errors, String... args) {
+    public int execute(final OutputStream out, OutputStream errors, String... args) {
         if (errors == null) {
             errors = LoggerOutputStream.error(getLogger(), true);
         }
@@ -52,17 +55,30 @@ public class CommandExecutorImpl implements CommandExecutor {
             Collections.addAll(command, args);
             LOG.info("Executing " + command);
             p = pb.start();
-            Copier copier       = copyThread(p.getInputStream(), out);
+            final ProcessTimeoutHandle handle;
+            if (processTimeout > 0l) {
+                handle = startProcessTimeoutMonitor(p, "" + command, processTimeout * 1000);
+            } else {
+                handle = null;
+            }
+            Copier copier       = out != null ? copyThread(p.getInputStream(), out) : null;
             Copier errorCopier  = copyThread(p.getErrorStream(), errors);
             p.waitFor();
-            copier.waitFor();
+            if (copier != null) {
+                copier.waitFor();
+            }
             errorCopier.waitFor();
             int result = p.exitValue();
             if (result != 0) {
                 LOG.error("Error {} occurred while calling {}  (see log)", result, command);
             }
-            out.flush();
+            if (out != null) {
+                out.flush();
+            }
             errors.close();
+            if (handle != null) {
+                handle.cancel();
+            }
             return result;
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
@@ -70,6 +86,10 @@ public class CommandExecutorImpl implements CommandExecutor {
             LOG.error(e.getMessage(), e);
             throw new RuntimeException(e);
         }
+    }
+
+    public void setProcessTimeout(long processTimeout) {
+        this.processTimeout = processTimeout;
     }
 
     Logger getLogger() {
@@ -134,6 +154,56 @@ public class CommandExecutorImpl implements CommandExecutor {
             return count;
         }
 
+    }
+
+    private static class ProcessTimeoutHandle {
+        private final ProcessTimeoutTask task;
+
+        protected ProcessTimeoutHandle(ProcessTimeoutTask task) {
+            this.task = task;
+        }
+
+        public void cancel() {
+            task.cancel();
+            PROCESS_MONITOR.purge();
+        }
+
+    }
+
+    private static class ProcessTimeoutTask extends TimerTask {
+        private final Process monitoredProcess;
+        private final String command;
+
+        protected ProcessTimeoutTask(Process monitoredProcess, String command) {
+            this.monitoredProcess = monitoredProcess;
+            this.command = command;
+        }
+
+        @Override
+        public void run() {
+            try {
+                // already terminated? If not, it throws IllegalThreadStateException, otherwise it will
+                // return forked process' exit value, which we aren't interested in
+                monitoredProcess.exitValue();
+            } catch (IllegalThreadStateException itse) {
+                // wasn't terminated, kill it
+                LOG.warn("The process {} took too long, killing it.", command);
+                monitoredProcess.destroy();
+            }
+        }
+    }
+
+
+    private static ProcessTimeoutHandle startProcessTimeoutMonitor(Process process, String command, long timeout) {
+        ProcessTimeoutTask task = new ProcessTimeoutTask(process, command); // task fires after timeout and kills the process
+        PROCESS_MONITOR.schedule(task, timeout); // schedule the task to fire
+
+        return new ProcessTimeoutHandle(task); // wrap the task so we can cancel when process finishes before timeout occurs
+    }
+
+    @Override
+    public String toString() {
+        return binary;
     }
 
 
