5c9340caf1aa3a37c41787dcdf1806a5bebe0f6d
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2013-01-22 17:02:13 +0000

    MSE-1832
    git-svn-id: svn+ssh://subversion.vpro.nl/export/svn/nl/vpro/images/trunk@53370 5d607819-5a00-0410-9ed8-a5c3eb9d915f

--- image-ws/src/main/java/nl/vpro/ws/image/DownloadFault.java
+++ image-ws/src/main/java/nl/vpro/ws/image/DownloadFault.java
@@ -9,14 +9,22 @@ import javax.xml.ws.WebFault;
 @WebFault(name = "downloadFault", targetNamespace = "urn:vpro:ws:image:2009")
 public class DownloadFault extends RuntimeException {
 
-    private ImageNotFound faultInfo;
+    private final ImageNotFound faultInfo;
 
-    public DownloadFault(String message, ImageNotFound faultInfo) {
+    public DownloadFault(String message) {
+        this(message, new ImageNotFound(message));
+    }
+
+    public DownloadFault(String message, Throwable cause) {
+        this(message, new ImageNotFound(message), cause);
+    }
+
+    private DownloadFault(String message, ImageNotFound faultInfo) {
         super(message);
         this.faultInfo = faultInfo;
     }
 
-    public DownloadFault(String message, ImageNotFound faultInfo, Throwable cause) {
+    private DownloadFault(String message, ImageNotFound faultInfo, Throwable cause) {
         super(message, cause);
         this.faultInfo = faultInfo;
     }
