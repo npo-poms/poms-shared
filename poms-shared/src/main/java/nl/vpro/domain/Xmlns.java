4e29d58ee209080ca94ec17eeb6512c3f234f9e0
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2014-06-30 14:03:52 +0000

    didn't compile
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=68257

--- poms-shared/src/main/java/nl/vpro/domain/Xmlns.java
+++ poms-shared/src/main/java/nl/vpro/domain/Xmlns.java
@@ -10,26 +10,22 @@ import javax.xml.validation.SchemaFactory;
 
 import org.xml.sax.SAXException;
 
-import nl.vpro.domain.media.MediaObject;
-import nl.vpro.domain.media.search.MediaForm;
-import nl.vpro.domain.media.update.MediaUpdate;
-
 /**
  * @author Michiel Meeuwissen
  * @since 1.5
  */
 public final class Xmlns {
     public static final String MEDIA_NAMESPACE = "urn:vpro:media:2009";
-    public static final URL    MEDIA_XSD       = MediaObject.class.getResource("vproMedia.xsd");
+    public static final URL    MEDIA_XSD       = Xmlns.class.getResource("/nl/vpro/domain/media/vproMedia.xsd");
 
     public static final String SHARED_NAMESPACE = "urn:vpro:shared:2009";
-    public static final URL    SHARED_XSD       = MediaObject.class.getResource("vproShared.xsd");
+    public static final URL    SHARED_XSD       = Xmlns.class.getResource("/nl/vpro/domain/media/vproShared.xsd");
 
     public static final String UPDATE_NAMESPACE = "urn:vpro:media:update:2009";
-    public static final URL    UPDATE_XSD       = MediaUpdate.class.getResource("vproMediaUpdate.xsd");
+    public static final URL    UPDATE_XSD       = Xmlns.class.getResource("//nl/vpro/domain/media/update/vproMediaUpdate.xsd");
 
     public static final String SEARCH_NAMESPACE = "urn:vpro:media:search:2012";
-    public static final URL    SEARCH_XSD        = MediaForm.class.getResource("vproMediaSearch.xsd");
+    public static final URL    SEARCH_XSD        = Xmlns.class.getResource("/nl/vpro/domain/media/search/vproMediaSearch.xsd");
 
     public static final Schema SCHEMA;
 
