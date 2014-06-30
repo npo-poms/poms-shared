a5b01090ee1db9eabde948e0b4df76ccfa72cbec
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2014-06-30 14:40:52 +0000

    MSE-2465
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=68260

--- poms-shared/src/main/java/nl/vpro/domain/Xmlns.java
+++ poms-shared/src/main/java/nl/vpro/domain/Xmlns.java
@@ -2,6 +2,8 @@ package nl.vpro.domain;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.XMLConstants;
 import javax.xml.transform.stream.StreamSource;
@@ -22,7 +24,7 @@ public final class Xmlns {
     public static final URL    SHARED_XSD       = Xmlns.class.getResource("/nl/vpro/domain/media/vproShared.xsd");
 
     public static final String UPDATE_NAMESPACE = "urn:vpro:media:update:2009";
-    public static final URL    UPDATE_XSD       = Xmlns.class.getResource("//nl/vpro/domain/media/update/vproMediaUpdate.xsd");
+    public static final URL    UPDATE_XSD       = Xmlns.class.getResource("/nl/vpro/domain/media/update/vproMediaUpdate.xsd");
 
     public static final String SEARCH_NAMESPACE = "urn:vpro:media:search:2012";
     public static final URL    SEARCH_XSD        = Xmlns.class.getResource("/nl/vpro/domain/media/search/vproMediaSearch.xsd");
@@ -32,16 +34,25 @@ public final class Xmlns {
     static {
         try {
             SCHEMA = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
-                .newSchema(
-                    new StreamSource[]{
-                        new StreamSource(SHARED_XSD.openStream()),
-                        new StreamSource(MEDIA_XSD.openStream()),
-                        new StreamSource(UPDATE_XSD.openStream()),
-                        new StreamSource(SEARCH_XSD.openStream())
-                    }
-                );
-        } catch (SAXException | IOException e) {
+                .newSchema(getStreamSources(SHARED_XSD, MEDIA_XSD, UPDATE_XSD, SEARCH_XSD));
+        } catch (SAXException e) {
             throw new RuntimeException(e);
         }
     }
+    private static StreamSource[] getStreamSources(URL... url) {
+        List<StreamSource> result = new ArrayList<>();
+        for (URL u : url) {
+            if (u != null) {
+                try {
+                    result.add(new StreamSource(u.openStream()));
+                } catch (IOException ioe) {
+                    System.err.println(ioe.getMessage());
+
+                }
+            }
+        }
+        return result.toArray(new StreamSource[result.size()]);
+
+    }
+
 }
