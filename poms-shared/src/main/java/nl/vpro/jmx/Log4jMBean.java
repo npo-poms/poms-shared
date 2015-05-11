d32a20e331cc4a52b001a4f440a9d5949b916ade
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-05-11 08:14:20 +0000

    moved from media-backend for easier reuse
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=78013

---
+++ poms-shared/src/main/java/nl/vpro/jmx/Log4jMBean.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (C) 2010 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.jmx;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedResource;
+
+@ManagedResource(
+        objectName="nl.vpro.media:name=logging",
+        description="Logging configuration.",
+        log=true,
+        logFile="jmx.log"
+)
+public class Log4jMBean {
+
+    @ManagedOperation(description="Get current level vor category")
+    public String getLevel(String category) {
+        return LogManager.getLogger(category).getLevel().toString();
+    }
+
+    @ManagedOperation(description="Level trace")
+    public void trace(String category) {
+        LogManager.getLogger(category).setLevel(Level.TRACE);
+    }
+
+    @ManagedOperation(description="Level debug")
+    public void debug(String category) {
+        LogManager.getLogger(category).setLevel(Level.DEBUG);
+    }
+
+    @ManagedOperation(description="Level info")
+    public void info(String category) {
+        LogManager.getLogger(category).setLevel(Level.INFO);
+    }
+
+    @ManagedOperation(description="Level warn")
+    public void warn(String category) {
+        LogManager.getLogger(category).setLevel(Level.WARN);
+    }
+
+    @ManagedOperation(description="Level error")
+    public void error(String category) {
+        LogManager.getLogger(category).setLevel(Level.ERROR);
+    }
+
+    @ManagedOperation(description="Level fatal")
+    public void fatal(String category) {
+        LogManager.getLogger(category).setLevel(Level.FATAL);
+    }
+}
