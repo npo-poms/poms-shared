4b185dc2abde323d57c6e60bef3e8fbe65fde1cb
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-02-29 12:09:49 +0000

    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=86392

--- media-classification/src/main/java/nl/vpro/domain/classification/ClassificationServiceLocator.java
+++ media-classification/src/main/java/nl/vpro/domain/classification/ClassificationServiceLocator.java
@@ -1,6 +1,7 @@
 package nl.vpro.domain.classification;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,32 +14,39 @@ public class ClassificationServiceLocator  {
 
     private static final Logger LOG = LoggerFactory.getLogger(ClassificationServiceLocator.class);
 
+    private static ClassificationServiceLocator singleton;
+
     @Inject
-    private static ClassificationService classificationService;
-    
+    private Provider<ClassificationService> classificationService;
+
     private ClassificationServiceLocator() {
+        singleton = this;
     }
 
     public static ClassificationService getInstance() {
-        if (classificationService == null) {
+        if (singleton == null || singleton.classificationService == null || singleton.classificationService.get() == null) {
             LOG.warn("No classification service bean found, returning the empty one");
             return EmptyClassificationService.INSTANCE;
         } else {
-            return classificationService;
+            return singleton.classificationService.get();
         }
     }
 
     public static void setInstance(ClassificationService classificationService) {
-        if (ClassificationServiceLocator.classificationService != null &&
-            ! ClassificationServiceLocator.classificationService.equals(classificationService)
+        if (singleton == null) {
+            new ClassificationServiceLocator();
+        }
+
+        if (singleton.classificationService != null && singleton.classificationService.get() != null &&
+            ! singleton.classificationService.get().equals(classificationService)
             ) {
             throw new IllegalStateException();
         }
         if (classificationService != null) {
             LOG.info("Using classification service {}", classificationService);
         }
-        ClassificationServiceLocator.classificationService = classificationService;
+        singleton.classificationService = () -> classificationService;
     }
 
- 
+
 }
