3e1e69d7e9ec8e4288dec8475d7c067894bd1626
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-04-15 12:29:12 +0000

    support what's on too.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=88264

--- media-organizations/src/main/java/nl/vpro/media/broadcaster/BroadcasterServiceImpl.java
+++ media-organizations/src/main/java/nl/vpro/media/broadcaster/BroadcasterServiceImpl.java
@@ -27,8 +27,8 @@ public class BroadcasterServiceImpl implements BroadcasterService {
 
     private Map<String, Broadcaster> broadcasterMap = new HashMap<>();
     private URLResource<Properties> displayNameResource;
-
     private URLResource<Properties> misResource;
+    private URLResource<Properties> whatsonResource;
 
 
     public BroadcasterServiceImpl(String configFile) {
@@ -36,22 +36,26 @@ public class BroadcasterServiceImpl implements BroadcasterService {
         if (uri.getScheme().startsWith("http")) {
             setMisResource(configFile + "mis");
         }
-        this.displayNameResource = new URLResource<>(URI.create(configFile), URLResource.PROPERTIES)
-            .setMinAge(Duration.of(1, ChronoUnit.HOURS))
-            .setAsync(true)
-            .setCallbacks(this::fillMap)
-        ;
+        this.displayNameResource = getURLResource(configFile);
     }
 
     public void setMisResource(String configFile) {
         LOG.info("Using {} for mis ids", configFile);
-        this.misResource = new URLResource<>(URI.create(configFile), URLResource.PROPERTIES)
-            .setMinAge(Duration.of(1, ChronoUnit.HOURS))
-            .setAsync(true)
-            .setCallbacks(this::fillMap)
-        ;
+        this.misResource = getURLResource(configFile);
     }
 
+
+    public void setWhatsonResource(String configFile) {
+        LOG.info("Using {} for what'son ids", configFile);
+        this.whatsonResource = getURLResource(configFile);
+    }
+
+    protected URLResource<Properties> getURLResource(String configFile) {
+        return new URLResource<>(URI.create(configFile), URLResource.PROPERTIES)
+            .setMinAge(Duration.of(1, ChronoUnit.HOURS))
+            .setAsync(true)
+            .setCallbacks(this::fillMap);
+    }
 
     @Override
     public Broadcaster find(String id) {
@@ -78,6 +82,9 @@ public class BroadcasterServiceImpl implements BroadcasterService {
         displayNameResource.get();
         if (misResource != null) {
             misResource.get();
+        }
+        if (whatsonResource!= null) {
+            whatsonResource.get();
         }
         return Collections.unmodifiableMap(broadcasterMap);
     }
