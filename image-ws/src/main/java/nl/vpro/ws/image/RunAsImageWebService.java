01f49e15d16062d0fc3db03a23e48465a619d11f
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2016-05-18 11:02:38 +0000

    Removed completely pointless javadoc.
    git-svn-id: svn+ssh://subversion.vpro.nl/export/svn/nl/vpro/images/trunk@89444 5d607819-5a00-0410-9ed8-a5c3eb9d915f

--- image-ws/src/main/java/nl/vpro/ws/image/RunAsImageWebService.java
+++ image-ws/src/main/java/nl/vpro/ws/image/RunAsImageWebService.java
@@ -26,15 +26,7 @@ import nl.vpro.domain.image.ImageType;
 public interface RunAsImageWebService {
 	// all methods get @WebParam(name = "principalId", targetNamespace = "") String principalId,
 
-	/**
-	 * upload
-     * @param principalId
-	 * @param title
-	 * @param description
-	 * @param type
-	 * @param data
-	 * @return String
-	 */
+
     @WebMethod
     @WebResult(
         name = "urn",
@@ -52,16 +44,6 @@ public interface RunAsImageWebService {
         @WebParam(name = "type", targetNamespace = "") ImageType type,
         @WebParam(name = "data", targetNamespace = "") DataHandler data);
 
-    /**
-     * uploadAsync
-     * @param principalId
-     * @param title
-     * @param description
-     * @param type
-     * @param data
-     * @param handler Future<?>
-     * @return
-     */
     @WebMethod(operationName = "upload")
     @RequestWrapper(
         localName = "upload",
@@ -77,15 +59,6 @@ public interface RunAsImageWebService {
         @WebParam(name = "data", targetNamespace = "") DataHandler data,
         AsyncHandler<UploadResponse> handler);
 
-    /**
-     * uploadAsync
-     * @param principalId
-     * @param title
-     * @param description
-     * @param type
-     * @param data
-     * @return UploadResponse
-     */
     @WebMethod(operationName = "upload")
     @RequestWrapper(
         localName = "upload",
@@ -100,16 +73,6 @@ public interface RunAsImageWebService {
         @WebParam(name = "type", targetNamespace = "") ImageType type,
         @WebParam(name = "data", targetNamespace = "") DataHandler data);
 
-    /**
-     * download
-     * @param principalId
-     * @param title
-     * @param description
-     * @param type
-     * @param url
-     * @return String result
-     * @throws DownloadFault
-     */
     @WebMethod
     @WebResult(
         name = "urn",
@@ -127,16 +90,7 @@ public interface RunAsImageWebService {
         @WebParam(name = "type", targetNamespace = "") ImageType type,
         @WebParam(name = "url", targetNamespace = "") String url) throws DownloadFault;
 
-    /**
-     * downloadAsync
-     * @param principalId
-     * @param title
-     * @param description
-     * @param type
-     * @param url
-     * @param handler
-     * @return Future<?>
-     */
+
     @WebMethod(operationName = "download")
     @RequestWrapper(
         localName = "download",
@@ -152,15 +106,6 @@ public interface RunAsImageWebService {
         @WebParam(name = "url", targetNamespace = "") String url,
         AsyncHandler<DownloadResponse> handler);
 
-    /**
-     * downloadAsync
-     * @param principalId
-     * @param title
-     * @param description
-     * @param type
-     * @param url
-     * @return DownloadResponse
-     */
     @WebMethod(operationName = "download")
     @RequestWrapper(
         localName = "download",
