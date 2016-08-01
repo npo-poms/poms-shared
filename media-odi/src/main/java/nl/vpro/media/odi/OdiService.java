010edaf5308c8f738299f154c050a6d46ac572de
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-08-01 12:31:31 +0000

    moved media-oid to shared
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=92220

---
+++ media-odi/src/main/java/nl/vpro/media/odi/OdiService.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2012 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.media.odi;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import nl.vpro.domain.media.Location;
+import nl.vpro.domain.media.MediaObject;
+import nl.vpro.media.odi.util.LocationResult;
+
+/**
+ * See https://jira.vpro.nl/browse/MSE-1788
+ *
+ * @author Roelof Jan Koekoek
+ * @since 1.8
+ */
+public interface OdiService {
+
+    LocationResult playMedia(MediaObject media, HttpServletRequest request, String... pubOptions);
+
+    LocationResult playLocation(Location location, HttpServletRequest request, String... pubOptions);
+
+    LocationResult playUrl(String url, HttpServletRequest request, String... pubOptions);
+
+}
