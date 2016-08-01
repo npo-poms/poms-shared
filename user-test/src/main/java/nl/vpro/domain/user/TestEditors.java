f1dacdad53c16d99809d743e79f4699d8495eadd
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-08-01 11:38:30 +0000

    moved user-test
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=92169

---
+++ user-test/src/main/java/nl/vpro/domain/user/TestEditors.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2014 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.domain.user;
+
+import java.time.Instant;
+
+/**
+ * @author Roelof Jan Koekoek
+ * @since 2.3
+ */
+public class TestEditors {
+
+    public static Editor vproEditor() {
+        return new Editor("editor@vpro.nl", "Editor", "editor@vpro.nl", new Broadcaster("VPRO", "VPRO"), "Test", "Editor", Instant.now());
+    }
+
+}
