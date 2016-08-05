d2e231ffc43271e3e1537504fdc427cb9754d3e8
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-08-05 10:57:46 +0000

    java.time
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=92497

--- media-domain/src/main/java/nl/vpro/domain/media/search/DateRange.java
+++ media-domain/src/main/java/nl/vpro/domain/media/search/DateRange.java
@@ -4,18 +4,14 @@
  */
 package nl.vpro.domain.media.search;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.function.Predicate;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
-
-import org.apache.commons.lang3.StringUtils;
 
 import nl.vpro.domain.media.Schedule;
 
@@ -24,33 +20,27 @@ import nl.vpro.domain.media.Schedule;
         "start",
         "stop"
         })
-public class DateRange implements Predicate<Date> {
+public class DateRange implements Predicate<Instant> {
 
     @XmlElement
-    private Date start;
+    private Instant start;
 
     @XmlElement
-    private Date stop;
+    private Instant stop;
 
     public  DateRange() {
     }
 
-    public DateRange(Date start, Date stop) {
+    public DateRange(Instant  start, Instant stop) {
         this.start = start;
         this.stop = stop;
     }
 
     public DateRange(LocalDateTime start, LocalDateTime stop) {
-        this.start = start == null ? null : Date.from(start.atZone(Schedule.ZONE_ID).toInstant());
-        this.stop = stop == null ? null : Date.from(stop.atZone(Schedule.ZONE_ID).toInstant());
+        this.start = start == null ? null : start.atZone(Schedule.ZONE_ID).toInstant();
+        this.stop = stop == null ? null : stop.atZone(Schedule.ZONE_ID).toInstant();
     }
 
-    public static DateRange fromString(String start, String stop, SimpleDateFormat format) throws ParseException {
-        Date startDate = (StringUtils.isNotEmpty(start)) ? format.parse(start) : null;
-        Date stopDate = (StringUtils.isNotEmpty(stop)) ? format.parse(stop) : null;
-
-        return new DateRange(startDate, stopDate);
-    }
 
     public boolean hasValues() {
         return start != null || stop != null;
@@ -58,23 +48,21 @@ public class DateRange implements Predicate<Date> {
 
     /**
      * Start date time of date-range (inclusive)
-     * @return
      */
-    public Date getStart() {
+    public Instant getStart() {
         return start;
     }
 
     /**
      * End date time of date-range (exclusive)
-     * @return
      */
-    public Date getStop() {
+    public Instant getStop() {
         return stop;
     }
 
     @Override
-    public boolean test(Date date) {
-        return (start == null || start.equals(date) || start.before(date)) && (stop == null || stop.after(date));
+    public boolean test(Instant date) {
+        return (start == null || start.equals(date) || start.isBefore(date)) && (stop == null || stop.isAfter(date));
 
     }
 }
