f8ecc610bc9f0be1079d813ee04a63a1423c4544
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-08-05 10:57:46 +0000

    java.time
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=92497

--- media-domain/src/test/java/nl/vpro/domain/media/search/DateRangeTest.java
+++ media-domain/src/test/java/nl/vpro/domain/media/search/DateRangeTest.java
@@ -1,6 +1,6 @@
 package nl.vpro.domain.media.search;
 
-import java.util.Date;
+import java.time.Instant;
 
 import org.junit.Test;
 
@@ -10,34 +10,34 @@ public class DateRangeTest {
 
     @Test
     public void testTest1() {
-        DateRange range = new DateRange(new Date(100), new Date(200));
-        assertThat(range.test(new Date(99))).isFalse();
-        assertThat(range.test(new Date(100))).isTrue();
-        assertThat(range.test(new Date(150))).isTrue();
-        assertThat(range.test(new Date(200))).isFalse();
-        assertThat(range.test(new Date(201))).isFalse();
+        DateRange range = new DateRange(Instant.ofEpochMilli(100), Instant.ofEpochMilli(200));
+        assertThat(range.test(Instant.ofEpochMilli(99))).isFalse();
+        assertThat(range.test(Instant.ofEpochMilli(100))).isTrue();
+        assertThat(range.test(Instant.ofEpochMilli(150))).isTrue();
+        assertThat(range.test(Instant.ofEpochMilli(200))).isFalse();
+        assertThat(range.test(Instant.ofEpochMilli(201))).isFalse();
 
     }
 
     @Test
     public void testTest2() {
-        DateRange range = new DateRange(null, new Date(200));
-        assertThat(range.test(new Date(99))).isTrue();
-        assertThat(range.test(new Date(100))).isTrue();
-        assertThat(range.test(new Date(150))).isTrue();
-        assertThat(range.test(new Date(200))).isFalse();
-        assertThat(range.test(new Date(201))).isFalse();
+        DateRange range = new DateRange(null, Instant.ofEpochMilli(200));
+        assertThat(range.test(Instant.ofEpochMilli(99))).isTrue();
+        assertThat(range.test(Instant.ofEpochMilli(100))).isTrue();
+        assertThat(range.test(Instant.ofEpochMilli(150))).isTrue();
+        assertThat(range.test(Instant.ofEpochMilli(200))).isFalse();
+        assertThat(range.test(Instant.ofEpochMilli(201))).isFalse();
 
     }
 
     @Test
     public void testTest3() {
-        DateRange range = new DateRange(new Date(100), null);
-        assertThat(range.test(new Date(99))).isFalse();
-        assertThat(range.test(new Date(100))).isTrue();
-        assertThat(range.test(new Date(150))).isTrue();
-        assertThat(range.test(new Date(200))).isTrue();
-        assertThat(range.test(new Date(201))).isTrue();
+        DateRange range = new DateRange(Instant.ofEpochMilli(100), null);
+        assertThat(range.test(Instant.ofEpochMilli(99))).isFalse();
+        assertThat(range.test(Instant.ofEpochMilli(100))).isTrue();
+        assertThat(range.test(Instant.ofEpochMilli(150))).isTrue();
+        assertThat(range.test(Instant.ofEpochMilli(200))).isTrue();
+        assertThat(range.test(Instant.ofEpochMilli(201))).isTrue();
 
     }
 
