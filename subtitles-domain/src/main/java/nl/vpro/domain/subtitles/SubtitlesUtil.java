5273167aedbe1b6cd6f49c5788593213c0144246
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-08-08 15:48:54 +0000

    backporting
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-4.7-SNAPSHOT/; revision=92587

--- subtitles-domain/src/main/java/nl/vpro/domain/subtitles/SubtitlesUtil.java
+++ subtitles-domain/src/main/java/nl/vpro/domain/subtitles/SubtitlesUtil.java
@@ -1,107 +1,48 @@
 package nl.vpro.domain.subtitles;
 
-import java.io.*;
-import java.nio.charset.Charset;
-import java.text.ParseException;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.Spliterator;
-import java.util.Spliterators;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
+import lombok.extern.slf4j.Slf4j;
 
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.*;
+import java.time.Duration;
+import java.util.Locale;
+import java.util.stream.Stream;
+
+import org.apache.commons.io.IOUtils;
+
+import static nl.vpro.util.ISO6937CharsetProvider.ISO6937;
 
 
 /**
  * @author Michiel Meeuwissen
  * @since 4.7
  */
+@Slf4j
 public class SubtitlesUtil {
 
-    private static Logger LOG = LoggerFactory.getLogger(SubtitlesUtil.class);
+    public static Locale DUTCH = new Locale("nl", "NL");
+    public static Locale FLEMISH = new Locale("nl", "BE");
 
+
+    public static Subtitles ebu(String parent, Duration duration, Locale locale, InputStream input) throws IOException {
+        StringWriter w = new StringWriter();
+        IOUtils.copy(new InputStreamReader(input, ISO6937), w);
+        return new Subtitles(parent, duration, locale, SubtitlesFormat.EBU,  w.toString());
+    }
 
     public static Stream<Cue> parse(Subtitles subtitles) {
-        switch (subtitles.getFormat()) {
+        switch (subtitles.getContent().getFormat()) {
             case EBU:
-                return parseEBU(subtitles.getMid(), new StringReader(subtitles.getContent()));
+                return EBU.parse(subtitles.getMid(), new StringReader(subtitles.getContent().getValue()));
             case WEBVTT:
-                return parseWEBVTT(subtitles.getMid(), new StringReader(subtitles.getContent()));
+                return WEBVTT.parse(subtitles.getMid(), new StringReader(subtitles.getContent().getValue()));
             default:
                 throw new IllegalStateException();
         }
 
     }
 
-
-    public static Stream<Cue> parseWEBVTT(String parent, Reader inputStream) {
-        throw new IllegalStateException();
+    public static Stream<StandaloneCue> standaloneStream(Subtitles subtitles) {
+        return parse(subtitles).map(c -> new StandaloneCue(c, subtitles.getLanguage(), subtitles.getType(), subtitles.getOffset()));
     }
 
-    public static Stream<Cue> parseEBU(String parent, InputStream inputStream) {
-        return parseEBU(parent, new InputStreamReader(inputStream, Charset.forName("ISO-6937")));
-    }
-
-    public static Stream<Cue> parseEBU(final String parent, Reader reader) {
-        final Iterator<String> stream = new BufferedReader(reader)
-            .lines().iterator();
-        Iterator<Cue> cues = new Iterator<Cue>() {
-
-            boolean needsFindNext = true;
-            String timeLine = null;
-            StringBuilder content = new StringBuilder();
-
-            @Override
-            public boolean hasNext() {
-                findNext();
-                return timeLine != null;
-            }
-
-            @Override
-            public Cue next() {
-                findNext();
-                if (timeLine == null) {
-                    throw new NoSuchElementException();
-                }
-                needsFindNext = true;
-                try {
-                    return Cue.parse(parent, timeLine, content.toString());
-                }  catch (ParseException e) {
-                    LOG.error(e.getMessage(), e);
-                    return null;
-                }
-
-            }
-
-            protected void findNext() {
-                if (needsFindNext) {
-                    timeLine = null;
-                    content.setLength(0);
-                    while(stream.hasNext()) {
-                        String l = stream.next();
-                        if (StringUtils.isNotBlank(l)) {
-                            timeLine = l.trim();
-                            break;
-                        }
-                    }
-                    while (stream.hasNext()) {
-                        String l = stream.next();
-                        if (StringUtils.isBlank(l)) {
-                            break;
-                        }
-                        if (content.length() > 0) {
-                            content.append('\n');
-                        }
-                        content.append(l.trim());
-                    }
-                    needsFindNext = false;
-                }
-            }
-        };
-        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(cues, Spliterator.ORDERED), false);
-
-    }
 }
