39761ce01e5af30648f3fee3e0fe4b784667c7a5
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-08-23 21:56:55 +0000

    Proposal for how to code Great Britain...
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=93218

--- media-i18n/src/main/java/nl/vpro/i18n/Locales.java
+++ media-i18n/src/main/java/nl/vpro/i18n/Locales.java
@@ -1,6 +1,7 @@
 package nl.vpro.i18n;
 
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 import nl.vpro.com.neovisionaries.i18n.CountryCode;
 import nl.vpro.com.neovisionaries.i18n.LanguageCode;
@@ -13,10 +14,20 @@ public class Locales {
     
     public static Locale DUTCH   = of(LanguageCode.nl, CountryCode.NL);
     public static Locale FLEMISH = of(LanguageCode.nl, CountryCode.BE);
+    
 
 
     public static Locale of(LanguageCode lc, CountryCode code) {
         return new Locale(lc.name(), code.getAlpha2());
+    }
+    
+    public static String getCountryName(CountryCode code, Locale locale) {
+        if (code.getIso3166_2() != null) {
+            ResourceBundle bundle = ResourceBundle.getBundle("CountryCode", locale);
+            return bundle.getString(code.getIso3166_2());
+        }  else {
+            return code.toLocale().getDisplayCountry(locale);
+        }
     }
 
 }
