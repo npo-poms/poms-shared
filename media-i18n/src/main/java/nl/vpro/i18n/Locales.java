37177d159556e712184dbf5a04735dd5f2d7b7fe
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-08-25 09:25:08 +0000

    MSE-3377
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=93262

--- media-i18n/src/main/java/nl/vpro/i18n/Locales.java
+++ media-i18n/src/main/java/nl/vpro/i18n/Locales.java
@@ -12,16 +12,18 @@ import nl.vpro.com.neovisionaries.i18n.LanguageCode;
  * @since 4.8
  */
 public class Locales {
-    
+
     public static Locale DUTCH   = of(LanguageCode.nl, CountryCode.NL);
     public static Locale FLEMISH = of(LanguageCode.nl, CountryCode.BE);
-    
+
 
 
     public static Locale of(LanguageCode lc, CountryCode code) {
         return new Locale(lc.name(), code.getAlpha2());
     }
-    
+
+
+
     public static String getCountryName(CountryCode code, Locale locale) {
         if (code.getIso3166_2() != null) {
             try {
