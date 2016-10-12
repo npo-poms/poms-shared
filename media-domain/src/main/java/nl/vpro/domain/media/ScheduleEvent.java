0b71f8bd7aa99984aaf741a4ed8ae903ab541608
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-10-12 15:23:03 +0000

    backport
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-4.8-SNAPSHOT/; revision=95035

--- media-domain/src/main/java/nl/vpro/domain/media/ScheduleEvent.java
+++ media-domain/src/main/java/nl/vpro/domain/media/ScheduleEvent.java
@@ -12,8 +12,6 @@ import java.io.Serializable;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
-import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 import javax.persistence.*;
@@ -305,16 +303,7 @@ public class ScheduleEvent implements Serializable, Identifiable<ScheduleEventId
     }
 
     private static LocalDate guideLocalDate(Instant start) {
-        if (start == null) {
-            return null;
-        }
-        ZonedDateTime dateTime = start.atZone(Schedule.ZONE_ID);
-
-        if (dateTime.toLocalTime().isBefore(Schedule.START_OF_SCHEDULE.minus(2, ChronoUnit.MINUTES))) {
-            dateTime = dateTime.minusDays(1);
-        }
-
-        return dateTime.toLocalDate();
+        return Schedule.guideDay(start);
     }
 
     private static Duration duration(Date duration) {
