49b4b69c2ab20582c7ac7903bef62a32bbb50d42
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-11-09 10:14:34 +0000

    json/xml tests.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=96429

--- media-domain/src/test/java/nl/vpro/domain/media/support/ImageTest.java
+++ media-domain/src/test/java/nl/vpro/domain/media/support/ImageTest.java
@@ -1,5 +1,7 @@
 package nl.vpro.domain.media.support;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
@@ -7,6 +9,10 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 
 import org.junit.Test;
+
+import nl.vpro.domain.media.Schedule;
+import nl.vpro.test.util.jackson2.Jackson2TestUtil;
+import nl.vpro.test.util.jaxb.JAXBTestUtil;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,7 +33,39 @@ public class ImageTest {
 
         Set<ConstraintViolation<Image>> validate = validator.validate(image);
         assertThat(validate).isEmpty();
+    }
 
+    @Test
+    public void json() throws Exception {
+        Image image = new Image();
+        image.setSource(null);
+        image.setImageUri("urn:vpro:image:123");
+        image.setCreationDate(Date.from(LocalDate.of(2016, 11, 9).atStartOfDay().atZone(Schedule.ZONE_ID).toInstant()));
+        image.setOffset(java.time.Duration.ofMillis(100));
+
+        Jackson2TestUtil.roundTripAndSimilar(image, "{\n" +
+            "  \"imageUri\" : \"urn:vpro:image:123\",\n" +
+            "  \"offset\" : 100,\n" +
+            "  \"owner\" : \"BROADCASTER\",\n" +
+            "  \"type\" : \"PICTURE\",\n" +
+            "  \"highlighted\" : false,\n" +
+            "  \"creationDate\" : 1478646000000,\n" +
+            "  \"workflow\" : \"FOR_PUBLICATION\"\n" +
+            "}");
+    }
+
+    @Test
+    public void xml() throws Exception {
+        Image image = new Image();
+        image.setSource(null);
+        image.setImageUri("urn:vpro:image:123");
+        image.setCreationDate(Date.from(LocalDate.of(2016, 11, 9).atStartOfDay().atZone(Schedule.ZONE_ID).toInstant()));
+        image.setOffset(java.time.Duration.ofMillis(100));
+
+        JAXBTestUtil.roundTripAndSimilar(image, "<local:image owner=\"BROADCASTER\" type=\"PICTURE\" highlighted=\"false\" creationDate=\"2016-11-09T00:00:00+01:00\" workflow=\"FOR PUBLICATION\" xmlns=\"urn:vpro:media:2009\" xmlns:shared=\"urn:vpro:shared:2009\" xmlns:local=\"uri:local\">\n" +
+            "    <shared:imageUri>urn:vpro:image:123</shared:imageUri>\n" +
+            "    <shared:offset>P0DT0H0M0.100S</shared:offset>\n" +
+            "</local:image>");
 
     }
 }
