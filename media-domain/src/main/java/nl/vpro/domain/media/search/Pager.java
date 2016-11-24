2fd29e9160cc833e7d4b1ff78f028c39f2136a15
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-11-24 14:20:54 +0000

    MSE-3507
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=97280

--- media-domain/src/main/java/nl/vpro/domain/media/search/Pager.java
+++ media-domain/src/main/java/nl/vpro/domain/media/search/Pager.java
@@ -4,6 +4,8 @@
  */
 package nl.vpro.domain.media.search;
 
+import lombok.Builder;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -20,7 +22,7 @@ import static nl.vpro.domain.Xmlns.SEARCH_NAMESPACE;
         })
 public class Pager {
 
-    public static enum Direction {
+    public enum Direction {
         ASC,
         DESC
     }
@@ -45,6 +47,7 @@ public class Pager {
         this(0, max, "creationDate", Direction.ASC);
     }
 
+    @Builder
     public Pager(long offset, Integer max, String sort, Direction order) {
         if(offset < 0 || (max != null && max < 0) || order == null) {
             throw new IllegalArgumentException(String.format("Must supply valid arguments, got offset: %1$s, max: %d$s %2$s, sort: %3$s, order: %4$s", offset, max, sort, order));
