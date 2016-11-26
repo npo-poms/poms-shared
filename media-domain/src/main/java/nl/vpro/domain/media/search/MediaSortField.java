b1aac1fe28a59c54199a3e837c1203c3824fa796
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-11-26 09:10:56 +0000

    TEst case failures.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=97355

--- media-domain/src/main/java/nl/vpro/domain/media/search/MediaSortField.java
+++ media-domain/src/main/java/nl/vpro/domain/media/search/MediaSortField.java
@@ -5,9 +5,23 @@ package nl.vpro.domain.media.search;
  * @since 5.0
  */
 public enum MediaSortField implements SortField {
-    lastModified(Type.LONG),
     sortTitle(Type.STRING),
+
+    mid(Type.STRING),
+    type(Type.STRING),
+    mediaType(Type.STRING),
+
+    sortDate(Type.LONG),
+    lastModified(Type.LONG),
     creationDate(Type.LONG),
+    publishStop(Type.LONG),
+    publishStart(Type.LONG),
+    lastPublished(Type.LONG),
+
+
+    lastModifiedBy(Type.STRING),
+    createdBy(Type.STRING),
+
     locations(Type.LONG) {
         @Override
         public String field() {
@@ -15,16 +29,11 @@ public enum MediaSortField implements SortField {
         }
     },
     memberofCount(Type.LONG),
-    sortDate(Type.LONG),
-    mid(Type.STRING),
-    type(Type.STRING),
-    mediaType(Type.STRING),
-    lastModifiedBy(Type.STRING),
-    createdBy(Type.STRING),
-    publishStop(Type.LONG),
-    publishStart(Type.LONG),
-    lastPublished(Type.LONG);
+    episodeofCount(Type.LONG)
 
+
+
+    ;
     private final Type t;
 
     MediaSortField(Type type) {
