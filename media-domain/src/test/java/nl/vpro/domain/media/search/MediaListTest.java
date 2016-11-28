983b110ee3f7f779f1365a1dc9ea1bc97b2f766d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-11-28 11:53:15 +0000

    Test case failure.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=97399

--- media-domain/src/test/java/nl/vpro/domain/media/search/MediaListTest.java
+++ media-domain/src/test/java/nl/vpro/domain/media/search/MediaListTest.java
@@ -2,7 +2,6 @@ package nl.vpro.domain.media.search;
 
 import java.io.IOException;
 import java.io.StringReader;
-import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.TreeSet;
@@ -11,9 +10,6 @@ import javax.xml.bind.JAXB;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Validator;
 
-import org.custommonkey.xmlunit.Diff;
-import org.custommonkey.xmlunit.XMLUnit;
-import org.junit.Before;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
@@ -24,19 +20,15 @@ import nl.vpro.domain.media.ProgramType;
 import nl.vpro.domain.media.support.Tag;
 import nl.vpro.domain.user.TestEditors;
 
+import static nl.vpro.test.util.jaxb.JAXBTestUtil.assertThatXml;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * @author Michiel Meeuwissen
  * @since 1.7
  */
 public class MediaListTest {
-    @Before
-    public void setup() {
-        XMLUnit.setIgnoreWhitespace(true);
-        XMLUnit.setIgnoreAttributeOrder(true);
-    }
+
 
     @Test
     public void marshalMediaListItem() throws IOException, SAXException {
@@ -53,7 +45,7 @@ public class MediaListTest {
 
 
         String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
-            "<s:list totalCount=\"1000\" offset=\"1\" max=\"10\" order=\"ASC\" size=\"1\" xmlns=\"urn:vpro:media:2009\" xmlns:shared=\"urn:vpro:shared:2009\" xmlns:s=\"urn:vpro:media:search:2012\" xmlns:update=\"urn:vpro:media:update:2009\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" xmlns:media=\"urn:vpro:media:2009\">\n" +
+            "<s:list totalCount=\"1000\" sort=\"creationDate\" offset=\"1\" max=\"10\" order=\"ASC\" size=\"1\" xmlns=\"urn:vpro:media:2009\" xmlns:shared=\"urn:vpro:shared:2009\" xmlns:s=\"urn:vpro:media:search:2012\" xmlns:update=\"urn:vpro:media:update:2009\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" xmlns:media=\"urn:vpro:media:2009\">\n" +
             "    <s:item xsi:type=\"s:mediaListItem\" avType=\"VIDEO\" id=\"123\" mediaType=\"nl.vpro.domain.media.Program\" urn=\"urn:vpro:media:program:123\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n" +
             "        <s:broadcaster>VPRO</s:broadcaster>\n" +
             "        <s:title></s:title>\n" +
@@ -68,15 +60,14 @@ public class MediaListTest {
             "        <s:tag>foo</s:tag>\n" +
             "    </s:item>\n" +
             "</s:list>";
-        StringWriter writer = new StringWriter();
-        JAXB.marshal(xmlList, writer);
-        System.out.println(writer.toString());
-        Diff diff = XMLUnit.compareXML(expected, writer.toString());
-        assertTrue(diff.toString() + " " + writer.toString(), diff.identical());
 
-        MediaList<MediaListItem> list = JAXB.unmarshal(new StringReader(writer.toString()), MediaList.class);
+
+        MediaList<MediaListItem> list = assertThatXml(xmlList)
+            .isSimilarTo(expected)
+            .isValid(Xmlns.SCHEMA.newValidator())
+            .get();
+
         assertEquals("urn:vpro:media:program:123", list.getList().get(0).getUrn());
-        validate(writer.toString());
     }
 
 
