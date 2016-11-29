4108d655ffb8d38fc977ac9ee5b01f9c082344bd
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-11-29 11:46:44 +0000

    A bit silly that license was in shared namespace.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=97458

---
+++ media-domain/src/test/java/nl/vpro/domain/media/update/ImageUpdateTest.java
@@ -0,0 +1,37 @@
+package nl.vpro.domain.media.update;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import nl.vpro.domain.image.ImageType;
+import nl.vpro.domain.media.support.License;
+import nl.vpro.test.util.jaxb.JAXBTestUtil;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.0
+ */
+public class ImageUpdateTest {
+
+    @Test
+    public void xml() throws IOException, SAXException {
+        ImageUpdate update = new ImageUpdate(ImageType.PICTURE, "title", null, new ImageLocation("http://placehold.it/150/7735a")); // redirects
+        update.setLicense(License.CC_BY);
+        update.setSourceName("placeholdit");
+        update.setCredits(getClass().getName());
+        JAXBTestUtil.roundTripAndSimilar(update, "\n" +
+            "<image type=\"PICTURE\" highlighted=\"false\" xmlns=\"urn:vpro:media:update:2009\" xmlns:shared=\"urn:vpro:shared:2009\" xmlns:media=\"urn:vpro:media:2009\">\n" +
+            "    <title>title</title>\n" +
+            "    <sourceName>placeholdit</sourceName>\n" +
+            "    <license>CC_BY</license>\n" +
+            "    <credits>nl.vpro.domain.media.update.ImageUpdateTest</credits>\n" +
+            "    <imageLocation>\n" +
+            "        <url>http://placehold.it/150/7735a</url>\n" +
+            "    </imageLocation>\n" +
+            "</image>");
+
+    }
+
+}
