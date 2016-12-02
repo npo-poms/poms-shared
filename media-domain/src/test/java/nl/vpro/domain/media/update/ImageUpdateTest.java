43b09326a2e299e6caf46fde707be7cf9463d777
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-12-02 15:46:42 +0000

    MSE-3414
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=97611

--- media-domain/src/test/java/nl/vpro/domain/media/update/ImageUpdateTest.java
+++ media-domain/src/test/java/nl/vpro/domain/media/update/ImageUpdateTest.java
@@ -34,4 +34,22 @@ public class ImageUpdateTest {
 
     }
 
+
+    @Test
+    public void xmlBackwards() throws IOException, SAXException {
+        ImageUpdate update = new ImageUpdate(ImageType.PICTURE, "title", null, new ImageLocation("http://placehold.it/150/7735a"));
+        update.setLicense(License.CC_BY);
+        update.setCredits(getClass().getName());
+        JAXBTestUtil.roundTripAndSimilar(update, "\n" +
+            "<image type=\"PICTURE\" highlighted=\"false\" xmlns=\"urn:vpro:media:update:2009\" xmlns:shared=\"urn:vpro:shared:2009\" xmlns:media=\"urn:vpro:media:2009\">\n" +
+            "    <title>title</title>\n" +
+            "    <license>CC_BY</license>\n" +
+            "    <credits>nl.vpro.domain.media.update.ImageUpdateTest</credits>\n" +
+            "    <imageLocation>\n" +
+            "        <url>http://placehold.it/150/7735a</url>\n" +
+            "    </imageLocation>\n" +
+            "</image>");
+
+    }
+
 }
