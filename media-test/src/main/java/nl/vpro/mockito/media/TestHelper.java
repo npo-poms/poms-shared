65de3617cb6fcd802a19856b72329aab85e32efc
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-12-08 15:59:33 +0000

    More cleaning.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=97905

--- media-test/src/main/java/nl/vpro/mockito/media/TestHelper.java
+++ media-test/src/main/java/nl/vpro/mockito/media/TestHelper.java
@@ -4,36 +4,33 @@
  */
 package nl.vpro.mockito.media;
 
-import org.mockito.invocation.InvocationOnMock;
+import org.mockito.internal.stubbing.answers.ReturnsArgumentAt;
 import org.mockito.stubbing.Answer;
 
 import nl.vpro.domain.media.*;
-import nl.vpro.mockito.media.answer.FirstArgument;
 
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 
 public class TestHelper {
 
-    public static <T> FirstArgument firstArgument(Class<T> clazz) {
-        return new FirstArgument<T>();
+    @Deprecated
+    public static <T> Answer<T> firstArgument(Class<T> clazz) {
+        return returnsFirstArg();
     }
 
-    public static FirstArgument withSameMediaObject() {
-        return new FirstArgument<MediaObject>();
+    @Deprecated
+    public static Answer<MediaObject> withSameMediaObject() {
+        return returnsFirstArg();
     }
 
-    public static FirstArgument withSameSchedule() {
-        return new FirstArgument<Schedule>();
+    @Deprecated
+    public static Answer<Schedule> withSameSchedule() {
+        return returnsFirstArg();
     }
 
-    public static <T> Answer argument(final int pos, Class<T> clazz) {
-        return new Answer<T>() {
-            @Override
-            public T answer(InvocationOnMock invocation) throws Throwable {
-                Object[] args = invocation.getArguments();
-                return (T)args[pos];
-            }
-        };
+    public static <T> Answer<T> argument(final int pos, Class<T> clazz) {
+        return (Answer<T>) new ReturnsArgumentAt(pos);
     }
 
     public static MediaObject anyMediaObject() {
