877ee31f09e388845651a9f96dac9246d5284eff
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-12-15 12:38:15 +0000

    Dropped rather empty Asset stuff.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=98237

---
+++ media-domain/src/main/java/nl/vpro/domain/asset/AssetService.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2011 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.domain.asset;
+
+import java.io.File;
+import java.io.InputStream;
+
+import nl.vpro.domain.media.update.Asset;
+
+public interface AssetService {
+
+    File get(String fileName);
+
+    /**
+     * Stores the given assets inputstream in the local asset store and replaces the wrapped asset with a new asset
+     * source referencing the filename in the local store.
+     */
+    void store(String fileName, Asset asset);
+
+    String store(String fileName, InputStream stream);
+
+    String append(String fileName, InputStream stream);
+
+    boolean remove(String fileName);
+
+}
