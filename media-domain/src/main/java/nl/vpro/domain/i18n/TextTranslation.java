ff68594178e0e98220e22117255c048ffa19627b
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-01-23 16:12:03 +0000

    MSE-3346
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=99560

--- media-domain/src/main/java/nl/vpro/domain/i18n/TextTranslation.java
+++ media-domain/src/main/java/nl/vpro/domain/i18n/TextTranslation.java
@@ -1,6 +1,6 @@
 package nl.vpro.domain.i18n;
 
-import lombok.ToString;
+import lombok.*;
 
 import java.util.function.Supplier;
 
@@ -15,6 +15,8 @@ import nl.vpro.validation.NoHtml;
  */
 @MappedSuperclass
 @ToString
+@Access(AccessType.FIELD)
+@EqualsAndHashCode
 public class TextTranslation<P>  implements Supplier<String> {
     @Column(nullable = false)
     @NoHtml
@@ -23,10 +25,9 @@ public class TextTranslation<P>  implements Supplier<String> {
     @Id
     @GeneratedValue
     @XmlTransient
+    @Getter @Setter(AccessLevel.PROTECTED)
     private Long id;
 
-    @ManyToOne
-    private P parent;
 
     public TextTranslation(String value) {
         this.value = value;
@@ -45,63 +46,5 @@ public class TextTranslation<P>  implements Supplier<String> {
         this.value = value;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    void setId(Long id) {
-        this.id = id;
-    }
-
-    public P getParent() {
-        return parent;
-    }
-
-    public void setParent(P parent) {
-        this.parent = parent;
-    }
-
-    /**
-     * Checks for database identity or object identity if one side of the comparison can
-     * not supply a database identity. It is advised to override this method with a more
-     * accurate test which should not rely on database identity. You can rely on this
-     * criterion when equality can not be deducted programmatic and a real and final
-     * check is in need of human interaction. In essence this check then states that two
-     * objects are supposed to be different if they can't supply the same database Id.
-     *
-     * @param object the object to compare with
-     * @return true if both objects are equal
-     */
-    @Override
-    public boolean equals(Object object) {
-        if (object == null) {
-            return false;
-        }
-
-        if (this.getClass() != object.getClass()) {
-            return false;
-        }
-
-        TextTranslation<P> that = (TextTranslation<P>) object;
-
-        if (this.getId() != null && that.getId() != null) {
-            return this.getId().equals(that.getId());
-        }
-
-        return this.value == null ? that.value == null : this.value.equals(that.value);
-    }
-
-    @Override
-    public int hashCode() {
-        if (id != null) {
-            return id.hashCode();
-        }
-
-        if (value != null) {
-            return value.hashCode();
-        }
-
-        return System.identityHashCode(this);
-    }
 
 }
