5e47852ce3c88952548e27191a223617bbba5a0d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-01-27 12:43:04 +0000

    MSE-3524
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=99770

---
+++ media-domain/src/main/java/nl/vpro/domain/media/support/ScheduleEventDescription.java
@@ -0,0 +1,34 @@
+package nl.vpro.domain.media.support;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Entity;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import nl.vpro.domain.AbstractOwnedTextEntity;
+import nl.vpro.domain.Xmlns;
+import nl.vpro.domain.media.ScheduleEvent;
+
+/**
+ * See https://jira.vpro.nl/browse/MSE-3524
+ * @author Michiel Meeuwissen
+ * @since 5.1
+ */
+@Entity
+@Cacheable
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlType(name = "scheduleEventDescription", namespace = Xmlns.MEDIA_NAMESPACE)
+@JsonPropertyOrder({"value", "owner", "type"})
+public class ScheduleEventDescription extends AbstractOwnedTextEntity<ScheduleEventDescription, ScheduleEvent> {
+
+    public ScheduleEventDescription(String title, OwnerType owner, TextualType type) {
+        super(title, owner, type);
+    }
+
+    public ScheduleEventDescription() {
+    }
+
+}
