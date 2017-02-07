41f722a161ab3ea1930cf943a74af0252c12715e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-02-07 12:26:05 +0000

    Never mind.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=100171

--- media-domain/src/main/java/nl/vpro/domain/media/Platform.java
+++ media-domain/src/main/java/nl/vpro/domain/media/Platform.java
@@ -2,19 +2,27 @@ package nl.vpro.domain.media;
 
 import javax.xml.bind.annotation.XmlEnum;
 import javax.xml.bind.annotation.XmlType;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-
-import nl.vpro.domain.media.bind.PlatformAdapter;
 
 /**
  * @author Michiel Meeuwissen
  */
 @XmlEnum
 @XmlType(name = "platformTypeEnum")
-@XmlJavaTypeAdapter(PlatformAdapter.class)
 public enum Platform {
+    /**
+     * Visible on internet
+     */
     INTERNETVOD,
+    /**
+     *
+     */
     TVVOD,
-    PLUSVOD, // NLZiet, platform "extra" in cooperation with dutch commercial broadcasters
-    NPOPLUSVOD // NPOPlus, platform "plusx" is the NPO only offspring/splitoff from NLZiet
+    /**
+     * NLZiet,platform "extra" in cooperation with dutch commercial broadcasters
+     */
+    PLUSVOD,
+    /**
+     *  NPOPlus, platform "plusx" is the NPO only offspring/splitoff from NLZiet
+     */
+    NPOPLUSVOD
 }
