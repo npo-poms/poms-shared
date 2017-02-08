3dc1e5416f599eb8f3b492a82dc71e509fbabc24
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-02-08 12:51:21 +0000

    Simplified.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=100250

--- subtitles-domain/src/main/java/nl/vpro/api/rs/subtitles/Util.java
+++ subtitles-domain/src/main/java/nl/vpro/api/rs/subtitles/Util.java
@@ -21,18 +21,21 @@ public class Util {
 
     public static Map<String, String> headers(SubtitlesId id, String extension) {
         Map<String, String> result = new HashMap<>();
-        result.put("Content-Disposition", "inline; fileName=" + id.getMid() + "." + id.getLanguage() + "." + extension + ";");
+        result.put("Content-Disposition", getContentDisposition(id, extension));
         result.put("X-subtitlesId", id.toString());
         return result;
 
     }
-
 
      static void headers(SubtitlesId id, MultivaluedMap<String, Object> httpHeaders, String extension) {
         for(Map.Entry<String, String> e : headers(id, extension).entrySet()) {
             httpHeaders.putSingle(e.getKey(), e.getValue());
         }
 
+    }
+
+    static String getContentDisposition(SubtitlesId id, String extension) {
+        return "inline; fileName=" + id.getMid() + "." + id.getLanguage() + "." + extension + ";";
     }
 
     static Iterator<Cue> headers(Iterator<Cue> cueIterator, MultivaluedMap<String, Object> httpHeaders, String extension) {
@@ -42,7 +45,7 @@ public class Util {
         if (peeking.hasNext()) {
             Cue head = peeking.peek();
             if (head instanceof StandaloneCue) {
-                httpHeaders.putSingle("Content-Disposition", "inline; fileName=" + head.getParent() + "." + ((StandaloneCue) head).getLanguage().toString() + "." + extension + ";");
+                httpHeaders.putSingle("Content-Disposition",  getContentDisposition(((StandaloneCue) head).getSubtitlesId(), extension));
             } else {
                 httpHeaders.putSingle("Content-Disposition", "inline; fileName=" + head.getParent() + "." + "." + extension + ";");
             }
