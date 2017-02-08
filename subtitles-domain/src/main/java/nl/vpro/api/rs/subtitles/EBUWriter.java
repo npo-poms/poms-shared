a16ef14b8c86f54d9ab97f8abbc62b59a9cbbe44
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-02-08 10:43:32 +0000

    Test case failures.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=100235

--- subtitles-domain/src/main/java/nl/vpro/api/rs/subtitles/EBUWriter.java
+++ subtitles-domain/src/main/java/nl/vpro/api/rs/subtitles/EBUWriter.java
@@ -2,21 +2,13 @@ package nl.vpro.api.rs.subtitles;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Type;
 import java.util.Iterator;
 
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 
 import nl.vpro.domain.subtitles.Cue;
-import nl.vpro.domain.subtitles.StandaloneCue;
 import nl.vpro.domain.subtitles.SubtitlesUtil;
-import nl.vpro.util.CountedIterator;
 
 /**
  * @author Michiel Meeuwissen
@@ -24,25 +16,16 @@ import nl.vpro.util.CountedIterator;
  */
 @Provider
 @Produces(Constants.EBU)
-public class EBUWriter implements MessageBodyWriter<CountedIterator<StandaloneCue>> {
+public class EBUWriter extends AbstractIteratorWriter {
 
 
-    @Override
-    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
-        return mediaType.isCompatible(Constants.EBU_TYPE) && Iterator.class.isAssignableFrom(type);
-
+    public EBUWriter() {
+        super(Constants.EBU_TYPE);
     }
 
     @Override
-    public long getSize(CountedIterator<StandaloneCue> cueIterator, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
-        return -1;
-
-    }
-
-    @Override
-    public void writeTo(CountedIterator<StandaloneCue> cueIterator, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
-        Iterator<? extends Cue> i = Util.headers(cueIterator, httpHeaders, "ebu");
-        SubtitlesUtil.toEBU(i, entityStream);
+    protected void stream(Iterator<Cue> cueIterator, OutputStream entityStream) throws IOException {
+        SubtitlesUtil.toEBU(cueIterator, entityStream);
     }
 
 
