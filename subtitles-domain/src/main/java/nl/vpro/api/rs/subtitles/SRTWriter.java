45d0db12d73ec907b6267abb97a649a4d27309d9
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-02-08 10:43:32 +0000

    Test case failures.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=100235

--- subtitles-domain/src/main/java/nl/vpro/api/rs/subtitles/SRTWriter.java
+++ subtitles-domain/src/main/java/nl/vpro/api/rs/subtitles/SRTWriter.java
@@ -2,21 +2,13 @@ package nl.vpro.api.rs.subtitles;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Type;
 import java.util.Iterator;
 
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 
 import nl.vpro.domain.subtitles.Cue;
-import nl.vpro.domain.subtitles.StandaloneCue;
 import nl.vpro.domain.subtitles.SubtitlesUtil;
-import nl.vpro.util.CountedIterator;
 
 /**
  * @author Michiel Meeuwissen
@@ -24,27 +16,15 @@ import nl.vpro.util.CountedIterator;
  */
 @Provider
 @Produces(Constants.SRT)
-public class SRTWriter implements MessageBodyWriter<CountedIterator<StandaloneCue>> {
+public class SRTWriter extends AbstractIteratorWriter {
 
-
-    @Override
-    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
-        return mediaType.isCompatible(Constants.SRT_TYPE) && Iterator.class.isAssignableFrom(type);
-
+    public SRTWriter() {
+        super(Constants.SRT_TYPE);
     }
 
     @Override
-    public long getSize(CountedIterator<StandaloneCue> cueIterator, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
-        return -1;
+    protected void stream(Iterator<Cue> cueIterator, OutputStream entityStream) throws IOException {
+        SubtitlesUtil.toSRT(cueIterator, entityStream);
 
     }
-
-    @Override
-    public void writeTo(CountedIterator<StandaloneCue> cueIterator, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
-        Iterator<? extends Cue> i = Util.headers(cueIterator, httpHeaders, "srt");
-        SubtitlesUtil.toSRT(i, entityStream);
-    }
-
-
-
 }
