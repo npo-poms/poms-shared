0bfe3ac295ae35b843e981a1e6a7449b99dd93bd
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-02-08 10:43:32 +0000

    Test case failures.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=100235

---
+++ subtitles-domain/src/main/java/nl/vpro/api/rs/subtitles/AbstractIteratorWriter.java
@@ -0,0 +1,48 @@
+package nl.vpro.api.rs.subtitles;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Iterator;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+
+import nl.vpro.domain.subtitles.Cue;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.1
+ */
+abstract class AbstractIteratorWriter implements MessageBodyWriter<Iterator<Cue>> {
+
+    private final MediaType mediaType;
+
+    public AbstractIteratorWriter(MediaType mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return mediaType.isCompatible(this.mediaType) && Iterator.class.isAssignableFrom(type);
+
+    }
+
+    @Override
+    public long getSize(Iterator<Cue> cueIterator, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return -1;
+    }
+
+    @Override
+    public void writeTo(Iterator<Cue> cueIterator, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+        stream(cueIterator, entityStream);
+    }
+
+    protected abstract void stream(Iterator<Cue> cueIterator, OutputStream entityStream) throws IOException;
+
+
+
+}
