9743042463c1c4f9c85bfbc1c65d7dbb0763c6e4
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-02-10 15:06:26 +0000

    MSE-3546
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=100439

--- media-domain/src/main/java/nl/vpro/domain/media/search/MediaSortField.java
+++ media-domain/src/main/java/nl/vpro/domain/media/search/MediaSortField.java
@@ -6,86 +6,57 @@ import nl.vpro.domain.media.support.OwnerType;
  * @author Michiel Meeuwissen
  * @since 5.0
  */
-public interface MediaSortField extends SortField {
-    Enum sortTitle = Enum.sortTitle;
-    Enum mid = Enum.mid;
-    Enum type = Enum.type;
-    Enum mediaType = Enum.mediaType;
-    Enum sortDate = Enum.sortDate;
-    Enum lastModified = Enum.lastModified;
-    Enum creationDate = Enum.creationDate;
-    Enum publishStop = Enum.publishStop;
-    Enum publishStart = Enum.publishStart;
-    Enum lastPublished = Enum.lastPublished;
-    Enum lastModifiedBy = Enum.lastModifiedBy;
-    Enum createdBy = Enum.createdBy;
-    Enum locations = Enum.locations;
-    Enum memberofCount = Enum.memberofCount;
-    Enum episodeofCount = Enum.episodeofCount;
+public enum MediaSortField implements SortField {
+    sortTitle(Type.STRING),
+    sortTitle_NPO(Type.STRING),
 
 
-    enum Enum implements MediaSortField {
-        sortTitle(Type.STRING),
+    mid(Type.STRING),
+    type(Type.STRING),
+    mediaType(Type.STRING),
 
-        mid(Type.STRING),
-        type(Type.STRING),
-        mediaType(Type.STRING),
-
-        sortDate(Type.LONG),
-        lastModified(Type.LONG),
-        creationDate(Type.LONG),
-        publishStop(Type.LONG),
-        publishStart(Type.LONG),
-        lastPublished(Type.LONG),
+    sortDate(Type.LONG),
+    lastModified(Type.LONG),
+    creationDate(Type.LONG),
+    publishStop(Type.LONG),
+    publishStart(Type.LONG),
+    lastPublished(Type.LONG),
 
 
-        lastModifiedBy(Type.STRING),
-        createdBy(Type.STRING),
+    lastModifiedBy(Type.STRING),
+    createdBy(Type.STRING),
 
-        locations(Type.LONG) {
-            @Override
-            public String field() {
-                return "locationCount";
-            }
-        },
-        memberofCount(Type.LONG),
-        episodeofCount(Type.LONG);
-        private final Type t;
-
-        Enum(Type type) {
-            this.t = type;
-        }
-
+    locations(Type.LONG) {
         @Override
-        public Type type() {
-            return t;
+        public String field() {
+            return "locationCount";
         }
+    },
+    memberofCount(Type.LONG),
+    episodeofCount(Type.LONG)
+
+
+
+    ;
+    private final Type t;
+
+    MediaSortField(Type type) {
+        this.t = type;
+    }
+    @Override
+    public Type type() {
+        return t;
     }
 
     static MediaSortField valueOfNullable(String string) {
         if (string == null) {
             return null;
         }
-        return Enum.valueOf(string);
+        return valueOf(string);
     }
 
-    static MediaSortField valueOf(String string) {
-        return Enum.valueOf(string);
-    }
 
-    static MediaSortField valueOf(MediaSortField.Enum e, OwnerType ot) {
-        return new MediaSortField() {
-            @Override
-            public String name() {
-                return e.name() + "_" + ot.name();
-
-            }
-
-            @Override
-            public Type type() {
-                return e.type();
-
-            }
-        };
+    static MediaSortField valueOf(MediaSortField sortTitle, OwnerType ownerType) {
+        return valueOf(sortTitle.name() + "_" + ownerType.name());
     }
 }
