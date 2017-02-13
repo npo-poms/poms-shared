a516d5e2275078892de2892a421ec5e68d54af65
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-02-13 09:10:07 +0000

    MSE-3546
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=100450

---
+++ media-domain/src/test/java/nl/vpro/domain/media/support/OwnerTypeTest.java
@@ -0,0 +1,32 @@
+package nl.vpro.domain.media.support;
+
+import java.util.Comparator;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.2
+ */
+public class OwnerTypeTest {
+    @Test
+    public void comparator() throws Exception {
+        Comparator<OwnerType> ot = OwnerType.comparator(OwnerType.NPO);
+
+        assertThat(ot.compare(OwnerType.NPO, OwnerType.BROADCASTER)).isNegative();
+
+
+    }
+
+    @Test
+    public void defaultorder() throws Exception {
+        Comparator<OwnerType> ot = OwnerType.comparator();
+
+        assertThat(ot.compare(OwnerType.BROADCASTER, OwnerType.NPO)).isNegative();
+
+
+    }
+
+}
