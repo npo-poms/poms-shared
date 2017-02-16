f914be0d62b1e29ae2f2e0c405f4ee2c84b92937
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-02-16 09:26:42 +0000

    Didn't compile
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=100595

--- api-domain/src/test/java/nl/vpro/domain/api/validation/TextMatcherValidatorTest.java
+++ api-domain/src/test/java/nl/vpro/domain/api/validation/TextMatcherValidatorTest.java
@@ -9,7 +9,7 @@ import nl.vpro.domain.api.StandardMatchType;
 import nl.vpro.domain.api.TextMatcher;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 /**
