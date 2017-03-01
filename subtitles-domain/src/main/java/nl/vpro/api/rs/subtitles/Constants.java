22c26a31a23efdb42ae0ddb79858cd46d9941170
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-03-01 15:44:12 +0000

    The common extension for EBU exchange format is .stl
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=101104

--- subtitles-domain/src/main/java/nl/vpro/api/rs/subtitles/Constants.java
+++ subtitles-domain/src/main/java/nl/vpro/api/rs/subtitles/Constants.java
@@ -18,10 +18,10 @@ public class Constants {
     public static final MediaType VTT_TYPE = new MediaType("text", "vtt");
 
     public static final String SRT = "text/srt";
-    public static final MediaType SRT_TYPE = new MediaType("text", "ebu-tt");
+    public static final MediaType SRT_TYPE = new MediaType("text", "srt");
 
-    public static final String EBU = "text/ebu-tt";
-    public static final MediaType EBU_TYPE = new MediaType("text", "ebu-tt");
+    public static final String EBU = "text/ebu-stl";
+    public static final MediaType EBU_TYPE = new MediaType("text", "ebu-stl");
 
 
 }
