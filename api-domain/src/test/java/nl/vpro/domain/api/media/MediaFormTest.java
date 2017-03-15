47b641c3b9b34a1ba0aab53d2364a318fd89ff5f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-03-15 11:22:42 +0000

    Didn't compile
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=101582

--- api-domain/src/test/java/nl/vpro/domain/api/media/MediaFormTest.java
+++ api-domain/src/test/java/nl/vpro/domain/api/media/MediaFormTest.java
@@ -213,7 +213,7 @@ public class MediaFormTest {
 
     @Test
     public void testTitleSearch() throws Exception {
-        MediaForm form = MediaForm.builder()..build();
+        MediaForm form = MediaForm.builder().build();
         JAXBTestUtil.roundTripAndSimilar(form, "<api:mediaForm xmlns:pages=\"urn:vpro:pages:2013\" xmlns:api=\"urn:vpro:api:2013\" xmlns:media=\"urn:vpro:media:2009\">\n" +
             "    <api:searches>\n" +
             "        <api:text fuzziness=\"AUTO\" match=\"SHOULD\">bla</api:text>\n" +
