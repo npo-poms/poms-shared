cadac1f7cc05430e8fb66bd8ed174f14e885d5bd
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-03-15 12:23:58 +0000

    test case failure
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-5.2-SNAPSHOT/; revision=101605

--- api-domain/src/test/java/nl/vpro/domain/api/media/MediaFormTest.java
+++ api-domain/src/test/java/nl/vpro/domain/api/media/MediaFormTest.java
@@ -213,7 +213,10 @@ public class MediaFormTest {
 
     @Test
     public void testTitleSearch() throws Exception {
-        MediaForm form = MediaForm.builder().build();
+        MediaForm form = MediaForm
+            .builder()
+            .fuzzyText("bla")
+            .build();
         JAXBTestUtil.roundTripAndSimilar(form, "<api:mediaForm xmlns:pages=\"urn:vpro:pages:2013\" xmlns:api=\"urn:vpro:api:2013\" xmlns:media=\"urn:vpro:media:2009\">\n" +
             "    <api:searches>\n" +
             "        <api:text fuzziness=\"AUTO\" match=\"SHOULD\">bla</api:text>\n" +
