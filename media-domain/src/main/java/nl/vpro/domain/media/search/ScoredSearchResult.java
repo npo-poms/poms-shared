a76bead06c8d79a7f085be8c8125d7a3a1806cb7
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-03-28 09:18:03 +0000

    Didn't compile
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=101995

--- media-domain/src/main/java/nl/vpro/domain/media/search/ScoredSearchResult.java
+++ media-domain/src/main/java/nl/vpro/domain/media/search/ScoredSearchResult.java
@@ -4,21 +4,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+
 /**
  * @author Michiel Meeuwissen
  * @since 5.3
  */
-public interface ScoredSearchResult<T> extends SearchResult<T>  {
+public interface ScoredSearchResult<T> extends SearchResult<T> {
 
     List<ScoredResult<T>> getScoredResult();
-    
+
     @Override
     default List<T> getResult() {
         return getScoredResult()
             .stream().map(ScoredResult::getResult)
             .collect(Collectors.toList());
     }
-    
+
     default Stream<ScoredResult<T>> stream() {
         return getScoredResult().stream();
     }
