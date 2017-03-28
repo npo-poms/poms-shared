7bcb85b24e4c3cc5a1ee6244806fa0943e5d4e59
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-03-28 09:19:02 +0000

    Didn't compile
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=101996

--- media-domain/src/main/java/nl/vpro/domain/media/search/SearchResult.java
+++ media-domain/src/main/java/nl/vpro/domain/media/search/SearchResult.java
@@ -1,12 +1,13 @@
 package nl.vpro.domain.media.search;
 
+import java.util.Iterator;
 import java.util.List;
 
 /**
  * @author Michiel Meeuwissen
  * @since 5.3
  */
-public interface SearchResult<T> {
+public interface SearchResult<T> extends Iterable<T> {
 
     List<T> getResult();
 
@@ -14,6 +15,10 @@ public interface SearchResult<T> {
 
     default int size() {
         return getResult().size();
+    }
+    @Override
+    default Iterator<T> iterator() {
+        return getResult().iterator();
     }
 
 }
