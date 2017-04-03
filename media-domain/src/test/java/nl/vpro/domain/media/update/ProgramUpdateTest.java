c540f817d7a71c574f87ca4a456bb07de3dcc5a1
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-04-03 14:17:40 +0000

    Test case failure.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=102182

--- media-domain/src/test/java/nl/vpro/domain/media/update/ProgramUpdateTest.java
+++ media-domain/src/test/java/nl/vpro/domain/media/update/ProgramUpdateTest.java
@@ -559,7 +559,7 @@ public class ProgramUpdateTest extends MediaUpdateTest {
 
     @Test
     public void testXSD() throws Exception {
-        Source xmlFile = new StreamSource(new StringReader("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><program avType=\"VIDEO\" embeddable=\"true\" xmlns=\"urn:vpro:media:update:2009\"><broadcaster>VPRO</broadcaster><portal>STERREN24</portal><title>bla</title><credits><person role='DIRECTOR'><givenName>Pietje</givenName><familyName>Puk</familyName></person></credits><locations/><scheduleEvents/><images/><segments/></program>"));
+        Source xmlFile = new StreamSource(new StringReader("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><program avType=\"VIDEO\" embeddable=\"true\" xmlns=\"urn:vpro:media:update:2009\"><broadcaster>VPRO</broadcaster><portal>STERREN24</portal><title type=\"MAIN\">bla</title><credits><person role='DIRECTOR'><givenName>Pietje</givenName><familyName>Puk</familyName></person></credits><locations/><scheduleEvents/><images/><segments/></program>"));
         SchemaFactory schemaFactory = SchemaFactory
             .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
         System.out.println(getClass().getResource("/nl/vpro/domain/media/update/vproMediaUpdate.xsd"));
