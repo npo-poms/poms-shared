ad833951bc0802f551147ef558a5c8df55c2d008
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-04-11 11:41:15 +0000

    Hash should not light heartedly change, that would make every object dirty.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=102476

--- media-domain/src/test/java/nl/vpro/domain/media/MediaObjectTest.java
+++ media-domain/src/test/java/nl/vpro/domain/media/MediaObjectTest.java
@@ -3,21 +3,23 @@
  */
 package nl.vpro.domain.media;
 
-import nl.vpro.domain.media.exceptions.CircularReferenceException;
-import nl.vpro.domain.media.support.*;
-import nl.vpro.domain.user.Broadcaster;
-import nl.vpro.test.util.jaxb.JAXBTestUtil;
-import org.assertj.core.api.Assertions;
-import org.custommonkey.xmlunit.XMLUnit;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import javax.validation.ConstraintViolation;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.SortedSet;
+
+import javax.validation.ConstraintViolation;
+
+import org.assertj.core.api.Assertions;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import nl.vpro.domain.media.exceptions.CircularReferenceException;
+import nl.vpro.domain.media.support.*;
+import nl.vpro.domain.user.Broadcaster;
+import nl.vpro.test.util.jaxb.JAXBTestUtil;
 
 import static nl.vpro.domain.media.MediaDomainTestHelper.validator;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -471,7 +473,7 @@ public class MediaObjectTest {
             .id(1L)
             .build();
         program.acceptChanges();
-        assertThat(program.getHash()).isEqualTo(3798152115L);
+        assertThat(program.getHash()).isEqualTo(362556323L);
     }
 
 
