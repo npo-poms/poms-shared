0667613b61c31460fa914d4b962bccb39e5f1d79
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-04-12 07:28:05 +0000

    Even more test case failures.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=102505

--- api-domain/src/test/java/nl/vpro/domain/api/ScheduleEventTest.java
+++ api-domain/src/test/java/nl/vpro/domain/api/ScheduleEventTest.java
@@ -4,16 +4,17 @@
  */
 package nl.vpro.domain.api;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+
 import nl.vpro.domain.api.media.ScheduleResult;
 import nl.vpro.domain.media.Channel;
 import nl.vpro.domain.media.MediaTestDataBuilder;
 import nl.vpro.domain.media.Program;
 import nl.vpro.test.util.jaxb.JAXBTestUtil;
-import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -51,8 +52,8 @@ public class ScheduleEventTest {
             "                <media:locations/>\n" +
             "                <media:scheduleEvents/>\n" +
             "                <media:images/>\n" +
-            "                <media:availableSubtitles>\n" + 
-            "                    <media:availableSubtitle language=\"nl\" type=\"CAPTION\"/>\n" + 
+            "                <media:availableSubtitles>\n" +
+            "                    <media:availableSubtitle language=\"nl\" type=\"CAPTION\"/>\n" +
             "                </media:availableSubtitles>" +
             "                <media:segments/>\n" +
             "            </media:program>\n" +
@@ -80,7 +81,7 @@ public class ScheduleEventTest {
             "    <media:start>1970-01-01T01:00:00+01:00</media:start>\n" +
             "    <media:duration>P0DT0H0M0.100S</media:duration>\n" +
             "    <media:poProgID>VPROWON_12346</media:poProgID>\n" +
-            "    <media:program embeddable=\"true\" hasSubtitles=\"false\" mid=\"VPROWON_12346\" sortDate=\"1970-01-01T01:00:00.100+01:00\" creationDate=\"1970-01-01T01:00:00.100+01:00\" workflow=\"FOR PUBLICATION\">\n" +
+            "    <media:program embeddable=\"true\" mid=\"VPROWON_12346\" sortDate=\"1970-01-01T01:00:00.100+01:00\" creationDate=\"1970-01-01T01:00:00.100+01:00\" workflow=\"FOR PUBLICATION\">\n" +
             "        <media:credits/>\n" +
             "        <media:locations/>\n" +
             "        <media:scheduleEvents/>\n" +
