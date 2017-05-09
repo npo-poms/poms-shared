eb47851ceffc01c6c1dedfa0608bc4a29df98fd9
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-05-09 11:10:17 +0000

    backport
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-5.3-SNAPSHOT/; revision=103236

--- poms-shared-domain/src/main/java/nl/vpro/domain/subtitles/SubtitlesType.java
+++ poms-shared-domain/src/main/java/nl/vpro/domain/subtitles/SubtitlesType.java
@@ -4,13 +4,15 @@ package nl.vpro.domain.subtitles;
 import javax.xml.bind.annotation.XmlEnum;
 import javax.xml.bind.annotation.XmlType;
 
+import nl.vpro.domain.Xmlns;
+
 /**
  * @author Michiel Meeuwissen
  * @since 4.7
  */
 
 @XmlEnum
-@XmlType(name = "subtitlesTypeEnum")
+@XmlType(name = "subtitlesTypeEnum", namespace = Xmlns.SHARED_NAMESPACE)
 public enum SubtitlesType {
     CAPTION,
     TRANSLATION,
