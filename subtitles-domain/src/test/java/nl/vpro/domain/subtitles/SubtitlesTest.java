33639b135db4c45c557ac3b6097542e974b8e591
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-05-09 06:44:11 +0000

    Test case failures.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=103222

--- subtitles-domain/src/test/java/nl/vpro/domain/subtitles/SubtitlesTest.java
+++ subtitles-domain/src/test/java/nl/vpro/domain/subtitles/SubtitlesTest.java
@@ -27,14 +27,18 @@ public class SubtitlesTest {
 
     @Test
     public void testMarshalToXml() throws IOException, SAXException {
-        Subtitles subtitles = Subtitles.webvtt("VPRO_1234", Duration.ofMillis(2 * 60 * 1000), NETHERLANDISH,  "Ondertiteling tekst");
+        Subtitles subtitles = Subtitles.webvtt("VPRO_1234", Duration.ofMillis(2 * 60 * 1000), NETHERLANDISH,  "WEBVTT\n\n1\n00:00:00.000 --> 00:01:04.000\nbla\n\n");
         subtitles.setCreationDate(Instant.ofEpochMilli(0));
         subtitles.setLastModified(Instant.ofEpochMilli(0));
 
 
         JAXBTestUtil.roundTripAndSimilar(subtitles,
-                "<subtitles:subtitles mid=\"VPRO_1234\" offset=\"P0DT0H2M0.000S\" creationDate=\"1970-01-01T01:00:00+01:00\" lastModified=\"1970-01-01T01:00:00+01:00\" type=\"CAPTION\" xml:lang=\"nl-NL\" xmlns:subtitles=\"urn:vpro:media:subtitles:2009\">\n" +
-                    "    <subtitles:content format=\"WEBVTT\">Ondertiteling tekst</subtitles:content>\n" +
+                "<subtitles:subtitles mid=\"VPRO_1234\" offset=\"P0DT0H2M0.000S\" creationDate=\"1970-01-01T01:00:00+01:00\" lastModified=\"1970-01-01T01:00:00+01:00\" type=\"CAPTION\"  cueCount=\"1\"  xml:lang=\"nl-NL\" xmlns:subtitles=\"urn:vpro:media:subtitles:2009\">\n" +
+                    "    <subtitles:content format=\"WEBVTT\">WEBVTT\n" +
+                    "\n" +
+                    "1\n" +
+                    "00:00:00.000 --> 00:01:04.000\n" +
+                    "bla</subtitles:content>\n" +
                     "</subtitles:subtitles>");
     }
 
@@ -56,27 +60,37 @@ public class SubtitlesTest {
 
     @Test
     public void json() throws Exception {
-        Subtitles subtitles = Subtitles.webvtt("VPRO_1234", Duration.ofMillis(2 * 60 * 1000), NETHERLANDISH, "Ondertiteling tekst");
+        Subtitles subtitles = Subtitles.webvtt("VPRO_1234",
+            Duration.ofMillis(2 * 60 * 1000), NETHERLANDISH,
+            "WEBVTT\n" +
+                "\n" +
+                "1\n" +
+                "00:00:02.200 --> 00:00:04.150\n" +
+                "888\n" +
+                "\n");
         subtitles.setCreationDate(Instant.ofEpochMilli(0));
         subtitles.setLastModified(Instant.ofEpochMilli(0));
+        assertThat(subtitles.getCueCount()).isEqualTo(1);
 
         Jackson2TestUtil.roundTripAndSimilar(subtitles, "{\n" +
             "  \"mid\" : \"VPRO_1234\",\n" +
             "  \"offset\" : 120000,\n" +
             "  \"content\" : {\n" +
-            "    \"value\" : \"Ondertiteling tekst\",\n" +
+            "    \"value\" : \"WEBVTT\\n\\n1\\n00:00:02.200 --> 00:00:04.150\\n888\\n\\n\",\n" +
             "    \"format\" : \"WEBVTT\"\n" +
             "  },\n" +
             "  \"creationDate\" : \"1970-01-01T01:00:00+01:00\",\n" +
             "  \"lastModified\" : \"1970-01-01T01:00:00+01:00\",\n" +
             "  \"type\" : \"CAPTION\",\n" +
-            "  \"lang\" : \"nl-NL\"\n" +
+            "  \"lang\" : \"nl-NL\",\n" +
+            "   \"cueCount\" : 1" +
             "}");
     }
 
     @Test
     public void from() throws Exception {
-        Subtitles subtitles = Subtitles.from(Arrays.asList(StandaloneCue.tt888(new Cue(
+        Subtitles subtitles = Subtitles.from(Arrays.asList(
+                StandaloneCue.tt888(new Cue(
                 "mid", 1, Duration.ZERO, Duration.ofSeconds(64), "bla"))).iterator());
         Jackson2TestUtil.roundTripAndSimilar(subtitles, "{\n" +
             "  \"mid\" : \"mid\",\n" +
@@ -85,6 +99,7 @@ public class SubtitlesTest {
             "    \"value\" : \"WEBVTT\\n\\n1\\n00:00:00.000 --> 00:01:04.000\\nbla\\n\\n\"\n" +
             "  },\n" +
             "  \"type\" : \"CAPTION\",\n" +
+            "  \"cueCount\" :1,\n" +
             "  \"lang\" : \"nl\"\n" +
             "}");
 
