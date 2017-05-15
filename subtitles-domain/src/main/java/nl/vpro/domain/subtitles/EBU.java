731dd99211e165620912d8754658a446f6743a17
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-05-15 09:26:57 +0000

    MSE-3674 Made parsing of EBU streaming.
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-5.3-SNAPSHOT/; revision=103422

--- subtitles-domain/src/main/java/nl/vpro/domain/subtitles/EBU.java
+++ subtitles-domain/src/main/java/nl/vpro/domain/subtitles/EBU.java
@@ -2,14 +2,20 @@ package nl.vpro.domain.subtitles;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * Code borrowed from https://github.com/JDaren/subtitleConverter
+ *
+ * Adapted so it returns a stream of our {@link Cue} objects
  * @author Michiel Meeuwissen
  * @since 5.3
  */
@@ -18,175 +24,215 @@ public class EBU {
 
 
     public static Stream<Cue> parse(String parent, InputStream is) {
-        return parseToList(parent, is).stream();
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(parseToIterator(parent, is), Spliterator.ORDERED), false);
     }
 
-    protected static List<Cue> parseToList(String parent, InputStream is) {
-
-        List<Cue> cues = new ArrayList<>();
-        byte[] gsiBlock = new byte[1024];
-        byte[] ttiBlock = new byte[128];
+    protected static Iterator<Cue> parseToIterator(final String parent, final InputStream is) {
 
 
-        try {
-            //we read the file
-            //but first we create the possible styles
-            int bytesRead = is.read(gsiBlock);
-            //the GSI block is loaded
-            if (bytesRead < 1024) {
-                //the file must contain at least a GSI block and a TTI block
-                //this is a fatal parsing error.
-                throw new IllegalArgumentException("The file must contain at least a GSI block");
-            }
-            //CPC : code page number 0..2
-            //DFC : disk format code 3..10
-            //save the number of frames per second
-            byte[] dfc = {gsiBlock[6], gsiBlock[7]};
-            int fps = Integer.parseInt(new String(dfc));
-            //DSC : Display Standard Code 11
-            //CCT : Character Code Table number 12..13
-            byte[] cct = {gsiBlock[12], gsiBlock[13]};
-            int table = Integer.parseInt(new String(cct));
-            //LC : Language Code 14..15
-            //OPT : Original Programme Title 16..47
-            byte[] opt = new byte[32];
-            System.arraycopy(gsiBlock, 16, opt, 0, 32);
-            String title = new String(opt);
-            //OEP : Original Episode Title 48..79
-            byte[] oet = new byte[32];
-            System.arraycopy(gsiBlock, 48, oet, 0, 32);
-            String episodeTitle = new String(oet);
-            //TPT : Translated Programme Title 80..111
-            //TEP : Translated Episode Title 112..143
-            //TN : Translator's Name 144..175
-            //TCD : Translators Contact Details 176..207
-            //SLR : Subtitle List Reference code 208..223
-            //CD : Creation Date 224..229
-            //RD : Revision Date 230..235
-            //RN : Revision Number 236..237
-            //TNB : Total Number of TTI Blocks 238..242
-            byte[] tnb = {gsiBlock[238], gsiBlock[239], gsiBlock[240], gsiBlock[241], gsiBlock[242]};
-            int numberOfTTIBlocks = Integer.parseInt(new String(tnb).trim());
-            //TNS : Total Number of Subtitles 243..247
-            byte[] tns = {gsiBlock[243], gsiBlock[244], gsiBlock[245], gsiBlock[246], gsiBlock[247]};
-            int numberOfSubtitles = Integer.parseInt(new String(tns).trim());
-            //TNG : Total Number of Subtitle Groups 248..250
-            //MNC : Max Number of characters in row 251..252
-            //MNR : Max number of rows 253..254
-            //TCS : Time Code: Status 255
-            //TCP : Time Code: Start-of-Programme 256..263
-            //TCF : Time Code: First In-Cue 264..271
-            //TND : Total Number of Disks 272
-            //DSN : Disk Sequence Number 273
-            //CO : Country of Origin 274..276
-            //PUB : Publisher 277..308
-            //EN : Editor's Name 309..340
-            //ECD : Editor's Contact Details 341..372
-            // Spare bytes 373..447
-            //UDA : User-Defined Area 448..1023
+        return new Iterator<Cue>() {
 
-            //we add the title
-            //this checks the reference to the characters coding employed.
-            if (table > 4 || table < 0) {
-                log.error("Invalid Character Code table number, corrupt data? will try to parse anyways assuming it is latin.");
-            } else if (table != 0) {
-                log.error("Only latin alphabet supported for import from STL, other languages may produce unexpected results.");
-            }
+            protected Cue next;
 
+            protected Boolean hasNext = null;
+            private int i = 0;
+            final byte[] ttiBlock = new byte[128];
+            final int numberOfTTIBlocks;
+            final int fps;
+            final int numberOfSubtitles;
             int subtitleNumber = 1;
-            boolean additionalText = false;
-            Cue currentCaption = null;
-            StringBuilder currentContent = new StringBuilder();
-            //the TTI blocks are read
-            for (int i = 0; i < numberOfTTIBlocks; i++) {
-                //the TTI block is loaded
-                bytesRead = is.read(ttiBlock);
-                if (bytesRead < 128) {
-                    //unexpected end of file
-                    log.error("Unexpected end of file, {}  blocks read, expecting {} blocks in total.", i, numberOfTTIBlocks);
-                    break;
-                }
 
-                //if we have additional text pending, we do not create a new caption
-                if (currentCaption != null) {
-                    currentCaption.content = currentContent.toString().trim();
-                }
-                if (!additionalText) {
-                    currentCaption = new Cue();
-                    currentContent.setLength(0);
-                    currentCaption.parent = parent;
 
-                    cues.add(currentCaption);
-                }
+            {
+                byte[] gsiBlock = new byte[1024];
 
-                //SGN : Subtitle group number 0
-                //SN : Subtitle Number 1..2
-                int currentSubNumber = Math.abs(ttiBlock[1] + 256 * ttiBlock[2]);
-                if (currentSubNumber != subtitleNumber) {
-                    //missing subtitle number?
-                    log.debug("Unexpected subtitle number {} (expected {}) at TTI block {}  Parsing proceeds...", subtitleNumber, currentSubNumber, i);
-                }
-                currentCaption.sequence = subtitleNumber;
 
-                //EBN : Extension Block Number 3
-                int ebn = ttiBlock[3];
-                if (ebn != -1 && ebn != -2) {
-                    additionalText = true;
-                } else if (ebn == -2) {
-                    //EBN is UserData so Jump it.
-                    additionalText = false;
-                    continue;
-                } else {
-                    additionalText = false;
-                }
-                //CS : Cumulative Status 4
-                //TCI : Time Code In 5..8
-                String startTime = "" + ttiBlock[5] + ":" + ttiBlock[6] + ":" + ttiBlock[7] + ":" + ttiBlock[8];
-                //TCO : Time Code Out 9..12
-                String endTime = "" + ttiBlock[9] + ":" + ttiBlock[10] + ":" + ttiBlock[11] + ":" + ttiBlock[12];
-                ;
-                //VP : Vertical Position 13
-                //JC : Justification Code 14
-                int justification = ttiBlock[14];
-                //0:none, 1:left, 2:centered, 3:right
-                //CF : Comment Flag 15
-                if (ttiBlock[15] == 0) {
-                    //comments are ignored
-                    //TF : Text Field 16..112
-                    byte[] textField = new byte[112];
-                    System.arraycopy(ttiBlock, 16, textField, 0, 112);
 
-                    if (additionalText)
-                        //if it is just additional text for the caption
-                        parseTextForSTL(currentContent, textField, justification);
-                    else {
-                        currentCaption.start = parseTime(startTime + "/" + fps);
-                        currentCaption.end = parseTime(endTime + "/" + fps);
-                        parseTextForSTL(currentContent, textField, justification);
+                try {
+                    //we read the file
+                    //but first we create the possible styles
+                    int bytesRead = is.read(gsiBlock);
+                    //the GSI block is loaded
+                    if (bytesRead < 1024) {
+                        //the file must contain at least a GSI block and a TTI block
+                        //this is a fatal parsing error.
+                        throw new IllegalArgumentException("The file must contain at least a GSI block");
+                    }
+                    //CPC : code page number 0..2
+                    //DFC : disk format code 3..10
+                    //save the number of frames per second
+                    byte[] dfc = {gsiBlock[6], gsiBlock[7]};
+                    fps = Integer.parseInt(new String(dfc));
+                    //DSC : Display Standard Code 11
+                    //CCT : Character Code Table number 12..13
+                    byte[] cct = {gsiBlock[12], gsiBlock[13]};
+                    int table = Integer.parseInt(new String(cct));
+                    //LC : Language Code 14..15
+                    //OPT : Original Programme Title 16..47
+                    byte[] opt = new byte[32];
+                    System.arraycopy(gsiBlock, 16, opt, 0, 32);
+                    String title = new String(opt);
+                    //OEP : Original Episode Title 48..79
+                    byte[] oet = new byte[32];
+                    System.arraycopy(gsiBlock, 48, oet, 0, 32);
+                    String episodeTitle = new String(oet);
+                    //TPT : Translated Programme Title 80..111
+                    //TEP : Translated Episode Title 112..143
+                    //TN : Translator's Name 144..175
+                    //TCD : Translators Contact Details 176..207
+                    //SLR : Subtitle List Reference code 208..223
+                    //CD : Creation Date 224..229
+                    //RD : Revision Date 230..235
+                    //RN : Revision Number 236..237
+                    //TNB : Total Number of TTI Blocks 238..242
+                    byte[] tnb = {gsiBlock[238], gsiBlock[239], gsiBlock[240], gsiBlock[241], gsiBlock[242]};
+                    numberOfTTIBlocks = Integer.parseInt(new String(tnb).trim());
+                    //TNS : Total Number of Subtitles 243..247
+                    byte[] tns = {gsiBlock[243], gsiBlock[244], gsiBlock[245], gsiBlock[246], gsiBlock[247]};
+                    numberOfSubtitles = Integer.parseInt(new String(tns).trim());
+                    //TNG : Total Number of Subtitle Groups 248..250
+                    //MNC : Max Number of characters in row 251..252
+                    //MNR : Max number of rows 253..254
+                    //TCS : Time Code: Status 255
+                    //TCP : Time Code: Start-of-Programme 256..263
+                    //TCF : Time Code: First In-Cue 264..271
+                    //TND : Total Number of Disks 272
+                    //DSN : Disk Sequence Number 273
+                    //CO : Country of Origin 274..276
+                    //PUB : Publisher 277..308
+                    //EN : Editor's Name 309..340
+                    //ECD : Editor's Contact Details 341..372
+                    // Spare bytes 373..447
+                    //UDA : User-Defined Area 448..1023
+
+                    //we add the title
+                    //this checks the reference to the characters coding employed.
+                    if (table > 4 || table < 0) {
+                        log.error("Invalid Character Code table number, corrupt data? will try to parse anyways assuming it is latin.");
+                    } else if (table != 0) {
+                        log.error("Only latin alphabet supported for import from STL, other languages may produce unexpected results.");
+                    }
+
+                    int subtitleNumber = 1;
+                } catch (IOException e) {
+                    log.error(e.getMessage(), e);
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                findNext();
+                return hasNext;
+            }
+
+            @Override
+            public Cue next() {
+                findNext();
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                hasNext = null;
+                return next;
+            }
+
+
+            protected void findNext() {
+
+                if (hasNext == null) {
+                    next = null;
+                    try {
+                        Cue toFill = new Cue();
+                        toFill.parent = parent;
+                        boolean additionalText = false;
+                        StringBuilder currentContent = new StringBuilder();
+                        //the TTI blocks are read
+                        while (i < numberOfTTIBlocks && next == null) {
+                            i++;
+                            //the TTI block is loaded
+                            int bytesRead = is.read(ttiBlock);
+                            if (bytesRead < 128) {
+                                //unexpected end of file
+                                log.error("Unexpected end of file, {}  blocks read, expecting {} blocks in total.", i, numberOfTTIBlocks);
+                                break;
+                            }
+
+
+                            //SGN : Subtitle group number 0
+                            //SN : Subtitle Number 1..2
+                            int currentSubNumber = Math.abs(ttiBlock[1] + 256 * ttiBlock[2]);
+                            if (currentSubNumber != subtitleNumber) {
+                                //missing subtitle number?
+                                log.debug("Unexpected subtitle number {} (expected {}) at TTI block {}  Parsing proceeds...", subtitleNumber, currentSubNumber, i);
+                            }
+                            toFill.sequence = subtitleNumber;
+
+                            //EBN : Extension Block Number 3
+                            int ebn = ttiBlock[3];
+                            if (ebn != -1 && ebn != -2) {
+                                additionalText = true;
+                            } else if (ebn == -2) {
+                                //EBN is UserData so Jump it.
+                                additionalText = false;
+                                continue;
+                            } else {
+                                additionalText = false;
+                            }
+                            //CS : Cumulative Status 4
+                            //TCI : Time Code In 5..8
+                            String startTime = "" + ttiBlock[5] + ":" + ttiBlock[6] + ":" + ttiBlock[7] + ":" + ttiBlock[8];
+                            //TCO : Time Code Out 9..12
+                            String endTime = "" + ttiBlock[9] + ":" + ttiBlock[10] + ":" + ttiBlock[11] + ":" + ttiBlock[12];
+                            ;
+                            //VP : Vertical Position 13
+                            //JC : Justification Code 14
+                            int justification = ttiBlock[14];
+                            //0:none, 1:left, 2:centered, 3:right
+                            //CF : Comment Flag 15
+                            if (ttiBlock[15] == 0) {
+                                //comments are ignored
+                                //TF : Text Field 16..112
+                                byte[] textField = new byte[112];
+                                System.arraycopy(ttiBlock, 16, textField, 0, 112);
+
+                                if (additionalText)
+                                    //if it is just additional text for the caption
+                                    parseTextForSTL(currentContent, textField, justification);
+                                else {
+                                    toFill.start = parseTime(startTime + "/" + fps);
+                                    toFill.end = parseTime(endTime + "/" + fps);
+                                    parseTextForSTL(currentContent, textField, justification);
+                                }
+                            }
+                            //we increase the subtitle number
+                            if (!additionalText) {
+                                subtitleNumber++;
+                                next = toFill;
+                                break;
+                            }
+
+                        }
+                        if (next != null) {
+                            hasNext = true;
+                            next.content = currentContent.toString().trim();
+                        } else {
+                            hasNext = false;
+                            //we close the reader
+                            is.close();
+                            if (subtitleNumber != numberOfSubtitles) {
+                                log.warn("Number of parsed subtitles ({}) different from expected number of subtitles ({})", subtitleNumber, numberOfSubtitles);
+                            }
+                        }
+
+                    } catch (Exception e) {
+                        hasNext = false;
+                        //format error
+                        throw new IllegalArgumentException("Format error in the file, might be due to corrupt data." + e.getMessage(), e);
                     }
                 }
-                //we increase the subtitle number
-                if (!additionalText)
-                    subtitleNumber++;
-
-            }
-            if (currentCaption != null) {
-                currentCaption.content = currentContent.toString().trim();
-            }
-            if (subtitleNumber != numberOfSubtitles) {
-                log.warn("Number of parsed subtitles ({}) different from expected number of subtitles ({})", subtitleNumber, numberOfSubtitles);
             }
 
-            //we close the reader
-            is.close();
-
-
-        } catch (Exception e) {
-            //format error
-            throw new IllegalArgumentException("Format error in the file, migth be due to corrupt data.\n" + e.getMessage());
-        }
-
-        return cues;
+        };
     }
 
 
@@ -334,9 +380,8 @@ public class EBU {
      */
     private static Duration parseTime(String value) {
         int h, m, s, f;
-        float fps;
         String[] args = value.split("/");
-        fps = Float.parseFloat(args[1]);
+        float fps = Float.parseFloat(args[1]);
         args = args[0].split(":");
         h = Integer.parseInt(args[0]);
         m = Integer.parseInt(args[1]);
