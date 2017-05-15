bc5368bb5692a175849017e5df03f323c9fa8b29
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-05-15 08:23:20 +0000

    MSE-3561
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-5.3-SNAPSHOT/; revision=103419

--- subtitles-domain/src/test/java/nl/vpro/domain/subtitles/EBUTest.java
+++ subtitles-domain/src/test/java/nl/vpro/domain/subtitles/EBUTest.java
@@ -2,6 +2,7 @@ package nl.vpro.domain.subtitles;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.util.stream.Stream;
 
 import org.junit.Test;
@@ -15,12 +16,9 @@ public class EBUTest {
 
 
     @Test
-    public void parse() {
+    public void parse() throws IOException {
         Stream<Cue> cues = EBU.parse("bla", getClass().getResourceAsStream("/VPWON_1272504.stl"));
 
-        cues.forEach(c -> {
-            System.out.println(c);
-            }
-        );
+        WEBVTTandSRT.formatSRT(cues.iterator(), System.out);
     }
 }
