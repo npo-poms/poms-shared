24d3d9a7a41a77e67792136d2ac1fed848c9f275
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-05-16 12:24:12 +0000

    available subtitles publishing.
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-5.3-SNAPSHOT/; revision=103506

--- media-domain/src/main/java/nl/vpro/domain/media/support/SubtitlesWorkflow.java
+++ media-domain/src/main/java/nl/vpro/domain/media/support/SubtitlesWorkflow.java
@@ -1,5 +1,7 @@
 package nl.vpro.domain.media.support;
 
+import lombok.Getter;
+
 /**
  * @author Michiel Meeuwissen
  * @since 5.3
@@ -8,27 +10,54 @@ public enum SubtitlesWorkflow {
     /**
      * it is unknown wether any subtitles need publishing or revoking
      */
-    UNDEFINED,
+    UNDEFINED(null),
     /**
      * All subtitles for this object are published
      */
-    PUBLISHED,
-    /**
-     * Subtitles needs publishing, but something changed
-     */
-    FOR_PUBLICATION,
-    /**
-     * Subtitles needs publishing and after that the status needs to become {@link #PUBLISHED_NEW}
-     */
-    FOR_PUBLICATION_NEW,
+    PUBLISHED(null),
 
     /**
      * All subtitles for this object are published, but there were new or removed ones, so the mediaobject itself still needs publishing
      */
-    PUBLISHED_NEW,
+    PUBLISHED_MEDIA(PUBLISHED),
+
+    /**
+     * Subtitles needs publishing, but something changed
+     */
+    FOR_PUBLICATION(PUBLISHED),
+
+    /**
+     * Subtitles needs publishing and after that the status needs to become {@link #PUBLISHED_MEDIA}
+     */
+    FOR_PUBLICATION_MEDIA(PUBLISHED_MEDIA),
+
 
     /**
      * All subtitles for this object are revoked
      */
-    REVOKED
+    REVOKED(null),
+
+
+    /**
+     * All subtitles for this object are revoked
+     */
+    REVOKED_MEDIA(REVOKED),
+
+    /**
+     * All subtitles for this object are revoked
+     */
+    FOR_REVOCATION(REVOKED),
+
+    /**
+     * All subtitles for this object are revoked
+     */
+    FOR_REVOCATION_MEDIA(REVOKED_MEDIA)
+    ;
+
+    @Getter
+    private final SubtitlesWorkflow target;
+
+    SubtitlesWorkflow(SubtitlesWorkflow target) {
+        this.target = target;
+    }
 }
