633d96eba375d7b445c40b38987bee01cc5fbf15
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-05-16 10:57:02 +0000

    Trying to fix publishing of available subtitles.
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-5.3-SNAPSHOT/; revision=103503

--- media-domain/src/main/java/nl/vpro/domain/media/MediaObject.java
+++ media-domain/src/main/java/nl/vpro/domain/media/MediaObject.java
@@ -4,6 +4,8 @@
  */
 package nl.vpro.domain.media;
 
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
@@ -501,6 +503,8 @@ public abstract class MediaObject extends PublishableObject
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
+    @Getter(AccessLevel.PACKAGE)
+    @Setter(AccessLevel.PACKAGE)
     private SubtitlesWorkflow subtitlesWorkflow = SubtitlesWorkflow.UNDEFINED;
 
     @ElementCollection(fetch = FetchType.LAZY)
@@ -2388,9 +2392,7 @@ public abstract class MediaObject extends PublishableObject
         }
     }
 
-    void setSubtitlesWorkflow(SubtitlesWorkflow subtitlesWorkflow) {
-        this.subtitlesWorkflow = subtitlesWorkflow;
-    }
+
 
     public abstract SubMediaType getType();
 
