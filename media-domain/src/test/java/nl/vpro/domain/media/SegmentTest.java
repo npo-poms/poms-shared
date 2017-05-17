07a879355c07a701b21e93fd32e587c1f73d5faf
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-05-17 08:33:51 +0000

    Test case failures.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=103543

--- media-domain/src/test/java/nl/vpro/domain/media/SegmentTest.java
+++ media-domain/src/test/java/nl/vpro/domain/media/SegmentTest.java
@@ -19,7 +19,27 @@ public class SegmentTest {
 
     @Test
     public void xml() throws Exception {
-        String xml = "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?><segment midRef=\"RBX_NTR_2648108\" type=\"SEGMENT\" urnRef=\"urn:vpro:media:program:83538010\" avType=\"AUDIO\" embeddable=\"true\" mid=\"RBX_NTR_4965178\" sortDate=\"2013-11-01T04:36:35.076+01:00\" creationDate=\"2016-11-01T04:36:35.076+01:00\" lastModified=\"2016-11-01T04:36:35.105+01:00\" publishDate=\"2016-11-01T04:42:23.506+01:00\" urn=\"urn:vpro:media:segment:83538015\" workflow=\"PUBLISHED\" xmlns=\"urn:vpro:media:2009\" xmlns:shared=\"urn:vpro:shared:2009\"><crid>crid://item.radiobox2/372578</crid><broadcaster id=\"NTR\">NTR</broadcaster><title owner=\"RADIOBOX\" type=\"MAIN\">Monteverdi - Orfeo</title><description owner=\"RADIOBOX\" type=\"MAIN\">Opname 12 oktober 2016, Opera LausanneJose Maria Lo Monaco &amp; Fernando Guimaraes &amp; Anais Yvoz &amp; Delphine Galou &amp; Nicolas Courjal &amp; Anicio Zorzi Giustiniani &amp; Alessandro Giangrande &amp; Mathilde Opinel</description><duration>P0DT0H3M0.000S</duration><credits/><descendantOf urnRef=\"urn:vpro:media:group:13405607\" midRef=\"AUTO_NTROPERALIVE\" type=\"SERIES\"/><descendantOf urnRef=\"urn:vpro:media:program:83538010\" midRef=\"RBX_NTR_2648108\" type=\"BROADCAST\"/><locations/><scheduleEvents/><images><shared:image owner=\"RADIOBOX\" type=\"PICTURE\" highlighted=\"false\" creationDate=\"2016-11-01T04:36:34.659+01:00\" lastModified=\"2016-11-01T04:36:35.079+01:00\" urn=\"urn:vpro:media:image:83538017\" workflow=\"PUBLISHED\"><shared:title>Orfeo ed Euridice</shared:title><shared:imageUri>urn:vpro:image:780595</shared:imageUri><shared:height>568</shared:height><shared:width>480</shared:width></shared:image></images><availableSubtitles/><start>P0DT0H0M0.000S</start></segment>";
+        String xml = "<segment midRef=\"RBX_NTR_2648108\" type=\"SEGMENT\" urnRef=\"urn:vpro:media:program:83538010\" avType=\"AUDIO\" embeddable=\"true\" mid=\"RBX_NTR_4965178\" sortDate=\"2013-11-01T04:36:35.076+01:00\" creationDate=\"2016-11-01T04:36:35.076+01:00\" lastModified=\"2016-11-01T04:36:35.105+01:00\" publishDate=\"2016-11-01T04:42:23.506+01:00\" urn=\"urn:vpro:media:segment:83538015\" workflow=\"PUBLISHED\" xmlns=\"urn:vpro:media:2009\" xmlns:shared=\"urn:vpro:shared:2009\">\n" +
+            "    <crid>crid://item.radiobox2/372578</crid>\n" +
+            "    <broadcaster id=\"NTR\">NTR</broadcaster>\n" +
+            "    <title owner=\"RADIOBOX\" type=\"MAIN\">Monteverdi - Orfeo</title>\n" +
+            "    <description owner=\"RADIOBOX\" type=\"MAIN\">Opname 12 oktober 2016, Opera LausanneJose Maria Lo Monaco &amp; Fernando Guimaraes &amp; Anais Yvoz &amp; Delphine Galou &amp; Nicolas Courjal &amp; Anicio Zorzi Giustiniani &amp; Alessandro Giangrande &amp; Mathilde Opinel</description>\n" +
+            "    <duration>P0DT0H3M0.000S</duration>\n" +
+            "    <credits/>\n" +
+            "    <descendantOf urnRef=\"urn:vpro:media:group:13405607\" midRef=\"AUTO_NTROPERALIVE\" type=\"SERIES\"/>\n" +
+            "    <descendantOf urnRef=\"urn:vpro:media:program:83538010\" midRef=\"RBX_NTR_2648108\" type=\"BROADCAST\"/>\n" +
+            "    <locations/>\n" +
+            "    <scheduleEvents/>\n" +
+            "    <images>\n" +
+            "        <shared:image owner=\"RADIOBOX\" type=\"PICTURE\" highlighted=\"false\" creationDate=\"2016-11-01T04:36:34.659+01:00\" lastModified=\"2016-11-01T04:36:35.079+01:00\" urn=\"urn:vpro:media:image:83538017\" workflow=\"PUBLISHED\">\n" +
+            "            <shared:title>Orfeo ed Euridice</shared:title>\n" +
+            "            <shared:imageUri>urn:vpro:image:780595</shared:imageUri>\n" +
+            "            <shared:height>568</shared:height>\n" +
+            "            <shared:width>480</shared:width>\n" +
+            "        </shared:image>\n" +
+            "    </images>\n" +
+            "    <start>P0DT0H0M0.000S</start>\n" +
+            "</segment>";
         Segment segment = JAXBTestUtil.unmarshal(xml,  Segment.class);
         JAXBTestUtil.roundTripAndSimilarAndEquals(segment, xml);
     }
