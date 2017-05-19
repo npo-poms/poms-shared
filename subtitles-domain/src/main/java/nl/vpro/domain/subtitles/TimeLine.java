e54224b00dd4d8c05fa314671d0781c3864713c5
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-05-19 13:36:39 +0000

    Support for offsetguessing.
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-5.3-SNAPSHOT/; revision=103656

---
+++ subtitles-domain/src/main/java/nl/vpro/domain/subtitles/TimeLine.java
@@ -0,0 +1,20 @@
+package nl.vpro.domain.subtitles;
+
+import java.time.Duration;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.3
+ */
+public class TimeLine {
+
+    final Integer sequence;
+    final Duration start;
+    final Duration end;
+
+    TimeLine(Integer sequence, Duration start, Duration end) {
+        this.sequence = sequence;
+        this.start = start;
+        this.end = end;
+    }
+}
