258fc5b8864c8b77c9e1a699e485dd37d5678eb6
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-05-24 17:22:04 +0000

    Still support the insane date format in XML, but at least default to something more sensible.
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-5.3-SNAPSHOT/; revision=103859

---
+++ api-domain/src/test/java/nl/vpro/domain/api/DurationRangeFacetsTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2013 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.domain.api;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.time.Duration;
+
+import org.junit.Test;
+
+import nl.vpro.jackson2.Jackson2Mapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.3
+ */
+public class DurationRangeFacetsTest {
+
+    @Test
+    public void testJsonArray() throws Exception {
+        Reader reader = new StringReader("[{\"name\":\"My range\",\"begin\":100,\"end\":200}]");
+        DurationRangeFacets<AbstractSearch> facet = (DurationRangeFacets<AbstractSearch>) Jackson2Mapper.INSTANCE.readValue(reader, DurationRangeFacets.class);
+        assertThat(facet.getRanges()).containsOnly(new DurationRangeFacetItem("My range", Duration.ofMillis(100), Duration.ofMillis(200)));
+    }
+
+}
