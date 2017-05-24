520457a96547c8400362027df16700f6f7fe0c75
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-05-24 15:59:54 +0000

    Durations cannot be negative.
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-5.3-SNAPSHOT/; revision=103857

---
+++ api-domain/src/main/java/nl/vpro/domain/api/jackson/DurationRangeFacetsToJson.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2014 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.domain.api.jackson;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+
+import nl.vpro.domain.api.*;
+import nl.vpro.jackson2.Jackson2Mapper;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.3
+ */
+public class DurationRangeFacetsToJson {
+
+    private static final ObjectMapper mapper = Jackson2Mapper.getInstance();
+
+    public static class Serializer extends JsonSerializer<DurationRangeFacets<?>> {
+
+        @Override
+        public void serialize(DurationRangeFacets<?> value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+            jgen.writeStartArray();
+            for(RangeFacet<Duration> item : value.getRanges()) {
+                if(item instanceof DurationRangeInterval) {
+                    jgen.writeObject(((DurationRangeInterval)item).getInterval());
+                } else {
+                    jgen.writeObject(item);
+                }
+            }
+            jgen.writeEndArray();
+        }
+    }
+
+    public static class Deserializer extends JsonDeserializer<DurationRangeFacets<?>> {
+
+        @Override
+        public DurationRangeFacets deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+            DurationRangeFacets result = new DurationRangeFacets();
+
+            TreeNode treeNode = mapper.readTree(jp);
+            if(treeNode instanceof ArrayNode) {
+                ArrayNode arrayNode = (ArrayNode)treeNode;
+                for(JsonNode jsonNode : arrayNode) {
+                    if(jsonNode.isTextual()) {
+                        result.addRanges(new DurationRangeInterval(jsonNode.textValue()));
+                    } else {
+                        result.addRanges(mapper.readValue(jsonNode.toString(), DurationRangeFacetItem.class));
+                    }
+                }
+            } else if(treeNode instanceof ObjectNode) {
+                result.addRanges(mapper.readValue((treeNode).toString(), DurationRangeFacetItem.class));
+            } else if(treeNode instanceof TextNode) {
+                result.addRanges(new DurationRangeInterval(((TextNode)treeNode).textValue()));
+            } else {
+                throw new IllegalArgumentException("Unsupported node: " + treeNode.toString());
+            }
+
+            return result;
+        }
+    }
+}
