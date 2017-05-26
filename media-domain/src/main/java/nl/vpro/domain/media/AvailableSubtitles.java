8942c92cfccedbecd508586c09034784be533250
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-05-26 14:58:55 +0000

    Simplified.
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-5.3-SNAPSHOT/; revision=103898

--- media-domain/src/main/java/nl/vpro/domain/media/AvailableSubtitles.java
+++ media-domain/src/main/java/nl/vpro/domain/media/AvailableSubtitles.java
@@ -1,5 +1,9 @@
 package nl.vpro.domain.media;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
 import java.util.Locale;
 
 import javax.persistence.Cacheable;
@@ -15,63 +19,28 @@ import nl.vpro.xml.bind.LocaleAdapter;
 
 @Embeddable
 @Cacheable
-@XmlType(name="AvailableSubtitlesType")
+@XmlType(name="availableSubtitlesType")
+@EqualsAndHashCode
+@ToString
+@Data
 public class AvailableSubtitles {
 
-	private Locale language;
+    @XmlJavaTypeAdapter(LocaleAdapter.class)
+    @XmlAttribute
+    private Locale language;
 
-	@Enumerated(EnumType.STRING)
-	private SubtitlesType type;
+    @Enumerated(EnumType.STRING)
+    @XmlAttribute
+    private SubtitlesType type;
 
-	public AvailableSubtitles() {
+    public AvailableSubtitles() {
 
-	}
-
-	public AvailableSubtitles(Locale language, SubtitlesType type) {
-		this.language = language;
-		this.type = type;
-
-	}
-
-	@XmlJavaTypeAdapter(LocaleAdapter.class)
-	@XmlAttribute
-	public Locale getLanguage() {
-		return language;
-	}
-
-	public void setLanguage(Locale language) {
-		this.language = language;
-	}
-
-	@XmlAttribute
-	public SubtitlesType getType() {
-		return type;
-	}
-
-	public void setType(SubtitlesType type) {
-		this.type = type;
-	}
-
-	@Override
-	public String toString() {
-		return "AvailableSubtitles [language=" + language + ", type=" + type + "]";
-	}
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        AvailableSubtitles that = (AvailableSubtitles) o;
-
-        if (language != null ? !language.equals(that.language) : that.language != null) return false;
-        return type == that.type;
     }
 
-    @Override
-    public int hashCode() {
-        int result = language != null ? language.hashCode() : 0;
-        result = 31 * result + (type != null ? type.hashCode() : 0);
-        return result;
+    @lombok.Builder
+    public AvailableSubtitles(Locale language, SubtitlesType type) {
+        this.language = language;
+        this.type = type;
+
     }
 }
