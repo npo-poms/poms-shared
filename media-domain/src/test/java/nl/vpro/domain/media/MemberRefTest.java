5a3cce0f07d61469053ca32a9a85937fa60d8206
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-05-26 10:54:38 +0000

    Added a test.
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-5.3-SNAPSHOT/; revision=103881

---
+++ media-domain/src/test/java/nl/vpro/domain/media/MemberRefTest.java
@@ -0,0 +1,35 @@
+package nl.vpro.domain.media;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import nl.vpro.test.util.jackson2.Jackson2TestUtil;
+import nl.vpro.test.util.jaxb.JAXBTestUtil;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.3
+ */
+public class MemberRefTest {
+
+    @Test
+    public void xml() throws IOException, SAXException {
+        MemberRef ref = new MemberRef();
+        ref.setAdded(Instant.EPOCH);
+        JAXBTestUtil.roundTripAndSimilar(ref, "<local:memberRef added=\"1970-01-01T01:00:00+01:00\" highlighted=\"false\" xmlns=\"urn:vpro:media:2009\" xmlns:shared=\"urn:vpro:shared:2009\" xmlns:local=\"uri:local\"/>");
+    }
+
+    @Test
+    public void json() throws Exception {
+        MemberRef ref = new MemberRef();
+        ref.setAdded(LocalDateTime.of(2017, 5, 24, 16, 30).atZone(Schedule.ZONE_ID).toInstant());
+        Jackson2TestUtil.roundTripAndSimilar(ref, "{\n" +
+            "  \"highlighted\" : false,\n" +
+            "  \"added\" : 1495636200000\n" +
+            "}");
+    }
+}
