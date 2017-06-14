797540f7704308739c4b29d21417028317593534
Machiel Groeneveld <m.groeneveld@vpro.nl>
2017-06-14 08:16:15 +0000

    NPA-398 handle 404 from topspin
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=104488

--- api-domain/src/main/java/nl/vpro/domain/api/topspin/Recommendations.java
+++ api-domain/src/main/java/nl/vpro/domain/api/topspin/Recommendations.java
@@ -3,10 +3,11 @@ package nl.vpro.domain.api.topspin;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Recommendations {
-    private List<Recommendation> recommendations;
+    private List<Recommendation> recommendations = new ArrayList<>();
 }
