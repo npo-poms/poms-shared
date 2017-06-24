a304aba7435735036158776324401ca331a79d85
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-06-24 18:44:48 +0000

    Details.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=104757

--- user-domain/src/main/java/nl/vpro/domain/user/AbstractUser.java
+++ user-domain/src/main/java/nl/vpro/domain/user/AbstractUser.java
@@ -22,6 +22,7 @@ public abstract class AbstractUser implements Serializable, Identifiable<String>
     private static final long serialVersionUID = 1L;
 
     @Id
+    @Column(name = "principalid")
     protected String principalId;
 
     protected String givenName;
