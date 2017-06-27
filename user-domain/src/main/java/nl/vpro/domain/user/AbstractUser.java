2357f71bbe36df2696f51f2c7ff3d7842516306e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-06-27 20:58:00 +0000

    Test case failures.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=104841

--- user-domain/src/main/java/nl/vpro/domain/user/AbstractUser.java
+++ user-domain/src/main/java/nl/vpro/domain/user/AbstractUser.java
@@ -10,6 +10,7 @@ import java.time.Instant;
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.Pattern;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
@@ -33,6 +34,8 @@ public abstract class AbstractUser implements Serializable, Identifiable<String>
     protected String displayName;
 
     @Column(nullable = false)
+    @Pattern(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+    )
     protected String email;
 
     @Column
