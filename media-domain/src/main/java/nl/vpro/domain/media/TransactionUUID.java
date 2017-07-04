678c385e82384d49f81c4e8c0b503c43563978ab
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-07-04 12:01:06 +0000

    Doesn't compile
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=104980

--- media-domain/src/main/java/nl/vpro/domain/media/TransactionUUID.java
+++ media-domain/src/main/java/nl/vpro/domain/media/TransactionUUID.java
@@ -1,30 +1,28 @@
 package nl.vpro.domain.media;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.*;
 import java.util.function.Consumer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author Michiel Meeuwissen
  * @since 3.4
  */
+@Slf4j
 public class TransactionUUID {
-    
-    private static final Logger LOG = LoggerFactory.getLogger(TransactionUUID.class);
-    
-    private static List<TransactionUUIDConsumer> consumers = new ArrayList<>();  
+
+    private static List<TransactionUUIDConsumer> consumers = new ArrayList<>();
     static {
         ServiceLoader.load(TransactionUUIDConsumer.class).iterator().forEachRemaining(p ->
             consumers.add(p)
         );
         if (! consumers.isEmpty()) {
-            LOG.info("Using consumers {}", consumers);
+            log.info("Using consumers {}", consumers);
         }
     }
 
-    private static ThreadLocal<UUID> threadLocal = ThreadLocal.withInitial(() -> UUID.randomUUID());
+    private static ThreadLocal<UUID> threadLocal = ThreadLocal.withInitial(UUID::randomUUID);
 
     public static UUID get() {
         UUID uuid = threadLocal.get();
@@ -37,18 +35,18 @@ public class TransactionUUID {
         consume(uuid.toString());
         return uuid;
     }
-    
+
     private static void consume(String uuid){
         for (TransactionUUIDConsumer consumer : consumers) {
             consumer.accept(uuid);
         }
-        
+
     }
 
     public static void reset() {
         threadLocal.remove();
     }
-    
+
     public static List<TransactionUUIDConsumer> getConsumers() {
         return Collections.unmodifiableList(consumers);
     }
