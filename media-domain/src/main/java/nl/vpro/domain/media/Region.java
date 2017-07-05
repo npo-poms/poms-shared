93cbcf1ea0eebd042a909d5d1fbe7a951668542f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-07-05 08:31:12 +0000

    MSE-3730
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=104996

--- media-domain/src/main/java/nl/vpro/domain/media/Region.java
+++ media-domain/src/main/java/nl/vpro/domain/media/Region.java
@@ -6,8 +6,21 @@ package nl.vpro.domain.media;
 
 import javax.xml.bind.annotation.XmlEnum;
 
+import org.apache.commons.lang3.StringUtils;
+
 @XmlEnum
 public enum Region {
+    /**
+     * @deprecated Not supported by VMV
+     */
+    @Deprecated
     BENELUX,
-    NL
+    NL;
+
+    public static Region valueOfOrNull(String v) {
+        if (StringUtils.isEmpty(v)) {
+            return null;
+        }
+        return valueOf(v);
+    }
 }
