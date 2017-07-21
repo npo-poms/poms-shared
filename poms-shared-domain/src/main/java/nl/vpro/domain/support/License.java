cc959761d3b9f52d92ea12fd58bd8681c2205cc8
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-07-21 14:06:53 +0000

    MSE-3449
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=105513

--- poms-shared-domain/src/main/java/nl/vpro/domain/support/License.java
+++ poms-shared-domain/src/main/java/nl/vpro/domain/support/License.java
@@ -20,24 +20,31 @@ import nl.vpro.domain.Xmlns;
 public enum License implements nl.vpro.domain.Displayable {
 
 
-    COPYRIGHTED("Copyrighted", null),
-    PUBLIC_DOMAIN("Publiek domein", null),
+    COPYRIGHTED("Copyrighted", null, true),
+    PUBLIC_DOMAIN("Publiek domein", null, true),
 
-    CC_BY("Naamsvermelding", "https://creativecommons.org/licenses/by/3.0/"),
-    CC_BY_SA("Naamsvermelding-GelijkDelen", "https://creativecommons.org/licenses/by-sa/3.0/"),
-    CC_BY_ND("Naamsvermelding-GeenAfgeleideWerken", "https://creativecommons.org/licenses/by-nd/3.0/"),
-    CC_BY_NC("Naamsvermelding-NietCommercieel", "https://creativecommons.org/licenses/by-nc/3.0/"),
-    CC_BY_NC_SA("Naamsvermelding-NietCommercieel-GelijkDelen", "https://creativecommons.org/licenses/by-nc-sa/3.0/"),
-    CC_BY_NC_ND("Naamsvermelding-NietCommercieel-GeenAfgeleideWerken", "https://creativecommons.org/licenses/by-nc-nd/3.0/");
+    CC_BY("Naamsvermelding", "https://creativecommons.org/licenses/by/3.0/", true),
+    CC_BY_SA("Naamsvermelding-GelijkDelen", "https://creativecommons.org/licenses/by-sa/3.0/", true),
+    CC_BY_ND("Naamsvermelding-GeenAfgeleideWerken", "https://creativecommons.org/licenses/by-nd/3.0/", true),
+    CC_BY_NC("Naamsvermelding-NietCommercieel", "https://creativecommons.org/licenses/by-nc/3.0/", true),
+    CC_BY_NC_SA("Naamsvermelding-NietCommercieel-GelijkDelen", "https://creativecommons.org/licenses/by-nc-sa/3.0/", true),
+    CC_BY_NC_ND("Naamsvermelding-NietCommercieel-GeenAfgeleideWerken", "https://creativecommons.org/licenses/by-nc-nd/3.0/", true),
+    USA_GOV("United States Government Work", "http://www.usa.gov/copyright.shtml", false)
+
+    ;
 
     final String displayName;
     @Getter
     final URI url;
 
+    @Getter
+    final boolean listed;
 
-    License(String displayName, String url) {
+
+    License(String displayName, String url, boolean listed) {
         this.displayName = displayName;
         this.url = url == null ? null : URI.create(url);
+        this.listed = listed;
     }
 
     @Override
