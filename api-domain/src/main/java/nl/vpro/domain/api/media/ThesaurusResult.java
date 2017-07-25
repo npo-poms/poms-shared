660e032ee3fb485d5428b9b22056b19a06ddf817
Machiel Groeneveld <m.groeneveld@vpro.nl>
2017-07-25 07:18:37 +0000

    NPA-409 add thesaurus result and updates model
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=105565

--- api-domain/src/main/java/nl/vpro/domain/api/media/ThesaurusResult.java
+++ api-domain/src/main/java/nl/vpro/domain/api/media/ThesaurusResult.java
@@ -5,13 +5,23 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
+import nl.vpro.domain.api.Result;
+import nl.vpro.domain.media.gtaa.GTAAPerson;
 
 @XmlRootElement(name = "thesaurusResult")
 @XmlType(name = "thesaurusResultType")
-public class ThesaurusResult /*extends Result<GTAAPerson>*/{
+public class ThesaurusResult extends Result<GTAAPerson>{
 
-//	public ThesaurusResult(List<GTAAPerson> asList, long l, Integer max, long m) {
-//		super(asList, l, max, m);
-//	}
+	public ThesaurusResult() {
+		
+	}
+	
+	public ThesaurusResult(List<GTAAPerson> asList, Long offset, Integer max, Long total) {
+		super(asList, offset, max, total);
+	}
+
+	public ThesaurusResult(List<GTAAPerson> list, long offset, Integer max, int size) {
+		super(list, offset, max, Long.valueOf(size));
+	}
 
 }
