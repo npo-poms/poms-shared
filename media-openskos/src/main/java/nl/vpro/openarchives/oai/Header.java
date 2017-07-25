83004398f1d52f06ed08cff09efad30c5f5550a4
Machiel Groeneveld <m.groeneveld@vpro.nl>
2017-07-25 07:31:49 +0000

    NPA-409 add shared openskos repository
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=105573

---
+++ media-openskos/src/main/java/nl/vpro/openarchives/oai/Header.java
@@ -0,0 +1,35 @@
+package nl.vpro.openarchives.oai;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import nl.vpro.xml.bind.ZonedDateTimeXmlAdapter;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.3
+ */
+@XmlAccessorType(XmlAccessType.NONE)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Header {
+
+    @XmlElement
+    String identifier;
+    @XmlElement
+    @XmlJavaTypeAdapter(ZonedDateTimeXmlAdapter.class)
+    ZonedDateTime datestamp;
+    @XmlElement
+    List<String> setSpec;
+
+}
