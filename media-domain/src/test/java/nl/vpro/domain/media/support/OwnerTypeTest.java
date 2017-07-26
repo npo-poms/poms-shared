9071c48c3668db7f4e615cd34fc4b29099c35127
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-07-26 15:37:38 +0000

    NPA-403
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=105664

--- media-domain/src/test/java/nl/vpro/domain/media/support/OwnerTypeTest.java
+++ media-domain/src/test/java/nl/vpro/domain/media/support/OwnerTypeTest.java
@@ -14,19 +14,18 @@ public class OwnerTypeTest {
     @Test
     public void comparator() throws Exception {
         Comparator<OwnerType> ot = OwnerType.comparator(OwnerType.NPO);
-
         assertThat(ot.compare(OwnerType.NPO, OwnerType.BROADCASTER)).isNegative();
-
-
     }
 
     @Test
     public void defaultorder() throws Exception {
         Comparator<OwnerType> ot = OwnerType.comparator();
-
         assertThat(ot.compare(OwnerType.BROADCASTER, OwnerType.NPO)).isNegative();
+    }
 
-
+    @Test
+    public void deprecated() {
+        assertThat(OwnerType.IMMIX.isDeprecated()).isTrue();
     }
 
 }
