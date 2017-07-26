420d3dcbc6d0de9ddcb95d687101f46d809d3ca4
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-07-26 18:59:20 +0000

    Simplified.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=105668

--- media-domain/src/main/java/nl/vpro/domain/BasicTextualObject.java
+++ media-domain/src/main/java/nl/vpro/domain/BasicTextualObject.java
@@ -1,23 +1,12 @@
 package nl.vpro.domain;
 
-import nl.vpro.domain.media.support.OwnerType;
-import nl.vpro.domain.media.support.TextualType;
-import nl.vpro.util.TriFunction;
-
 /**
  * @author Michiel Meeuwissen
  * @since 5.5
  */
 public class BasicTextualObject extends AbstractTextualObject<BasicOwnedText, BasicOwnedText, BasicTextualObject> {
 
-    @Override
-    public TriFunction<String, OwnerType, TextualType, BasicOwnedText> getOwnedTitleCreator() {
-        return BasicOwnedText::new;
+    public BasicTextualObject() {
+        super(BasicOwnedText::new, BasicOwnedText::new);
     }
-
-    @Override
-    public TriFunction<String, OwnerType, TextualType, BasicOwnedText> getOwnedDescriptionCreator() {
-        return BasicOwnedText::new;
-    }
-
 }
