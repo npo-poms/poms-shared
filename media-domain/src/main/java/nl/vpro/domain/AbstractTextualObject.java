d327f8f55fbd237aeaebb085a70fcca49a9ca183
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-07-26 18:59:20 +0000

    Simplified.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=105668

--- media-domain/src/main/java/nl/vpro/domain/AbstractTextualObject.java
+++ media-domain/src/main/java/nl/vpro/domain/AbstractTextualObject.java
@@ -1,5 +1,11 @@
 package nl.vpro.domain;
 
+import lombok.Getter;
+
+import nl.vpro.domain.media.support.OwnerType;
+import nl.vpro.domain.media.support.TextualType;
+import nl.vpro.util.TriFunction;
+
 /**
  * @author Michiel Meeuwissen
  * @since 5.3
@@ -8,5 +14,20 @@ public abstract class AbstractTextualObject<T extends OwnedText, D extends Owned
     extends AbstractTextualObjectUpdate<T, D, TO>
     implements TextualObject<T, D, TO> {
 
+    @Getter
+    private final TriFunction<String, OwnerType, TextualType, T> ownedTitleCreator;
+    @Getter
+    private final TriFunction<String, OwnerType, TextualType, D> ownedDescriptionCreator;
 
+
+    public AbstractTextualObject(
+        TriFunction<String, OwnerType, TextualType, T> ownedTitleCreator,
+        TriFunction<String, OwnerType, TextualType, D> ownedDescriptionCreator) {
+        super(
+            (t, s) -> ownedTitleCreator.apply(t, DEFAULT_OWNER, s),
+            (t, s) -> ownedDescriptionCreator.apply(t, DEFAULT_OWNER, s)
+        );
+        this.ownedTitleCreator = ownedTitleCreator;
+        this.ownedDescriptionCreator = ownedDescriptionCreator;
+    }
 }
--- media-domain/src/main/java/nl/vpro/domain/BasicTextualUpdate.java
+++ media-domain/src/main/java/nl/vpro/domain/BasicTextualUpdate.java
@@ -1,22 +1,12 @@
 package nl.vpro.domain;
 
-import java.util.function.BiFunction;
-
-import nl.vpro.domain.media.support.TextualType;
-
 /**
  * @author Michiel Meeuwissen
  * @since 5.4
  */
 public class BasicTextualUpdate extends AbstractTextualObjectUpdate<BasicTypedText, BasicTypedText, BasicTextualUpdate> {
 
-    @Override
-    public BiFunction<String, TextualType, BasicTypedText> getTitleCreator() {
-        return BasicTypedText::new;
-    }
-
-    @Override
-    public BiFunction<String, TextualType, BasicTypedText> getDescriptionCreator() {
-        return BasicTypedText::new;
+    public BasicTextualUpdate() {
+        super(BasicTypedText::new, BasicTypedText::new);
     }
 }
