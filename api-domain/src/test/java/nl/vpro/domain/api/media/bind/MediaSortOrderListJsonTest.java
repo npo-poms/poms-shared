118bd6eb30f62ff78cc487c6ff7cef3b6967f7d6
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-07-26 12:45:28 +0000

    NPA-403
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=105653

---
+++ api-domain/src/test/java/nl/vpro/domain/api/media/bind/MediaSortOrderListJsonTest.java
@@ -0,0 +1,68 @@
+package nl.vpro.domain.api.media.bind;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import nl.vpro.domain.api.media.MediaForm;
+import nl.vpro.domain.api.media.MediaSortField;
+import nl.vpro.jackson2.Jackson2Mapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.5
+ */
+public class MediaSortOrderListJsonTest {
+
+    @Test
+    public void test() throws IOException {
+        String example = "{\n" +
+            "    \"searches\" : {\n" +
+            "        \"text\" : \"Twee vandaag\"\n" +
+            "    },\n" +
+            "    \"sort\" : {\n" +
+            "        \"title\" :{\n" +
+            "            \"order\" : \"ASC\",\n" +
+            "            \"type\" : \"LEXICO\",\n" +
+            "            \"owner\" : \"BROADCASTER\"\n" +
+            "        },\n" +
+            "        \"sortDate\" : \"DESC\"\n" +
+            "    }\n" +
+            "}\n";
+
+        MediaForm form = Jackson2Mapper.getLenientInstance().readValue(example, MediaForm.class);
+        assertThat(form.getSortFields()).hasSize(2);
+        assertThat(form.getSortFields().get(0).getSortField()).isEqualTo(MediaSortField.title);
+    }
+
+
+    @Test
+    public void testArray() throws IOException {
+        String example = "{\n" +
+            "    \"searches\" : {\n" +
+            "        \"text\" : \"Twee vandaag\"\n" +
+            "    },\n" +
+            "    /* json doesn't actually have a order, so we should support arrays too */\n" +
+            "    \"sort\" : [\n" +
+            "        {\n" +
+            "            \"field\": \"title\",\n" +
+            "            \"order\" : \"ASC\",\n" +
+            "            \"type\" : \"LEXICO\",\n" +
+            "            \"owner\" : \"BROADCASTER\"\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"field\": \"sortDate\",\n" +
+            "            \"order\" : \"DESC\"\n" +
+            "        },\n" +
+            "        \"lastModified\"\n" +
+            "    ]\n" +
+            "}\n";
+
+        MediaForm form = Jackson2Mapper.getLenientInstance().readValue(example, MediaForm.class);
+        assertThat(form.getSortFields()).hasSize(3);
+        assertThat(form.getSortFields().get(0).getSortField()).isEqualTo(MediaSortField.title);
+    }
+
+}
