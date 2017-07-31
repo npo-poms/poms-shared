05511b9037f55a594c7fded8208deecb7a010e98
Machiel Groeneveld <m.groeneveld@vpro.nl>
2017-07-31 09:37:32 +0000

    removed tabs
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=105793

--- api-domain/src/test/java/nl/vpro/domain/api/TextMatcherListTest.java
+++ api-domain/src/test/java/nl/vpro/domain/api/TextMatcherListTest.java
@@ -15,17 +15,17 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class TextMatcherListTest {
 
-	@Test
-	public void marshal() throws IOException, SAXException {
-		final TextMatcherList textMatcherList = new TextMatcherList(Arrays.asList(new TextMatcher("a", Match.SHOULD), new TextMatcher("b", Match.SHOULD)), Match.MUST);
-		TextMatcherList result = JAXBTestUtil.roundTripAndSimilar(textMatcherList,
-			"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
-				"<local:textMatcherList match=\"MUST\" xmlns:api=\"urn:vpro:api:2013\" xmlns:media=\"urn:vpro:media:2009\" xmlns:local=\"uri:local\">\n" +
-				"    <api:matcher match=\"SHOULD\">a</api:matcher>\n" +
-				"    <api:matcher match=\"SHOULD\">b</api:matcher>\n" +
-				"</local:textMatcherList>");
+    @Test
+    public void marshal() throws IOException, SAXException {
+        final TextMatcherList textMatcherList = new TextMatcherList(
+                Arrays.asList(new TextMatcher("a", Match.SHOULD), new TextMatcher("b", Match.SHOULD)), Match.MUST);
+        TextMatcherList result = JAXBTestUtil.roundTripAndSimilar(textMatcherList,
+                "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                        + "<local:textMatcherList match=\"MUST\" xmlns:api=\"urn:vpro:api:2013\" xmlns:media=\"urn:vpro:media:2009\" xmlns:local=\"uri:local\">\n"
+                        + "    <api:matcher match=\"SHOULD\">a</api:matcher>\n"
+                        + "    <api:matcher match=\"SHOULD\">b</api:matcher>\n" + "</local:textMatcherList>");
 
-		assertThat(result.asList().get(0).getMatch()).isEqualTo(Match.SHOULD);
-	}
+        assertThat(result.asList().get(0).getMatch()).isEqualTo(Match.SHOULD);
+    }
 
 }
