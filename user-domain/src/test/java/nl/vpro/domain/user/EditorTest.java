e81202d4e20e8b9bebbe54a8f77003b9e05eb729
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-08-18 08:18:15 +0000

    MSE-3765
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=106181

---
+++ user-domain/src/test/java/nl/vpro/domain/user/EditorTest.java
@@ -0,0 +1,43 @@
+package nl.vpro.domain.user;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Set;
+
+import javax.validation.*;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.5
+ */
+@Slf4j
+public class EditorTest {
+
+    private Validator validator;
+    {
+        try {
+            ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+            validator = factory.getValidator();
+        } catch (ValidationException ve) {
+            log.info(ve.getClass().getName() + " " + ve.getMessage());
+            validator = null;
+
+        }
+    }
+
+
+    @Test
+    public void testValidation() {
+        User user = Editor.builder()
+            .email("Vera.van.Slooten@ntr.nl")
+            .build();
+
+        Set<ConstraintViolation<User>> validate = validator.validate(user);
+        assertThat(validate).isEmpty();
+    }
+
+}
