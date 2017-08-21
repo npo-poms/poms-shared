ac2607976b04d23fa06a496b4c81c06366826e84
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-08-21 07:14:49 +0000

    Added TODO.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=106244

--- poms-shared-domain/src/main/java/nl/vpro/domain/support/License.java
+++ poms-shared-domain/src/main/java/nl/vpro/domain/support/License.java
@@ -14,6 +14,9 @@ import nl.vpro.domain.Xmlns;
 /**
  * @author Michiel Meeuwissen
  * @since 5.0
+ * @TODO Licenses have no support for versions. I now suggested 4.0 for the the CC-licenses, but other version may occur too!
+ *       Perhaps License should not be enum, or have many more values.
+ *       The trouble with enum is any way that java clients are hard to keep in sync.
  */
 @XmlEnum
 @XmlType(name = "licenseEnum", namespace = Xmlns.SHARED_NAMESPACE)
@@ -23,12 +26,12 @@ public enum License implements nl.vpro.domain.Displayable {
     COPYRIGHTED("Copyrighted", null, true),
     PUBLIC_DOMAIN("Publiek domein", null, true),
 
-    CC_BY("Naamsvermelding", "https://creativecommons.org/licenses/by/3.0/", true),
-    CC_BY_SA("Naamsvermelding-GelijkDelen", "https://creativecommons.org/licenses/by-sa/3.0/", true),
-    CC_BY_ND("Naamsvermelding-GeenAfgeleideWerken", "https://creativecommons.org/licenses/by-nd/3.0/", true),
-    CC_BY_NC("Naamsvermelding-NietCommercieel", "https://creativecommons.org/licenses/by-nc/3.0/", true),
-    CC_BY_NC_SA("Naamsvermelding-NietCommercieel-GelijkDelen", "https://creativecommons.org/licenses/by-nc-sa/3.0/", true),
-    CC_BY_NC_ND("Naamsvermelding-NietCommercieel-GeenAfgeleideWerken", "https://creativecommons.org/licenses/by-nc-nd/3.0/", true),
+    CC_BY("Naamsvermelding", "https://creativecommons.org/licenses/by/4.0/", true),
+    CC_BY_SA("Naamsvermelding-GelijkDelen", "https://creativecommons.org/licenses/by-sa/4.0/", true),
+    CC_BY_ND("Naamsvermelding-GeenAfgeleideWerken", "https://creativecommons.org/licenses/by-nd/4.0/", true),
+    CC_BY_NC("Naamsvermelding-NietCommercieel", "https://creativecommons.org/licenses/by-nc/4.0/", true),
+    CC_BY_NC_SA("Naamsvermelding-NietCommercieel-GelijkDelen", "https://creativecommons.org/licenses/by-nc-sa/4.0/", true),
+    CC_BY_NC_ND("Naamsvermelding-NietCommercieel-GeenAfgeleideWerken", "https://creativecommons.org/licenses/by-nc-nd/4.0/", true),
     USA_GOV("United States Government Work", "http://www.usa.gov/copyright.shtml", false)
 
     ;
