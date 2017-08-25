fbcf016b12d1cf8481ef0b8b5cd642224184d418
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-08-25 14:07:10 +0000

    license validation.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=106439

--- poms-shared-domain/src/test/java/nl/vpro/domain/support/LicenseTest.java
+++ poms-shared-domain/src/test/java/nl/vpro/domain/support/LicenseTest.java
@@ -1,5 +1,12 @@
 package nl.vpro.domain.support;
 
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -36,5 +43,16 @@ public class LicenseTest {
     }
 
 
+    @Test
+    public void getLicenseInvalidId() throws Exception {
+        ValidatorFactory config = Validation.buildDefaultValidatorFactory();
+        Validator validator = config.getValidator();
+        License testLicense = new License("bla");
+        Set<ConstraintViolation<License>> validate = validator.validate(testLicense);
+        assertThat(validate).isNotEmpty();
+
+        assertThat(validator.validate(License.PUBLIC_DOMAIN)).isEmpty();
+
+    }
 
 }
