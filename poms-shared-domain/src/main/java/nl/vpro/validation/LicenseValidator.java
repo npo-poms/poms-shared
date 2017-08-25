2f6e0719173040ea699feacfee1a1f20a33bef12
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-08-25 14:07:10 +0000

    license validation.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=106439

---
+++ poms-shared-domain/src/main/java/nl/vpro/validation/LicenseValidator.java
@@ -0,0 +1,25 @@
+package nl.vpro.validation;
+
+import java.util.Arrays;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import nl.vpro.domain.support.License;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.5
+ */
+public class LicenseValidator implements ConstraintValidator<LicenseId, String> {
+
+    @Override
+    public void initialize(LicenseId license) {
+    }
+
+    @Override
+    public boolean isValid(String id, ConstraintValidatorContext constraintValidatorContext) {
+        return Arrays.stream(License.values()).anyMatch(l -> l.getId().equals(id));
+    }
+}
+
