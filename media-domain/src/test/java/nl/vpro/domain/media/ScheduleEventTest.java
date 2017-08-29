272cdc5dc9f87830579e7a2e11ea611acff63ce4
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-08-29 07:06:46 +0000

    NPA-334
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=106509

--- media-domain/src/test/java/nl/vpro/domain/media/ScheduleEventTest.java
+++ media-domain/src/test/java/nl/vpro/domain/media/ScheduleEventTest.java
@@ -6,8 +6,11 @@ import org.junit.Test;
 import org.xml.sax.SAXException;
 
 import nl.vpro.domain.media.support.TextualType;
+import nl.vpro.jackson2.Jackson2Mapper;
+import nl.vpro.test.util.jackson2.Jackson2TestUtil;
 import nl.vpro.test.util.jaxb.JAXBTestUtil;
 
+import static java.time.LocalDateTime.of;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 /**
@@ -29,7 +32,35 @@ public class ScheduleEventTest {
             "    <title owner=\"BROADCASTER\" type=\"MAIN\">aa</title>\n" +
             "    <title owner=\"BROADCASTER\" type=\"ABBREVIATION\">bbb</title>\n" +
             "</local:scheduleEvent>");
-
-
     }
+
+    @Test
+    public void testJson() throws Exception {
+        ScheduleEvent e = ScheduleEvent.builder()
+            .localStart(of(2017, 8, 28, 15, 51))
+            .channel(Channel.NED1)
+            .build();
+
+        Jackson2TestUtil.roundTripAndSimilar(e, "{\n" +
+            "  \"channel\" : \"NED1\",\n" +
+            "  \"start\" : 1503928260000,\n" +
+            "  \"guideDay\" : 1503871200000\n" +
+            "}");
+    }
+
+    @Test
+    public void testJsonPublisher() throws Exception {
+        ScheduleEvent e = ScheduleEvent.builder()
+            .localStart(of(2017, 8, 28, 15, 51))
+            .channel(Channel.NED1)
+            .build();
+
+        Jackson2TestUtil.roundTripAndSimilar(Jackson2Mapper.PRETTY_PUBLISHER, e, "{\n" +
+            "  \"channel\" : \"NED1\",\n" +
+            "  \"start\" : 1503928260000,\n" +
+            "  \"guideDay\" : 1503871200000,\n" +
+            "  \"eventStart\" : 1503928260000\n" +
+            "}");
+    }
+
 }
