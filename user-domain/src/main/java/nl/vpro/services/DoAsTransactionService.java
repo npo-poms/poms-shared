744c814e71a7959044df1e7abf6167333ed4df1b
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-09-11 07:08:13 +0000

    Added some missing methods to DoAsTransactionService.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=106816

--- user-domain/src/main/java/nl/vpro/services/DoAsTransactionService.java
+++ user-domain/src/main/java/nl/vpro/services/DoAsTransactionService.java
@@ -1,6 +1,7 @@
 package nl.vpro.services;
 
 import java.util.concurrent.Callable;
+import java.util.function.Function;
 
 /**
  * @author Michiel Meeuwissen
@@ -11,9 +12,14 @@ public interface DoAsTransactionService extends TransactionService {
 
     <T> T executeInNewTransaction(String user, Callable<T> callable) throws Exception;
 
+    <T, S> T executeInNewTransaction(String user, S argument, Function<S, T> function);
+
     void executeInNewTransaction(String user,  Runnable runnable);
 
     <T> T executeInReadonlyTransaction(String user, Callable<T> callable) throws Exception;
+
+    <T, S> T executeInReadonlyTransaction(String user, S argument, Function<S, T> function);
+
 
     void executeInReadonlyTransaction(String user, Runnable runnable);
 }
