deba1b947787411a9ee56737248f5b100207470d
Lies Kombrink <lieskombrink@gmail.com>
2017-09-19 12:30:20 +0000

    NPA-412
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=107053

---
+++ api-domain/src/main/java/nl/vpro/domain/api/thesaurus/ThesaurusChange.java
@@ -0,0 +1,69 @@
+package nl.vpro.domain.api.thesaurus;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import nl.vpro.domain.AbstractChange;
+import nl.vpro.domain.Xmlns;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElements;
+
+import java.time.Instant;
+
+@ToString
+@EqualsAndHashCode
+public class ThesaurusChange<T extends ThesaurusObject> extends AbstractChange<T> {
+
+    @XmlAttribute
+    private Long sequence;
+
+    @XmlAttribute
+    private Long revision;
+
+    @XmlAttribute
+    private String mergedTo;
+
+    public ThesaurusChange() {
+    }
+
+    @Builder
+    public ThesaurusChange(Instant publishDate, String id, Boolean deleted, Boolean tail, T object) {
+        super(publishDate, id, deleted, tail, object);
+    }
+
+
+    @XmlElements({
+            @XmlElement(name = "person", namespace = Xmlns.MEDIA_NAMESPACE, type = GTAAPerson.class),
+            @XmlElement(name = "item", type = ThesaurusItem.class)
+    })
+
+    public T getObject() {
+        return super.getObject();
+    }
+
+    public void setObject(T p) {
+        super.setObject(p);
+    }
+
+
+
+    @JsonProperty("object")
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "objectType")
+    @JsonSubTypes({
+            @JsonSubTypes.Type(value = GTAAPerson.class, name = "person"),
+            @JsonSubTypes.Type(value = ThesaurusItem.class, name = "item")}
+    )
+    protected T getJsonObject() {
+        return super.getObject();
+    }
+
+
+}
+
+
+
