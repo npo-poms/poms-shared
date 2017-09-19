9c5299da2acd0caaf3512e801e6460fbf66fad26
Lies Kombrink <lieskombrink@gmail.com>
2017-09-19 13:01:21 +0000

    NPA-412
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=107056

---
+++ api-domain/src/test/java/nl/vpro/domain/api/thesaurus/ThesaurusResultTest.java
@@ -0,0 +1,46 @@
+package nl.vpro.domain.api.thesaurus;
+
+import nl.vpro.test.util.jackson2.Jackson2TestUtil;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThesaurusResultTest {
+
+    @Test
+    public void toJson() throws Exception {
+        GTAAPerson person = new GTAAPerson();
+        person.setGivenName("Pietje");
+        person.setFamilyName("Puk");
+        List<GTAAPerson> list = new ArrayList<>();
+        list.add(person);
+
+        ThesaurusResult.PersonList result =
+                ThesaurusResult.PersonList.builder()
+                        .list(list)
+                        .max(10)
+                        .build();
+
+        Jackson2TestUtil.roundTripAndSimilarAndEquals(result, "{\n" +
+                "  \"offset\" : 0,\n" +
+                "  \"max\" : 10,\n" +
+                "  \"items\" : [ {\n" +
+                "    \"objectType\" : \"person\",\n" +
+                "    \"givenName\" : \"Pietje\",\n" +
+                "    \"familyName\" : \"Puk\",\n" +
+                "    \"value\" : \"Pietje Puk\",\n" +
+                "    \"prefLabel\" : \"Pietje Puk\"\n" +
+                "  } ]\n" +
+                "}");
+
+    }
+
+
+
+
+
+}
+
+
+
