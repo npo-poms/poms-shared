4c7b1c4fa88feca1c842484820f3a3f838ca7624
Lies Kombrink <lieskombrink@gmail.com>
2017-09-19 13:01:21 +0000

    NPA-412
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=107056

---
+++ api-domain/src/test/java/nl/vpro/domain/api/thesaurus/GTAAPersonTest.java
@@ -0,0 +1,61 @@
+package nl.vpro.domain.api.thesaurus;
+
+import java.io.StringReader;
+
+import org.junit.Test;
+
+import nl.vpro.jackson2.Jackson2Mapper;
+import nl.vpro.test.util.jackson2.Jackson2TestUtil;
+import nl.vpro.test.util.jaxb.JAXBTestUtil;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class GTAAPersonTest {
+    @Test
+    public void json() throws Exception {
+        GTAAPerson person = GTAAPerson.builder().givenName("Pietje").familyName("puk").build();
+
+        Jackson2TestUtil.roundTripAndSimilarAndEquals(person, "{\n" +
+            "  \"objectType\" : \"person\",\n" +
+            "  \"givenName\" : \"Pietje\",\n" +
+            "  \"familyName\" : \"puk\",\n" +
+            "}");
+
+    }
+
+    @Test
+    public void jsonAsThesaurusObjectReturnsPerson() throws Exception {
+
+
+        ThesaurusObject object = Jackson2Mapper.getInstance().readValue(new StringReader("{\n" +
+            "  \"objectType\" : \"person\",\n" +
+            "  \"familyName\" : \"puk\",\n" +
+            "  \"value\" : \"null puk\",\n" +
+            "  \"prefLabel\" : \"null puk\"\n" +
+            "}"), ThesaurusObject.class);
+
+        assertThat(object).isInstanceOf(GTAAPerson.class);
+
+    }
+
+    @Test
+    public void jsonAsThesaurusObjectReturnsItem() throws Exception {
+
+
+        ThesaurusObject object = Jackson2Mapper.getInstance().readValue(new StringReader("{\n" +
+            "  \"objectType\" : \"item\",\n" +
+            "  \"prefLabel\" : \"augustijnen\"\n" +
+            "}"), ThesaurusObject.class);
+
+        assertThat(object).isInstanceOf(ThesaurusItem.class);
+
+    }
+
+    @Test
+    public void xml() throws Exception {
+        GTAAPerson person = GTAAPerson.builder().familyName("puk").build();
+
+        JAXBTestUtil.roundTripAndSimilarAndEquals(person, "<a />");
+
+    }
+}
