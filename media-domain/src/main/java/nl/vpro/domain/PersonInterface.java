08d42623059be418ce6af8388a3519355826111e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-09-20 07:22:29 +0000

    Introdcues ThesaurusObjects utility. Removed status from Person.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=107087

--- media-domain/src/main/java/nl/vpro/domain/PersonInterface.java
+++ media-domain/src/main/java/nl/vpro/domain/PersonInterface.java
@@ -1,16 +1,11 @@
 package nl.vpro.domain;
 
-import nl.vpro.domain.media.gtaa.GTAARecord;
-
 public interface PersonInterface {
-
 
     String getGivenName();
 
     String getFamilyName();
 
     String getGtaaUri();
-
-    String getPrefLabel();
 
 }
