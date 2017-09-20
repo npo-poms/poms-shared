cba8e7c294bc99aaac39613680b06f6bc32a8543
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-09-20 07:22:29 +0000

    Introdcues ThesaurusObjects utility. Removed status from Person.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=107087

--- media-domain/src/main/java/nl/vpro/domain/media/gtaa/ThesaurusObject.java
+++ media-domain/src/main/java/nl/vpro/domain/media/gtaa/ThesaurusObject.java
@@ -1,16 +1,19 @@
 package nl.vpro.domain.media.gtaa;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
 import java.time.Instant;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "objectType")
-@JsonSubTypes({ @JsonSubTypes.Type(value = GTAAPerson.class, name = "person"),
+@JsonSubTypes(
+    {
+        @JsonSubTypes.Type(value = GTAAPerson.class, name = "person"),
+        @JsonSubTypes.Type(value = GTAATopic.class, name = "topic"),
         @JsonSubTypes.Type(value = ThesaurusItem.class, name = "item"),
-          })
+    })
 public interface ThesaurusObject {
 
     List<Label> getNotes();
@@ -23,9 +26,6 @@ public interface ThesaurusObject {
 
     Status getStatus();
 
-    String getType();
-
     String getRedirectedFrom();
-
 
 }
