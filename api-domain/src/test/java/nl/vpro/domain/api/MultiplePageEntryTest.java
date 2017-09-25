a518ec773d38576448d5af3863b4c7c47024e72c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-09-25 10:52:41 +0000

    Test case failures.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=107262

---
+++ api-domain/src/test/java/nl/vpro/domain/api/MultiplePageEntryTest.java
@@ -0,0 +1,30 @@
+package nl.vpro.domain.api;
+
+import org.junit.Test;
+
+import nl.vpro.domain.page.Page;
+import nl.vpro.domain.page.PageType;
+import nl.vpro.test.util.jackson2.Jackson2TestUtil;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.5
+ */
+public class MultiplePageEntryTest {
+
+
+    @Test
+    public void json() throws Exception {
+        MultiplePageEntry entry = new MultiplePageEntry("bla", Page.builder().type(PageType.HOME).url("http://www.vpro.nl/bla").build());
+
+        Jackson2TestUtil.roundTripAndSimilar(entry, "{\n" +
+            "  \"id\" : \"bla\",\n" +
+            "  \"result\" : {\n" +
+            "    \"objectType\" : \"page\",\n" +
+            "    \"type\" : \"HOME\",\n" +
+            "    \"url\" : \"http://www.vpro.nl/bla\"\n" +
+            "  }\n" +
+            "}");
+    }
+
+}
