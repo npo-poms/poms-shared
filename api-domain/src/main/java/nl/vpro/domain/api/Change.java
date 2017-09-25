76a784278593223aeda8db19d5581920635d4117
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-09-25 10:58:27 +0000

    Temporary restored to communicate to roekoe (?) what has to be changed.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=107263

---
+++ api-domain/src/main/java/nl/vpro/domain/api/Change.java
@@ -0,0 +1,8 @@
+package nl.vpro.domain.api;
+
+/**
+ * @deprecated Use {@link MediaChange}. This class wil be removed in future version.
+ */
+@Deprecated
+public class Change extends MediaChange {
+}
