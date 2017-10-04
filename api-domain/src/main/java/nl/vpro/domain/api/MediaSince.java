3de2c37373141db071ecd1199100bb991799ab0b
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-10-04 15:27:28 +0000

    Details.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=107576

---
+++ api-domain/src/main/java/nl/vpro/domain/api/MediaSince.java
@@ -0,0 +1,36 @@
+package nl.vpro.domain.api;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.Instant;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.5
+ */
+@Data
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@lombok.Builder
+public class MediaSince {
+
+    private final Instant instant;
+
+    private final String mid;
+
+
+    static Instant instant(Instant explicit, MediaSince mediaSince) {
+        if (explicit != null) {
+            return explicit;
+        }
+        return mediaSince == null ? null : mediaSince.instant;
+    }
+
+    static String mid(String explicit, MediaSince mediaSince) {
+        if (explicit != null) {
+            return explicit;
+        }
+        return mediaSince == null ? null : mediaSince.mid;
+    }
+}
