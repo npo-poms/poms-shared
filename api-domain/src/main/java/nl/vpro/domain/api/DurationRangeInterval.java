75360d6178264d016aa155a4a498547e8819cd3f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-10-07 08:35:44 +0000

    NPA-334
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=107685

--- api-domain/src/main/java/nl/vpro/domain/api/DurationRangeInterval.java
+++ api-domain/src/main/java/nl/vpro/domain/api/DurationRangeInterval.java
@@ -4,11 +4,15 @@
  */
 package nl.vpro.domain.api;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.Duration;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
 
 /**
  * @author Michiel Meeuwissen
@@ -17,14 +21,48 @@ import javax.xml.bind.annotation.XmlType;
 @XmlType(name = "durationRangeIntervalType", propOrder = {
 })
 @XmlAccessorType(XmlAccessType.FIELD)
-public class DurationRangeInterval extends AbstractTemporalAmountRangeInterval<Duration> {
+public class DurationRangeInterval implements RangeFacet<Duration> {
+
+
+    @XmlValue
+    @Getter
+    @Setter
+    private Interval interval;
 
 
     public DurationRangeInterval() {
     }
 
     public DurationRangeInterval(String interval) {
-        super(interval);
+        this.interval = new Interval(ParsedInterval.parse(interval));
+    }
+
+
+    @Override
+    public boolean matches(Duration begin, Duration end) {
+        Interval parsed = getInterval();
+        return parsed.isBucketBegin(begin)
+            && parsed.isBucketEnd(end);
+    }
+
+    public static class Interval extends ParsedInterval<Duration> {
+
+        public Interval(ParseResult pair) {
+            super(pair);
+        }
+
+        @Override
+        public boolean isBucketBegin(Duration begin) {
+            //return begin != null && begin.get(getUnit().getChronoField().getRangeUnit());
+            return false;
+
+        }
+
+        @Override
+        public boolean isBucketEnd(Duration end) {
+            return false;
+
+        }
     }
 
 }
