52aee67855d87bb52abad728efc936f8ada62d1c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-10-07 08:25:50 +0000

    NPA-334
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=107684

--- api-domain/src/main/java/nl/vpro/domain/api/ParsedInterval.java
+++ api-domain/src/main/java/nl/vpro/domain/api/ParsedInterval.java
@@ -1,8 +1,13 @@
 package nl.vpro.domain.api;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 
 import java.time.Duration;
+import java.util.regex.Pattern;
+
+import javax.xml.bind.annotation.XmlValue;
 
 /**
  * @author Michiel Meeuwissen
@@ -11,12 +16,34 @@ import java.time.Duration;
 @Getter
 abstract class ParsedInterval<T extends Comparable<T>> {
 
+    static final String TEMPORAL_AMOUNT_INTERVAL = "(\\d+)?\\s*(YEAR|MONTH|WEEK|DAY|HOUR|MINUTE)S?";
 
-    private final Unit unit;
+    static final Pattern PATTERN = Pattern.compile(TEMPORAL_AMOUNT_INTERVAL);
+
+
+    public static ParseResult parse(String toParse) {
+        java.util.regex.Matcher matcher = Pattern.compile(TEMPORAL_AMOUNT_INTERVAL).matcher(toParse.toUpperCase());
+
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(toParse);
+        }
+        final int number = matcher.group(1) == null ? 1 : Integer.valueOf(matcher.group(1));
+        final IntervalUnit unit = IntervalUnit.valueOf(matcher.group(2));
+        return ParseResult.builder().amount(number).unit(unit).build();
+
+    }
+
+
+    private final IntervalUnit unit;
 
     private final int amount;
 
-    protected ParsedInterval(int amount, Unit unit) {
+    protected ParsedInterval(ParseResult pair) {
+        this(pair.getAmount(), pair.getUnit());
+    }
+
+    protected ParsedInterval(int amount, IntervalUnit unit) {
         this.amount = amount;
         this.unit = unit;
     }
@@ -25,6 +52,12 @@ abstract class ParsedInterval<T extends Comparable<T>> {
     public abstract boolean isBucketBegin(T begin);
 
     public abstract boolean isBucketEnd(T end);
+
+    @XmlValue
+    @javax.validation.constraints.Pattern(regexp = TEMPORAL_AMOUNT_INTERVAL)
+    public String getValue() {
+        return (amount != 1 ? amount + " " : "") + unit;
+    }
 
     public String print(T value) {
         return "";
@@ -35,6 +68,12 @@ abstract class ParsedInterval<T extends Comparable<T>> {
         return unit.getChronoField().getBaseUnit().getDuration();
     }
 
-
+    @AllArgsConstructor
+    @Data
+    @lombok.Builder
+    public static class ParseResult {
+        final int amount;
+        final IntervalUnit unit;
+    }
 
 }
