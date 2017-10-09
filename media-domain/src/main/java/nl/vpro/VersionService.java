f80ecbb05273e427b692617b96abed5b6362ec90
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-10-09 13:07:11 +0000

    MSE-3770
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=107714

---
+++ media-domain/src/main/java/nl/vpro/VersionService.java
@@ -0,0 +1,54 @@
+package nl.vpro;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.5
+ */
+@Slf4j
+public class VersionService {
+
+    private static final String FALLBACK = "5.5";
+
+    private static String version;
+
+    private static Float floatVersion;
+
+    public static String version() {
+        if (version == null) {
+            try {
+                InputStream inputStream = VersionService.class.getClassLoader().getResourceAsStream("/maven.properties");
+                if (inputStream != null) {
+                    Properties properties = new Properties();
+                    properties.load(inputStream);
+                    version = properties.getProperty("media.version");
+                } else {
+                    log.warn("Could not find /maven.properties");
+                    return FALLBACK;
+                }
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
+                return FALLBACK;
+            }
+        }
+        return version;
+    }
+
+    public static Float floatVersion() {
+        if (floatVersion == null) {
+            String version = version();
+            Matcher matcher = Pattern.compile("(\\d+\\.?\\d*).*").matcher(version);
+            if (matcher.matches()) {
+                floatVersion = Float.parseFloat(matcher.group(1));
+            }
+        }
+        return floatVersion;
+    }
+}
