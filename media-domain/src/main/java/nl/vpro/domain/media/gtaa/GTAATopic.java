060d2f289ad7abde2cc25432e7370eac171cbd44
Lies Kombrink <lieskombrink@gmail.com>
2017-10-24 07:47:14 +0000

    rename
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=108110

--- media-domain/src/main/java/nl/vpro/domain/media/gtaa/GTAATopic.java
+++ media-domain/src/main/java/nl/vpro/domain/media/gtaa/GTAATopic.java
@@ -6,11 +6,11 @@ import nl.vpro.w3.rdf.Description;
  * @author Michiel Meeuwissen
  * @since 5.5
  */
-public class GTAASubject extends AbstractThesaurusItem {
+public class GTAATopic extends AbstractThesaurusItem {
 
 
-    public static GTAASubject create(Description description) {
-        final GTAASubject answer = new GTAASubject();
+    public static GTAATopic create(Description description) {
+        final GTAATopic answer = new GTAATopic();
         fill(description, answer);
         return answer;
     }
