4cf73be406835ff83d84406548e66bfe8598e920
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-10-25 15:46:29 +0000

    NPA-424
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=108157

--- api-domain/src/main/java/nl/vpro/domain/api/media/TitleFacetList.java
+++ api-domain/src/main/java/nl/vpro/domain/api/media/TitleFacetList.java
@@ -25,10 +25,10 @@ import nl.vpro.domain.api.jackson.media.TitleFacetListJson;
 @JsonDeserialize(using = TitleFacetListJson.Deserializer.class)
 public class TitleFacetList
     extends MediaFacet /* extending MediaFacet is mainly done for backwards compatibility */
-    implements SearchableFacet<TitleSearch>, Iterable<TitleFacet> {
+    implements SearchableFacet<TitleMatcher>, Iterable<TitleFacet> {
 
     @Valid
-    private TitleSearch subSearch;
+    private TitleMatcher subSearch;
 
     @Valid
     @XmlElement(name = "title")
@@ -47,12 +47,12 @@ public class TitleFacetList
     }
 
     @Override
-    public TitleSearch getSubSearch() {
+    public TitleMatcher getSubSearch() {
         return subSearch;
     }
 
     @Override
-    public void setSubSearch(TitleSearch subSearch) {
+    public void setSubSearch(TitleMatcher subSearch) {
         this.subSearch = subSearch;
     }
 
