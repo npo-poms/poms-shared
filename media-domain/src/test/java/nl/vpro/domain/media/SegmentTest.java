b2458c48881d4488be4fb15c7496674fdb577fdc
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-10-26 12:09:33 +0000

    Trying to reproduce MSE-3803
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=108168

--- media-domain/src/test/java/nl/vpro/domain/media/SegmentTest.java
+++ media-domain/src/test/java/nl/vpro/domain/media/SegmentTest.java
@@ -76,4 +76,53 @@ public class SegmentTest {
 
     }
 
+    @Test
+    public void sortDate() throws Exception {
+        Program program = MediaBuilder.program()
+            .creationDate(LocalDateTime.of(2017, 10, 24, 0, 0))
+            .build();
+        Segment segment = MediaBuilder.segment()
+            .creationDate(LocalDateTime.of(2017, 10, 25, 0, 0))
+            .mainTitle("bla")
+            .duration(Duration.ofSeconds(123))
+            .parent(program)
+            .build();
+
+        assertThat(segment.getSortInstant()).isEqualTo(program.getCreationInstant());
+
+        assertThat(
+            JAXBTestUtil.roundTripAndSimilar(segment, "<segment type=\"SEGMENT\" embeddable=\"true\" sortDate=\"2017-10-24T00:00:00+02:00\" workflow=\"FOR PUBLICATION\" creationDate=\"2017-10-25T00:00:00+02:00\" xmlns=\"urn:vpro:media:2009\" xmlns:shared=\"urn:vpro:shared:2009\">\n" +
+                "    <title owner=\"BROADCASTER\" type=\"MAIN\">bla</title>\n" +
+                "    <duration>P0DT0H2M3.000S</duration>\n" +
+                "    <credits/>\n" +
+                "    <descendantOf/>\n" +
+                "    <locations/>\n" +
+                "    <scheduleEvents/>\n" +
+                "    <images/>\n" +
+                "</segment>").getSortInstant()
+        ).isEqualTo(program.getCreationInstant());
+
+        assertThat(
+            Jackson2TestUtil.roundTripAndSimilar(segment, "{\n" +
+                "  \"objectType\" : \"segment\",\n" +
+                "  \"type\" : \"SEGMENT\",\n" +
+                "  \"workflow\" : \"FOR_PUBLICATION\",\n" +
+                "  \"sortDate\" : 1508796000000,\n" +
+                "  \"creationDate\" : 1508882400000,\n" +
+                "  \"embeddable\" : true,\n" +
+                "  \"broadcasters\" : [ ],\n" +
+                "  \"titles\" : [ {\n" +
+                "    \"value\" : \"bla\",\n" +
+                "    \"owner\" : \"BROADCASTER\",\n" +
+                "    \"type\" : \"MAIN\"\n" +
+                "  } ],\n" +
+                "  \"genres\" : [ ],\n" +
+                "  \"countries\" : [ ],\n" +
+                "  \"languages\" : [ ],\n" +
+                "  \"duration\" : 123000,\n" +
+                "  \"descendantOf\" : [ { } ]\n" +
+                "}").getSortInstant()
+        ).isEqualTo(program.getCreationInstant());
+    }
+
 }
