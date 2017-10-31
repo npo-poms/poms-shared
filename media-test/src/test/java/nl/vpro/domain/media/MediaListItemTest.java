875ccc8ae2e5ab32aee1add09672f82f2d08e767
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-10-31 10:43:18 +0000

    Test case failures.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=108311

--- media-test/src/test/java/nl/vpro/domain/media/MediaListItemTest.java
+++ media-test/src/test/java/nl/vpro/domain/media/MediaListItemTest.java
@@ -19,8 +19,7 @@ public class MediaListItemTest {
         Program program = MediaTestDataBuilder.program().withEverything().build();
         MediaListItem item = new MediaListItem(program);
 
-        JAXBTestUtil.roundTripAndSimilar(item, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
-            "<s:item mid=\"VPROWON_20001\" avType=\"VIDEO\" mediaType=\"nl.vpro.domain.media.Program\" id=\"12\" urn=\"urn:vpro:media:program:12\" workflow=\"PUBLISHED\" xmlns=\"urn:vpro:media:2009\" xmlns:shared=\"urn:vpro:shared:2009\" xmlns:s=\"urn:vpro:media:search:2012\" xmlns:update=\"urn:vpro:media:update:2009\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\">\n" +
+        JAXBTestUtil.roundTripAndSimilar(item, "<s:item mid=\"VPROWON_20001\" avType=\"VIDEO\" mediaType=\"nl.vpro.domain.media.Program\" id=\"12\" urn=\"urn:vpro:media:program:12\" workflow=\"PUBLISHED\" xmlns=\"urn:vpro:media:2009\" xmlns:shared=\"urn:vpro:shared:2009\" xmlns:s=\"urn:vpro:media:search:2012\" xmlns:update=\"urn:vpro:media:update:2009\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\">\n" +
             "    <s:broadcaster id=\"BNN\">BNN</s:broadcaster>\n" +
             "    <s:broadcaster id=\"AVRO\">AVRO</s:broadcaster>\n" +
             "    <s:title>Main title</s:title>\n" +
@@ -35,19 +34,38 @@ public class MediaListItemTest {
             "    <s:publishStart>1970-01-01T01:00:00+01:00</s:publishStart>\n" +
             "    <s:publishStop>2500-01-01T00:00:00+01:00</s:publishStop>\n" +
             "    <s:lastPublished>2015-03-06T02:00:00+01:00</s:lastPublished>\n" +
-            "    <s:firstScheduleEvent channel=\"NED3\" midRef=\"VPROWON_20001\" urnRef=\"urn:vpro:media:program:12\">\n" +
+            "    <s:firstShowing channel=\"NED3\" midRef=\"VPROWON_20001\" urnRef=\"urn:vpro:media:program:12\">\n" +
             "        <guideDay>1969-12-31+01:00</guideDay>\n" +
             "        <start>1970-01-01T01:00:00.100+01:00</start>\n" +
             "        <duration>P0DT0H0M0.200S</duration>\n" +
             "        <poProgID>VPROWON_20001</poProgID>\n" +
-            "    </s:firstScheduleEvent>\n" +
+            "    </s:firstShowing>\n" +
+            "    <s:firstShowingNoRerun channel=\"NED3\" midRef=\"VPROWON_20001\" urnRef=\"urn:vpro:media:program:12\">\n" +
+            "        <guideDay>1969-12-31+01:00</guideDay>\n" +
+            "        <start>1970-01-01T01:00:00.100+01:00</start>\n" +
+            "        <duration>P0DT0H0M0.200S</duration>\n" +
+            "        <poProgID>VPROWON_20001</poProgID>\n" +
+            "    </s:firstShowingNoRerun>\n" +
+            "    <s:lastShowing channel=\"CONS\" midRef=\"VPROWON_20001\" urnRef=\"urn:vpro:media:program:12\">\n" +
+            "        <repeat isRerun=\"true\"></repeat>\n" +
+            "        <guideDay>1970-01-10+01:00</guideDay>\n" +
+            "        <start>1970-01-11T01:00:00.600+01:00</start>\n" +
+            "        <duration>P0DT0H0M0.200S</duration>\n" +
+            "        <poProgID>VPROWON_20001</poProgID>\n" +
+            "    </s:lastShowing>\n" +
+            "    <s:lastShowingNoRerun channel=\"NED3\" midRef=\"VPROWON_20001\" urnRef=\"urn:vpro:media:program:12\">\n" +
+            "        <guideDay>1969-12-31+01:00</guideDay>\n" +
+            "        <start>1970-01-01T01:00:00.100+01:00</start>\n" +
+            "        <duration>P0DT0H0M0.200S</duration>\n" +
+            "        <poProgID>VPROWON_20001</poProgID>\n" +
+            "    </s:lastShowingNoRerun>\n" +
             "    <s:sortDateScheduleEvent channel=\"NED3\" midRef=\"VPROWON_20001\" urnRef=\"urn:vpro:media:program:12\">\n" +
             "        <guideDay>1969-12-31+01:00</guideDay>\n" +
             "        <start>1970-01-01T01:00:00.100+01:00</start>\n" +
             "        <duration>P0DT0H0M0.200S</duration>\n" +
             "        <poProgID>VPROWON_20001</poProgID>\n" +
             "    </s:sortDateScheduleEvent>\n" +
-            "    <s:locations owner=\"BROADCASTER\" creationDate=\"2016-03-04T15:45:00+01:00\" urn=\"urn:vpro:media:location:6\" workflow=\"FOR PUBLICATION\">\n" +
+            "    <s:locations owner=\"BROADCASTER\" workflow=\"FOR PUBLICATION\" creationDate=\"2016-03-04T15:45:00+01:00\" urn=\"urn:vpro:media:location:6\">\n" +
             "        <programUrl>http://cgi.omroep.nl/legacy/nebo?/ceres/1/vpro/rest/2009/VPRO_1132492/bb.20090317.m4v</programUrl>\n" +
             "        <avAttributes>\n" +
             "            <avFileFormat>MP4</avFileFormat>\n" +
@@ -55,26 +73,26 @@ public class MediaListItemTest {
             "        <offset>P0DT0H13M0.000S</offset>\n" +
             "        <duration>P0DT0H10M0.000S</duration>\n" +
             "    </s:locations>\n" +
-            "    <s:locations owner=\"BROADCASTER\" creationDate=\"2016-03-04T14:45:00+01:00\" urn=\"urn:vpro:media:location:7\" workflow=\"FOR PUBLICATION\">\n" +
+            "    <s:locations owner=\"BROADCASTER\" workflow=\"FOR PUBLICATION\" creationDate=\"2016-03-04T14:45:00+01:00\" urn=\"urn:vpro:media:location:7\">\n" +
             "        <programUrl>http://cgi.omroep.nl/legacy/nebo?/ceres/1/vpro/rest/2009/VPRO_1135479/sb.20091106.asf</programUrl>\n" +
             "        <avAttributes>\n" +
             "            <avFileFormat>WM</avFileFormat>\n" +
             "        </avAttributes>\n" +
             "    </s:locations>\n" +
-            "    <s:locations owner=\"BROADCASTER\" creationDate=\"2016-03-04T13:45:00+01:00\" urn=\"urn:vpro:media:location:8\" workflow=\"FOR PUBLICATION\">\n" +
+            "    <s:locations owner=\"BROADCASTER\" workflow=\"FOR PUBLICATION\" creationDate=\"2016-03-04T13:45:00+01:00\" urn=\"urn:vpro:media:location:8\">\n" +
             "        <programUrl>http://cgi.omroep.nl/legacy/nebo?/id/KRO/serie/KRO_1237031/KRO_1242626/sb.20070211.asf</programUrl>\n" +
             "        <avAttributes>\n" +
             "            <avFileFormat>WM</avFileFormat>\n" +
             "        </avAttributes>\n" +
             "        <duration>P0DT0H30M33.000S</duration>\n" +
             "    </s:locations>\n" +
-            "    <s:locations owner=\"NEBO\" creationDate=\"2016-03-04T12:45:00+01:00\" urn=\"urn:vpro:media:location:9\" workflow=\"FOR PUBLICATION\">\n" +
+            "    <s:locations owner=\"NEBO\" workflow=\"FOR PUBLICATION\" creationDate=\"2016-03-04T12:45:00+01:00\" urn=\"urn:vpro:media:location:9\">\n" +
             "        <programUrl>http://player.omroep.nl/?aflID=4393288</programUrl>\n" +
             "        <avAttributes>\n" +
             "            <avFileFormat>HTML</avFileFormat>\n" +
             "        </avAttributes>\n" +
             "    </s:locations>\n" +
-            "    <s:locations owner=\"NPO\" creationDate=\"2017-03-04T15:45:00+01:00\" urn=\"urn:vpro:media:location:10\" workflow=\"FOR PUBLICATION\">\n" +
+            "    <s:locations owner=\"NPO\" workflow=\"FOR PUBLICATION\" creationDate=\"2017-03-04T15:45:00+01:00\" urn=\"urn:vpro:media:location:10\">\n" +
             "        <programUrl>http://www.npo.nl/location/1</programUrl>\n" +
             "        <avAttributes>\n" +
             "            <avFileFormat>UNKNOWN</avFileFormat>\n" +
@@ -82,7 +100,7 @@ public class MediaListItemTest {
             "        <offset>P0DT0H13M0.000S</offset>\n" +
             "        <duration>P0DT0H10M0.000S</duration>\n" +
             "    </s:locations>\n" +
-            "    <s:locations owner=\"BROADCASTER\" creationDate=\"2017-02-05T11:42:00+01:00\" urn=\"urn:vpro:media:location:11\" workflow=\"PUBLISHED\">\n" +
+            "    <s:locations owner=\"BROADCASTER\" workflow=\"PUBLISHED\" creationDate=\"2017-02-05T11:42:00+01:00\" urn=\"urn:vpro:media:location:11\">\n" +
             "        <programUrl>http://www.vpro.nl/location/1</programUrl>\n" +
             "        <avAttributes>\n" +
             "            <avFileFormat>UNKNOWN</avFileFormat>\n" +
