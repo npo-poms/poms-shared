c85e99897c015c95cf0a6556b9c2a152060c08e3
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-11-08 14:54:00 +0000

    Made all public.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=108594

--- media-nep/src/main/java/nl/vpro/nep/PlayreadyResponse.java
+++ media-nep/src/main/java/nl/vpro/nep/PlayreadyResponse.java
@@ -4,12 +4,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * @TODO this is NEP specifific You'd expect in in a package nl.vpro.nep or so.
  * @since 5.5
  */
 @Data
 @NoArgsConstructor
-class PlayreadyResponse {
+public class PlayreadyResponse {
     private boolean success;
     private String token;
 }
