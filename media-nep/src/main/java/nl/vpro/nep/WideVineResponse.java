bb21bd570a2a194aa59cab9c3f82d090f0c0452f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-11-08 14:54:00 +0000

    Made all public.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=108594

--- media-nep/src/main/java/nl/vpro/nep/WideVineResponse.java
+++ media-nep/src/main/java/nl/vpro/nep/WideVineResponse.java
@@ -4,13 +4,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- *  *  @TODO this is NEP specifific You'd expect in in a package nl.vpro.nep or so.
-
  * @since 5.5
  */
 @Data
 @NoArgsConstructor
-class WideVineResponse {
+public class WideVineResponse {
     private boolean success;
     private String token;
 }
