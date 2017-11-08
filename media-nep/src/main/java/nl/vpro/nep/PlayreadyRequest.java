12e5bd4202ea84f6729ab91d2e020d3a83a482ff
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-11-08 14:54:00 +0000

    Made all public.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=108594

--- media-nep/src/main/java/nl/vpro/nep/PlayreadyRequest.java
+++ media-nep/src/main/java/nl/vpro/nep/PlayreadyRequest.java
@@ -5,14 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- *  *  @TODO this is NEP specifific You'd expect in in a package nl.vpro.nep or so.
 
  * @since 5.5
  */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-class PlayreadyRequest {
+public class PlayreadyRequest {
     private String client_ip;
     private String authorization_key;
 }
