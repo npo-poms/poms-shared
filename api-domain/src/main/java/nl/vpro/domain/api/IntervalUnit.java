d1adddde262b0da7d1174e4620807f97ae5d3c58
Rico Jansen <rico.jansen@gmail.com>
2017-12-04 09:32:32 +0000

    NPA-334 Date facetting fixes.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=109185

--- api-domain/src/main/java/nl/vpro/domain/api/IntervalUnit.java
+++ api-domain/src/main/java/nl/vpro/domain/api/IntervalUnit.java
@@ -8,21 +8,28 @@ import java.time.temporal.ChronoField;
 */
 public enum IntervalUnit {
 
-    YEAR(ChronoField.YEAR),
-    MONTH(ChronoField.MONTH_OF_YEAR),
-    WEEK(ChronoField.ALIGNED_WEEK_OF_YEAR),
-    DAY(ChronoField.DAY_OF_YEAR),
-    HOUR(ChronoField.HOUR_OF_DAY),
-    MINUTE(ChronoField.MINUTE_OF_DAY);
+    YEAR(ChronoField.YEAR, "yyyy"),
+    MONTH(ChronoField.MONTH_OF_YEAR, "yyyy-MM"),
+    WEEK(ChronoField.ALIGNED_WEEK_OF_YEAR, "yyyy-'W'w"),
+    DAY(ChronoField.DAY_OF_YEAR, "yyyy-MM-dd"),
+    HOUR(ChronoField.HOUR_OF_DAY, "yyyy-MM-dd'T'HH:mm:ss'Z'"),
+    MINUTE(ChronoField.MINUTE_OF_DAY, "yyyy-MM-dd'T'HH:mm:ss'Z'");
 
     private final ChronoField chronoField;
 
-    IntervalUnit(ChronoField chronoField) {
+    private final String format;
+
+    IntervalUnit(ChronoField chronoField, String format) {
         this.chronoField = chronoField;
+        this.format = format;
     }
 
 
     public ChronoField getChronoField() {
         return chronoField;
+    }
+
+    public String getFormat() {
+        return format;
     }
 }
