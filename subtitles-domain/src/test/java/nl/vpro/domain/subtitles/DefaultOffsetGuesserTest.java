a2c344e263c3f17bff55a7a4275172de0debadbb
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-01-10 15:43:15 +0000

    Details.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=109923

--- subtitles-domain/src/test/java/nl/vpro/domain/subtitles/DefaultOffsetGuesserTest.java
+++ subtitles-domain/src/test/java/nl/vpro/domain/subtitles/DefaultOffsetGuesserTest.java
@@ -6,7 +6,7 @@ import java.time.ZoneId;
 
 import org.junit.Test;
 
-import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Michiel Meeuwissen
