a99bf82a4105a7c1ca31eab3e923b89fde55f910
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-01-21 15:02:45 +0000

    backport missing version attribute
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-5.5-SNAPSHOT/; revision=110168

---
+++ pages-domain/src/main/java/nl/vpro/domain/page/update/PageUpdateRepository.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2014 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.domain.page.update;
+
+import java.time.Instant;
+import java.util.concurrent.Future;
+import java.util.function.Function;
+
+import nl.vpro.domain.Changes;
+
+/**
+ * @author Roelof Jan Koekoek
+ * @since 3.0
+ */
+public interface PageUpdateRepository extends SectionRepository {
+
+    PageUpdate loadByUrl(String url);
+
+    PageUpdate loadByCrid(String crid);
+
+
+    PageUpdate loadFirstByUrlPrefix(String url);
+
+    PageUpdate loadFirstByCridPrefix(String crid);
+
+    Future<?> save(PageUpdate update);
+
+    Future<?> delete(String pageRef);
+
+    Future<?> deleteAll(String url, int max, Function<PageUpdate, Boolean> permissionEvaluator);
+
+    Changes<PageUpdate> getChanges(Instant since);
+
+    class TooManyUpdatesException extends RuntimeException {
+
+        public TooManyUpdatesException(String message) {
+            super(message);
+        }
+    }
+
+}
