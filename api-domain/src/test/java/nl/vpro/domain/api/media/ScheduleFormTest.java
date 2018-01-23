36859bc94e2a416099d4324a188b61b1d41a04cf
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-01-23 21:40:53 +0000

    Targeting API-329. Better generics. Will try to use the cardinality of enums and the like (e.g. genres) to avoid wildcard and regexp queries.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=110219

--- api-domain/src/test/java/nl/vpro/domain/api/media/ScheduleFormTest.java
+++ api-domain/src/test/java/nl/vpro/domain/api/media/ScheduleFormTest.java
@@ -1,10 +1,14 @@
 package nl.vpro.domain.api.media;
 
 import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.bind.JAXB;
 
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
+import nl.vpro.jackson2.Jackson2Mapper;
 import nl.vpro.test.util.jaxb.JAXBTestUtil;
 
 /**
@@ -17,6 +21,12 @@ public class ScheduleFormTest {
     public void adapter() throws IOException, SAXException {
         ScheduleForm form = new ScheduleForm();
         JAXBTestUtil.roundTripAndSimilar(form, "<api:scheduleForm xmlns:api=\"urn:vpro:api:2013\" xmlns:media=\"urn:vpro:media:2009\"/>");
+    }
+
+    @Test
+    public void json() throws IOException {
+        String example = "<api:scheduleForm xmlns:api=\"urn:vpro:api:2013\" xmlns:media=\"urn:vpro:media:2009\" xmlns:pages=\"urn:vpro:pages:2013\"><api:searches><api:genres match=\"MUST\"><api:matcher match=\"SHOULD\">3.0.1.2</api:matcher><api:matcher matchType=\"WILDCARD\" match=\"SHOULD\">3.0.1.2.*</api:matcher></api:genres><api:scheduleEvents><api:begin>2018-01-23T06:00:00+01:00</api:begin><api:end>2018-01-24T06:00:00+01:00</api:end><api:channel>NDR3</api:channel></api:scheduleEvents></api:searches></api:scheduleForm>";
+        Jackson2Mapper.getLenientInstance().writeValue(System.out, JAXB.unmarshal(new StringReader(example), ScheduleForm.class));
     }
 
 }
