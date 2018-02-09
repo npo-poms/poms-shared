0966e7805f549d423c62f51f3c841b1ff65423dd
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-02-09 16:42:54 +0000

    MSE-3819
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=110558

--- media-domain/src/main/java/nl/vpro/domain/media/update/TranscodeRequest.java
+++ media-domain/src/main/java/nl/vpro/domain/media/update/TranscodeRequest.java
@@ -20,7 +20,7 @@ import javax.xml.bind.annotation.XmlType;
 @XmlRootElement(name = "transcode")
 public class TranscodeRequest {
 
-    private TranscodeRequest() {
+    public TranscodeRequest() {
 
     }
 
