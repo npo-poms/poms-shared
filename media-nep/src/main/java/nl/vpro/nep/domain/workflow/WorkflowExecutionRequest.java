1e3382b6b3ccaf2049f0e684b88ebd437a0002ba
Sajid Salehi <s.salehi@vpro.nl>
2018-02-09 11:56:18 +0000

    MSE-3819 initiele versie workflowexecution service
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=110534

---
+++ media-nep/src/main/java/nl/vpro/nep/domain/workflow/WorkflowExecutionRequest.java
@@ -0,0 +1,18 @@
+package nl.vpro.nep.domain.workflow;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Builder
+@Getter
+public class WorkflowExecutionRequest implements Serializable {
+    String mid;
+    String fileName;
+    EncryptionType encryption;
+    PriorityType priority;
+    Type type;
+    List<String> platforms;
+}
