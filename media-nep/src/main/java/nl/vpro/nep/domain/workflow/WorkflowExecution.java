fb7ceb42d7fb4ebce1ef4feb1f9b0ec64209ab10
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-02-12 16:18:53 +0000

    MSE-3820
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=110599

--- media-nep/src/main/java/nl/vpro/nep/domain/workflow/WorkflowExecution.java
+++ media-nep/src/main/java/nl/vpro/nep/domain/workflow/WorkflowExecution.java
@@ -5,12 +5,14 @@ import io.openapitools.jackson.dataformat.hal.annotation.Link;
 import io.openapitools.jackson.dataformat.hal.annotation.Resource;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.time.Instant;
 
 @Builder
 //@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
 @Resource
+@ToString
 public class WorkflowExecution {
 
     @Link
@@ -23,6 +25,7 @@ public class WorkflowExecution {
     String statusMessage;
     @Getter
     String workflowType;
+    @Getter
     CustomerMetadata customerMetadata;
     @Getter
     Instant startTime;
