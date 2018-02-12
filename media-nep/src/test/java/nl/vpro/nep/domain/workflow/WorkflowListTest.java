f425d2625c2886e83127c56e3f59dec4b8f9c9a4
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-02-12 11:50:29 +0000

    MSE-3820
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=110583

--- media-nep/src/test/java/nl/vpro/nep/domain/workflow/WorkflowListTest.java
+++ media-nep/src/test/java/nl/vpro/nep/domain/workflow/WorkflowListTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 import nl.vpro.nep.service.WorkflowExecutionServiceImpl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * @author Michiel Meeuwissen
  * @since 5.6
@@ -16,5 +18,6 @@ public class WorkflowListTest {
     @Test
     public void unmarshall() throws IOException {
         WorkflowList list = WorkflowExecutionServiceImpl.MAPPER.readValue(getClass().getResourceAsStream("/example.json"), WorkflowList.class);
+        assertThat(list.getWorkflowExecutions()).hasSize(20);
     }
 }
