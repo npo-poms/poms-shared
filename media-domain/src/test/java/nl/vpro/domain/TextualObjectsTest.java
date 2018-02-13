576a67f6b395c9f97794a3d35011dc4288fa1b01
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-02-13 21:46:32 +0000

    Resolving buckets of code duplication.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=110666

--- media-domain/src/test/java/nl/vpro/domain/TextualObjectsTest.java
+++ media-domain/src/test/java/nl/vpro/domain/TextualObjectsTest.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.SortedSet;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import nl.vpro.domain.media.MediaBuilder;
@@ -15,6 +16,7 @@ import nl.vpro.domain.media.support.Title;
 
 import static nl.vpro.domain.TextualObjects.findOwnersForTextFields;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 /**
  * @author Michiel Meeuwissen
@@ -103,6 +105,33 @@ public class TextualObjectsTest {
         assertThat(basicOwnedTexts).hasSize(6);
         assertThat(basicOwnedTexts.toString()).isEqualTo("[MAIN:BROADCASTER:a, MAIN:NPO:a, SUB:BROADCASTER:d, SUB:NPO:d, LEXICO:BROADCASTER:a, LEXICO:NPO:a]");
         basicOwnedTexts.forEach(bo -> log.info("{}", bo));
+    }
+
+
+    @Test
+    public void testUpdateDescriptionsForOwner() {
+        Description e1 = new Description("e1", OwnerType.BROADCASTER, TextualType.MAIN);
+        Description e2 = new Description("e2", OwnerType.NEBO, TextualType.MAIN); // Should stay
+        Description e3 = new Description("e3", OwnerType.BROADCASTER, TextualType.SHORT); // Has to be deleted
+        Description n1 = new Description("n1", OwnerType.BROADCASTER, TextualType.MAIN); // Should replace e1
+        Description n2 = new Description("n2", OwnerType.NEBO, TextualType.SHORT); // Should not be added
+        Description n3 = new Description("n3", OwnerType.BROADCASTER, TextualType.EPISODE); // Has to be added
+
+        Program existing = new Program();
+        Program incoming = new Program();
+
+        existing.addDescription(e1);
+        existing.addDescription(e2);
+        existing.addDescription(e3);
+        incoming.addDescription(n1);
+        incoming.addDescription(n2);
+        incoming.addDescription(n3);
+
+        TextualObjects.updateDescriptionsForOwner(incoming, existing, OwnerType.BROADCASTER);
+
+        Assert.assertThat("Insertion or ordering of new descriptions failed", existing.getDescriptions().first().getDescription(), equalTo(n1.getDescription()));
+        Assert.assertThat("Deletion or ordering of obsolete descriptions failed", existing.getDescriptions().last().getDescription(), equalTo(n3.getDescription()));
+        Assert.assertThat("Number of descriptions does not match", existing.getDescriptions().size(), equalTo(3));
     }
 
 
