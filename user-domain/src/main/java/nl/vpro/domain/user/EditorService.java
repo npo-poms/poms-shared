9d1f02a947c9cbc4965e92d727383cac0dfef1ba
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-02-14 12:43:09 +0000

    MSE-3820 Made it match on _all_ broadcasters associated with the account.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=110675

--- user-domain/src/main/java/nl/vpro/domain/user/EditorService.java
+++ user-domain/src/main/java/nl/vpro/domain/user/EditorService.java
@@ -4,8 +4,22 @@
  */
 package nl.vpro.domain.user;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedSet;
+
 public interface EditorService extends UserService<Editor> {
 
     Broadcaster currentEmployer();
+
+    SortedSet<Broadcaster> allowedBroadcasters();
+
+    default List<String> allowedBroadcasterIds() {
+        List<String> broadcasters = new ArrayList<>();
+        for (Broadcaster broadcaster : allowedBroadcasters()) {
+            broadcasters.add(broadcaster.getId());
+        }
+        return broadcasters;
+    }
 
 }
