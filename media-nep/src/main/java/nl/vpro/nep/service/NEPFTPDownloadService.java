70a6b0d07d9a176d5377f685e6e2efdcc93ff7ed
Sajid Salehi <s.salehi@vpro.nl>
2018-03-06 13:49:19 +0000

    Transcoding new ftp implementation
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=110933

--- media-nep/src/main/java/nl/vpro/nep/service/NEPFTPDownloadService.java
+++ media-nep/src/main/java/nl/vpro/nep/service/NEPFTPDownloadService.java
@@ -1,13 +1,8 @@
 package nl.vpro.nep.service;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.concurrent.Future;
 
-public interface NEPFTPService {
-
+public interface NEPFTPDownloadService {
     Future<?> download(String nepFile, Runnable... callbacks) throws IOException;
-
-    Future<?> upload(String nepFile, InputStream stream, Runnable... callbacks) throws IOException;
-
 }
