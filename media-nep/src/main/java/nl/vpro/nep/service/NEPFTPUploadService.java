e7b678ac91f4c48524da211eeb6ce480642bdbb6
Sajid Salehi <s.salehi@vpro.nl>
2018-03-08 15:58:56 +0000

    code improvement
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=111012

--- media-nep/src/main/java/nl/vpro/nep/service/NEPFTPUploadService.java
+++ media-nep/src/main/java/nl/vpro/nep/service/NEPFTPUploadService.java
@@ -5,5 +5,5 @@ import java.io.InputStream;
 import java.util.concurrent.Future;
 
 public interface NEPFTPUploadService {
-    Future<?> upload(String nepFile, InputStream stream, Runnable... callbacks) throws IOException;
+    Future<?> upload(String nepFile, InputStream stream, Runnable... callbacks);
 }
