4a6c2ebab4daad8a3a0a4b4e2e0c148e845dfd6a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-03-09 08:48:49 +0000

    moved nebo/projectm to shared (used by publisher too)
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=111020

---
+++ media-nebo/src/main/java/nl/vpro/domain/media/nebo/shared/TimeAdapter.java
@@ -0,0 +1,38 @@
+package nl.vpro.domain.media.nebo.shared;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ */
+public class TimeAdapter extends XmlAdapter<String, Date> {
+
+    protected SimpleDateFormat getDateFormat() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UT"));
+        return simpleDateFormat;
+    }
+
+
+    @Override
+    public Date unmarshal(String v) throws Exception {
+        if (StringUtils.isNotBlank(v)) {
+            return getDateFormat().parse(v);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public String marshal(Date date) throws Exception {
+        if (date != null) {
+            return getDateFormat().format(date);
+        }
+        return null;
+    }
+}
