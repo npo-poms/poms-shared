ad49d4dee379d07dbb6dac953f807def02d5f118
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-03-09 08:48:49 +0000

    moved nebo/projectm to shared (used by publisher too)
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=111020

---
+++ media-projectm/src/main/java/nl/vpro/domain/npo/projectm/MetaData_v2_1.java
@@ -0,0 +1,237 @@
+package nl.vpro.domain.npo.projectm;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import nl.vpro.domain.classification.ClassificationServiceLocator;
+import nl.vpro.domain.classification.TermId;
+import nl.vpro.domain.image.ImageType;
+import nl.vpro.domain.media.*;
+import nl.vpro.domain.media.support.*;
+import nl.vpro.domain.npo.projectm.metadata.v2_1.*;
+import nl.vpro.domain.user.Broadcaster;
+import nl.vpro.domain.user.BroadcasterService;
+import nl.vpro.domain.user.ServiceLocator;
+import nl.vpro.util.TextUtil;
+import nl.vpro.xml.util.XmlUtils;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 3.0
+ */
+@Slf4j
+public class MetaData_v2_1 {
+
+    protected static final String imageFormat = "s720";
+
+
+    public static Aflevering createAflevering(Program program, MediaProvider provider) throws DatatypeConfigurationException {
+        Aflevering aflevering = new Aflevering();
+
+        aflevering.setPrid(program.getMid());
+        aflevering.setPridexport(program.getMid());
+        aflevering.setTitel(program.getMainTitle());
+        aflevering.setLexicoTitel(program.getLexicoTitle());
+        aflevering.setIcon(getIcon(program));
+        aflevering.setInhk(TextUtil.truncate(getDescription(program, TextualType.SHORT), 256));
+        aflevering.setKykw(MediaObjects.getKijkwijzer(program));
+        aflevering.setOrti(getTitle(program, TextualType.ORIGINAL));
+        aflevering.setAfltitel(getTitle(program, TextualType.SUB));
+        aflevering.setMail(firstOrNull(program.getEmail()));
+        aflevering.setWebs(firstOrNullWebsite(program.getWebsites()));
+        aflevering.setTwitteraccount(TwitterRef.getValueOrNull(MediaObjects.getTwitterAccount(program)));
+        aflevering.setTwitterhashtag(TwitterRef.getValueOrNull(MediaObjects.getTwitterHash(program)));
+        aflevering.setGenre(getGenre(program));
+        aflevering.setSubgenre(getSubGenre(program));
+        aflevering.setOmroepen(createOmroepen(program, ServiceLocator.getBroadcasterService()));
+        aflevering.setPersonen(createPersonen(program));
+
+
+        if (program.isEpisode()) {
+            MemberRef seasonRef = getEpisodeRef(program, MediaType.SEASON);
+            if (seasonRef != null) {
+                Group season = provider.findByMid(seasonRef.getMidRef());
+                if (season != null) {
+                    Serie serie = createSerie(season);
+                    aflevering.setAflnr(String.valueOf(seasonRef.getNumber()));
+                    aflevering.setSerie(serie);
+                    MemberRef serieRef = getMemberRef(season, MediaType.SERIES);
+                    if (serieRef != null) {
+                        Group series = provider.findByMid(serieRef.getMidRef());
+                        if (series != null) {
+                            Parentserie parent = createParentserie(series);
+                            aflevering.setParentserie(parent);
+                        } else {
+                            log.warn("Series {} could not be found in {}", serieRef.getMidRef(), provider);
+                        }
+                    }
+                } else {
+                    log.warn("Season {} could not be found in {}", seasonRef.getMidRef(), provider);
+                }
+            }
+        }
+
+        aflevering.setTimestamp(getCalendar(program.getLastModifiedInstant()));
+
+        return aflevering;
+
+    }
+
+    public static Serie createSerie(Group group) {
+        Serie serie = new Serie();
+        serie.setSrid(group.getMid());
+        serie.setTitel(group.getMainTitle());
+        serie.setLexicoTitel(group.getLexicoTitle());
+        serie.setIcon(getIcon(group));
+        serie.setOrti(getTitle(group, TextualType.ORIGINAL));
+        serie.setMail(firstOrNull(group.getEmail()));
+        serie.setWebs(firstOrNullWebsite(group.getWebsites()));
+        serie.setTwitteraccount(TwitterRef.getValueOrNull(MediaObjects.getTwitterAccount(group)));
+        serie.setTwitterhashtag(TwitterRef.getValueOrNull(MediaObjects.getTwitterHash(group)));
+        serie.setGenre(getGenre(group));
+        serie.setSubgenre(getSubGenre(group));
+        serie.setOmroepen(createOmroepen(group, ServiceLocator.getBroadcasterService()));
+        serie.setInh1(TextUtil.truncate(group.getMainDescription(), 4096));
+        return serie;
+    }
+    public static Parentserie createParentserie(Group group) {
+        Parentserie parentserie = new Parentserie();
+        parentserie.setPsrid(group.getMid());
+        parentserie.setTitel(group.getMainTitle());
+        return parentserie;
+    }
+
+    protected static String getIcon(MediaObject media) {
+        if (media.getImages().size() == 0) {
+            return null;
+        }
+
+        Image image = media.findImage(ImageType.ICON);
+        if (image == null) {
+            image = media.getImages().get(0);
+        }
+
+        if (image == null) {
+            return null;
+        }
+
+        return Images.getImageLocation(image, "jpg", imageFormat);
+    }
+    protected static String getDescription(MediaObject media, TextualType type) {
+        Description description = media.findDescription(type);
+        return description != null ? description.getDescription() : null;
+    }
+
+    protected static String getTitle(MediaObject media, TextualType type) {
+        Title description = media.findTitle(type);
+        return description != null ? description.getTitle() : null;
+    }
+    protected static String firstOrNull(Collection<?> col) {
+        if (col == null || col.isEmpty()) return null;
+        return String.valueOf(col.iterator().next());
+    }
+    protected static String firstOrNullWebsite(Collection<Website> col) {
+        if (col == null || col.isEmpty()) return null;
+        return col.iterator().next().getUrl();
+    }
+
+    protected static String getGenre(MediaObject mediaObject) {
+        if (mediaObject.getGenres().isEmpty()) {
+            return null;
+        }
+        return getGenreAtLevel(mediaObject.getGenres().first(), 4);
+
+    }
+
+
+    protected static String getSubGenre(MediaObject mediaObject) {
+        if (mediaObject.getGenres().isEmpty()) {
+            return null;
+        }
+        return getGenreAtLevel(mediaObject.getGenres().first(), 5);
+    }
+
+    protected static String getGenreAtLevel(Genre genre, int level) {
+        TermId id = new TermId(genre.getTermId());
+        int parts[] = id.getParts();
+        if (parts.length < level) {
+            return null;
+        }
+        parts = Arrays.copyOf(parts, level);
+        id = new TermId(parts);
+        return ClassificationServiceLocator.getInstance().getTerm(id.toString()).getName();
+    }
+
+    protected static Omroepen createOmroepen(MediaObject mediaObject, BroadcasterService broadcasterService) {
+
+        List<Broadcaster> broadcasters = mediaObject.getBroadcasters();
+        if (broadcasters.isEmpty()) {
+            return null;
+        }
+        Omroepen omroepen = new Omroepen();
+        for (Broadcaster broadcaster : broadcasters) {
+            if (broadcasterService != null) {
+                Broadcaster b = broadcasterService.find(broadcaster.getId());
+                if (b == null) {
+                    log.warn("Could not find broadcaster {} in {}", broadcaster.getId(), broadcasterService);
+                } else {
+                    broadcaster = b;
+                }
+            }
+            omroepen.getOmroep().add(broadcaster.getMisId());
+        }
+        return omroepen;
+    }
+
+    protected static Personen createPersonen(MediaObject mediaObject) {
+
+        List<Person> persons = mediaObject.getPersons();
+        if (persons.isEmpty()) {
+            return null;
+        }
+        Personen personen = new Personen();
+        for (Person person : persons) {
+            Persoon persoon = new Persoon();
+            persoon.setVoornaam(person.getGivenName());
+            persoon.setAchternaam(person.getFamilyName());
+            Rol rol = new Rol();
+            rol.setValue(person.getRole().toString());
+            rol.setId(person.getRole().ordinal());
+            persoon.setRol(rol);
+            personen.getPersoon().add(persoon);
+        }
+        return personen;
+
+    }
+
+    protected static XMLGregorianCalendar getCalendar(Instant date) throws DatatypeConfigurationException {
+        return XmlUtils.toXml(Schedule.ZONE_ID, date);
+    }
+
+    private static MemberRef getEpisodeRef(Program media, MediaType type) {
+        for (MemberRef memberRef : media.getEpisodeOf()) {
+            if (type.equals(memberRef.getType())) {
+                return memberRef;
+            }
+        }
+        return null;
+    }
+
+    private static MemberRef getMemberRef(MediaObject media, MediaType type) {
+        for (MemberRef memberRef : media.getMemberOf()) {
+            if (type.equals(memberRef.getType())) {
+                return memberRef;
+            }
+        }
+        return null;
+    }
+
+
+}
