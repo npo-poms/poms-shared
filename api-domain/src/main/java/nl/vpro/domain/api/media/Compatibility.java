2207c3adf5f93b235d944d8c453c9ec526410247
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-03-13 10:05:29 +0000

    Backwards compatibility.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=111136

---
+++ api-domain/src/main/java/nl/vpro/domain/api/media/Compatibility.java
@@ -0,0 +1,27 @@
+package nl.vpro.domain.api.media;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.5
+ */
+public class Compatibility {
+
+
+    private static final ThreadLocal<Float> compatibility = ThreadLocal.withInitial(() -> null);
+
+    public static void setCompatibility(float value) {
+        compatibility.set(value);
+    }
+
+    public static void clearCompatibility() {
+        compatibility.remove();
+    }
+
+
+    public static boolean compatibleBefore(float version) {
+        if (compatibility.get() == null) {
+            return true;
+        }
+        return version < compatibility.get();
+    }
+}
