2b8ed745eb5c510234fb47177eb7831d07b858d8
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-03-16 09:19:54 +0000

    Logging and other details.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=111228

--- media-nep/src/main/java/nl/vpro/nep/service/NEPFTPUploadService.java
+++ media-nep/src/main/java/nl/vpro/nep/service/NEPFTPUploadService.java
@@ -3,6 +3,8 @@ package nl.vpro.nep.service;
 import java.io.InputStream;
 import java.util.concurrent.CompletableFuture;
 
+import org.slf4j.Logger;
+
 public interface NEPFTPUploadService {
-    CompletableFuture<?> upload(String nepFile, Long size, InputStream stream);
+    CompletableFuture<?> upload(Logger logger, String nepFile, Long size, InputStream stream);
 }
