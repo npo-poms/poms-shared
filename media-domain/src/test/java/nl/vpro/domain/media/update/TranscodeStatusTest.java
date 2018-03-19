ec1d8445ba2512ad4bf59ff47eeae4c7192ac0cb
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-03-19 15:32:30 +0000

    MSE-3821
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=111306

---
+++ media-domain/src/test/java/nl/vpro/domain/media/update/TranscodeStatusTest.java
@@ -0,0 +1,34 @@
+package nl.vpro.domain.media.update;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import nl.vpro.test.util.jaxb.JAXBTestUtil;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.6
+ */
+public class TranscodeStatusTest {
+
+
+    @Test
+    public void xml() throws IOException, SAXException {
+        TranscodeStatus status = TranscodeStatus
+            .builder()
+            .mid("mid_123")
+            .broadcasters(Arrays.asList("VPRO", "EO"))
+            .build();
+
+        JAXBTestUtil.roundTripAndSimilar(status, "<transcodeStatus mid=\"mid_123\" xmlns=\"urn:vpro:media:update:2009\" xmlns:shared=\"urn:vpro:shared:2009\" xmlns:media=\"urn:vpro:media:2009\">\n" +
+            "    <broadcasters>\n" +
+            "        <broadcaster>VPRO</broadcaster>\n" +
+            "        <broadcaster>EO</broadcaster>\n" +
+            "    </broadcasters>\n" +
+            "</transcodeStatus>");
+    }
+
+}
