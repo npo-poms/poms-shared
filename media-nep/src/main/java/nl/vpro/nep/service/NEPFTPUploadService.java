6d626ff1ba392ddcc866cc82c38deb31bcce88c7
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-03-19 09:45:55 +0000

    MSE-3912
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=111266

--- media-nep/src/main/java/nl/vpro/nep/service/NEPFTPUploadService.java
+++ media-nep/src/main/java/nl/vpro/nep/service/NEPFTPUploadService.java
@@ -1,10 +1,10 @@
 package nl.vpro.nep.service;
 
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.concurrent.CompletableFuture;
 
 import nl.vpro.logging.SimpleLogger;
 
 public interface NEPFTPUploadService {
-    CompletableFuture<?> upload(SimpleLogger logger, String nepFile, Long size, InputStream stream);
+    long upload(SimpleLogger logger, String nepFile, Long size, InputStream stream) throws IOException;
 }
