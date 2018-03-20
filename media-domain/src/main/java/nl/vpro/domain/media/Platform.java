2d0e9461f455de8ccb2ce3fecec657a236de8c72
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-03-20 12:48:34 +0000

    Made platform displayable too.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=111360

--- media-domain/src/main/java/nl/vpro/domain/media/Platform.java
+++ media-domain/src/main/java/nl/vpro/domain/media/Platform.java
@@ -5,38 +5,44 @@ import lombok.Getter;
 import javax.xml.bind.annotation.XmlEnum;
 import javax.xml.bind.annotation.XmlType;
 
+import nl.vpro.domain.Displayable;
+
 /**
  * @author Michiel Meeuwissen
  */
 @XmlEnum
 @XmlType(name = "platformTypeEnum")
-public enum Platform {
+public enum Platform implements Displayable {
     /**
      * Visible on internet
      */
-    INTERNETVOD(true),
+    INTERNETVOD(true, "Beschikbaar op internet"),
 
     /**
      *
      */
-    TVVOD(true),
+    TVVOD(true, "Beschikbaar voor kabelaars"),
 
     /**
      * NLZiet,platform "extra" in cooperation with dutch commercial broadcasters
      */
-    PLUSVOD(true),
+    PLUSVOD(true, "NLZiet"),
 
     /**
      *  NPOPlus, platform "plusx" is the NPO only offspring/splitoff from NLZiet
      */
-    NPOPLUSVOD(false);
+    NPOPLUSVOD(false, "NPOPlus");
 
 
     @Getter
     private final boolean streamingPlatform;
 
+    @Getter
+    private final String displayName;
 
-    Platform(boolean streamingPlatform) {
+
+    Platform(boolean streamingPlatform, String displayName) {
         this.streamingPlatform = streamingPlatform;
+        this.displayName = displayName;
     }
 }
