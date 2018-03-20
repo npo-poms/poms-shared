64828efa0476e5a9ca55bf33061f7b030094d078
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-03-20 11:29:43 +0000

    Moved 'simple' logging system to its own package.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=111333

--- media-nep/src/main/java/nl/vpro/nep/service/NEPFTPUploadService.java
+++ media-nep/src/main/java/nl/vpro/nep/service/NEPFTPUploadService.java
@@ -3,7 +3,7 @@ package nl.vpro.nep.service;
 import java.io.IOException;
 import java.io.InputStream;
 
-import nl.vpro.logging.SimpleLogger;
+import nl.vpro.logging.simple.SimpleLogger;
 
 public interface NEPFTPUploadService {
     long upload(SimpleLogger logger, String nepFile, Long size, InputStream stream) throws IOException;
