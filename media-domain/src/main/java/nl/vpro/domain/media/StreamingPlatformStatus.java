850917412c2e8654779718db919d7309b0d7cf1d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-03-20 13:38:42 +0000

    Texts and details.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=111363

--- media-domain/src/main/java/nl/vpro/domain/media/StreamingPlatformStatus.java
+++ media-domain/src/main/java/nl/vpro/domain/media/StreamingPlatformStatus.java
@@ -1,11 +1,13 @@
 package nl.vpro.domain.media;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement (name="streamingstatus")
 @XmlAccessorType(XmlAccessType.NONE)
@@ -17,6 +19,8 @@ public class StreamingPlatformStatus {
     @XmlAttribute
     private String status;
 
-    public StreamingPlatformStatus (String status){this.status = status;}
+    public StreamingPlatformStatus(String status){
+        this.status = status;
+    }
 
 }
