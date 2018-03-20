9bcfe8b3478d6cffaefffe9cd1450509f12a5228
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-03-20 13:38:42 +0000

    Texts and details.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=111363

--- media-domain/src/main/java/nl/vpro/domain/media/support/StreamingStatus.java
+++ media-domain/src/main/java/nl/vpro/domain/media/support/StreamingStatus.java
@@ -1,47 +1,50 @@
 package nl.vpro.domain.media.support;
 
+import lombok.Getter;
+
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Collectors;
+
+import nl.vpro.domain.Displayable;
 
 /**
  * @author Michiel Meeuwissen
  * @since 5.1
  */
-public enum StreamingStatus {
+public enum StreamingStatus implements Displayable {
 
     /**
      * Not notified by NEP
      */
-    NOT_AVAILABLE(null, false),
+    NOT_AVAILABLE(null, false, "niet beschikbaar"),
 
     /**
      * Explicitely notified by NEP to be offline
      */
-    OFFLINE(null, false),
+    OFFLINE(null, false, "offline"),
 
     /**
      * Explicitely notified by NEP to be online
      */
-    AVAILABLE(false, true),
+    AVAILABLE(false, true, "beschikbaar"),
 
     /**
      * Explicitely notified by NEP to be online with DRM
      */
-    AVAILABLE_WITH_DRM(true, true),
+    AVAILABLE_WITH_DRM(true, true, "beschikbaar met DRM");
 
-    /**
-     * Explicitely notified by NEP to be online with DRM and AES
-     * @since 5.6
-     */
-    AVAILABLE_WITH_AES_DRM(true, true);
 
     private final Boolean drm;
     private final boolean available;
+    @Getter
+    private final String displayName;
 
-    StreamingStatus(Boolean drm, boolean available) {
+
+    StreamingStatus(Boolean drm, boolean available, String displayName) {
         this.drm = drm;
         this.available = available;
+        this.displayName = displayName;
     }
 
     public static StreamingStatus available(boolean drm) {
