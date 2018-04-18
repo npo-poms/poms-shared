2c3ed1593f9048dc3cd36f210f02b75b3ce3498a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-04-18 07:42:01 +0000

    Created copyIfLessRestricted.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=112019

--- poms-shared-domain/src/main/java/nl/vpro/domain/Embargo.java
+++ poms-shared-domain/src/main/java/nl/vpro/domain/Embargo.java
@@ -2,6 +2,8 @@ package nl.vpro.domain;
 
 import java.time.Instant;
 
+import com.google.common.collect.Range;
+
 /**
  * Also includes setters.
  *
@@ -13,5 +15,11 @@ public interface Embargo<T extends Embargo<T>> extends ReadonlyEmbargo {
     T setPublishStartInstant(Instant publishStart);
 
     T setPublishStopInstant(Instant publishStop);
+
+    default T set(Range<Instant> range) {
+        setPublishStartInstant(range.lowerEndpoint());
+        setPublishStopInstant(range.upperEndpoint());
+        return (T) this;
+    }
 
 }
--- poms-shared-domain/src/main/java/nl/vpro/domain/ReadonlyEmbargo.java
+++ poms-shared-domain/src/main/java/nl/vpro/domain/ReadonlyEmbargo.java
@@ -59,4 +59,5 @@ public interface ReadonlyEmbargo {
         return true;
     }
 
+
 }
