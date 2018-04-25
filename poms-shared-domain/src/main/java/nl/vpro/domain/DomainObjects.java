eb7eb349241c9481813f45ad4019cea05a6f3320
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-04-25 08:20:23 +0000

    MSE-3831
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=112120

---
+++ poms-shared-domain/src/main/java/nl/vpro/domain/DomainObjects.java
@@ -0,0 +1,43 @@
+package nl.vpro.domain;
+
+import java.io.File;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.7
+ */
+public class DomainObjects {
+
+
+
+    public static String getCanonicalFilePath(DomainObject domain) {
+        String id = String.valueOf(domain.getId());
+        StringBuilder path = new StringBuilder(id.length() * 2);
+        appendCanonicalFilePath(domain, path);
+        return path.toString();
+    }
+
+
+    public static void appendCanonicalFilePath(DomainObject domain, StringBuilder builder) {
+        appendCanonicalFilePath(domain.getId(), builder);
+    }
+
+    public static void appendCanonicalFilePath(Long i, StringBuilder builder) {
+        String id = String.valueOf(i);
+
+        CharacterIterator it = new StringCharacterIterator(id);
+        for(char c = it.first(); c != CharacterIterator.DONE; c = it.next()) {
+            builder.append(c);
+            // Second digit
+            c = it.next();
+            if(c != CharacterIterator.DONE) {
+                builder.append(c);
+            }
+
+            builder.append(File.separator);
+        }
+    }
+
+}
