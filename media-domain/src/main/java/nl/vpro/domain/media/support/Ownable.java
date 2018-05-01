05e91fbf139fac682472e957ef7dd2178e7e9f0d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-05-01 09:40:32 +0000

    javadoc.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=112228

--- media-domain/src/main/java/nl/vpro/domain/media/support/Ownable.java
+++ media-domain/src/main/java/nl/vpro/domain/media/support/Ownable.java
@@ -4,6 +4,12 @@
  */
 package nl.vpro.domain.media.support;
 
+/**
+ * Ownable objects are owned by a certain {@link OwnerType}. This indicates something about the authority of the value, and normally the same field value exist for different 'owner types'. E.g. a main title may be owned by 'MIS', but may have a another value but than owned by 'BROADCASTER'. Both values remain available, but in principle the BROADCASTER value overwrites the MIS value (but one may interpret differently).
+ *
+ * @see OwnerType
+ */
+
 public interface Ownable {
 
     OwnerType getOwner();
