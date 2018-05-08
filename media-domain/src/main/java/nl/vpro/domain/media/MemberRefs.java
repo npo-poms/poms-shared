07cf62e5b3c7410e1c71130ae951c51ec1fab3ef
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-05-08 09:35:40 +0000

    More stuff similar to MSE-4013
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=112337

--- media-domain/src/main/java/nl/vpro/domain/media/MemberRefs.java
+++ media-domain/src/main/java/nl/vpro/domain/media/MemberRefs.java
@@ -5,7 +5,11 @@
 package nl.vpro.domain.media;
 
 import java.util.AbstractList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+
+import javax.swing.text.html.Option;
 
 /**
  * @author Roelof Jan Koekoek
@@ -40,5 +44,59 @@ public class MemberRefs {
                 return memberRefs.size();
             }
         };
+    }
+
+
+    /**
+     * @since 5.7
+     */
+    public static Optional<MemberRef> findRef(Collection<MemberRef> collection, MediaObject owner) {
+         if(collection == null) {
+            return Optional.empty();
+        }
+
+        for(MemberRef memberRef : collection) {
+            if (memberRef.getMidRef() != null && memberRef.getMidRef().equals(owner.getMid())) {
+                return Optional.of(memberRef);
+            }
+            if(memberRef.getOwner().equals(owner)) {
+                return Optional.of(memberRef);
+            }
+        }
+        return Optional.empty();;
+    }
+
+     /**
+     * @since 5.7
+     */
+    public static Optional<MemberRef> findRef(Collection<MemberRef> collection, MediaObject owner, Integer number) {
+        if (number == null) {
+            return findRef(collection, owner);
+        }
+        for (MemberRef memberRef : collection) {
+            if (memberRef.getNumber().equals(number)) {
+                if (memberRef.getMidRef() != null && memberRef.getMidRef().equals(owner.getMid())) {
+                    return Optional.of(memberRef);
+                }
+                if (memberRef.getOwner().equals(owner)) {
+                    return Optional.of(memberRef);
+                }
+            }
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * @since 5.7
+     */
+    public static boolean isOf(Collection<MemberRef> collection, MediaObject owner) {
+        return findRef(collection, owner).isPresent();
+    }
+
+     /**
+     * @since 5.7
+     */
+    public static boolean isOf(Collection<MemberRef> collection, MediaObject owner, Integer number) {
+        return findRef(collection, owner, number).isPresent();
     }
 }
