4ab0208e4c8d70037d89e959a1ad3b0bdf5d5bab
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-05-09 11:18:36 +0000

    MSE-4015
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=112426

---
+++ media-domain/src/main/java/nl/vpro/domain/media/update/LiveItemizeRequest.java
@@ -0,0 +1,60 @@
+package nl.vpro.domain.media.update;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.Instant;
+
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import nl.vpro.jackson2.StringInstantToJsonTimestamp;
+import nl.vpro.xml.bind.InstantXmlAdapter;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.6
+ */
+@Data
+@AllArgsConstructor
+@lombok.Builder(builderClassName = "Builder")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "liveItemize")
+@XmlRootElement(name = "liveitemize")
+public class LiveItemizeRequest {
+
+
+    @NotNull
+    @XmlAttribute
+    private String stream;
+
+    @XmlElement(required = false)
+    @XmlJavaTypeAdapter(InstantXmlAdapter.class)
+    @XmlSchemaType(name = "dateTime")
+    @JsonDeserialize(using = StringInstantToJsonTimestamp.Deserializer.class)
+    @JsonSerialize(using = StringInstantToJsonTimestamp.Serializer.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Instant start;
+
+    @XmlElement(required = false)
+    @XmlJavaTypeAdapter(InstantXmlAdapter.class)
+    @XmlSchemaType(name = "dateTime")
+    @JsonDeserialize(using = StringInstantToJsonTimestamp.Deserializer.class)
+    @JsonSerialize(using = StringInstantToJsonTimestamp.Serializer.class)
+    private Instant stop;
+
+
+
+
+    public LiveItemizeRequest() {
+
+    }
+
+
+
+}
