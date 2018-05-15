853a43cc5f9729581f2d90fa5e33c8613d296630
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-05-15 14:49:18 +0000

    MSE-4023
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=112539

--- media-domain/src/main/java/nl/vpro/domain/media/update/AssemblageConfig.java
+++ media-domain/src/main/java/nl/vpro/domain/media/update/AssemblageConfig.java
@@ -2,26 +2,42 @@ package nl.vpro.domain.media.update;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import nl.vpro.domain.media.support.OwnerType;
 
 /**
- * This look a lot like {@link MediaUpdateConfig}
- *
- * TODO: MediaUpdate needs heavy refactoring, and we should think about this kind of stuff then too.
  * @author Michiel Meeuwissen
  * @since 5.5
  */
+@NoArgsConstructor
 @AllArgsConstructor
 @lombok.Builder
 @Data
 @ToString
 public class AssemblageConfig {
+
     @lombok.Builder.Default
     OwnerType ownerType = OwnerType.BROADCASTER;
-    final boolean copyWorkflow;
-    final boolean copyLanguageAndCountry;
-    final boolean imageMetaData;
-    final boolean copyPredictions;
+
+    @lombok.Builder.Default
+    boolean copyWorkflow = false;
+
+    @lombok.Builder.Default
+    boolean copyLanguageAndCountry = false;
+
+    @lombok.Builder.Default
+    boolean imageMetaData = false;
+
+    @lombok.Builder.Default
+    boolean copyPredictions = false;
+
+    @lombok.Builder.Default
+    boolean episodeOfUpdate = true;
+    @lombok.Builder.Default
+    boolean memberOfUpdate = true;
+    @lombok.Builder.Default
+    boolean ratingsUpdate = true;
+
 }
