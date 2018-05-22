f043920d57f4f9ebdfbe98a3d7034d9309f6bc62
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-05-22 12:27:44 +0000

    MSE-4032 It seems that a combination of curl and sshj will do everything correctly.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=112635

--- media-nep/src/test/java/nl/vpro/nep/service/impl/NEPCurlDownloadServiceImplTest.java
+++ media-nep/src/test/java/nl/vpro/nep/service/impl/NEPCurlDownloadServiceImplTest.java
@@ -16,21 +16,21 @@ import nl.vpro.nep.service.NEPDownloadService;
  * @since 5.8
  */
 @Slf4j
-public class NEPCurlFTPDownloadServiceImplTest {
+public class NEPCurlDownloadServiceImplTest {
 
     @Test
     public void test() throws IOException {
 
         Instant start = Instant.now();
 
-        NEPDownloadService impl = new NEPCurlFTPDownloadServiceImpl( "sftp-itemizer.nepworldwide.nl",
+        NEPDownloadService impl = new NEPCurlDownloadServiceImpl( "sftp-itemizer.nepworldwide.nl",
             "npo",
             "***REMOVED***",
             "AAAAB3NzaC1yc2EAAAADAQABAAABAQCV4gmmgKyPVyOyZv1jdVpu/KzS9w2v4/vxDeKbuXvl0tldvDAmMi/QY1XvLueuZJy8PmilpGj6po1JuU0V2RGX/Js18b9lyCAQptdaeUk45lYvM8bpGfkzB509i3+CaM6U1onEIftFs4vzDLMwHrZQ6kdlRGGs6bLYy1vpqs7h6mO/XGDeLLVpjLPZbz/TrWt98kinn+Rg/TwYV0VNyqac5DkpWtFEUucIrq6zZs1q3Pw8YHMo02BWlWXFR/yi41ODb+RH1dTlZEs3vrMgwFvVD5c+4EKy1hZ65SJ6xVXwaMyN4w1LaHLwwe3K8rNDS+m5gyaswhdeZthqDiXysFwj"
             //"94:06:26:d5:e4:f5:18:b5:52:a9:19:b1:97:db:94:9e"
             );
         FileOutputStream outputStream = new FileOutputStream("/tmp/test.mp4");
-        impl.download(NEPFTPDownloadServiceImplTest.fileName, outputStream, Duration.ofSeconds(10), (fd) -> {
+        impl.download(NEPSSHJDownloadServiceImplTest.fileName, outputStream, Duration.ofSeconds(10), (fd) -> {
             log.info("{}", fd);
             return true;}
             );
