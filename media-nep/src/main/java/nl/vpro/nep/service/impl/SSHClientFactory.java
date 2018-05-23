0627e0391308dbaedeb57238c0843e3b0872447c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-05-23 10:19:04 +0000

    Fixed last modified.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=112670

--- media-nep/src/main/java/nl/vpro/nep/service/impl/SSHClientFactory.java
+++ media-nep/src/main/java/nl/vpro/nep/service/impl/SSHClientFactory.java
@@ -11,6 +11,8 @@ import net.schmizz.sshj.common.SecurityUtils;
 import java.io.IOException;
 import java.security.PublicKey;
 import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 final class SSHClientFactory {
@@ -21,27 +23,38 @@ final class SSHClientFactory {
     private static final Duration sshTimeout  = Duration.ofSeconds(300);
     private static final Duration sshConnectionTimeout  = Duration.ofSeconds(5);
 
+    private static Map<String, String> FINGERPRINTS = new ConcurrentHashMap<>();
+
     /**
      * @param hostKey the RSA host key or if containing a semicolon one of the fingerprints supported by sshj.
      */
-    static SSHClient create(String hostKey, String host, String username, String password) throws IOException {
+    static SSHClient create(final String hostKey, final String host, String username, String password) throws IOException {
 
         final DefaultConfig configuration = new DefaultConfig();
         configuration.setKeepAliveProvider(KeepAliveProvider.KEEP_ALIVE);
         final SSHClient ssh = new SSHClient(configuration);
         ssh.useCompression();
 
-        if (hostKey.startsWith("MD5:") || hostKey.startsWith("SHA256:") || hostKey.startsWith("SHA1:")) {
-            // This is a fingerprint already
-            log.info("Validating {} with fingerprint {}", host, hostKey);
-            ssh.addHostKeyVerifier(hostKey);
-        } else {
-            byte[] keyBytes = Base64.decode(hostKey);
-            PublicKey key = new Buffer.PlainBuffer(keyBytes).readPublicKey();
-            String fingerPrint = SecurityUtils.getFingerprint(key);
-            log.info("Validating {} with fingerprint {}", host, fingerPrint);
-            ssh.addHostKeyVerifier(fingerPrint);
-        }
+        String fingerprint = FINGERPRINTS.computeIfAbsent(hostKey, (hk) -> {
+            if (hostKey.startsWith("MD5:") || hostKey.startsWith("SHA256:") || hostKey.startsWith("SHA1:")) {
+                // This is a fingerprint already
+                log.info("Validating {} with fingerprint {}", host, hostKey);
+                return hostKey;
+
+            } else {
+                try {
+                    byte[] keyBytes = Base64.decode(hostKey);
+                    PublicKey key = new Buffer.PlainBuffer(keyBytes).readPublicKey();
+                    String fingerPrint = SecurityUtils.getFingerprint(key);
+                    log.info("Validating {} with fingerprint {}", host, fingerPrint);
+                    return fingerPrint;
+                } catch (IOException ioe) {
+                    log.error(ioe.getMessage());
+                    return hostKey;
+                }
+            }
+        });
+        ssh.addHostKeyVerifier(fingerprint);
 
         ssh.setTimeout((int) sshTimeout.toMillis());
         ssh.setConnectTimeout((int) sshConnectionTimeout.toMillis());
