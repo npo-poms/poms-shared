222ee36976c143d0a6c9ff911f68e7240155b1c3
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-05-23 11:01:40 +0000

    actually is one word.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=112675

--- media-nep/src/main/java/nl/vpro/nep/service/NEPDownloadService.java
+++ media-nep/src/main/java/nl/vpro/nep/service/NEPDownloadService.java
@@ -5,19 +5,19 @@ import java.io.OutputStream;
 import java.time.Duration;
 import java.util.function.Function;
 
-import nl.vpro.util.FileMetaData;
+import nl.vpro.util.FileMetadata;
 
 public interface NEPDownloadService {
     void download(
         String nepFile,
         OutputStream outputStream,
         Duration timeout,
-        Function<FileMetaData, Boolean> descriptorConsumer) throws IOException;
+        Function<FileMetadata, Boolean> descriptorConsumer) throws IOException;
 
     default void download(
         String nepFile,
         OutputStream outputStream,
-        Function<FileMetaData, Boolean> descriptorConsumer) throws IOException {
+        Function<FileMetadata, Boolean> descriptorConsumer) throws IOException {
         download(nepFile, outputStream, Duration.ZERO, descriptorConsumer);
     }
 }
