99f306408296536b51e0d5fe99dd2f41aabacc95
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-05-23 07:41:30 +0000

    MSE-4032
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=112648

---
+++ media-nep/src/main/java/nl/vpro/nep/service/impl/NEPScpDownloadServiceImpl.java
@@ -0,0 +1,84 @@
+package nl.vpro.nep.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.function.Function;
+
+import javax.inject.Named;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import nl.vpro.nep.service.FileDescriptor;
+import nl.vpro.nep.service.NEPDownloadService;
+import nl.vpro.util.CommandExecutor;
+import nl.vpro.util.CommandExecutorImpl;
+
+/**
+ * See MSE-4032. It's kind of a disgrace that we have to fall back to external commands...
+ *
+ * osx: sudo port install curl +sftp_scp (or use brew)
+ *
+ * @author Michiel Meeuwissen
+ * @since 5.8
+ */
+@Named("NEPDownloadService")
+@Slf4j
+public class NEPScpDownloadServiceImpl implements NEPDownloadService {
+
+
+    private final String url;
+    private final CommandExecutor scp;
+    private final NEPSSJDownloadServiceImpl sshj;
+
+
+    public NEPScpDownloadServiceImpl(
+        @Value("${nep.sftp.host}") String ftpHost,
+        @Value("${nep.sftp.username}") String username,
+        @Value("${nep.sftp.password}") String password,
+        @Value("${nep.sftp.hostkey}") String hostkey
+    ) {
+        this.url = username + "@" + ftpHost;
+        File scpcommand = CommandExecutorImpl.getExecutable("/usr/bin/scp").orElseThrow(IllegalArgumentException::new);
+        scp = CommandExecutorImpl.builder()
+            .executablesPaths("/usr/bin/sshpass", "/opt/local/bin/sshpass")
+            .wrapLogInfo((message) -> message.replaceAll(password, "??????"))
+            //.useFileCache(true)
+            .commonArgs(Arrays.asList("-p", password, scpcommand.getAbsolutePath()))
+            .build();
+        // just used for the checkAvailability call (actually for the descriptorConsumer callback)
+        sshj = new NEPSSJDownloadServiceImpl(ftpHost, username, password, hostkey);
+    }
+
+    @Override
+    public void download(String nepFile, OutputStream outputStream, Duration timeout, Function<FileDescriptor, Boolean> descriptorConsumer) {
+        try {
+            checkAvailability(nepFile, timeout, descriptorConsumer);
+            scp.execute(outputStream, getUrl(nepFile), "/dev/stdout");
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        } catch (CommandExecutor.BrokenPipe bp) {
+            log.debug(bp.getMessage());
+            throw bp;
+        } catch (RuntimeException rte) {
+            log.error(rte.getMessage(), rte);
+            throw rte;
+        }
+
+    }
+
+    protected String getUrl(String nepFile) {
+        return url + ":" + nepFile;
+    }
+
+
+    protected void checkAvailability(String nepFile, Duration timeout,  Function<FileDescriptor, Boolean> descriptorConsumer) throws IOException {
+        sshj.checkAvailabilityAndConsume(nepFile, timeout, descriptorConsumer, (handle) -> {});
+
+    }
+
+}
