dbe5e9752a57ce736973a8cd391c85a21e9304b5
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-05-23 09:35:50 +0000

    Details.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=112664

--- media-nep/src/main/java/nl/vpro/nep/service/impl/SSHClientFactory.java
+++ media-nep/src/main/java/nl/vpro/nep/service/impl/SSHClientFactory.java
@@ -21,28 +21,28 @@ final class SSHClientFactory {
     private static final Duration sshTimeout  = Duration.ofSeconds(300);
     private static final Duration sshConnectionTimeout  = Duration.ofSeconds(5);
 
+    /**
+     * @param hostKey the RSA host key or if containing a semicolon one of the fingerprints supported by sshj.
+     */
     static SSHClient create(String hostKey, String host, String username, String password) throws IOException {
 
         final DefaultConfig configuration = new DefaultConfig();
         configuration.setKeepAliveProvider(KeepAliveProvider.KEEP_ALIVE);
         final SSHClient ssh = new SSHClient(configuration);
         ssh.useCompression();
-        //ssh.loadKnownHosts();
 
-
-        if (hostKey.indexOf(':') > 0) {
+        if (hostKey.startsWith("MD5:") || hostKey.startsWith("SHA256:") || hostKey.startsWith("SHA1:")) {
+            // This is a fingerprint already
             ssh.addHostKeyVerifier(hostKey);
         } else {
-             byte[] keyBytes = Base64.decode(hostKey);
+            byte[] keyBytes = Base64.decode(hostKey);
             PublicKey key = new Buffer.PlainBuffer(keyBytes).readPublicKey();
             ssh.addHostKeyVerifier(SecurityUtils.getFingerprint(key));
         }
 
         ssh.setTimeout((int) sshTimeout.toMillis());
         ssh.setConnectTimeout((int) sshConnectionTimeout.toMillis());
-
         ssh.connect(host);
-
         ssh.authPassword(username, password);
 
         return ssh;
