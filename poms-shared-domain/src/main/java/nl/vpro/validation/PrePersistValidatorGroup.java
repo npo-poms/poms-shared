9144334f08982d86514395623145f6f3a4aafbfa
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-06-04 21:13:38 +0000

    Test case failures.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=112911

---
+++ poms-shared-domain/src/main/java/nl/vpro/validation/PrePersistValidatorGroup.java
@@ -0,0 +1,11 @@
+package nl.vpro.validation;
+
+/**
+ * Marks a javax.constraint as only relevant for the database layer.
+ *
+ *
+ * @author Michiel Meeuwissen
+ * @since 5.7
+ */
+public interface PrePersistValidatorGroup {
+}
