a80bf4fab361d500caf51219b8dbeab1171a6344
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-06-08 20:06:31 +0000

    MSE-4053
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=113016

--- media-nep/src/main/java/nl/vpro/nep/service/NEPDownloadService.java
+++ media-nep/src/main/java/nl/vpro/nep/service/NEPDownloadService.java
@@ -4,19 +4,20 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.time.Duration;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import nl.vpro.util.FileMetadata;
 
 public interface NEPDownloadService {
     void download(
         String nepFile,
-        OutputStream outputStream,
+        Supplier<OutputStream> outputStream,
         Duration timeout,
         Function<FileMetadata, Boolean> descriptorConsumer) throws IOException;
 
     default void download(
         String nepFile,
-        OutputStream outputStream,
+        Supplier<OutputStream> outputStream,
         Function<FileMetadata, Boolean> descriptorConsumer) throws IOException {
         download(nepFile, outputStream, Duration.ZERO, descriptorConsumer);
     }
