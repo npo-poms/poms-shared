47de0f76c037a69cd90ad3ff5c8fded49db77957
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-06-08 21:55:41 +0000

    MSE-4053
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=113020

--- media-nep/src/main/java/nl/vpro/nep/service/impl/NEPServiceImpl.java
+++ media-nep/src/main/java/nl/vpro/nep/service/impl/NEPServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.Iterator;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Provider;
@@ -67,7 +68,7 @@ public class NEPServiceImpl implements NEPService {
     }
 
     @Override
-    public void download(String nepFile, Supplier<OutputStream> outputStream, Duration timeout, Function<FileMetadata, Boolean> descriptorConsumer) throws IOException {
+    public void download(@Nonnull String nepFile, @Nonnull Supplier<OutputStream> outputStream, @Nonnull Duration timeout, Function<FileMetadata, Boolean> descriptorConsumer) throws IOException {
         nepftpDownloadService.get()
             .download(nepFile, outputStream, timeout, descriptorConsumer);
 
