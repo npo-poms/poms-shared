e6df4e3c9a410d4c3f6f8ee8c5290ae1a07c54b0
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-06-08 21:59:03 +0000

    Bunch of annotations.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=113021

--- media-nep/src/main/java/nl/vpro/nep/service/NEPTranscodeService.java
+++ media-nep/src/main/java/nl/vpro/nep/service/NEPTranscodeService.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.time.Instant;
 import java.util.Iterator;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import nl.vpro.nep.domain.workflow.StatusType;
 import nl.vpro.nep.domain.workflow.WorkflowExecution;
 import nl.vpro.nep.domain.workflow.WorkflowExecutionRequest;
@@ -16,7 +19,9 @@ import nl.vpro.nep.domain.workflow.WorkflowExecutionRequest;
  */
 public interface NEPTranscodeService {
 
-    WorkflowExecution transcode(WorkflowExecutionRequest request) throws IOException;
+    @Nonnull
+    WorkflowExecution transcode(@Nonnull WorkflowExecutionRequest request) throws IOException;
 
-    Iterator<WorkflowExecution> getTranscodeStatuses(String mid, StatusType status, Instant from, Long limit);
+    @Nonnull
+    Iterator<WorkflowExecution> getTranscodeStatuses(@Nullable String mid, @Nullable StatusType status, @Nullable Instant from, @Nullable  Long limit);
 }
