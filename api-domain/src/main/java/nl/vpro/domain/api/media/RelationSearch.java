55bb5590ad0cd3bfd3700b401d480029ead4a7f6
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-06-29 09:39:54 +0000

    ported from branch
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=113475

--- api-domain/src/main/java/nl/vpro/domain/api/media/RelationSearch.java
+++ api-domain/src/main/java/nl/vpro/domain/api/media/RelationSearch.java
@@ -4,11 +4,17 @@
  */
 package nl.vpro.domain.api.media;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
 
 import nl.vpro.domain.api.AbstractRelationSearch;
+import nl.vpro.domain.api.ExtendedTextMatcherList;
+import nl.vpro.domain.api.TextMatcherList;
 
 /**
  * @author Roelof Jan Koekoek
@@ -17,6 +23,15 @@ import nl.vpro.domain.api.AbstractRelationSearch;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "mediaRelationSearchType",
     propOrder = {"types", "broadcasters", "values", "uriRefs"})
+@NoArgsConstructor
 public class RelationSearch extends AbstractRelationSearch {
 
+    @lombok.Builder
+    protected RelationSearch(
+        @Valid TextMatcherList types,
+        @Valid TextMatcherList broadcasters,
+        @Valid ExtendedTextMatcherList values,
+        @Valid TextMatcherList uriRefs) {
+        super(types, broadcasters, values, uriRefs);
+    }
 }
