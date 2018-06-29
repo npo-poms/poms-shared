c10fb129e139c1ce9b2b479294e1a7442ca6ba7e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-06-29 14:57:51 +0000

    ported from branch
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=113495

--- api-domain/src/main/java/nl/vpro/domain/api/SearchableFacet.java
+++ api-domain/src/main/java/nl/vpro/domain/api/SearchableFacet.java
@@ -5,10 +5,19 @@
 package nl.vpro.domain.api;
 
 /**
+ * A searchable facet can have a 'sub search'. This is needed as the facet is not on precisely one field, but on some
+ * selection of it.
+ *
+ * See e.g. http://wiki.publiekeomroep.nl/display/npoapi/Media-+and+Schedule-API#Media-andSchedule-API-relations
+ *
+ * Another example are genres, you may be interested in just the genres starting with 3.0.1 or so.
+ *
+ * In other words this has no influence on the number of documents the faceting is applied, this in contraction to filtering {@link Facet#getFilter()}
+ *
  * @author Roelof Jan Koekoek
  * @since 3.3
  */
-public interface SearchableFacet<T extends AbstractSearch>  {
+public interface SearchableFacet<T extends AbstractSearch> { //},  F extends AbstractSearch>  extends Facet<F> {
     boolean hasSubSearch();
 
     T getSubSearch();
