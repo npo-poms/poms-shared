6772ad9160d3e90f6ff094b7795ec22e13d12956
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-07-03 10:24:24 +0000

    MSE-4077
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=113529

--- media-nep/src/test/java/nl/vpro/nep/service/impl/NEPFTPUploadServiceImplTest.java
+++ media-nep/src/test/java/nl/vpro/nep/service/impl/NEPFTPUploadServiceImplTest.java
@@ -5,8 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 
 import org.junit.Before;
 import org.junit.Ignore;
@@ -25,6 +31,9 @@ import nl.vpro.logging.simple.Slf4jSimpleLogger;
 public class NEPFTPUploadServiceImplTest {
 
     private NEPFTPUploadServiceImpl impl;
+
+    private String file1 = "/Users/michiel/npo/media/huge1.mp4";
+    private String file2 = "/Users/michiel/npo/media/huge2.mp4";
 
 
     @Before
@@ -48,12 +57,54 @@ public class NEPFTPUploadServiceImplTest {
 
 
     @Test
-    @Ignore("This actually does something")
+    //@Ignore("This actually does something")
     public void uploadHuge() throws Exception {
         Instant start = Instant.now();
-        File file = new File("/Users/michiel/Downloads/npo-1dvr__2018-03-16T062112392-2018-03-16T084607989.mp4");
+        File file = new File(file1);
         String filename = "test.1235";
         impl.upload(new Slf4jSimpleLogger(log), filename, file.length(), new FileInputStream(file));
         log.info("Took {}", Duration.between(start, Instant.now()));
+    }
+
+    @Test
+
+    public void async() {
+        List<ForkJoinTask> tasks = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            tasks.add(ForkJoinPool.commonPool().submit(copy(new File(file1), "test." + i)));
+
+        }
+        tasks.forEach((t) -> {
+            try {
+                t.get();
+            } catch (InterruptedException | ExecutionException e) {
+                log.error(e.getMessage(), e);
+            }
+        });
+        tasks.clear();
+        for (int i = 0; i < 10; i++) {
+            tasks.add(ForkJoinPool.commonPool().submit(copy(new File(file1), "test." + i)));
+        }
+        tasks.forEach((t) -> {
+            try {
+                t.get();
+            } catch (InterruptedException | ExecutionException e) {
+                log.error(e.getMessage(), e);
+            }
+        });
+    }
+
+    public Runnable copy(File from, String to) {
+        return () -> {
+            Instant start = Instant.now();
+            File file = from;
+            String filename = to;
+            try {
+                impl.upload(new Slf4jSimpleLogger(log), filename, file.length(), new FileInputStream(file));
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
+            }
+            log.info("Took {}", Duration.between(start, Instant.now()));
+        };
     }
 }
