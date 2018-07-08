e08a282693d18f6c9a7c2fda4905f8c0c374c2c0
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-07-08 20:40:24 +0000

    MSE-4082
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=113619

---
+++ media-domain/src/main/java/nl/vpro/domain/media/MediaObjectLocker.java
@@ -0,0 +1,101 @@
+package nl.vpro.domain.media;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.time.Duration;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Semaphore;
+import java.util.function.Supplier;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+import nl.vpro.services.TransactionService;
+
+/**
+ * Tool to make sure that the 'authority' related dropboxes don't run at the same time for the same mid.
+ *
+ * It may be better to introduce more decent hibernate locking (MSE-3751), in which case this class can be removed again.
+ *
+ * @author Michiel Meeuwissen
+ * @since 5.5
+ */
+@Slf4j
+public class MediaObjectLocker {
+
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Mid {
+        int argNumber() default 0;
+        String reason() default "midlock annotation";
+    }
+
+    public static <T> T runAlone(TransactionService transactionService, String mid, String reason, Callable<T> callable) {
+        return runAlone(
+            mid,
+            reason,
+            () -> transactionService.executeInNewTransaction(callable));
+    }
+
+
+    public static <T> T getAlone(String mid, String reason, Supplier<T> callable) {
+        return runAlone(mid, reason, callable::get);
+    }
+
+
+    @SneakyThrows
+    public static <T> T runAlone(String mid, String reason, Callable<T> callable) {
+        Long nanoStart = System.nanoTime();
+        Semaphore semaphore = get(mid);
+        try {
+            if (semaphore.hasQueuedThreads()) {
+                log.info("There are already threads for {}, waiting", mid);
+            }
+            semaphore.acquire();
+            Duration aquireTime = Duration.ofNanos(System.nanoTime() - nanoStart);
+            log.debug("Acquired lock for {}  ({}) in {}", mid, reason, aquireTime);
+
+            return callable.call();
+        } finally {
+            release(semaphore, mid);
+            log.debug("Released lock for {} ({}) in {}", mid, reason, Duration.ofNanos(System.nanoTime() - nanoStart));
+
+        }
+    }
+
+
+
+    private static LoadingCache<String, Semaphore> LOCKED_MEDIA = CacheBuilder.newBuilder()
+        .build(
+            new CacheLoader<String, Semaphore>() {
+                @Override
+                public Semaphore load(String mid) {
+                    return new Semaphore(1);
+                }
+            }
+        );
+
+    private static Semaphore get(String mid) {
+        if (mid == null) {
+            log.warn("Called with mid null!");
+            return new Semaphore(1);
+        }
+        try {
+            return LOCKED_MEDIA.get(mid);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static void release(Semaphore semaphore, String mid) {
+        if (mid != null) {
+            LOCKED_MEDIA.invalidate(mid);
+        }
+        semaphore.release();
+
+    }
+
+}
