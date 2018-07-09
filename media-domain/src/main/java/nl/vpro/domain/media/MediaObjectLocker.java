c74b8efdf127bc6bb1f355162a285292d7f12ad0
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-07-09 20:00:15 +0000

    MSE-4082
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=113657

--- media-domain/src/main/java/nl/vpro/domain/media/MediaObjectLocker.java
+++ media-domain/src/main/java/nl/vpro/domain/media/MediaObjectLocker.java
@@ -1,18 +1,25 @@
 package nl.vpro.domain.media;
 
+import lombok.Getter;
+import lombok.Lombok;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.management.ManagementFactory;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
 
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
@@ -23,22 +30,59 @@ import nl.vpro.services.TransactionService;
 /**
  * Tool to make sure that the 'authority' related dropboxes don't run at the same time for the same mid.
  *
- * It may be better to introduce more decent hibernate locking (MSE-3751), in which case this class can be removed again.
+ * It may be better to (also) introduce more decent hibernate locking (MSE-3751)
  *
  * @author Michiel Meeuwissen
  * @since 5.5
  */
 @Slf4j
-public class MediaObjectLocker {
+public class MediaObjectLocker implements MediaObjectLockerMXBean {
 
-    private static ThreadLocal<Map<String, AtomicInteger>> mids = ThreadLocal.withInitial(HashMap::new);
+    private static ThreadLocal<Map<String, AtomicInteger>> MIDS = ThreadLocal.withInitial(HashMap::new);
+
+
+    private static LoadingCache<String, Semaphore> LOCKED_MEDIA = CacheBuilder.newBuilder()
+        .build(
+            new CacheLoader<String, Semaphore>() {
+                @Override
+                public Semaphore load(String mid) {
+                    return new Semaphore(1);
+                }
+            }
+        );
+
+    private static final MediaObjectLocker instance = new MediaObjectLocker();
+
+    @Getter
+    private int lockCount = 0;
+    @Getter
+    private int currentCount = 0;
+    @Getter
+    private int maxConcurrency = 0;
+    @Getter
+    private int currentConcurrency = 0;
+
+
+    static {
+        try {
+            MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+            mbs.registerMBean(instance, new ObjectName("nl.vpro.media:name=mediaobjectLocker"));
+        } catch (Throwable t) {
+            throw Lombok.sneakyThrow(t);
+        }
+    }
+
+    @Override
+    public Set<String> getLocks() {
+        return LOCKED_MEDIA.asMap().keySet();
+    }
 
 
     /**
      * Adding this annotation of a method with a string or mediaidentifiable object will 'lock' the identifier, and will make sure
      * that not other code doing the same will run simultaneously.
      *
-     * Much code like this will be get a miediaobject using this mid, change it and then commit the mediaobject.
+     * Much code like this will be get a mediaobject using this mid, change it and then commit the mediaobject.
      *
      * If another thread is changing the mediaobject in between those event, those changes will be lost.
      *
@@ -48,7 +92,7 @@ public class MediaObjectLocker {
     @Retention(RetentionPolicy.RUNTIME)
     public @interface Mid {
         int argNumber() default 0;
-        String reason() default "midlock annotation";
+        String reason() default "";
     }
 
     public static <T> T runAlone(TransactionService transactionService, String mid, String reason, Callable<T> callable) {
@@ -67,10 +111,13 @@ public class MediaObjectLocker {
     @SneakyThrows
     public static <T> T runAlone(String mid, String reason, Callable<T> callable) {
         Long nanoStart = System.nanoTime();
-        AtomicInteger integer = mids.get().computeIfAbsent(mid, (m) -> new AtomicInteger(0));
+        AtomicInteger integer = MIDS.get().computeIfAbsent(mid, (m) -> new AtomicInteger(0));
         boolean outer = integer.incrementAndGet() == 1;
+        instance.maxConcurrency = Math.max(integer.intValue(), instance.maxConcurrency);
         try {
             if (outer) {
+                instance.lockCount++;
+                instance.currentCount++;
                 Semaphore semaphore = get(mid);
                 try {
                     if (semaphore.hasQueuedThreads()) {
@@ -96,22 +143,14 @@ public class MediaObjectLocker {
                 assert get == 0;
             }
             if (get == 0) {
-                mids.get().remove(mid);
+                instance.currentCount--;
+                MIDS.get().remove(mid);
             }
         }
     }
 
 
 
-    private static LoadingCache<String, Semaphore> LOCKED_MEDIA = CacheBuilder.newBuilder()
-        .build(
-            new CacheLoader<String, Semaphore>() {
-                @Override
-                public Semaphore load(String mid) {
-                    return new Semaphore(1);
-                }
-            }
-        );
 
     private static Semaphore get(String mid) {
         if (mid == null) {
