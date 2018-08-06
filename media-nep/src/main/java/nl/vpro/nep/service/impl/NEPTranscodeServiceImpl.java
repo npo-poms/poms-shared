60ac2661c7290078f6a1707ccd22d8aa648db1da
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-08-06 09:23:41 +0000

    MSE-4106
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=114349

--- media-nep/src/main/java/nl/vpro/nep/service/impl/NEPTranscodeServiceImpl.java
+++ media-nep/src/main/java/nl/vpro/nep/service/impl/NEPTranscodeServiceImpl.java
@@ -118,6 +118,7 @@ public class NEPTranscodeServiceImpl implements NEPTranscodeService {
         if (!request.getFilename().startsWith(ftpUserName)) {
             log.warn("The file given in {} does not start with ftp user name {}", request, ftpUserName);
         }
+        log.info("Transcode request {}", request);
         HttpResponse response = client.execute(httpPost, clientContext);
 
         if (response.getStatusLine().getStatusCode() >= 300) {
