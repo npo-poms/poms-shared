59429a06122c41e5582bd5a98487f19562f550af
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-08-07 20:48:20 +0000

    NPA-438, NPA-446
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-5.8-SNAPSHOT/; revision=114391

--- api-domain/src/main/java/nl/vpro/domain/api/SearchableLimitableFacet.java
+++ api-domain/src/main/java/nl/vpro/domain/api/SearchableLimitableFacet.java
@@ -4,5 +4,5 @@ package nl.vpro.domain.api;
  * @author Michiel Meeuwissen
  * @since 5.8
  */
-public interface SearchableLimitableFacet<T extends AbstractSearch, S extends AbstractSearch>  extends LimitableFacet<T>, SearchableFacet<T, S> {
+public interface SearchableLimitableFacet<F extends AbstractSearch, S extends AbstractSearch>  extends LimitableFacet<F>, SearchableFacet<F, S> {
 }
