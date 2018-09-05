9aedf40e3146bac643f3f00fd15d968e30b0a354
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-09-05 09:56:09 +0000

    backport MSE-4177 MSE-4171
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-5.8-SNAPSHOT/; revision=114928

--- media-nep/src/main/java/nl/vpro/nep/service/NEPDownloadService.java
+++ media-nep/src/main/java/nl/vpro/nep/service/NEPDownloadService.java
@@ -7,6 +7,7 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import nl.vpro.util.FileMetadata;
 
@@ -17,12 +18,14 @@ public interface NEPDownloadService {
      * Before that, you the descriptorConsumer will be called. It if return false, the copying will not happen
      *
      * @throws IllegalStateException If the file didn't appear in time
+     * @param descriptorConsumer If the file is found, and this is not <code>null</code> it's descriptor will be fed to this function
+     *                           You can use it for logging only and return {@link Proceed#TRUE} always. You can also return something else.
      */
     void download(
         @Nonnull String nepFile,
         @Nonnull Supplier<OutputStream> outputStream,
         @Nonnull Duration timeout,
-        Function<FileMetadata, Boolean> descriptorConsumer) throws IOException;
+        @Nullable Function<FileMetadata, Proceed> descriptorConsumer) throws IOException;
 
 
     /**
@@ -31,7 +34,23 @@ public interface NEPDownloadService {
     default void download(
         @Nonnull String nepFile,
         @Nonnull  Supplier<OutputStream> outputStream,
-        @Nonnull Function<FileMetadata, Boolean> descriptorConsumer) throws IOException {
+        @Nonnull Function<FileMetadata, Proceed> descriptorConsumer) throws IOException {
         download(nepFile, outputStream, Duration.ZERO, descriptorConsumer);
+    }
+
+
+    enum Proceed {
+        /**
+         * The file is ok, simply proceed downloading it.
+         */
+        TRUE,
+        /**
+         * The file is not ok, it may be to old or different than expected. Wait a bit and retry this.
+         */
+        RETRY,
+        /**
+         * The file is not ok, it may be to old or different than expected. There is no point in waiting.
+         */
+        FALSE
     }
 }
