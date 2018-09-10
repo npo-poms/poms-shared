492a0bbfa412c424dcd832caa2d098a71d448fff
Eric Kuijt <erickuytb@gmail.com>
2018-09-10 11:48:32 +0000

    Accidently found a typo
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=114995

--- media-nep/src/test/java/nl/vpro/nep/service/impl/NEPCurlDownloadServiceImplITest.java
+++ media-nep/src/test/java/nl/vpro/nep/service/impl/NEPCurlDownloadServiceImplITest.java
@@ -30,7 +30,7 @@ public class NEPCurlDownloadServiceImplITest {
             "***REMOVED***",
             "AAAAB3NzaC1yc2EAAAADAQABAAABAQCV4gmmgKyPVyOyZv1jdVpu/KzS9w2v4/vxDeKbuXvl0tldvDAmMi/QY1XvLueuZJy8PmilpGj6po1JuU0V2RGX/Js18b9lyCAQptdaeUk45lYvM8bpGfkzB509i3+CaM6U1onEIftFs4vzDLMwHrZQ6kdlRGGs6bLYy1vpqs7h6mO/XGDeLLVpjLPZbz/TrWt98kinn+Rg/TwYV0VNyqac5DkpWtFEUucIrq6zZs1q3Pw8YHMo02BWlWXFR/yi41ODb+RH1dTlZEs3vrMgwFvVD5c+4EKy1hZ65SJ6xVXwaMyN4w1LaHLwwe3K8rNDS+m5gyaswhdeZthqDiXysFwj",
             //"94:06:26:d5:e4:f5:18:b5:52:a9:19:b1:97:db:94:9e"
-            Arrays.asList("/usr/local/opt/curl/bin/curl", "/opt/local/bin/curl,/usr/bin/curl")
+            Arrays.asList("/usr/local/opt/curl/bin/curl", "/opt/local/bin/curl", "/usr/bin/curl")
             );
         FileOutputStream outputStream = new FileOutputStream("/tmp/test.mp4");
         final long size[] = {-1L};
