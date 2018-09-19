671b0babe0683a95382dea68f2da3d04dfabd2eb
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-09-19 11:16:15 +0000

    small backport
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-5.8-SNAPSHOT/; revision=115198

--- poms-shared-domain/src/main/java/nl/vpro/domain/Changes.java
+++ poms-shared-domain/src/main/java/nl/vpro/domain/Changes.java
@@ -1,11 +1,14 @@
 package nl.vpro.domain;
 
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Iterator;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 import nl.vpro.util.CloseableIterator;
 
@@ -13,14 +16,40 @@ import nl.vpro.util.CloseableIterator;
  * @author Michiel Meeuwissen
  * @since 5.1
  */
-@AllArgsConstructor
 @Getter
 @Setter
 public class Changes<T> implements AutoCloseable, Iterable<Change<T>> {
 
     private final Instant until;
+    private final Instant since;
     private final Long count;
     private final CloseableIterator<? extends Change<T>> iterator;
+
+
+    @Getter(AccessLevel.NONE)
+    private final Supplier<Instant> eta;
+
+    @lombok.Builder
+    public Changes(
+        Instant since,
+        Instant until,
+        Long count,
+        CloseableIterator<? extends Change<T>> iterator,
+        Supplier<Instant> eta) {
+        this.since = since;
+        this.until = until;
+        this.count = count;
+        this.iterator = iterator;
+        this.eta = eta == null ? () -> null : eta;
+    }
+
+    public Changes(
+        Instant since,
+        Instant until,
+        Long count,
+        CloseableIterator<? extends Change<T>> iterator) {
+        this(since, until, count, iterator, null);
+    }
 
     @Override
     public void close() throws Exception {
@@ -29,6 +58,10 @@ public class Changes<T> implements AutoCloseable, Iterable<Change<T>> {
     @Override
     public Iterator<Change<T>> iterator() {
         return (Iterator<Change<T>>) iterator;
+    }
+
+    public Optional<Instant> getETA() {
+        return Optional.ofNullable(this.eta.get()).map((i) -> i.truncatedTo(ChronoUnit.MINUTES));
     }
 
     @Override
