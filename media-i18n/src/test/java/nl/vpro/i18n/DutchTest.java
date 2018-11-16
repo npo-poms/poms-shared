e1c9342f093970cb13dd080ab8cb76bc8eafc18b
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-11-16 09:11:42 +0000

    Smarter dates
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=116129

---
+++ media-i18n/src/test/java/nl/vpro/i18n/DutchTest.java
@@ -0,0 +1,31 @@
+package nl.vpro.i18n;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since ...
+ */
+public class DutchTest {
+
+
+    @Test
+    public void formatInstantSmartly() {
+        ZonedDateTime now = LocalDateTime.of(2018, 11, 16, 10, 0).atZone(Dutch.ZONE_ID);
+
+
+        assertThat(Dutch.formatInstantSmartly(now, LocalDateTime.of(2018, 11, 16, 13, 0).atZone(Dutch.ZONE_ID))).isEqualTo("13:00");
+
+        assertThat(Dutch.formatInstantSmartly(now, LocalDateTime.of(2018, 12, 16, 13, 0).atZone(Dutch.ZONE_ID))).isEqualTo("16 december 13:00");
+
+        assertThat(Dutch.formatInstantSmartly(now, LocalDateTime.of(2019, 12, 16, 13, 0).atZone(Dutch.ZONE_ID))).isEqualTo("16 december 2019 13:00");
+
+
+    }
+
+}
