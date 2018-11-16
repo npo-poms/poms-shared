8830232032df6d6494d8b60a5044a921ab20c3de
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-11-16 16:26:37 +0000

    MSE-4262
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=116152

---
+++ media-nep/src/test/java/nl/vpro/nep/domain/NEPItemizeResponseTest.java
@@ -0,0 +1,27 @@
+package nl.vpro.nep.domain;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import nl.vpro.jackson2.Jackson2Mapper;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since ...
+ */
+@Slf4j
+public class NEPItemizeResponseTest {
+
+    @Test
+    public void unbind() throws IOException {
+
+
+
+        NEPItemizeResponse response = Jackson2Mapper.getLenientInstance().readValue("{'id': 'aaa'}", NEPItemizeResponse.class);
+        log.info("" + response.getId());
+    }
+
+}
