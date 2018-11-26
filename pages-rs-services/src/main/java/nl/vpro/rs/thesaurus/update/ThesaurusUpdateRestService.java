347093100e36485e31603497bcdfa1f0245be73d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-11-26 11:18:17 +0000

    javax.ws.rs annotations.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=116271

--- pages-rs-services/src/main/java/nl/vpro/rs/thesaurus/update/ThesaurusUpdateRestService.java
+++ pages-rs-services/src/main/java/nl/vpro/rs/thesaurus/update/ThesaurusUpdateRestService.java
@@ -5,10 +5,7 @@
 package nl.vpro.rs.thesaurus.update;
 
 import javax.validation.constraints.NotNull;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 import nl.vpro.domain.media.gtaa.GTAANewPerson;
@@ -31,11 +28,11 @@ public interface ThesaurusUpdateRestService {
     @POST
     @Path("/person")
     @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    GTAAPerson submitSignedPerson(String jws, @NotNull GTAANewPerson person);
+    GTAAPerson submitSignedPerson(@HeaderParam("authentication") String jws, @NotNull GTAANewPerson person);
 
     @POST
     @Path("/item")
     @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    ThesaurusObject submitSignedConcept(String jws, @NotNull GTAANewThesaurusObject thesaurusObject);
+    ThesaurusObject submitSignedConcept(@HeaderParam("authentication") String jws, @NotNull GTAANewThesaurusObject thesaurusObject);
 
 }
