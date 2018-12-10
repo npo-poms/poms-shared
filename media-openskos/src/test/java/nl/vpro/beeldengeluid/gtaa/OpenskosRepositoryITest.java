fae4bec0719a22353a033e5e2f202923f6a158be
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-12-10 13:25:34 +0000

    Cleaning up a bit.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=116629

--- media-openskos/src/test/java/nl/vpro/beeldengeluid/gtaa/OpenskosRepositoryITest.java
+++ media-openskos/src/test/java/nl/vpro/beeldengeluid/gtaa/OpenskosRepositoryITest.java
@@ -201,21 +201,24 @@ public class OpenskosRepositoryITest {
         //OpenskosRepository impl = new OpenskosRepository("http://accept.openskos.beeldengeluid.nl.pictura-dp.nl/", "1dX1nJHX5GNeT8O7", template);
 
         // test
-        OpenskosRepository impl = new OpenskosRepository("http://test.openskos.beeldengeluid.nl.pictura-dp.nl/", "1dX1nJHX5GNeT8O7", template);
-        impl.setTenant("beng");
-        impl.setPersonsSpec("beng:gtaa:138d0e62-d688-e289-f136-05ad7acc85a2");
-        //impl.setPersonsSpec("beng:gtaa:8fcb1c4f-663d-00d3-95b2-cccd5abda352");
+//        OpenskosRepository impl = new OpenskosRepository("http://test.openskos.beeldengeluid.nl.pictura-dp.nl/", "1dX1nJHX5GNeT8O7", template);
 
-        impl.setUseXLLabels(true);
+
         // Acceptatie
         //GTAARepositoryImpl impl = new GTAARepositoryImpl("http://accept-v1.openskos.beeldengeluid.nl.pictura-dp.nl/", "1dX1nJHX5GNeT8O7", template);
 
         // productie
-        //GTAARepositoryImpl impl = new GTAARepositoryImpl("http://openskos.beeldengeluid.nl/", "1dX1nJHX5GNeT8O7", template);
+        OpenskosRepository impl = new OpenskosRepository("http://openskos.beeldengeluid.nl/", "1dX1nJHX5GNeT8O7", template);
+
+        impl.setUseXLLabels(true);
 
         // dev
 
         //GTAARepositoryImpl impl = new GTAARepositoryImpl("http://accept-v1.openskos.beeldengeluid.nl.pictura-dp.nl/", "1dX1nJHX5GNeT8O7", template);
+
+        impl.setTenant("beng");
+        impl.setPersonsSpec("beng:gtaa:138d0e62-d688-e289-f136-05ad7acc85a2");
+        //impl.setPersonsSpec("beng:gtaa:8fcb1c4f-663d-00d3-95b2-cccd5abda352");
 
         impl.init();
         /* Acceptatie */
