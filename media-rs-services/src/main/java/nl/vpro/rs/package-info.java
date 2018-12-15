a079cfc76d1e286f8cc6dca78df8a63c922267e5
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-12-15 15:15:50 +0000

    Moved Error class to make it available to clients.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=116742

---
+++ media-rs-services/src/main/java/nl/vpro/rs/package-info.java
@@ -0,0 +1,18 @@
+@XmlSchema(
+    namespace = Xmlns.UPDATE_NAMESPACE,
+    xmlns =
+        {
+            @XmlNs(prefix = "",      namespaceURI = Xmlns.UPDATE_NAMESPACE),
+            @XmlNs(prefix = "media", namespaceURI = Xmlns.MEDIA_NAMESPACE),
+            @XmlNs(prefix = "shared",namespaceURI = Xmlns.SHARED_NAMESPACE)
+
+        },
+    elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED
+
+)
+package nl.vpro.rs;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlSchema;
+
+import nl.vpro.domain.Xmlns;
