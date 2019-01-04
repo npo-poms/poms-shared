a3b25ac15b1aebad39ed2e15ec21b223ec76d117
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-01-04 12:21:01 +0000

    PMP-21
    svn path=/nl/publiekeomroep/poms-shared/branches/REL-5.4-SNAPSHOT/; revision=117037

--- media-domain/src/main/java/nl/vpro/domain/media/MediaObject.java
+++ media-domain/src/main/java/nl/vpro/domain/media/MediaObject.java
@@ -1792,10 +1792,8 @@ public abstract class MediaObject extends PublishableObject
                         if (location.getPlatform() == prediction.getPlatform()
                                 && Workflow.PUBLICATIONS.contains(location.getWorkflow())
                                 && prediction.inPublicationWindow(Instant.now())) {
-                            log.info("Silentely set state of {} to REALIZED (by {}) of object {}", prediction,
+                            log.warn("The state of {} could have been REALIZED (by {}) of object {}", prediction,
                                     location.getProgramUrl(), MediaObject.this.mid);
-                            prediction.setState(Prediction.State.REALIZED);
-                            MediaObjects.markForRepublication(MediaObject.this, "realized prediction");
                             break;
                         }
                     }
