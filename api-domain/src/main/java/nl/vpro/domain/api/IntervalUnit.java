11ea0f0f63d282a56a1f2e051c991ea270d4d1c0
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-01-04 10:54:10 +0000

    Comments/TODO/cleanup.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=117036

--- api-domain/src/main/java/nl/vpro/domain/api/IntervalUnit.java
+++ api-domain/src/main/java/nl/vpro/domain/api/IntervalUnit.java
@@ -1,5 +1,7 @@
 package nl.vpro.domain.api;
 
+import lombok.Getter;
+
 import java.time.temporal.ChronoField;
 
 /**
@@ -13,23 +15,23 @@ public enum IntervalUnit {
     WEEK(ChronoField.ALIGNED_WEEK_OF_YEAR, "xxxx-'W'w"),
     DAY(ChronoField.DAY_OF_YEAR, "yyyy-MM-dd"),
     HOUR(ChronoField.HOUR_OF_DAY, "yyyy-MM-dd'T'HH:mm:ss'Z'"),
-    MINUTE(ChronoField.MINUTE_OF_DAY, "yyyy-MM-dd'T'HH:mm:ss'Z'");
+    MINUTE(ChronoField.MINUTE_OF_DAY, "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    ;
 
+    @Getter
     private final ChronoField chronoField;
 
+
+    /**
+     * The format string to format an datetime value . This is used to communicate to ES.
+     * TODO: Since ES uses joda-time syntax this is joda-time  (only relevant for WEEK). I'd say we don't want joda-time but javax.time, so I think xxxx should be YYYY?
+     *
+     */
+    @Getter
     private final String format;
 
     IntervalUnit(ChronoField chronoField, String format) {
         this.chronoField = chronoField;
         this.format = format;
-    }
-
-
-    public ChronoField getChronoField() {
-        return chronoField;
-    }
-
-    public String getFormat() {
-        return format;
     }
 }
