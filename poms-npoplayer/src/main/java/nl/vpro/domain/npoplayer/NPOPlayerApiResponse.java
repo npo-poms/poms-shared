76a502536b8c173b876e46a5f4fa8a759aebb875
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-01-23 13:54:48 +0000

    MSE-4195
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=117469

---
+++ poms-npoplayer/src/main/java/nl/vpro/domain/npoplayer/NPOPlayerApiResponse.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2018 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.domain.npoplayer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * @author r.jansen
+ */
+@XmlRootElement
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlType(propOrder = {
+    "token",
+    "embedUrl",
+    "embedCode"
+})
+public class NPOPlayerApiResponse {
+    private String token;
+    private String embedUrl;
+    private String embedCode;
+}
---
+++ poms-npoplayer/src/main/java/nl/vpro/rs/npoplayer/NPOPlayerRestService.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2018 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.rs.npoplayer;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import nl.vpro.domain.npoplayer.PlayerRequest;
+import nl.vpro.domain.npoplayer.PlayerResponse;
+
+/**
+ * @author r.jansen
+ */
+@Path("/npoplayer")
+@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+public interface NPOPlayerRestService {
+
+    @POST
+    @Path("/request")
+    PlayerResponse forMid(PlayerRequest request);
+}
