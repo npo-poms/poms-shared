6cc37ecd976a2e5c9bddbc35c15a865f6061a929
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-01-23 13:54:48 +0000

    MSE-4195
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=117469

---
+++ poms-npoplayer/src/main/java/nl/vpro/domain/npoplayer/NPOPlayerTopSpin.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2018 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.domain.npoplayer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.XmlType;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+/**
+ * @author e.kuijt
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlType(propOrder = {
+    "partyId",
+    "profileId"
+})
+@JsonTypeName("comscore")
+public class NPOPlayerTopSpin {
+    String partyId;
+    String profileId;
+}
---
+++ poms-npoplayer/src/main/java/nl/vpro/domain/npoplayer/PlayerRequest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2018 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.domain.npoplayer;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * @author r.jansen
+ */
+@XmlRootElement
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlType(propOrder = {
+    "mid",
+    "id",
+    "stylesheet",
+    "autoplay",
+    "startAt",
+    "endAt",
+    "noAds",
+    "subtitleLanguage"
+})
+@JsonTypeName("request")
+public class PlayerRequest {
+    String mid;
+    String id;
+    String stylesheet;
+    Boolean autoplay;
+    Integer startAt;
+    Integer endAt;
+    Boolean noAds;
+    String subtitleLanguage;
+    String sterReferralUrl;
+    String sterSiteId;
+    String sterIdentifier;
+    Boolean hasAdConsent;
+    String pageUrl;
+    String atInternetSiteId;
+}
