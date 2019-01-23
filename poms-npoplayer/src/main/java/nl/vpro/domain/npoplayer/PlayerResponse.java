48bffaa1f9864f4c8e78518cbbe50b06d591fd32
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-01-23 14:14:39 +0000

    MSE-4195
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=117474

--- poms-npoplayer/src/main/java/nl/vpro/domain/npoplayer/PlayerResponse.java
+++ poms-npoplayer/src/main/java/nl/vpro/domain/npoplayer/PlayerResponse.java
@@ -4,21 +4,21 @@
  */
 package nl.vpro.domain.npoplayer;
 
-import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
 /**
  * @author r.jansen
  */
 @XmlRootElement
 @Data
-@Builder
+@lombok.Builder(builderClassName = "Builder")
 @NoArgsConstructor
 @AllArgsConstructor
 @XmlType(propOrder = {
@@ -33,4 +33,13 @@ public class PlayerResponse {
     private String token;
     private String embedUrl;
     private String embedCode;
+
+    public static class Builder {
+        public Builder from(NPOPlayerApiResponse response) {
+            return
+                token(response.getToken())
+                    .embedUrl(response.getEmbedUrl())
+                    .embedCode(response.getEmbedCode());
+        }
+    }
 }
