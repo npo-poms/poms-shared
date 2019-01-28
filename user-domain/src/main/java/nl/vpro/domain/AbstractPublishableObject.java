c21ab89cf6a450c2a9bf29b5c8a96049ce7e70f5
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-01-28 14:39:43 +0000

    Removed conflicting annotations.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=117572

--- user-domain/src/main/java/nl/vpro/domain/AbstractPublishableObject.java
+++ user-domain/src/main/java/nl/vpro/domain/AbstractPublishableObject.java
@@ -31,7 +31,6 @@ import nl.vpro.xml.bind.InstantXmlAdapter;
  * Abstract publishable object is an abstract implemention of {@link Accountable} and {@link Embargo}.
  *
  */
-@MappedSuperclass
 @Inheritance(strategy = InheritanceType.JOINED)
 @EmbargoValidation(groups = {PomsValidatorGroup.class})
 @XmlAccessorType(XmlAccessType.NONE)
