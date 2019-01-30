8f6acddc6ae12a90ad4189cb72c4061a8ee8299a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-01-30 11:41:00 +0000

    Added comments/documentation about the subtitle types.
    svn path=/nl/publiekeomroep/poms-shared/trunk/; revision=117637

--- poms-shared-domain/src/main/java/nl/vpro/domain/subtitles/SubtitlesType.java
+++ poms-shared-domain/src/main/java/nl/vpro/domain/subtitles/SubtitlesType.java
@@ -14,7 +14,17 @@ import nl.vpro.domain.Xmlns;
 @XmlEnum
 @XmlType(name = "subtitlesTypeEnum", namespace = Xmlns.SHARED_NAMESPACE)
 public enum SubtitlesType {
+    /**
+     * The subtitles represent a textual version of what is spoken or wat is happening. They are expected to be in the same language as the video itself. Teletekst 888 subtitles are captions.
+     */
     CAPTION,
+    /**
+     * The subtitles represent a translation. They are expected to be in a different language than the main language that can be heard
+     */
     TRANSLATION,
+    /**
+     * The subtitles represent a precise or automatic version of what is being said
+     * TODO I think it is a bit unclear what this means. TT 889?
+     */
     TRANSCRIPT
 }
