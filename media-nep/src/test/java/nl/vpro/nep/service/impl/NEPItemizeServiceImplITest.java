75519cc6f4a644ec07b6813003426c60a61aea71
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-02-13 17:02:44 +0100

    Also adding tests for dvr itemizing.

--- media-nep/src/test/java/nl/vpro/nep/service/impl/NEPItemizeServiceImplITest.java
+++ media-nep/src/test/java/nl/vpro/nep/service/impl/NEPItemizeServiceImplITest.java
@@ -27,8 +27,34 @@ public class NEPItemizeServiceImplITest {
         NEPItemizeServiceImpl itemizer = new NEPItemizeServiceImpl(NEPTest.PROPERTIES);
         NEPItemizeRequest request = new NEPItemizeRequest();
         request.setIdentifier("AT_2073522");
-        request.setStarttime("00:00:00.000");
-        request.setEndtime("00:02:21.151");
+        request.setStarttime(NEPItemizeRequest.fromDuration(Duration.ZERO).orElseThrow(IllegalArgumentException::new));
+        request.setEndtime(NEPItemizeRequest.fromDuration(Duration.ofMinutes(2).plusSeconds(21).plusMillis(151)).orElseThrow(IllegalAccessError::new));
+        NEPItemizeResponse response = itemizer.itemize(request);
+        log.info("response: {} {}", response, start);
+
+        NEPDownloadService downloadService = new NEPScpDownloadServiceImpl(NEPTest.PROPERTIES);
+        File dest = new File("/tmp", "dest.mp4");
+        downloadService.download(response.getOutput_filename(), () -> {
+            try {
+                return new FileOutputStream(dest);
+            } catch (IOException ioe) {
+                throw new RuntimeException(ioe);
+            }
+        }, Duration.ofMinutes(10), (fm) -> {
+            log.info("Found {}", fm);
+            return NEPDownloadService.Proceed.TRUE;
+        });
+        log.info("Found {} bytes", dest.length());
+    }
+
+    @Test
+    public void itemizeDvr() throws IOException {
+        Instant start = Instant.now();
+        NEPItemizeServiceImpl itemizer = new NEPItemizeServiceImpl(NEPTest.PROPERTIES);
+        NEPItemizeRequest request = new NEPItemizeRequest();
+        request.setIdentifier("'npo-1dvr");
+        request.setStarttime(NEPItemizeRequest.fromInstant(Instant.now().minusSeconds(300)).orElseThrow(IllegalArgumentException::new));
+        request.setEndtime(NEPItemizeRequest.fromInstant(Instant.now().minusSeconds(60)).orElseThrow(IllegalArgumentException::new));
         NEPItemizeResponse response = itemizer.itemize(request);
         log.info("response: {} {}", response, start);
 
