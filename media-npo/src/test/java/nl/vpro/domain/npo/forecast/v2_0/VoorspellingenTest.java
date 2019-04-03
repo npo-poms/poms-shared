128a90d9bf167517cb038c9c175e4c465a026515
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-04-03 20:48:17 +0200

    moved media-npo to poms-shared

---
+++ media-npo/src/test/java/nl/vpro/domain/npo/forecast/v2_0/VoorspellingenTest.java
@@ -0,0 +1,50 @@
+package nl.vpro.domain.npo.forecast.v2_0;
+
+import java.io.InputStream;
+
+import javax.xml.bind.JAXB;
+
+import nl.vpro.domain.npo.AuthorityPlatform;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 4.8.6
+ */
+public class VoorspellingenTest {
+
+
+    @Test
+    public void unmarshal() {
+        InputStream example = getClass().getClassLoader().getResourceAsStream("voorspel.xml");
+        Voorspellingen voorspellingen = JAXB.unmarshal(example, Voorspellingen.class);
+
+        assertThat(voorspellingen.getAflevering()).hasSize(2);
+        assertThat(voorspellingen.getAflevering().get(0).getPrid()).isEqualTo("VPWON_1266972");
+        assertThat(voorspellingen.getAflevering().get(0).getPlatform()).isEqualTo("internetvod");
+
+
+    }
+
+    @Test
+    public void extra() {
+        InputStream example = getClass().getClassLoader().getResourceAsStream("voorspel_extra.xml");
+
+        Voorspellingen voorspellingen = JAXB.unmarshal(example, Voorspellingen.class);
+
+        AuthorityPlatform platform = AuthorityPlatform.valueOf(voorspellingen.getAflevering()
+                .get(0)
+                .getPlatform()
+                .toLowerCase()
+        );
+
+        assertThat(voorspellingen.getAflevering()).hasSize(2);
+        assertThat(voorspellingen.getAflevering().get(0).getPrid()).isEqualTo("VPWON_1266972");
+        assertThat(platform.equals("extra"));
+
+
+    }
+
+}
