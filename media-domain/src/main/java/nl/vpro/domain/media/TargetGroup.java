11e366e75815033ddf842641e77034e86eb05128
Giorgio Vinci <giorgio.vinci@spronq.com>
2019-04-18 14:58:00 +0200

    First draft TargetGroup metadata. MSE-4370

---
+++ media-domain/src/main/java/nl/vpro/domain/media/TargetGroup.java
@@ -0,0 +1,41 @@
+package nl.vpro.domain.media;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import nl.vpro.domain.Child;
+import nl.vpro.domain.DomainObject;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.*;
+import java.io.Serializable;
+
+@Entity
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlType(name = "targetGroupType")
+@Data
+@EqualsAndHashCode(callSuper = true, exclude = "parent")
+public class TargetGroup extends DomainObject implements Serializable, Child<TargetGroups> {
+
+
+    @ManyToOne(targetEntity = TargetGroups.class, fetch = FetchType.LAZY)
+    @XmlTransient
+    private TargetGroups parent;
+
+    @Enumerated(EnumType.STRING)
+    @XmlAttribute(name = "type")
+    public TargetGroupType value;
+
+
+    public TargetGroup() {}
+
+    @lombok.Builder(builderClassName = "Builder")
+    public TargetGroup(TargetGroupType value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+
+}
