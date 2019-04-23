e140ed3f9b844427a97231815e28b22fd4cd07be
Giorgio Vinci <giorgio.vinci@spronq.com>
2019-04-23 15:11:08 +0200

    Exposed GUI API for TargetGroups (DB Fixes). MSE-4369

--- media-domain/src/main/java/nl/vpro/domain/media/TargetGroup.java
+++ media-domain/src/main/java/nl/vpro/domain/media/TargetGroup.java
@@ -35,6 +35,11 @@ public class TargetGroup extends DomainObject implements Serializable, Child<Tar
     }
 
     @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
@@ -46,10 +51,4 @@ public class TargetGroup extends DomainObject implements Serializable, Child<Tar
     public int hashCode() {
         return Objects.hash(super.hashCode(), value);
     }
-
-    @Override
-    public String toString() {
-        return String.valueOf(value);
-    }
-
 }
