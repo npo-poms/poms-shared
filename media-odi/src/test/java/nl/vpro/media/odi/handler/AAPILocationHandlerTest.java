c91f9864fd0770a5d03a754b628638a2e43351dd
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-05-06 20:56:02 +0200

    Test didn't run on travis.

--- media-odi/src/test/java/nl/vpro/media/odi/handler/AAPILocationHandlerTest.java
+++ media-odi/src/test/java/nl/vpro/media/odi/handler/AAPILocationHandlerTest.java
@@ -1,26 +1,45 @@
 package nl.vpro.media.odi.handler;
 
-import java.util.regex.Pattern;
-
+import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 import nl.vpro.domain.media.Location;
 import nl.vpro.domain.media.support.OwnerType;
 import nl.vpro.media.odi.util.LocationResult;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AAPILocationHandlerTest {
 
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(options()
+        .dynamicPort()
+    );
+
     @Test
     public void testHandle() {
 
+
+        wireMockRule.stubFor(
+            post(urlEqualTo("/"))
+                .willReturn(
+                    ok().withBody("https://adaptive.npostream.nl/live")
+                )
+        );
         AAPILocationHandler handler = new AAPILocationHandler();
+        handler.setAAPIServer(wireMockRule.baseUrl());
+
         MockHttpServletRequest request = new MockHttpServletRequest("GET", "somepath");
         Location location = new Location("odiw+http://livestreams.omroep.nl/live/npo/thematv/cultura24/cultura24.isml", OwnerType.BROADCASTER);
         LocationResult result = handler.produce(location, request, "m3u8");
-        assertThat(result.getProgramUrl()).matches(Pattern.quote("http://adaptive.npostreaming.nl/live/npo/thematv/cultura24/cultura24.isml/cultura24.m3u8?hash=") + ".*?" + Pattern.quote("&protection=url&type=http"));
+        //assertThat(result.getProgramUrl()).matches(Pattern.quote("http://adaptive.npostreaming.nl/live/npo/thematv/cultura24/cultura24.isml/cultura24.m3u8?hash=") + ".*?" + Pattern.quote("&protection=url&type=http"));
+
+        assertThat(result.getProgramUrl()).isEqualTo("https://adaptive.npostream.nl/live?protection=url&type=http");
 
     }
 }
