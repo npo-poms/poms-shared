2ef1503e893f37a526e44d64849ed44cd39cc97b
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-05-07 14:55:45 +0200

    Fixing test cases. Upgrade neo4j even further.

--- media-domain/src/main/java/nl/vpro/domain/media/gtaa/GTAARepository.java
+++ media-domain/src/main/java/nl/vpro/domain/media/gtaa/GTAARepository.java
@@ -5,11 +5,9 @@
 package nl.vpro.domain.media.gtaa;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import nl.vpro.openarchives.oai.Label;
 import nl.vpro.openarchives.oai.Record;
 import nl.vpro.util.CountedIterator;
 import nl.vpro.w3.rdf.Description;
