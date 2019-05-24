999e414c905b68089fe2f291aae0380ef0970abf
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-05-24 09:44:41 +0200

    Added warning log.

--- media-domain/src/main/java/nl/vpro/domain/media/support/ImageBackendServiceHolder.java
+++ media-domain/src/main/java/nl/vpro/domain/media/support/ImageBackendServiceHolder.java
@@ -1,15 +1,24 @@
 package nl.vpro.domain.media.support;
 
+import lombok.extern.slf4j.Slf4j;
+
+import javax.annotation.Nonnull;
+
 /**
  * @author Michiel Meeuwissen
  * @since 5.11
  */
+@Slf4j
 public class ImageBackendServiceHolder {
 
 
     private static ImageBackendService instance;
 
+    @Nonnull
     public static ImageBackendService getInstance() {
+        if (instance == null) {
+            log.warn("No image backend service configured");
+        }
         return instance;
     }
     public static void setInstance(ImageBackendService instance) {
