932335e38e524310579a3ab997fbf3f43c87f50f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-05-24 10:19:07 +0200

    gave image service a better name.

--- media-domain/src/main/java/nl/vpro/domain/media/support/AbsoluteImageUrlServiceImpl.java
+++ media-domain/src/main/java/nl/vpro/domain/media/support/AbsoluteImageUrlServiceImpl.java
@@ -4,22 +4,22 @@ package nl.vpro.domain.media.support;
 import javax.annotation.PostConstruct;
 
 /**
- * Implementation of {@link ImageBackendService} that can only be configured with an absolute base url.
+ * Implementation of {@link ImageUrlService} that can only be configured with an absolute base url.
  * @author Michiel Meeuwissen
  * @since 5.11
  */
-public class AbsoluteImageBackendServiceImpl implements ImageBackendService {
+public class AbsoluteImageUrlServiceImpl implements ImageUrlService {
 
     protected final String imageServerBaseUrl;
 
-    public AbsoluteImageBackendServiceImpl(String imageServerBaseUrl) {
+    public AbsoluteImageUrlServiceImpl(String imageServerBaseUrl) {
         this.imageServerBaseUrl = imageServerBaseUrl;
     }
 
 
     @PostConstruct
     public void init() {
-        ImageBackendServiceHolder.setInstance(this);
+        ImageUrlServiceHolder.setInstance(this);
     }
 
     @Override
