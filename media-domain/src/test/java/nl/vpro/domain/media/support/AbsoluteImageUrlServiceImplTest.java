46dd94bb26506aa80a6c02db534e9dff19fb49f7
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-05-24 10:28:36 +0200

    Added test.

---
+++ media-domain/src/test/java/nl/vpro/domain/media/support/AbsoluteImageUrlServiceImplTest.java
@@ -0,0 +1,26 @@
+package nl.vpro.domain.media.support;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.11
+ */
+@Slf4j
+public class AbsoluteImageUrlServiceImplTest {
+
+    public ImageUrlService test = new AbsoluteImageUrlServiceImpl("https://images-test.poms.omroep.nl/image/");
+
+    @Test
+    public void test() {
+        log.info("{}", test.getOriginalUrl(123L));
+
+
+        assertThat(test.getOriginalUrl(123L)).isEqualTo("https://images-test.poms.omroep.nl/image/123");
+    }
+
+}
