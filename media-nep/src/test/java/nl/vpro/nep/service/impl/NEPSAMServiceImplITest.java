1d31c0acdde504d679fe7abb479a78a13aede031
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-06-07 14:29:30 +0200

    Adding tests.

--- media-nep/src/test/java/nl/vpro/nep/service/impl/NEPSAMServiceImplITest.java
+++ media-nep/src/test/java/nl/vpro/nep/service/impl/NEPSAMServiceImplITest.java
@@ -2,10 +2,7 @@ package nl.vpro.nep.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.time.Duration;
-import java.util.Properties;
 
 import org.junit.Test;
 
@@ -16,49 +13,28 @@ import static nl.vpro.nep.service.NEPSAMService.createStreamAccessItem;
  * @since 5.11
  */
 @Slf4j
-public class NEPSAMServiceImplITest {
+public class NEPSAMServiceImplITest extends  AbstractNEPTest {
 
 
-    Properties properties = new Properties();
-    {
-        File propFile = new File(new File(new File(System.getProperty("user.home")), "conf"), "sam.properties");
-        try (FileInputStream input = new FileInputStream(propFile)) {
-            properties.load(input);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
 
-    }
-
-    String url = properties.getProperty("url", "https://api.samgcloud.nepworldwide.nl/");
     NEPSAMAuthenticator authenticator = new NEPSAMAuthenticator(
             "npo_poms",
-        properties.getProperty("password"),
-        url
+        getProperty("password"),
+        getProperty("baseUrl")
 
     );
     NEPSAMServiceImpl impl = new NEPSAMServiceImpl(
-        url,
+        getProperty("baseUrl"),
         null,
         null,
         null,
         authenticator
     );
 
-   /* @Test
-    public void widevine() {
-        WideVineResponse wideVineResponse = impl.widevineToken(new WideVineRequest("145.58.169.92", null));
-        log.info("{}", wideVineResponse);
-
+    public NEPSAMServiceImplITest() {
+        super("nep.sam-api");
     }
 
-
-    @Test
-    public void playready() {
-        PlayreadyResponse wideVineResponse = impl.playreadyToken(new PlayreadyRequest("145.58.169.92"));
-        log.info("{}", wideVineResponse);
-
-    }*/
 
     @Test
     public void streamUrlForMid() {
