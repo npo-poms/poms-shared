b94c4fd447958fedef96ea05b376a9e4129c418a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-07-15 10:45:16 +0200

    javadoc.

--- poms-shared-domain/src/main/java/nl/vpro/validation/WarningValidatorGroup.java
+++ poms-shared-domain/src/main/java/nl/vpro/validation/WarningValidatorGroup.java
@@ -2,6 +2,9 @@ package nl.vpro.validation;
 
 /**
  * Marker interface for validation messages that must be logged as a WARNING.
+ *
+ * Currently only implemented in backend rest api.
+ *
  * @author Jeroen van Vianen
  * @since 5.1
  */
