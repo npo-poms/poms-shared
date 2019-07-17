a515a90d75116b5c7c565260926cb351d48dacdd
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-07-17 14:06:59 +0200

    Trying to resolve jsr305 dependency.

--- poms-shared-domain/src/main/java/nl/vpro/services/MockTransactionService.java
+++ poms-shared-domain/src/main/java/nl/vpro/services/MockTransactionService.java
@@ -5,66 +5,66 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-import javax.annotation.Nonnull;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 
 public class MockTransactionService implements TransactionService {
 
     @Override
-    public <T> T executeInNewTransaction(@Nonnull Callable<T> callable) throws Exception {
+    public <T> T executeInNewTransaction(@NonNull Callable<T> callable) throws Exception {
         return callable.call();
     }
 
     @Override
-    public <T> T getInNewTransaction(@Nonnull Supplier<T> supplier) {
+    public <T> T getInNewTransaction(@NonNull Supplier<T> supplier) {
         return supplier.get();
 
     }
 
     @Override
-    public void executeInNewTransaction(@Nonnull Runnable runnable) {
+    public void executeInNewTransaction(@NonNull Runnable runnable) {
         runnable.run();
     }
 
     @Override
-    public <T, S> T executeInNewTransaction(S argument, @Nonnull Function<S, T> function) {
+    public <T, S> T executeInNewTransaction(S argument, @NonNull Function<S, T> function) {
         return function.apply(argument);
     }
 
     @Override
-    public <T> void executeInNewTransaction(T argument, @Nonnull Consumer<T> consumer) {
+    public <T> void executeInNewTransaction(T argument, @NonNull Consumer<T> consumer) {
         consumer.accept(argument);
 
     }
 
 
     @Override
-    public <T> T executeInReadonlyTransaction(@Nonnull Callable<T> callable) throws Exception {
+    public <T> T executeInReadonlyTransaction(@NonNull Callable<T> callable) throws Exception {
         readonly();
         return callable.call();
     }
 
     @Override
-    public <T> T getInReadonlyTransaction(@Nonnull Supplier<T> supplier) {
+    public <T> T getInReadonlyTransaction(@NonNull Supplier<T> supplier) {
         readonly();
         return supplier.get();
     }
 
     @Override
-    public void executeInReadonlyTransaction(@Nonnull Runnable runnable) {
+    public void executeInReadonlyTransaction(@NonNull Runnable runnable) {
         readonly();
         runnable.run();
     }
 
     @Override
-    public <T, S> T executeInReadonlyTransaction(S argument, @Nonnull Function<S, T> function) {
+    public <T, S> T executeInReadonlyTransaction(S argument, @NonNull Function<S, T> function) {
         readonly();
         return function.apply(argument);
 
     }
 
     @Override
-    public <T> void executeInReadonlyTransaction(T argument, @Nonnull Consumer<T> consumer) {
+    public <T> void executeInReadonlyTransaction(T argument, @NonNull Consumer<T> consumer) {
         readonly();
         consumer.accept(argument);
     }
