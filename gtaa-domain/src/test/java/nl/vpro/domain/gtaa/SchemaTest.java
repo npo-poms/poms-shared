d6aff0dfa906a6288d9b5ba03d52a0bbc41bbfb0
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-07-20 14:59:19 +0200

    testing gtaa-schemas

---
+++ gtaa-domain/src/test/java/nl/vpro/domain/gtaa/SchemaTest.java
@@ -0,0 +1,48 @@
+package nl.vpro.domain.gtaa;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+import javax.xml.bind.JAXBException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import nl.vpro.domain.Xmlns;
+import nl.vpro.test.util.jaxb.AbstractSchemaTest;
+
+
+/**
+ * Tests whether the POMS schema's are changed. If tests-cases fail here, fix them, but <em>also don't forget to make the changes to the XSD's also to the manually maintained ones</em>.
+ * in nl/vpro/domain/media
+ *
+ *
+ *
+ * @author Michiel Meeuwissen
+ * @since 5.11
+ */
+@Slf4j
+public class SchemaTest extends AbstractSchemaTest {
+
+
+    @BeforeClass
+    public static void generateXSDs() throws JAXBException, IOException {
+        context = generate(
+            GTAAPerson.class,
+            GTAAGeographicName.class,
+            GTAAName.class,
+            GTAAGenre.class,
+            GTAAMaker.class,
+            GTAATopic.class,
+            GTAANewThesaurusObject.class,
+            GTAANewPerson.class
+        );
+
+    }
+    @Test
+    public void testGtaa() throws IOException {
+        testNamespace(Xmlns.GTAA_NAMESPACE);
+    }
+
+}
