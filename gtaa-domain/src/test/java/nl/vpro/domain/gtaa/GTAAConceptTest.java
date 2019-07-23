a30bc687dd63c5fd805c008c3c5006121581e5b0
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-07-23 13:27:40 +0200

    Adding tests.

---
+++ gtaa-domain/src/test/java/nl/vpro/domain/gtaa/GTAAConceptTest.java
@@ -0,0 +1,84 @@
+package nl.vpro.domain.gtaa;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.InvocationTargetException;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import nl.vpro.openarchives.oai.Label;
+import nl.vpro.test.util.jackson2.Jackson2TestUtil;
+import nl.vpro.test.util.jaxb.JAXBTestUtil;
+import nl.vpro.util.BindingUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.11
+ */
+@RunWith(Parameterized.class)
+@Slf4j
+public class GTAAConceptTest {
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        Scheme.classes();
+        return Arrays.stream(Scheme.values()).filter(s -> s != Scheme.person).map(s -> new Object[]{s}).collect(Collectors.toList());
+    }
+
+
+    Scheme scheme;
+    AbstractThesaurusItem  concept;
+
+    String xmlType;
+    String jsonType;
+
+    public GTAAConceptTest(Scheme scheme) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        this.scheme = scheme;
+        log.info("{}", this.scheme);
+        xmlType = scheme.getXmlElement();
+        jsonType = scheme.getJsonObjectType();
+        assertThat(jsonType).isEqualTo(xmlType.toLowerCase());
+        concept = (AbstractThesaurusItem) this.scheme.getImplementation().getConstructor().newInstance();
+        concept.setValue("concept");
+        concept.setId("http://gtaa/1234");
+        concept.setNotes(Arrays.asList(Label.builder().value("bla").lang("nl").build()));
+        concept.setLastModified(LocalDateTime.of(2017, 9, 20, 10, 43, 0).atZone(BindingUtils.DEFAULT_ZONE).toInstant());
+        concept.setStatus(Status.approved);
+    }
+
+     @Test
+    public void xml() throws Exception {
+
+        JAXBTestUtil.roundTripAndSimilarAndEquals(concept, "<gtaa:" + xmlType + " gtaa:id=\"http://gtaa/1234\" gtaa:status=\"approved\" gtaa:lastModified=\"2017-09-20T10:43:00+02:00\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:skosxl=\"http://www.w3.org/2008/05/skos-xl#\" xmlns:oai=\"http://www.openarchives.org/OAI/2.0/\" xmlns:dcterms=\"http://purl.org/dc/terms/\" xmlns:skos=\"http://www.w3.org/2004/02/skos/core#\" xmlns:gtaa=\"urn:vpro:gtaa:2017\" xmlns:openskos=\"http://openskos.org/xmlns#\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n" +
+            "    <gtaa:value>concept</gtaa:value>\n" +
+            "    <gtaa:notes xml:lang=\"nl\">bla</gtaa:notes>\n" +
+            "</gtaa:" + xmlType + ">");
+
+    }
+
+    @Test
+    public void json() throws Exception {
+
+        Jackson2TestUtil.roundTripAndSimilarAndEquals(concept, "{\n" +
+            "  \"objectType\" : \"" + jsonType + "\",\n" +
+            "  \"value\" : \"concept\",\n" +
+            "  \"notes\" : [ {\n" +
+            "    \"value\" : \"bla\",\n" +
+            "    \"lang\" : \"nl\"\n" +
+            "  } ],\n" +
+            "  \"id\" : \"http://gtaa/1234\",\n" +
+            "  \"status\" : \"approved\",\n" +
+            "  \"lastModified\" : 1505896980000\n" +
+            "}");
+
+    }
+
+}
