19fbbe9587ea6cdb872b036fff36e539b29ae93c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-07-24 09:58:46 +0200

    More javadoc.

--- gtaa-domain/src/main/java/nl/vpro/domain/gtaa/package-info.java
+++ gtaa-domain/src/main/java/nl/vpro/domain/gtaa/package-info.java
@@ -1,18 +1,23 @@
+/**
+ * See https://docs.google.com/document/d/16L_Gp2awzwa3GHOPNIcK-9LNB0iMZgif-JUfFCMOkkg/edit?pref=2&pli=1}
+ */
 @javax.xml.bind.annotation.XmlSchema(namespace = Xmlns.GTAA_NAMESPACE,
     xmlns = {
-        @XmlNs(prefix = "rdf", namespaceURI = Namespaces.RDF),
-        @XmlNs(prefix = "skos", namespaceURI = Namespaces.SKOS),
-        @XmlNs(prefix = "skosxl", namespaceURI = Namespaces.SKOS_XL),
+        @XmlNs(prefix = "rdf",      namespaceURI = Namespaces.RDF),
+        @XmlNs(prefix = "skos",     namespaceURI = Namespaces.SKOS),
+        @XmlNs(prefix = "skosxl",   namespaceURI = Namespaces.SKOS_XL),
         @XmlNs(prefix = "openskos", namespaceURI = Namespaces.OPEN_SKOS),
-        @XmlNs(prefix = "dcterms", namespaceURI = Namespaces.DC_TERMS),
-        @XmlNs(prefix = "dc", namespaceURI = Namespaces.DC_TERMS_ELEMENTS),
-        @XmlNs(prefix = "gtaa", namespaceURI = Xmlns.GTAA_NAMESPACE),
+        @XmlNs(prefix = "dcterms",  namespaceURI = Namespaces.DC_TERMS),
+        @XmlNs(prefix = "dc",       namespaceURI = Namespaces.DC_TERMS_ELEMENTS),
+        @XmlNs(prefix = "gtaa",     namespaceURI = Xmlns.GTAA_NAMESPACE),
 
 
     },
     elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
     attributeFormDefault = XmlNsForm.QUALIFIED
-) package nl.vpro.domain.gtaa;
+)
+
+package nl.vpro.domain.gtaa;
 
 import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlNsForm;
@@ -20,4 +25,3 @@ import javax.xml.bind.annotation.XmlNsForm;
 import nl.vpro.domain.Xmlns;
 import nl.vpro.openarchives.oai.Namespaces;
 
-// See https://docs.google.com/document/d/16L_Gp2awzwa3GHOPNIcK-9LNB0iMZgif-JUfFCMOkkg/edit?pref=2&pli=1
