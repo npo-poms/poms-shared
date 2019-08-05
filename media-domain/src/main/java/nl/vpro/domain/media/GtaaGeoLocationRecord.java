83962216a56905056529ad9921b12abe3496086a
Giorgio Vinci <giorgiovinci@gmail.com>
2019-08-05 21:59:11 +0200

    MSE-4372 Normalize gtaaRecord structure. (Fix Tests)

---
+++ media-domain/src/main/java/nl/vpro/domain/media/GtaaGeoLocationRecord.java
@@ -0,0 +1,69 @@
+package nl.vpro.domain.media;
+
+import lombok.Getter;
+import lombok.Setter;
+import nl.vpro.domain.gtaa.Status;
+import nl.vpro.validation.NoHtml;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.search.annotations.Indexed;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlElement;
+import java.io.Serializable;
+
+@Entity
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@Getter
+@Setter
+public class GtaaGeoLocationRecord implements Serializable, Comparable<GtaaGeoLocationRecord> {
+    private static final long serialVersionUID = 0L;
+
+    @Column(name = "gtaa_uri")
+    @Getter
+    @Id
+    @lombok.NonNull
+    private String uri;
+
+    @Column(nullable = true, length = 30, name = "gtaa_status")
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @NoHtml
+    @XmlElement
+    @lombok.NonNull
+    private String name;
+
+    @NoHtml
+    @XmlElement
+    private String description;
+
+    public GtaaGeoLocationRecord() {}
+
+    @lombok.Builder(builderClassName = "Builder")
+    public GtaaGeoLocationRecord(@lombok.NonNull String uri, Status status, @lombok.NonNull String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.uri = uri;
+        this.status = status;
+    }
+
+    public void setStatus(Status status){
+        this.status = status;
+    }
+    public void setStatus(String status){
+        this.status = (status == null)? null : Status.valueOf(status);
+    }
+
+    @Override
+    public int compareTo(@NonNull GtaaGeoLocationRecord geoLocationRecord) {
+        if (uri == null) {
+            if (geoLocationRecord.uri == null) {
+                return 0;
+            }
+            return -1;
+        }
+        int result = uri.compareToIgnoreCase(geoLocationRecord.uri);
+        return result == 0 ? uri.compareTo(geoLocationRecord.uri) : result;
+    }
+}
