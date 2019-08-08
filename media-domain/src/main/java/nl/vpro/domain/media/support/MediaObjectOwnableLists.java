f4818239b5bb53750a2e76be2c18ad9f44d854c3
Giorgio Vinci <giorgiovinci@gmail.com>
2019-08-08 12:32:41 +0200

    MSE-4372 Refactored GeoLocation (fix)

--- media-domain/src/main/java/nl/vpro/domain/media/support/MediaObjectOwnableLists.java
+++ media-domain/src/main/java/nl/vpro/domain/media/support/MediaObjectOwnableLists.java
@@ -4,7 +4,6 @@ import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Supplier;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 import nl.vpro.domain.media.MediaObject;
@@ -119,7 +118,7 @@ public class MediaObjectOwnableLists {
             BiFunction<OwnerType, List, OL> creator,
             List<OwnerType> ownersToExpand) {
 
-        if(CollectionUtils.isEmpty(values)) return null;
+        if(values == null || values.isEmpty()) return null;
         SortedSet additions = new TreeSet();
         for(OwnerType owner: ownersToExpand){
 
