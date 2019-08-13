d7c153c5b81d0cf5b705a3209d3ed04418f18e07
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-08-13 15:37:50 +0200

    Test case failures.

--- media-domain/src/test/java/nl/vpro/domain/media/update/SegmentUpdateTest.java
+++ media-domain/src/test/java/nl/vpro/domain/media/update/SegmentUpdateTest.java
@@ -28,7 +28,7 @@ public class SegmentUpdateTest extends MediaUpdateTest {
 
         String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>" +
                 "<segment embeddable=\"true\" xmlns=\"urn:vpro:media:update:2009\">" +
-                "<locations/><scheduleEvents/><images/><start>P0DT0H0M0.100S</start></segment>";
+                "<locations/><images/><start>P0DT0H0M0.100S</start></segment>";
 
         JAXBTestUtil.roundTripAndSimilar(update, expected);
     }
