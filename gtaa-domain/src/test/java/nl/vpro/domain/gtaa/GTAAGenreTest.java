4aa11c5ca4dc84b9b9e6cf8bc60c33daff4b8425
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-08-16 13:32:28 +0200

    Test case failures.

--- gtaa-domain/src/test/java/nl/vpro/domain/gtaa/GTAAGenreTest.java
+++ gtaa-domain/src/test/java/nl/vpro/domain/gtaa/GTAAGenreTest.java
@@ -28,7 +28,7 @@ public class GTAAGenreTest {
     public void xml() throws Exception {
 
         JAXBTestUtil.roundTripAndSimilarAndEquals(concept, "<gtaa:genre gtaa:id=\"http://gtaa/1234\" gtaa:status=\"approved\" gtaa:lastModified=\"2017-09-20T10:43:00+02:00\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:skosxl=\"http://www.w3.org/2008/05/skos-xl#\" xmlns:oai=\"http://www.openarchives.org/OAI/2.0/\" xmlns:dcterms=\"http://purl.org/dc/terms/\" xmlns:skos=\"http://www.w3.org/2004/02/skos/core#\" xmlns:gtaa=\"urn:vpro:gtaa:2017\" xmlns:openskos=\"http://openskos.org/xmlns#\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n" +
-            "    <gtaa:value>Amsterdam</gtaa:value>\n" +
+            "    <gtaa:name>Amsterdam</gtaa:name>\n" +
             "    <gtaa:scopeNote>bla</gtaa:scopeNote>\n" +
             "</gtaa:genre>");
 
@@ -39,7 +39,7 @@ public class GTAAGenreTest {
 
         Jackson2TestUtil.roundTripAndSimilarAndEquals(concept, "{\n" +
             "  \"objectType\" : \"genre\",\n" +
-            "  \"value\" : \"Amsterdam\",\n" +
+            "  \"name\" : \"Amsterdam\",\n" +
             "  \"scopeNotes\" : [ \"bla\" ],\n" +
             "  \"id\" : \"http://gtaa/1234\",\n" +
             "  \"status\" : \"approved\",\n" +
