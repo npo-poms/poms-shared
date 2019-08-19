35a7ca5a0e5d0ddb6b36a94336b21d73a97186de
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-08-19 09:57:49 +0200

    More constants, droppped stuff that was there for junction.

--- poms-shared-domain/src/main/java/nl/vpro/domain/Roles.java
+++ poms-shared-domain/src/main/java/nl/vpro/domain/Roles.java
@@ -8,87 +8,101 @@ package nl.vpro.domain;
 public class Roles {
 
 
+    public static final String ROLE = "ROLE_";
     /**
      * This is the default role of normal users. They have access, and can only write content of their own broadcaster(s)
      */
-    public static final String USER_ROLE = "ROLE_MEDIA_USER";
+    public static final String USER = "MEDIA_USER";
+    public static final String USER_ROLE = ROLE + USER;
 
     /**
      * A super user has user access to nodes of every broadcaster.
      */
-    public static final String SUPERUSER_ROLE = "ROLE_MEDIA_SUPERUSER";
+    public static final String SUPERUSER = "MEDIA_SUPERUSER";
+    public static final String SUPERUSER_ROLE = ROLE + SUPERUSER;
 
     /**
      * A super process is like a super user
      */
-    public static final String SUPERPROCESS_ROLE = "ROLE_MEDIA_SUPERPROCESS";
+    public static final String SUPERPROCESS = "MEDIA_SUPERPROCESS";
+    public static final String SUPERPROCESS_ROLE = ROLE + SUPERPROCESS;
 
     /**
      * A super-admin may do (nearly) everything
      */
-    public static final String SUPERADMIN_ROLE = "ROLE_MEDIA_SUPERADMIN";
+    public static final String SUPERADMIN = "MEDIA_SUPERADMIN";
+    public static final String SUPERADMIN_ROLE = ROLE + SUPERADMIN;
 
 
     /**
      * This is a system role which is assigned to one user which may do stuff 'on behalf' of other users.  See the RunAs-services.
      */
-    public static final String RUNAS_ROLE = "ROLE_RUNAS";
+    public static final String RUNAS  = "RUNAS";
+    public static final String RUNAS_ROLE = ROLE + RUNAS;
 
 
-    /**
-     * Used by junction only. Has the right to add locations to any object.
-     */
-    public static final String ENCODER_SUPERPROCESS_ROLE = "ROLE_ENCODER_SUPERPROCESS";
 
-
-    public static final String PUBLISHER_ROLE = "ROLE_MEDIA_PUBLISHER";
+    public static final String PUBLISHER  = "MEDIA_PUBLISHER";
+    public static final String PUBLISHER_ROLE = ROLE + PUBLISHER;
 
     /**
      * Support are people at NPO-helpdesk who can see everything, including deleted record, but edit nothing. (MSE-2015)
      */
-    public static final String SUPPORT_ROLE = "ROLE_MEDIA_SUPPORT";
+    public static final String SUPPORT     = "MEDIA_SUPPORT";
+    public static final String SUPPORT_ROLE = ROLE + SUPPORT;
 
     /**
      * Process roles may use the backend api.
      */
-    public static final String PROCESS_ROLE = "ROLE_MEDIA_PROCESS";
+    public static final String PROCESS      = "MEDIA_PROCESS";
+    public static final String PROCESS_ROLE = ROLE + PROCESS;
 
     /**
      * A limited role. May only edit objects originally created by theirselves.
      */
-    public static final String EXTERNALUSER_ROLE = "ROLE_MEDIA_EXTERNALUSER";
+    public static final String EXTERNALUSER  = "MEDIA_EXTERNALUSER";
+    public static final String EXTERNALUSER_ROLE = ROLE + EXTERNALUSER;
 
     /**
      * Has more rights on schedule event data
      */
-    public static final String ARCHIVIST_ROLE = "ROLE_MEDIA_ARCHIVIST";
+    public static final String ARCHIVIST       = "MEDIA_ARCHIVIST";
+    public static final String ARCHIVIST_ROLE = ROLE + ARCHIVIST;
 
     /**
      * https://jira.vpro.nl/browse/MSE-3999
      */
-    public static final String SCHEDULE_ROLE = "ROLE_MEDIA_SCHEDULE";
+    public static final String SCHEDULE = "MEDIA_SCHEDULE";
+    public static final String SCHEDULE_ROLE = ROLE + SCHEDULE;
 
     /**
      * Have the right to upload files as locations.
      */
-    public static final String UPLOAD_ROLE = "ROLE_MEDIA_UPLOAD";
+    public static final String UPLOAD = "MEDIA_UPLOAD";
+    public static final String UPLOAD_ROLE = ROLE + UPLOAD;
 
 
-    public static final String MERGE_SERIES_ROLE = "ROLE_MEDIA_MERGESERIES";
-    public static final String MERGE_EPISODE_ROLE = "ROLE_MEDIA_MERGEEPISODE";
+    public static final String MERGE_SERIES = "MEDIA_MERGESERIES";
+    public static final String MERGE_SERIES_ROLE = ROLE + MERGE_SERIES;
+    public static final String MERGE_EPISODE = "MEDIA_MERGEEPISODE";
+    public static final String MERGE_EPISODE_ROLE = ROLE + MERGE_EPISODE;
+    public static final String MERGE_ALL  = "MEDIA_MERGEALL";
     public static final String MERGE_ALL_ROLE = "ROLE_MEDIA_MERGEALL";
 
-    public static final String TRANSLATOR_ROLE = "ROLE_MEDIA_TRANSLATOR";
+    public static final String TRANSLATOR  = "MEDIA_TRANSLATOR";
+    public static final String TRANSLATOR_ROLE = ROLE + TRANSLATOR;
 
     /**
      * Role for users that are allowed to edit MIS owned fields, do note that they still need the regular permssions to actually do so.
      */
-    public static final String MIS_ROLE = "ROLE_MEDIA_MIS";
+    public static final String MIS  = "MEDIA_MIS";
+    public static final String MIS_ROLE = ROLE + MIS;
 
     /**
      * The system role is only assigned via {@link nl.vpro.domain.user.UserService#systemAuthenticate)}
      */
-    public static final String SYSTEM_ROLE = "ROLE_SYSTEM";
+    public static final String SYSTEM = "SYSTEM";
+    public static final String SYSTEM_ROLE = ROLE + SYSTEM;
 
     public static final String API_USER = "hasAnyRole('" + USER_ROLE + "','ROLE_API_CLIENT','ROLE_API_USER','ROLE_API_SUPERUSER','ROLE_API_SUPERCLIENT')";
 
