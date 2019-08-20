80d25e37ce1ccdca4de355995166d447a15e9c07
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-08-20 09:19:41 +0200

    NPA-493

--- media-domain/src/main/java/nl/vpro/domain/media/support/PublishableObject.java
+++ media-domain/src/main/java/nl/vpro/domain/media/support/PublishableObject.java
@@ -179,7 +179,8 @@ public abstract class PublishableObject<T extends PublishableObject<T>>
             || Workflow.FOR_REPUBLICATION.equals(workflow)
             || Workflow.PUBLISHED.equals(workflow)
             || Workflow.REVOKED.equals(workflow)
-            || Workflow.MERGED.equals(workflow)) {
+            || Workflow.MERGED.equals(workflow)
+            || workflow == null /* may happen when property filtering active NPA-493 */) {
 
             return inPublicationWindow(Instant.now());
         }
