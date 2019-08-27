2bb76e0ed025b1fb6fde1c7082c1e7008d9e096a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-08-27 17:44:53 +0200

    MSE-4589

--- media-domain/src/main/java/nl/vpro/domain/media/MediaPublisherHeaders.java
+++ media-domain/src/main/java/nl/vpro/domain/media/MediaPublisherHeaders.java
@@ -30,9 +30,6 @@ public interface MediaPublisherHeaders {
     enum Destination implements Displayable {
 
         UG("Uitzending gemist"),
-        CouchDB_MID("CouchDB Poms (mids)"),
-        CouchDB_URN("CouchDB Poms (urns)"),
-        CouchDB_API("NPO API CouchDB"),
         ElasticSearch("NPO API Elastic Search"),
         TVVOD("TvVOD"),
         PROJECTM("Project M")
