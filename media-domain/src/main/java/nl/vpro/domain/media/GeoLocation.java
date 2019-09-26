ba2f1b1178bf1c1797d81896bd7830d42cf52d84
Giorgio Vinci <giorgiovinci@gmail.com>
2019-09-26 17:26:39 +0200

    MSE-4578 Changed db structure to avoid using gtaa_uri as key

--- media-domain/src/main/java/nl/vpro/domain/media/GeoLocation.java
+++ media-domain/src/main/java/nl/vpro/domain/media/GeoLocation.java
@@ -63,7 +63,7 @@ public class GeoLocation extends DomainObject implements MediaObjectOwnableListI
 
     @XmlTransient
     @ManyToOne(cascade = { CascadeType.MERGE, CascadeType.PERSIST }, targetEntity = GTAAGeoLocationRecord.class)
-    @JoinColumn(name = "gtaa_uri")
+    @JoinColumn(name = "gtaa_id")
     @Getter
     @Setter
     private GTAAGeoLocationRecord gtaaRecord = new GTAAGeoLocationRecord();
