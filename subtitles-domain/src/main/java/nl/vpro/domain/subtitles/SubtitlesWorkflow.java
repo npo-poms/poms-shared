5ac2f062d404b8f669c74f0c2198ed38c8d49f4c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-10-25 15:04:29 +0200

    Backporting.

--- subtitles-domain/src/main/java/nl/vpro/domain/subtitles/SubtitlesWorkflow.java
+++ subtitles-domain/src/main/java/nl/vpro/domain/subtitles/SubtitlesWorkflow.java
@@ -2,10 +2,19 @@ package nl.vpro.domain.subtitles;
 
 import java.util.*;
 
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+import nl.vpro.domain.Xmlns;
+
+import static java.util.Collections.unmodifiableSet;
+
 /**
  * @author Michiel Meeuwissen
  * @since 5.11
  */
+@XmlEnum
+@XmlType(name = "subtitlesWorkflowEnum", namespace = Xmlns.SHARED_NAMESPACE)
 public enum SubtitlesWorkflow {
     IGNORE,
 
@@ -15,8 +24,21 @@ public enum SubtitlesWorkflow {
     DELETED,
 
     FOR_PUBLICATION,
-    PUBLISHED;
+    FOR_REPUBLICATION,
+    PUBLISHED,
+
+    /**
+     * Could not be published because of some error
+     */
+    PUBLISH_ERROR
+    ;
 
 
-    public static final  Set<SubtitlesWorkflow> NEEDS_WORK = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(FOR_DELETION, FOR_PUBLICATION)));
+    public static final  Set<SubtitlesWorkflow> NEEDS_WORK = unmodifiableSet(new HashSet<>(Arrays.asList(FOR_DELETION, FOR_PUBLICATION, FOR_REPUBLICATION)));
+
+    public static final  Set<SubtitlesWorkflow> NOT_PUBLISHED = unmodifiableSet(new HashSet<>(Arrays.asList(FOR_DELETION, DELETED, REVOKED, FOR_PUBLICATION)));
+
+    public static final  Set<SubtitlesWorkflow> DELETEDS = unmodifiableSet(new HashSet<>(Arrays.asList(FOR_DELETION, DELETED)));
+
+
 }
