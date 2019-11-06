56ea8d1f0d3e27913a6b94da5564451611b18022
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-11-06 17:21:53 +0100

    Trying to clean this stuff up a bit. E.g. using a value as a class is impossible since a value may begin with a digit.

--- poms-shared-domain/src/main/java/nl/vpro/domain/XmlValued.java
+++ poms-shared-domain/src/main/java/nl/vpro/domain/XmlValued.java
@@ -3,6 +3,11 @@ package nl.vpro.domain;
 import javax.xml.bind.annotation.XmlEnumValue;
 
 /**
+ * An enum can be made to extend this, which indicates that an extra method will be present {@link #getXmlValue()} which
+ * will be the {@link XmlEnumValue} of the enum value.
+ *
+ * Normally this would be {@link Enum#name()}}, but sometimes this is overriden, via the said annotation and you need programmatic access to it.
+ *
  * @author Michiel Meeuwissen
  * @since 5.5
  */
