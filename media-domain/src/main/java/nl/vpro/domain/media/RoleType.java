3b4c1c8dda65f3fe92e047abb64acdfb881b108d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-11-11 20:55:30 +0100

    Resolved stack overlfow.

--- media-domain/src/main/java/nl/vpro/domain/media/RoleType.java
+++ media-domain/src/main/java/nl/vpro/domain/media/RoleType.java
@@ -1,5 +1,7 @@
 package nl.vpro.domain.media;
 
+import lombok.Getter;
+
 import javax.xml.bind.annotation.XmlEnum;
 
 import nl.vpro.domain.Displayable;
@@ -7,145 +9,53 @@ import nl.vpro.domain.Displayable;
 @XmlEnum
 public enum RoleType  implements Displayable {
 
-    DIRECTOR {
-
-        @Override
-        public String toString() {
-            return "Regisseur";
-        }
-    },
-    CHIEF_EDITOR {
-
-        @Override
-        public String toString() {
-            return "Eindredactie";
-        }
-    },
-    EDITOR {
-
-        @Override
-        public String toString() {
-            return "Redacteur";
-        }
-    },
-    PRESENTER {
-
-        @Override
-        public String toString() {
-            return "Presentator";
-        }
-    },
-    INTERVIEWER {
-
-        @Override
-        public String toString() {
-            return "Interviewer";
-        }
-    },
-    PRODUCER {
-
-        @Override
-        public String toString() {
-            return "Productie";
-        }
-    },
-    RESEARCH {
-
-        @Override
-        public String toString() {
-            return "Research";
-        }
-    },
-    GUEST {
-
-        @Override
-        public String toString() {
-            return "Gast";
-        }
-    },
-    REPORTER {
-
-        @Override
-        public String toString() {
-            return "Verslaggever";
-        }
-    },
-    ACTOR {
-
-        @Override
-        public String toString() {
-            return "Acteur";
-        }
-    },
-    COMMENTATOR {
-
-        @Override
-        public String toString() {
-            return "Commentaar";
-        }
-    },
-    COMPOSER {
-
-        @Override
-        public String toString() {
-            return "Componist";
-        }
-    },
-    SCRIPTWRITER {
-
-        @Override
-        public String toString() {
-            return "Scenario";
-        }
-    },
+    DIRECTOR("Regisseur"),
+    CHIEF_EDITOR("Eindredactie"),
+    EDITOR("Redacteur"),
+    PRESENTER("Presentator"),
+    INTERVIEWER("Interviewer"),
+    PRODUCER("Productie"),
+    RESEARCH("Research"),
+    GUEST("Gast"),
+    REPORTER("Verslaggever"),
+    ACTOR("Acteur"),
+    COMMENTATOR("Commentaar"),
+    COMPOSER("Componist"),
+    SCRIPTWRITER("Scenario"),
     /**
      * For radio shows this seems useful
      * @since 5.8
      */
-    NEWS_PRESENTER {
-        @Override
-        public String toString() {
-            return "Nieuwslezer";
-        }
-
-    },
+    NEWS_PRESENTER("Nieuwslezer"),
      /**
      * For radio shows this seems useful
      * @since 5.9
      */
-    SIDEKICK {
-        @Override
-        public String toString() {
-            return "Sidekick";
-        }
-
-    },
+    SIDEKICK("Sidekick"),
     /**
      * https://jira.vpro.nl/browse/MSE-4371
      * @since 5.11
      */
-    SUBJECT {
-        @Override
-        public String toString() {
-            return "Onderwerp";
-        }
-    },
+    SUBJECT("Onderwerp"),
     /**
      * See https://jira.vpro.nl/browse/MSE-4371
      * @since 5.11
      */
-    PARTICIPANT {
-        @Override
-        public String toString() {
-            return "Deelnemer";
-        }
-    },
-    UNDEFINED {
-        @Override
-        public String toString() {
-            return "Overig";
-        }
-    };
+    PARTICIPANT("Deelnemer"),
+    UNDEFINED("Overig")
+    ;
+
+    @Getter
+    private final String displayValue;
+
+    RoleType(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    @Override
+    public String toString() {
+        return getDisplayName();
+    }
 
     public static RoleType fromToString(String string) {
         for (RoleType role : RoleType.values()) {
