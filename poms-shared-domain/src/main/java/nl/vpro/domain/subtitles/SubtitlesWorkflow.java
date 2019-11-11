6c5d1dc17c4cc0243cf9d7425892ff15b7fd0d47
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-11-11 15:58:27 +0100

    Moved to a constant in SubtitlesWorkflow. javadoc.

--- poms-shared-domain/src/main/java/nl/vpro/domain/subtitles/SubtitlesWorkflow.java
+++ poms-shared-domain/src/main/java/nl/vpro/domain/subtitles/SubtitlesWorkflow.java
@@ -14,15 +14,42 @@ import nl.vpro.domain.Xmlns;
 @XmlEnum
 @XmlType(name = "subtitlesWorkflowEnum", namespace = Xmlns.SHARED_NAMESPACE)
 public enum SubtitlesWorkflow {
+
+    /**
+     * Completely excluded from publishing/unpublishing logic. Not a real status, can can only be
+     * attained by manually setting it in the database.
+     */
     IGNORE,
 
+    /**
+     * Revoked because the media itself is not published, or is not playable
+     */
     REVOKED,
 
+
+    /**
+     * Marked for deletion, but not yet processed
+     */
     FOR_DELETION,
+
+    /**
+     * Marked deleted, and already revoked from API
+     */
     DELETED,
 
+    /**
+     * New subtitles, not yet processed for publication.
+     */
     FOR_PUBLICATION,
+
+     /**
+     * Changed subtitles, not yet processed for publication
+     */
     FOR_REPUBLICATION,
+
+    /**
+     * These subtitles are published to the API
+     */
     PUBLISHED,
 
     /**
@@ -34,6 +61,9 @@ public enum SubtitlesWorkflow {
 
     public static final  Set<SubtitlesWorkflow> NEEDS_WORK = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(FOR_DELETION, FOR_PUBLICATION, FOR_REPUBLICATION)));
     public static final  Set<SubtitlesWorkflow> NOT_PUBLISHED = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(FOR_DELETION, DELETED, REVOKED, FOR_PUBLICATION)));
+
+    public static final Set<SubtitlesWorkflow> INVISIBLE = DELETEDS;
+
 
 
 }
