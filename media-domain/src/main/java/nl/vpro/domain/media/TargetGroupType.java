a83b83ee3d400e99371ef3c3e21549228a9534ba
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-11-11 17:03:02 +0100

    implemented displayable without stack overlflows...

--- media-domain/src/main/java/nl/vpro/domain/media/TargetGroupType.java
+++ media-domain/src/main/java/nl/vpro/domain/media/TargetGroupType.java
@@ -1,5 +1,7 @@
 package nl.vpro.domain.media;
 
+import lombok.Getter;
+
 import javax.xml.bind.annotation.XmlEnum;
 import javax.xml.bind.annotation.XmlType;
 
@@ -8,46 +10,23 @@ import nl.vpro.domain.Displayable;
 @XmlEnum
 @XmlType(name = "targetGroupEnum")
 public enum TargetGroupType implements Displayable {
-    KIDS_6 {
-        @Override
-        public String toString() {
-            return "Kinderen tot 6 jaar (Zappelin)";
-        }
-    },
-    KIDS_12 {
-        @Override
-        public String toString() {
-            return "Kinderen 6-12 (Zapp)";
-        }
-    },
-    YOUNG_ADULTS {
-        @Override
-        public String toString() {
-            return "Jongeren (NPO3)";
-        }
-    },
-    ADULTS {
-        @Override
-        public String toString() {
-            return "Volwassenen";
-        }
-    },
-    ADULTS_WITH_KIDS_6 {
-        @Override
-        public String toString() {
-            return "Volwassenen met kinderen 0-6 jaar";
-        }
-    },
-    ADULTS_WITH_KIDS_12 {
-        @Override
-        public String toString() {
-            return "Volwassenen met kinderen 6-12 jaar";
-        }
-    },
-    EVERYONE {
-        @Override
-        public String toString() {
-            return "Iedereen";
-        }
+    KIDS_6("Kinderen tot 6 jaar (Zappelin)"),
+    KIDS_12("Kinderen 6-12 (Zapp)"),
+    YOUNG_ADULTS("Jongeren (NPO3)"),
+    ADULTS("Volwassenen"),
+    ADULTS_WITH_KIDS_6( "Volwassenen met kinderen 0-6 jaar"),
+    ADULTS_WITH_KIDS_12("Volwassenen met kinderen 6-12 jaar"),
+    EVERYONE("Iedereen")
+    ;
+
+    @Getter
+    private final String displayName;
+
+    TargetGroupType(String displayName) {
+        this.displayName = displayName;
+    }
+   @Override
+   public String toString() {
+        return getDisplayName();
     }
 }
