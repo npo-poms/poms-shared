884a849d374f6c1df3d3459dc91f2cbd919873b7
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-11-22 14:27:05 +0100

    Using closeable iterators.

--- api-domain/src/main/java/nl/vpro/domain/api/media/MediaService.java
+++ api-domain/src/main/java/nl/vpro/domain/api/media/MediaService.java
@@ -5,18 +5,13 @@
 package nl.vpro.domain.api.media;
 
 import java.time.Instant;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
-import nl.vpro.domain.api.Deletes;
-import nl.vpro.domain.api.MediaChange;
-import nl.vpro.domain.api.Order;
-import nl.vpro.domain.api.SuggestResult;
+import nl.vpro.domain.api.*;
 import nl.vpro.domain.api.profile.exception.ProfileNotFoundException;
-import nl.vpro.domain.media.MediaObject;
-import nl.vpro.domain.media.MediaProvider;
-import nl.vpro.domain.media.MediaType;
+import nl.vpro.domain.media.*;
+import nl.vpro.util.CloseableIterator;
 import nl.vpro.util.FilteringIterator;
 
 /**
@@ -27,7 +22,7 @@ public interface MediaService extends MediaProvider {
 
     SuggestResult suggest(String input, String profile, Integer max);
 
-    Iterator<MediaChange> changes(String profile, boolean profileCheck, Instant since, String mid, Order order, Integer max, Long keepAlive, boolean withSequences, Deletes deletes) throws ProfileNotFoundException;
+    CloseableIterator<MediaChange> changes(String profile, boolean profileCheck, Instant since, String mid, Order order, Integer max, Long keepAlive, boolean withSequences, Deletes deletes) throws ProfileNotFoundException;
 
     @Override
     <T extends MediaObject> T findByMid(String mid);
@@ -38,7 +33,7 @@ public interface MediaService extends MediaProvider {
 
     MediaResult list(Order order, Long offset, Integer max);
 
-    Iterator<MediaObject> iterate(String profile, MediaForm form, Long offset, Integer max, FilteringIterator.KeepAlive keepAlive) throws ProfileNotFoundException;
+    CloseableIterator<MediaObject> iterate(String profile, MediaForm form, Long offset, Integer max, FilteringIterator.KeepAlive keepAlive) throws ProfileNotFoundException;
 
     MediaSearchResult find(String profile, MediaForm form, Long offset, Integer max) throws ProfileNotFoundException;
 
