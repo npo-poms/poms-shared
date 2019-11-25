88e51f45dedbc5688fdee8eaa03329a638407e8d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-11-25 10:47:07 +0100

    Test case failures.

--- api-domain/src/test/java/nl/vpro/domain/api/thesaurus/ThesaurusResultTest.java
+++ api-domain/src/test/java/nl/vpro/domain/api/thesaurus/ThesaurusResultTest.java
@@ -18,6 +18,7 @@ public class ThesaurusResultTest {
             Arrays.asList(
                 GTAAPerson.builder().build(),
                 GTAAGeographicName.builder().build()), 10), "{\n" +
+            "  \"totalQualifier\" : \"MISSING\",\n" +
             "  \"offset\" : 0,\n" +
             "  \"max\" : 10,\n" +
             "  \"items\" : [ {\n" +
@@ -36,7 +37,7 @@ public class ThesaurusResultTest {
             GTAAGeographicName.builder().value("Amsterdam").build()
             ), 10),
                 "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
-                    "<api:thesaurusItems offset=\"0\" max=\"10\" xmlns=\"urn:vpro:media:2009\" xmlns:shared=\"urn:vpro:shared:2009\" xmlns:skosxl=\"http://www.w3.org/2008/05/skos-xl#\" xmlns:skos=\"http://www.w3.org/2004/02/skos/core#\" xmlns:media=\"urn:vpro:media:2009\" xmlns:pages=\"urn:vpro:pages:2013\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:oai=\"http://www.openarchives.org/OAI/2.0/\" xmlns:dcterms=\"http://purl.org/dc/terms/\" xmlns:api=\"urn:vpro:api:2013\" xmlns:gtaa=\"urn:vpro:gtaa:2017\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:openskos=\"http://openskos.org/xmlns#\">\n" +
+                    "<api:thesaurusItems totalQualifier=\"MISSING\"  offset=\"0\" max=\"10\" xmlns=\"urn:vpro:media:2009\" xmlns:shared=\"urn:vpro:shared:2009\" xmlns:skosxl=\"http://www.w3.org/2008/05/skos-xl#\" xmlns:skos=\"http://www.w3.org/2004/02/skos/core#\" xmlns:media=\"urn:vpro:media:2009\" xmlns:pages=\"urn:vpro:pages:2013\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:oai=\"http://www.openarchives.org/OAI/2.0/\" xmlns:dcterms=\"http://purl.org/dc/terms/\" xmlns:api=\"urn:vpro:api:2013\" xmlns:gtaa=\"urn:vpro:gtaa:2017\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:openskos=\"http://openskos.org/xmlns#\">\n" +
                     "    <api:items>\n" +
                     "        <api:item xsi:type=\"gtaa:personType\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n" +
                     "            <gtaa:name>puk, pietje</gtaa:name>\n" +
