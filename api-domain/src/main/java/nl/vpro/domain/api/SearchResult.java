cba2d421066e326b8e617ab44c0307602cb4f1b9
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-11-27 09:35:24 +0100

    Trying to reproduce the problem on test. Without success by the way, this is all working, still.

--- api-domain/src/main/java/nl/vpro/domain/api/SearchResult.java
+++ api-domain/src/main/java/nl/vpro/domain/api/SearchResult.java
@@ -34,8 +34,7 @@ public class SearchResult<S> extends Result<SearchResultItem<? extends S>> {
         return new AbstractList<S>() {
             @Override
             public S get(int index) {
-                SearchResultItem<? extends S> item = SearchResult.this.getItems().get(index);
-                return item == null ? null : item.getResult();
+                return SearchResult.this.getResult(index);
             }
 
             @Override
@@ -43,6 +42,15 @@ public class SearchResult<S> extends Result<SearchResultItem<? extends S>> {
                 return SearchResult.this.getSize();
             }
         };
+    }
+
+    /**
+     * @since 5.12
+     * @throws IndexOutOfBoundsException  if the index is out of range (index < 0 || index >= size())
+     */
+    public S getResult(int i) {
+        SearchResultItem<? extends S>  item =  get(i);
+        return item == null ? null : item.getResult();
     }
 
     /**
