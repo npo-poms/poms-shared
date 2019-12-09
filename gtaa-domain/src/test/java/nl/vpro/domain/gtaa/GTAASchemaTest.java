25c625dbf4bda3844682c3816fdcf61062febde5
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-12-09 21:47:22 +0100

    Junit5

--- gtaa-domain/src/test/java/nl/vpro/domain/gtaa/GTAASchemaTest.java
+++ gtaa-domain/src/test/java/nl/vpro/domain/gtaa/GTAASchemaTest.java
@@ -6,8 +6,9 @@ import java.io.IOException;
 
 import javax.xml.bind.JAXBException;
 
-import org.junit.BeforeClass;
-import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import nl.vpro.domain.Xmlns;
 import nl.vpro.test.util.jaxb.AbstractSchemaTest;
@@ -23,7 +24,7 @@ import nl.vpro.test.util.jaxb.AbstractSchemaTest;
 public class GTAASchemaTest extends AbstractSchemaTest {
 
 
-    @BeforeClass
+    @BeforeAll
     public static void generateXSDs() throws JAXBException, IOException {
         context = generate(Scheme.classesAndNew());
     }
