0f1f1224b5c8dab271214cb518e9f1ac5433dc08
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-12-27 16:11:12 +0100

    Missing properties.

--- media-domain/src/main/java/nl/vpro/validation/NoDuplicateOwner.java
+++ media-domain/src/main/java/nl/vpro/validation/NoDuplicateOwner.java
@@ -3,21 +3,28 @@ package nl.vpro.validation;
 import java.lang.annotation.*;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 
 import nl.vpro.domain.media.support.Ownable;
 
-import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Ensures that a collection of {@link Ownable} contains no two entries with the same {@link Ownable#getOwner()}
  *
  * @author Michiel Meeuwissen
- * @since 5.512
+ * @since 5.12
  */
-@Target({FIELD})
+@Target({METHOD, FIELD, PARAMETER})
 @Retention(RUNTIME)
 @Constraint(validatedBy = NoDuplicateOwnerValidator.class)
 @Documented
 public @interface NoDuplicateOwner {
+
+    String message() default "The collection contains Ownables with the same owner";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
