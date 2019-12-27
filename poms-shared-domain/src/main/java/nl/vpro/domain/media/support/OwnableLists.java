c39edcc5171677a2baaeb5c9018cb2dea87d16ef
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-12-27 16:22:43 +0100

    details.

--- poms-shared-domain/src/main/java/nl/vpro/domain/media/support/OwnableLists.java
+++ poms-shared-domain/src/main/java/nl/vpro/domain/media/support/OwnableLists.java
@@ -13,6 +13,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class OwnableLists {
 
 
+    /**
+     * TODO: I think this may be superseded by using {@link @NoDuplicateOwner}
+     */
     public static <T extends Ownable> boolean containsDuplicateOwner(Iterable<T> newValues){
         if (newValues != null) {
             Map<OwnerType, AtomicInteger> counts = new HashMap<>();
@@ -33,7 +36,9 @@ public class OwnableLists {
      * @return the element matching the given owner or the one to display if nothing matches.
      */
     public static <T extends Ownable> Optional<T>  filterByOwner(SortedSet<T> ownableSet, OwnerType owner){
-        if(ownableSet == null || ownableSet.isEmpty()) return Optional.empty();
+        if(ownableSet == null || ownableSet.isEmpty()) {
+            return Optional.empty();
+        }
         for (T intention : ownableSet) {
             if (intention.getOwner() == owner) {
                 return Optional.of(intention);
