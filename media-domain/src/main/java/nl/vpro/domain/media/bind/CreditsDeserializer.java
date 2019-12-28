8e19dccf2c5f734827284f58a9254880ae5b5c15
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-12-28 23:36:51 +0100

    javadoc.

--- media-domain/src/main/java/nl/vpro/domain/media/bind/CreditsDeserializer.java
+++ media-domain/src/main/java/nl/vpro/domain/media/bind/CreditsDeserializer.java
@@ -9,8 +9,18 @@ import com.fasterxml.jackson.databind.*;
 import nl.vpro.domain.media.*;
 
 /**
+ * Resolves polymorphism of credits. Make sure to unmarshal as a {@link Person} if that is appropriate, and to {@link Name} otherwise.
+ *
+ * Using {@link com.fasterxml.jackson.annotation.JsonTypeId} would have been possible too, but that would make the 'objectType' required, and we have to republish everything.
+ *
+ * There is not real reason, it is simple to recognize by other fields wether we want a Person or not.
+ *
+ * This class is added as a JsonDeserializer only on {@link Credits}, and to avoid infinite recursion, in all extentions this is overridden by the default again.
+ *
+ * We do however include {@link Credits#getObjectType()} for the serializer, so other implementors (perhaps we in the future) may choose to switch on that after all.
+ *
  * @author Michiel Meeuwissen
- * @since ...
+ * @since 5.12
  */
 public class CreditsDeserializer extends JsonDeserializer<Credits> {
     @Override
