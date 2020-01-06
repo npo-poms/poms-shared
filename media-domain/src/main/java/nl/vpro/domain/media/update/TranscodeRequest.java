71f2b2b2602e5b7c33ab99e287a3e7eecb19f13c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-01-06 17:27:20 +0100

    Details.

--- media-domain/src/main/java/nl/vpro/domain/media/update/TranscodeRequest.java
+++ media-domain/src/main/java/nl/vpro/domain/media/update/TranscodeRequest.java
@@ -1,8 +1,6 @@
 package nl.vpro.domain.media.update;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
+import lombok.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,9 +9,7 @@ import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.*;
 
 import nl.vpro.domain.Displayable;
-import nl.vpro.domain.media.Encryption;
-import nl.vpro.domain.media.MediaType;
-import nl.vpro.domain.media.MediaIdentifiable;
+import nl.vpro.domain.media.*;
 
 /**
  * @author Michiel Meeuwissen
@@ -26,7 +22,6 @@ import nl.vpro.domain.media.MediaIdentifiable;
 @XmlType(name = "transcodeType")
 @XmlRootElement(name = "transcode")
 public class TranscodeRequest implements MediaIdentifiable {
-
 
     @NotNull
     @XmlAttribute
