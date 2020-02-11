8aee6fe42ce7d3ceecc5150345bf5135a481bb16
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-02-11 16:17:00 +0100

    persistence dependenc is optional, do that didn't test.

--- media-domain/src/main/java/nl/vpro/domain/media/gtaa/GTAARecord.java
+++ media-domain/src/main/java/nl/vpro/domain/media/gtaa/GTAARecord.java
@@ -4,13 +4,12 @@ package nl.vpro.domain.media.gtaa;
 import lombok.*;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.persistence.*;
 
 import nl.vpro.validation.NoHtml;
-
-import static nl.vpro.persistence.StringListConverter.INSTANCE;
 
 /**
  * The GTAARecord represents a record from the GTAA service.
@@ -47,17 +46,32 @@ public class GTAARecord implements Serializable {
     private GTAARecord(String uri, GTAAStatus status, String name, List<String> scopeNotes) {
 
         this.name = name;
-        this.scopeNotes = scopeNotes != null ? INSTANCE.convertToDatabaseColumn(scopeNotes) : "";
+        this.scopeNotes = scopeNotes != null ? convertToDatabaseColumn(scopeNotes) : "";
         this.uri = uri;
         this.status = status;
     }
 
     public List<String> getScopeNotes() {
-        return INSTANCE.convertToEntityAttribute(scopeNotes);
+        return convertToEntityAttribute(scopeNotes);
     }
 
     public void setScopeNotes(List<String> scopeNotes) {
-        this.scopeNotes = INSTANCE.convertToDatabaseColumn(scopeNotes);
+        this.scopeNotes = convertToDatabaseColumn(scopeNotes);
     }
 
+    private static final String SPLIT_CHAR = "\t";
+
+    private static  String convertToDatabaseColumn(List<String> stringList) {
+        if (stringList == null || stringList.isEmpty()) {
+            return null;
+        }
+        return String.join(SPLIT_CHAR, stringList);
+    }
+
+    private static  List<String> convertToEntityAttribute(String string) {
+        if (string == null || string.isEmpty()) {
+            return null;
+        }
+        return Arrays.asList(string.split(SPLIT_CHAR));
+    }
 }
