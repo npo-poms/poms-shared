dcd6b4334cb1931e4c6974f91837719cd3fb300d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-02-28 16:43:01 +0100

    backport MSE-4734

--- user-domain/src/main/java/nl/vpro/domain/user/Trusted.java
+++ user-domain/src/main/java/nl/vpro/domain/user/Trusted.java
@@ -1,5 +1,9 @@
 package nl.vpro.domain.user;
 
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Objects;
+
 import static nl.vpro.domain.Roles.SUPERPROCESS_ROLE;
 import static nl.vpro.domain.Roles.SYSTEM_ROLE;
 
@@ -17,12 +21,16 @@ public interface Trusted {
 
     String[] getRoles();
 
-    static Trusted of(String principal, String... roles) {
+    /**
+     * The last login valid for this user. Default to {@link Instant#now()}, but a {@link #copy()} will make this undynamic
+     */
+    Instant getLastLogin();
+
+    static Trusted of(Instant login, String principal, String... roles) {
         return new Trusted() {
             @Override
             public String getPrincipal() {
                 return principal;
-
             }
 
             @Override
@@ -35,10 +43,36 @@ public interface Trusted {
             }
 
             @Override
+            public Instant getLastLogin() {
+                return login == null ? Instant.now() : login;
+            }
+
+            @Override
             public boolean equals(Object o) {
                 return o instanceof Trusted && ((Trusted) o).getPrincipal().equals(principal);
+            }
+            @Override
+            public int hashCode() {
+                return Objects.hashCode(principal);
+            }
+
+            @Override
+            public String toString() {
+                return "Trusted:" + principal + ":" + Arrays.asList(getRoles());
             }
 
         };
     }
+
+    static Trusted of(String principal, String... roles) {
+        return of(null, principal, roles);
+    }
+    default Trusted of(Instant login) {
+        return of(login, getPrincipal(), getRoles());
+    }
+    default Trusted copy() {
+        return of(getLastLogin());
+    }
+
+
 }
