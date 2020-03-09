61007fa393b0af524276b1a7753f40ff38720903
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-03-09 10:20:59 +0100

    Refactoring security. A.o. targeting PIS-14

--- user-domain/src/main/java/nl/vpro/domain/user/Editors.java
+++ user-domain/src/main/java/nl/vpro/domain/user/Editors.java
@@ -3,7 +3,6 @@ package nl.vpro.domain.user;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
-import java.util.function.Function;
 
 import javax.validation.Validation;
 import javax.validation.Validator;
@@ -15,39 +14,24 @@ import javax.validation.Validator;
 @Slf4j
 public class Editors {
 
-    private static Validator VALIDATOR = Validation.buildDefaultValidatorFactory().getValidator();
+    private static final Validator VALIDATOR = Validation.buildDefaultValidatorFactory().getValidator();
 
 
-    public static Editor mapWorksFor( List<Organization> organizations, Editor user, Function<Organization, Organization> consumer) {
-        for (Organization organization : organizations) {
-            if (! VALIDATOR.validate(organization).isEmpty()) {
-                log.warn("The organization {} for {} is not valid. Ignoring", organization, user);
-                continue;
-            }
-
-            if (organization.getDisplayName() == null) {
-                organization.setDisplayName(organization.getId());
-            }
-            if (organization instanceof Broadcaster) {
-                Broadcaster existing = (Broadcaster) consumer.apply(organization);
-                if (existing != null) {
-                    user.addBroadcaster(existing);
-                } else {
-                    log.warn("Cannot add broadcaster {} to user {} because this broadcaster does not exist", organization, user);
+    public static Editor mapWorksFor( List<Organization> organizations, Editor user) {
+        if (organizations != null) {
+            for (Organization organization : organizations) {
+                if (!VALIDATOR.validate(organization).isEmpty()) {
+                    log.warn("The organization {} for {} is not valid. Ignoring", organization, user);
+                    continue;
                 }
-            } else if (organization instanceof Portal) {
-                Portal portal = (Portal) consumer.apply(organization);
-                user.addPortal(portal);
-            } else if (organization instanceof ThirdParty) {
-                ThirdParty thirdParty = (ThirdParty) consumer.apply(organization);
-                user.addThirdParty(thirdParty);
-            } else {
-                throw new RuntimeException("Unknown organization type: " + organization);
+
+                if (organization.getDisplayName() == null) {
+                    organization.setDisplayName(organization.getId());
+                }
+                user.addOrganization(organization);
             }
+            log.debug("User works for : {}", user.getOrganizations());
         }
-
-        log.debug("User works for : {}", user.getOrganizations());
-
         return user;
     }
 }
