d99ad6cb754bd3650d1c7df9f7c228c8f290bad5
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-03-09 09:47:39 +0100

    They fixed the certificate.

--- media-nep/src/test/java/nl/vpro/nep/service/impl/NEPGatekeeperServiceImplITest.java
+++ media-nep/src/test/java/nl/vpro/nep/service/impl/NEPGatekeeperServiceImplITest.java
@@ -21,21 +21,26 @@ public class NEPGatekeeperServiceImplITest {
     NEPGatekeeperServiceImpl gatekeeperService = new NEPGatekeeperServiceImpl(NEPTest.PROPERTIES);
 
 
-       @Test
-       public void testGet() {
-           WorkflowExecution transcodeStatus = gatekeeperService.getTranscodeStatus("e51b3502-f653-4ac7-a504-2853122add77").get();
-           log.info("{}", transcodeStatus);
-       }
+    @Test
+    public void testGet() {
+        WorkflowExecution transcodeStatus = gatekeeperService.getTranscodeStatus("e51b3502-f653-4ac7-a504-2853122add77").get();
+        log.info("{}", transcodeStatus);
+    }
+
+
+
+    String mid = "WO_VPRO_783763";
+
 
 
     @Test
-       public void test() {
-           @NonNull Iterator<WorkflowExecution> i = gatekeeperService.getTranscodeStatuses(null, null, Instant.now().minus(Duration.ofHours(2)), 200L);
-           while (i.hasNext()) {
-               log.info("{}", i.next());
-           }
+    public void test() {
+        @NonNull Iterator<WorkflowExecution> i = gatekeeperService.getTranscodeStatuses(null, null, Instant.now().minus(Duration.ofHours(2)), 200L);
+        while (i.hasNext()) {
+            log.info("{}", i.next());
+        }
 
-       }
+    }
 
 
 
