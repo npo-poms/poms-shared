9562e8e1a3e1b21c1ad0fe0812b349f18bf1fceb
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-03-12 10:14:10 +0100

    javadoc.

--- media-domain/src/main/java/nl/vpro/domain/media/gtaa/GTAAManaged.java
+++ media-domain/src/main/java/nl/vpro/domain/media/gtaa/GTAAManaged.java
@@ -2,7 +2,18 @@ package nl.vpro.domain.media.gtaa;
 
 import java.util.List;
 
+import com.google.common.annotations.Beta;
+
 /**
+ * An object that is 'GTAA managed' represents a link to  meta data that is synced wit the thesaurus of Beeld en Geluid.
+ *
+ * The link to this system is in {@link #getGtaaUri()}
+ *
+ * This object is fully modifiable though that does not make sense in all situations, because the data cannot be actually modified since it is maintained by the GTAA.
+ *
+ * But for marshalling and testing purposes it can be useful. But this interface may still be split up.
+ *
+ *
  * @author Michiel Meeuwissen
  * @since 5.12
  */
@@ -10,10 +21,12 @@ public interface GTAAManaged {
 
     String getName();
 
+    @Beta
     void setName(String name);
 
     List<String> getScopeNotes();
 
+    @Beta
     void setScopeNotes(List<String> scopeNotes);
 
     /**
@@ -28,6 +41,7 @@ public interface GTAAManaged {
      */
     GTAAStatus getGtaaStatus();
 
+    @Beta
     void setGtaaStatus(GTAAStatus status);
 
 }
