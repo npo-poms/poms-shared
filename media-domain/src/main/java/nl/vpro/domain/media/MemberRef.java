232f9dfa84a36ed2d46040535bd7272342f04040
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-03-18 20:06:57 +0100

    errorneous merge

--- media-domain/src/main/java/nl/vpro/domain/media/MemberRef.java
+++ media-domain/src/main/java/nl/vpro/domain/media/MemberRef.java
@@ -5,23 +5,8 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
-<<<<<<< HEAD
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
-import javax.persistence.Transient;
-=======
 import javax.persistence.*;
 import javax.persistence.Entity;
->>>>>>> 93a3c31eb... Fixed confusing toString.
 import javax.validation.constraints.NotNull;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAccessType;
