32f9bb88e4f848fa03ecbf0ab3b1903bfa13470d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-04-17 15:12:10 +0200

    Collected POMs relevant headers to one utility class.

---
+++ poms-shared/src/main/java/nl/vpro/util/Headers.java
@@ -0,0 +1,42 @@
+package nl.vpro.util;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.13
+ */
+public class Headers {
+
+    private  Headers() {
+
+    }
+
+    /**
+     * A request header signifying the preferred {@link OwnerType}
+     */
+    public static final String OWNER_TYPE_HEADER = "X-Poms-CurrentOwner";
+
+    /**
+     * A prefix for response headers related to NPO
+     */
+    public static final String X_NPO                        = "X-NPO";
+
+    /**
+     * The request was authenticated, the response acknowledges that via this header
+     */
+    public static final String NPO_CURRENT_USER             = X_NPO + "-currentUser";
+
+    /**
+     * The request was authenticated, the response informs via this header which is the current employer of the authenticated user
+     */
+    public static final String NPO_CURRENT_CURRENT_EMPLOYER = X_NPO + "-currentEmployer";
+     /**
+      * The request was authenticated, the response informs via this header on which broadcasters the authenticated user has rights
+     */
+    public static final String NPO_BROADCASTERS             = X_NPO + "-broadcasters";
+      /**
+       * The request was authenticated, the response informs via this header on which roles were assigned to the authenticated user
+     */
+    public static final String NPO_ROLES                    = X_NPO + "-roles";
+}
+
+
