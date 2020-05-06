dc3f2a9d32a91953810cdeb586b921f699ea8331
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-05-06 12:00:37 +0200

    javadoc

--- api-domain/src/main/java/nl/vpro/util/Truthiness.java
+++ api-domain/src/main/java/nl/vpro/util/Truthiness.java
@@ -5,11 +5,25 @@ import java.util.function.BooleanSupplier;
 /**
  * @author Michiel Meeuwissen
  * @since ...
+ * TODO: move to vpro-shared.
  */
 public enum Truthiness implements BooleanSupplier {
+    /**
+     * Definitely true
+     */
     TRUE(true),
+    /**
+     * We are not sure, but we guess it's true
+     */
     PROBABLY(true),
+    /**
+     * We really don't know
+     */
     UNKNOWN(true),
+    /**
+     * It might be true, but provisionally we think it's not.
+     * E.g. the condition may become true for some later reason.
+     */
     MAYBE_NOT(true),
     FALSE(false);
 
