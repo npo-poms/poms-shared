a59ba03c24205abee04d6fdf4653497e3053166a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-05-06 11:47:02 +0200

    Fixing test cases.

---
+++ api-domain/src/main/java/nl/vpro/domain/api/SimpleRangeMatcher.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2013 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.domain.api;
+
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 5.13
+ */
+@XmlTransient
+public abstract class SimpleRangeMatcher<V extends Comparable<V>> extends RangeMatcher<V, V> {
+    public SimpleRangeMatcher() {
+    }
+
+    protected SimpleRangeMatcher(V begin, V end) {
+        super(begin, end);
+    }
+
+    protected SimpleRangeMatcher(V begin, V end, Boolean inclusiveEnd) {
+        super(begin, end, inclusiveEnd);
+    }
+
+    protected SimpleRangeMatcher(V begin, V end, Boolean inclusiveEnd, Match match) {
+        super(begin, end, inclusiveEnd, match);
+    }
+
+    @Override
+    public boolean test(V t) {
+        return valueTest(t);
+    }
+
+}
