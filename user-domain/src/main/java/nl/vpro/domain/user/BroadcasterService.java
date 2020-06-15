7d71a4e4e6142825b3332d4c2cc5a545b0f93061
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-06-15 23:05:43 +0200

    Added default method #findForIds

--- user-domain/src/main/java/nl/vpro/domain/user/BroadcasterService.java
+++ user-domain/src/main/java/nl/vpro/domain/user/BroadcasterService.java
@@ -4,6 +4,8 @@
  */
 package nl.vpro.domain.user;
 
+import java.util.Optional;
+
 import nl.vpro.domain.NotFoundException;
 
 public interface BroadcasterService extends OrganizationService<Broadcaster> {
@@ -43,6 +45,25 @@ public interface BroadcasterService extends OrganizationService<Broadcaster> {
         }
         throw new NotFoundException(id, "No broadcaster found for nebo broadcaster id");
 
+    }
+
+    /**
+     * @since 5.13
+     */
+    default Optional<Broadcaster> findForIds(String value) {
+        try {
+            return Optional.of(findForWhatsOnId(value));
+        } catch (NotFoundException e) {
+            try {
+                return Optional.of(findForMisId(value));
+            } catch (NotFoundException e2) {
+                try {
+                    return Optional.of(findForNeboId(value));
+                } catch (Exception e1) {
+                    return Optional.empty();
+                }
+            }
+        }
     }
 
 }
