ed1ee62e088d365e03353092121d736af1e6a9aa
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-07-01 13:04:08 +0200

    Added setters to interface too.

--- media-domain/src/main/java/nl/vpro/domain/media/RecursiveParentChildRelation.java
+++ media-domain/src/main/java/nl/vpro/domain/media/RecursiveParentChildRelation.java
@@ -1,6 +1,7 @@
 package nl.vpro.domain.media;
 
 import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  *
@@ -12,9 +13,30 @@ public interface RecursiveParentChildRelation extends ParentChildRelation {
 
 	SortedSet<RecursiveMemberRef> getMemberOf();
 
+	void setMemberOf(SortedSet<RecursiveMemberRef>  memberOf);
+
+	default SortedSet<RecursiveMemberRef> getOrCreateMemberOf() {
+		if (getMemberOf() == null) {
+			setMemberOf(new TreeSet<>());
+		}
+		return getMemberOf();
+	}
+
 	SortedSet<RecursiveMemberRef> getEpisodeOf();
 
+	void setEpisodeOf(SortedSet<RecursiveMemberRef> episodeOf);
+
+	default SortedSet<RecursiveMemberRef> getOrCreateEpisodeOf() {
+		if (getEpisodeOf() == null) {
+			setEpisodeOf(new TreeSet<>());
+		}
+		return getEpisodeOf();
+	}
+
+
 	RecursiveMemberRef getSegmentOf();
+
+	void setSegmentOf(RecursiveMemberRef recursiveMemberRef);
 
 
 }
