4e046a462b2815649dfb99693ef71ee6c135acfd
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2021-02-16 11:54:22 +0100

    Added display names for status.

--- media-nep/src/main/java/nl/vpro/nep/domain/ItemizerStatus.java
+++ media-nep/src/main/java/nl/vpro/nep/domain/ItemizerStatus.java
@@ -2,25 +2,30 @@ package nl.vpro.nep.domain;
 
 import lombok.Getter;
 
+import nl.vpro.domain.Displayable;
+
 /**
  * @author Michiel Meeuwissen
  * @since 5.24
  */
-public enum ItemizerStatus {
-    COMPLETED(true),
-    RUNNING(false),
-    QUEUED(false),
-    STARTED(false),
-    RETRY(false),
-    UNKNOWN(true),
-    ERROR(true),
-    CANCELLED(true);
+public enum ItemizerStatus implements Displayable {
+    COMPLETED(true, "Klaar"),
+    RUNNING(false, "Bezig"),
+    QUEUED(false, "Gequeued"),
+    STARTED(false, "Gestart"),
+    RETRY(false, "Opnieuw proberen"),
+    UNKNOWN(true, "Onbekend"),
+    ERROR(true, "Fout"),
+    CANCELLED(true, "Afgebroken");
 
     @Getter
     private final boolean endStatus;
-;
 
-    ItemizerStatus(boolean endStatus) {
+    @Getter
+    private final String displayName;
+
+    ItemizerStatus(boolean endStatus, String displayName) {
         this.endStatus = endStatus;
+        this.displayName = displayName;
     }
 }
