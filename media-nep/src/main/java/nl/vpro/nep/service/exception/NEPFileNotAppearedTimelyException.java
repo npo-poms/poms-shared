715bf014934c454309992dbe2933fa9c30e04c2d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2021-03-12 16:56:30 +0100

    MSE-5073

--- media-nep/src/main/java/nl/vpro/nep/service/exception/NEPFileNotAppearedTimelyException.java
+++ media-nep/src/main/java/nl/vpro/nep/service/exception/NEPFileNotAppearedTimelyException.java
@@ -2,9 +2,9 @@ package nl.vpro.nep.service.exception;
 
 /**
  * @author Michiel Meeuwissen
- * @since ...
+ * @since 5.25
  */
-public class NEPFileNotAppearedTimelyException extends NEPException {
+public class NEPFileNotAppearedTimelyException extends NEPFileNotFoundException {
     public NEPFileNotAppearedTimelyException(Exception originalException, String errorMessage) {
         super(originalException, errorMessage);
     }
