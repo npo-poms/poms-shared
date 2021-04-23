657ba0763d57b40582adff1e096e3ce3f9b24fed
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2021-04-23 17:00:29 +0200

    details

--- release.sh
+++ release.sh
@@ -33,7 +33,7 @@ git checkout REL-$SNAPSHOT_VERSION
 git branch -l
 # this command will make and deploy the actual release.
 # echo it for review, it then can be copy/pasted to execute
-echo mvn -Pdeploy release:prepare release:perform -Dtag=REL-$TARGET_VERSION =DreleaseVersion=$TARGET_VERSION -DdevelopmentVersion=$BRANCH_DEVELOPMENT_VERSION
-echo git push --set-upstream origin REL-$SNAPSHOT_VERSION ; git checkout master
+echo "mvn -Pdeploy release:prepare release:perform -Dtag=REL-$TARGET_VERSION -DreleaseVersion=$TARGET_VERSION -DdevelopmentVersion=$BRANCH_DEVELOPMENT_VERSION"
+echo "git push --set-upstream origin REL-$SNAPSHOT_VERSION ; git checkout master"
 
 
