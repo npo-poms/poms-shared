dc64474034071e3613be2fac49b0bd35a17dcd3f
Michiel Meeuwissen <git@meeuw.org>
2021-06-03 16:15:00 +0200

    Details only.

--- media-domain/src/test/java/nl/vpro/domain/media/MediaObjectLockerTest.java
+++ media-domain/src/test/java/nl/vpro/domain/media/MediaObjectLockerTest.java
@@ -7,46 +7,35 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import nl.vpro.util.locker.ObjectLockerAdmin;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * @author Michiel Meeuwissen
- * @since ...
  */
 @Slf4j
 class MediaObjectLockerTest {
+
     @BeforeEach
     public void setup() {
         ObjectLockerAdmin.JMX_INSTANCE.setMaxLockAcquireTime(Duration.ofSeconds(10).toString());
         ObjectLockerAdmin.JMX_INSTANCE.setStrictlyOne(false);
     }
 
-
-
     @Test
     public void strictlyOne() {
         ObjectLockerAdmin.JMX_INSTANCE.setStrictlyOne(true);
         final List<String> result = new ArrayList<>();
 
-        Assertions.assertThatThrownBy(() -> {
-            MediaObjectLocker.withMidLock("mid_1", "test", new Runnable() {
-                @Override
-                public void run() {
-                    MediaObjectLocker.withMidLock("mid_2", "test sub", new Runnable() {
-                        @Override
-                        public void run() {
-                            result.add("bla");
-                        }
-                    });
-                }
-            });
-        }).isInstanceOf(IllegalStateException.class);
+        assertThatThrownBy(() -> MediaObjectLocker.withMidLock("mid_1", "test", () ->
+            MediaObjectLocker.withMidLock("mid_2", "test sub", () -> {
+                result.add("bla");
+            }))).isInstanceOf(IllegalStateException.class);
         assertThat(result).isEmpty();
 
     }
@@ -59,9 +48,9 @@ class MediaObjectLockerTest {
         MediaObjectLocker.withMidLock("mid_1", "test", new Runnable() {
             @Override
             public void run() {
-                MediaObjectLocker.withCorrelationLock(MediaIdentifiable.Correlation.crid("crid_1"), "test sub", new Callable() {
+                MediaObjectLocker.withCorrelationLock(MediaIdentifiable.Correlation.crid("crid_1"), "test sub", new Callable<Object>() {
                     @Override
-                    public Object call() throws Exception {
+                    public Object call() {
                         result.add("bla");
                         return null;
                     }
