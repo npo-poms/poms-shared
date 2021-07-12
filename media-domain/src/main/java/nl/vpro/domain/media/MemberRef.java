e3f751300a4449608cf09671e16016167894700a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2021-07-12 16:49:53 +0200

    some schema fixes as proposed by John

--- media-domain/src/main/java/nl/vpro/domain/media/MemberRef.java
+++ media-domain/src/main/java/nl/vpro/domain/media/MemberRef.java
@@ -421,6 +421,7 @@ public class MemberRef implements Identifiable<Long>, Comparable<MemberRef>, Ser
     }
 
     @XmlAttribute(name = "index")
+    @XmlSchemaType(name = "nonNegativeInteger")
     public Integer getNumber() {
         return number;
     }
