d4ec0b194934a997ea780087ff8d812f4e96b94a
Michiel Meeuwissen <git@meeuw.org>
2021-07-16 20:02:56 +0200

    spacing only

--- media-domain/src/main/java/nl/vpro/domain/media/MediaObjectLocker.java
+++ media-domain/src/main/java/nl/vpro/domain/media/MediaObjectLocker.java
@@ -140,9 +140,10 @@ public class MediaObjectLocker {
         MediaIdentifiable.Correlation lock,
         @NonNull String reason,
         @NonNull Callable<T> callable) {
-        return ObjectLocker.withObjectLock(lock, reason, callable, LOCKED_MEDIA, (
-            o1, o2) ->
-            o1 instanceof MediaIdentifiable.Correlation && Objects.equals(((MediaIdentifiable.Correlation) o1).getType(), o2.getType()));
+        return ObjectLocker.withObjectLock(lock, reason, callable, LOCKED_MEDIA,
+            (o1, o2) ->
+                o1 instanceof MediaIdentifiable.Correlation && Objects.equals(((MediaIdentifiable.Correlation) o1).getType(), o2.getType())
+        );
     }
 
 }
