a1786cec27e9ec5f84f9d1d2f2ead9b65f560b01
Michiel Meeuwissen <git@meeuw.org>
2021-08-26 14:03:19 +0200

    Logging.

--- media-tva/src/main/java/nl/vpro/media/tva/saxon/extension/FindNetFunction.java
+++ media-tva/src/main/java/nl/vpro/media/tva/saxon/extension/FindNetFunction.java
@@ -14,8 +14,7 @@ import net.sf.saxon.trans.XPathException;
 import net.sf.saxon.value.SequenceType;
 import net.sf.saxon.value.StringValue;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
@@ -38,8 +37,16 @@ public class FindNetFunction extends ExtensionFunctionDefinition {
 
     private final Supplier<Collection<Net>> netsSupplier;
 
-    private final Map<String, AtomicInteger> warns = new ConcurrentHashMap<>();
+    private final static Map<String, AtomicInteger> WARNS = new ConcurrentHashMap<>();
 
+    private final static Set<String> AKNOWLEDGED;
+
+    static {
+        Set<String> aknowledged = new HashSet<>();
+        aknowledged.add("NEDERLAND 1");
+        AKNOWLEDGED = Collections.unmodifiableSet(aknowledged);
+
+    }
     @Inject
     public FindNetFunction(@Named("netsSupplier") Supplier<Collection<Net>> netsSupplier) {
         this.netsSupplier = netsSupplier;
@@ -71,9 +78,11 @@ public class FindNetFunction extends ExtensionFunctionDefinition {
                         return new StringValue(net.getId());
                     }
                 }
-                AtomicInteger occurence = warns.computeIfAbsent(value, (v) -> new AtomicInteger(0));
-                Level level = occurence.getAndIncrement() % 100 == 0 ? Level.WARN : Level.DEBUG;
-                Slf4jHelper.log(log, level, "No such net {} (#{}, now returning empty string, which indicates that it can be ignored)",  value, occurence.get());
+                AtomicInteger occurence = WARNS.computeIfAbsent(value, (v) -> new AtomicInteger(0));
+                boolean aknowledged =  AKNOWLEDGED.contains(value);
+                Level level = occurence.incrementAndGet() % 100 == 1 ?
+                   aknowledged ? Level.INFO : Level.WARN : Level.DEBUG;
+                Slf4jHelper.log(log, level, "No such net {} (#{}, now returning empty string, which indicates that it can be ignored){}",  value, occurence.get(), aknowledged ? " (aknowledged)" : "");
                 return new StringValue("");
             }
         };
