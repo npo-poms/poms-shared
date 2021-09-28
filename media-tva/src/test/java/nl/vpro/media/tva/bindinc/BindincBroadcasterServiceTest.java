6ddcc5c4a47969b42d1ee4108aa41a1c876cb435
Michiel Meeuwissen <git@meeuw.org>
2021-09-28 12:08:40 +0200

    Using wiremock to cut dependency

--- media-tva/src/test/java/nl/vpro/media/tva/bindinc/BindincBroadcasterServiceTest.java
+++ media-tva/src/test/java/nl/vpro/media/tva/bindinc/BindincBroadcasterServiceTest.java
@@ -1,20 +1,42 @@
 package nl.vpro.media.tva.bindinc;
 
+import ru.lanwen.wiremock.ext.WiremockResolver;
+import ru.lanwen.wiremock.ext.WiremockUriResolver;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
 
 import nl.vpro.domain.user.Broadcaster;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Michiel Meeuwissen
  * @since 5.33
  */
+@ExtendWith({
+    WiremockResolver.class,
+    WiremockUriResolver.class
+})
 class BindincBroadcasterServiceTest {
 
     @Test
-    public void find() {
-        BindincBroadcasterService bs = new BindincBroadcasterService("https://poms.omroep.nl/broadcasters/");
+    public void find(@WiremockResolver.Wiremock WireMockServer server, @WiremockUriResolver.WiremockUri String uri) throws IOException {
+
+        server.stubFor(get(urlPathEqualTo("/broadcasters/")).willReturn(okForContentType("text/plain", IOUtils.toString(Objects.requireNonNull(getClass().getResourceAsStream("/broadcasters.properties")), StandardCharsets.UTF_8))));
+        server.stubFor(get(urlPathEqualTo("/broadcasters/mis")).willReturn(okForContentType("text/plain", IOUtils.toString(Objects.requireNonNull(getClass().getResourceAsStream("/broadcasters.mis.properties")), StandardCharsets.UTF_8))));
+        server.stubFor(get(urlPathEqualTo("/broadcasters/whats_on")).willReturn(okForContentType("text/plain", IOUtils.toString(Objects.requireNonNull(getClass().getResourceAsStream("/broadcasters.mis.properties")), StandardCharsets.UTF_8))));
+
+
+        BindincBroadcasterService bs = new BindincBroadcasterService(uri  + "/broadcasters/");
 
         assertThat(bs.find("BNNVARA")).isNotNull();
 
@@ -23,6 +45,8 @@ class BindincBroadcasterServiceTest {
         assertThat(bs.find("OMROP FRYSLAN")).isEqualTo(new Broadcaster("ROFR", "Omrop Fryslân"));
 
     }
+
+
 
 
 }
