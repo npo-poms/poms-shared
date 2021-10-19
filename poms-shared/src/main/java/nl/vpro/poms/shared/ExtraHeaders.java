753480bd936abfdcd5d5eb1d408a0f3ba14b3b52
Michiel Meeuwissen <git@meeuw.org>
2021-10-19 17:54:18 +0200

    Javadoc

--- poms-shared/src/main/java/nl/vpro/poms/shared/ExtraHeaders.java
+++ poms-shared/src/main/java/nl/vpro/poms/shared/ExtraHeaders.java
@@ -10,6 +10,8 @@ import org.slf4j.helpers.MessageFormatter;
 import nl.vpro.util.Pair;
 
 /**
+ * Maintains thread locals to be able to set 'extra' response headers.
+ *
  * @author Michiel Meeuwissen
  */
 @Slf4j
