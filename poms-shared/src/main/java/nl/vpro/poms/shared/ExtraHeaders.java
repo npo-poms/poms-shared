d252d77e8ba4c7cae60e31cf492736f76a0b7b10
Michiel Meeuwissen <git@meeuw.org>
2021-10-19 16:54:40 +0200

    Smarter extra headers.

--- poms-shared/src/main/java/nl/vpro/poms/shared/ExtraHeaders.java
+++ poms-shared/src/main/java/nl/vpro/poms/shared/ExtraHeaders.java
@@ -1,28 +1,48 @@
 package nl.vpro.poms.shared;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.*;
+
+import org.slf4j.helpers.FormattingTuple;
+import org.slf4j.helpers.MessageFormatter;
 
 import nl.vpro.util.Pair;
 
 /**
  * @author Michiel Meeuwissen
  */
+@Slf4j
 public class ExtraHeaders {
 
     static final ThreadLocal<List<Pair<String, String>>> EXTRA_HEADERS = ThreadLocal.withInitial(ArrayList::new);
+    static final ThreadLocal<Boolean> EXTRA_HEADERS_USED = ThreadLocal.withInitial(() -> Boolean.FALSE);
 
     private  ExtraHeaders() {
     }
 
     public static List<Pair<String, String>> get() {
-        return EXTRA_HEADERS.get();
+        EXTRA_HEADERS_USED.set(Boolean.TRUE);
+        return Collections.unmodifiableList(EXTRA_HEADERS.get());
     }
 
     public static void remove() {
         EXTRA_HEADERS.remove();
+        EXTRA_HEADERS_USED.remove();
     }
 
-    public static void warn(String message) {
-        EXTRA_HEADERS.get().add(Pair.of(Headers.NPO_WARNING_HEADER, message));
+    public static String warn(String message, Object... args) {
+        return add(Headers.NPO_WARNING_HEADER, message, args);
     }
+
+    public static String add(String header, String message, Object... args) {
+        if (EXTRA_HEADERS_USED.get()) {
+            log.warn("Headers already used");
+        }
+        FormattingTuple ft = MessageFormatter.arrayFormat(message, args);
+        String m = ft.getMessage();
+        EXTRA_HEADERS.get().add(Pair.of(header, m));
+        return m;
+    }
+
 }
