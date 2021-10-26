41723bce1f4d1e6e72bccf3077594602345c71b6
Michiel Meeuwissen <git@meeuw.org>
2021-10-26 10:47:11 +0200

    Added some logging only (MSE-5164)

--- poms-npoplayer/src/main/java/nl/vpro/npoplayer/service/NPOPlayerApiServiceImpl.java
+++ poms-npoplayer/src/main/java/nl/vpro/npoplayer/service/NPOPlayerApiServiceImpl.java
@@ -4,24 +4,26 @@
  */
 package nl.vpro.npoplayer.service;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import nl.vpro.domain.npoplayer.NPOPlayerApiRequest;
-import nl.vpro.domain.npoplayer.NPOPlayerApiResponse;
-import nl.vpro.domain.npoplayer.NPOPlayerApiService;
+import nl.vpro.domain.npoplayer.*;
 import nl.vpro.npoplayer.NPOPlayerApiClient;
 
 /**
  * @author r.jansen
  */
 @Named
+@Slf4j
 public class NPOPlayerApiServiceImpl implements NPOPlayerApiService {
     private final NPOPlayerApiClient client;
 
     @Inject
     public NPOPlayerApiServiceImpl(NPOPlayerApiClient client) {
         this.client = client;
+        log.info("Created NPO Player Api Client: {}", client);
     }
 
     @Override
