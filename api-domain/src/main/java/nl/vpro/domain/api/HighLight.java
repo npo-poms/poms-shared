ab8d615648b8f2e058347f4968010c1acdda1656
Michiel Meeuwissen <git@meeuw.org>
2021-12-14 09:22:33 +0100

    Fixed typo.

--- api-domain/src/main/java/nl/vpro/domain/api/HighLight.java
+++ api-domain/src/main/java/nl/vpro/domain/api/HighLight.java
@@ -1,18 +1,16 @@
 package nl.vpro.domain.api;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlType;
 import java.util.Arrays;
 import java.util.List;
+
+import javax.xml.bind.annotation.*;
 
 /**
  * @author Michiel Meeuwissen
  * @since 2.0
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "hightlightType")
+@XmlType(name = "highlightType")
 public class HighLight {
 
 
