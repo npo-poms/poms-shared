52c09fad24a98dce175c00c3514a1950f701f9db
Michiel Meeuwissen <git@meeuw.org>
2022-02-06 11:47:21 +0100

    Test case failures.

--- media-domain/src/main/java/nl/vpro/domain/media/PomsValidCountry.java
+++ media-domain/src/main/java/nl/vpro/domain/media/PomsValidCountry.java
@@ -13,6 +13,15 @@ import org.meeuw.i18n.regions.validation.ValidRegion;
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+
+/**
+ * A meta annotation for country codes which are considered valid in poms.
+ *
+ * These are all former, officially assigned and most known user assigned countries.
+ * Additionally, the countries of UK are accepted (these are really subdivisions).
+ *
+ * @since 5.31
+ */
 @ValidRegion(classes = {Country.class}, includes = {"GB-ENG", "GB-NIR", "GB-SCT", "GB-WLS"})
 @ValidCountry(value = ValidCountry.OFFICIAL | ValidCountry.USER_ASSIGNED | ValidCountry.FORMER,
     excludes = {"XN"} // patent institute assigned
@@ -22,10 +31,6 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 @Constraint(validatedBy = {})
 @Documented
-/**
- * A meta annotation for country codes which are considered valid in poms.
- * @since 5.31
- */
 public @interface PomsValidCountry {
 
     String message() default "";
