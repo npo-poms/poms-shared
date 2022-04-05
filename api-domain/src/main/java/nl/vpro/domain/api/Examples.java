40986035636dd5a69d4eca65db8ebef8c9966c50
Michiel Meeuwissen <git@meeuw.org>
2022-04-05 10:03:11 +0200

    Fixing example.

--- api-domain/src/main/java/nl/vpro/domain/api/Examples.java
+++ api-domain/src/main/java/nl/vpro/domain/api/Examples.java
@@ -7,19 +7,13 @@ package nl.vpro.domain.api;
 public class Examples {
 
     private Examples() {
-
     }
 
     public static final String IDLIST_JSON = "[\"AVRO_1656037\", \"NCRV_1413393\"]";
     public static final String IDLIST_XML = "<api:idList xmlns:api=\"urn:vpro:api:2013\" >\n" +
-        "            \"    <api:id>AVRO_1656037</api:id>\\n\" +\n" +
-        "            \"    <api:id>NCRV_1413393</api:id>\\n\" +\n" +
-        "            \"</api:idList>";
-
-  /*  public static final Example IDLIST = {
-    @ExampleProperty(mediaType = "application/json", value = Examples.IDLIST_JSON),
-    @ExampleProperty(mediaType = "application/xml", value = Examples.IDLIST_XML)
-};*/
+        "    <api:id>AVRO_1656037</api:id>\n" +
+        "    <api:id>NCRV_1413393</api:id>\n" +
+        "</api:idList>";
 
     public static final String FORM_JSON = "{\n" +
         "    \"searches\" : {\n" +
@@ -28,12 +22,11 @@ public class Examples {
         "        }\n" +
         "    }\n" +
         "}";
-    public static final String FORM_XML = "{\n" +
-        "    \"searches\" : {\n" +
-        "        \"text\" : {\n" +
-        "                \"value\" : \"Argos\"\n" +
-        "        }\n" +
-        "    }\n" +
-        "}";
+
+    public static final String FORM_XML = "<api:mediaForm xmlns:pages=\"urn:vpro:pages:2013\" xmlns:api=\"urn:vpro:api:2013\" xmlns:media=\"urn:vpro:media:2009\">\n" +
+            "    <api:searches>\n" +
+            "        <api:text fuzziness=\"AUTO\" match=\"SHOULD\">bla</api:text>\n" +
+            "    </api:searches>\n" +
+            "</api:mediaForm>";
 
 }
