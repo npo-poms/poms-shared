b16ff66d9be8395f187867ca4046fca4c0cdc061
Michiel Meeuwissen <git@meeuw.org>
2022-04-19 14:28:05 +0200

    Using junit5 itself provided wiremock integration (and dropping ru.*!).

--- media-tva/src/test/java/nl/vpro/media/tva/bindinc/BindincBroadcasterServiceTest.java
+++ media-tva/src/test/java/nl/vpro/media/tva/bindinc/BindincBroadcasterServiceTest.java
@@ -1,17 +1,14 @@
 package nl.vpro.media.tva.bindinc;
 
-import ru.lanwen.wiremock.ext.WiremockResolver;
-import ru.lanwen.wiremock.ext.WiremockUriResolver;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 
 import nl.vpro.domain.user.Broadcaster;
 
@@ -22,31 +19,25 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Michiel Meeuwissen
  * @since 5.33
  */
-@ExtendWith({
-    WiremockResolver.class,
-    WiremockUriResolver.class
-})
+@WireMockTest
 class BindincBroadcasterServiceTest {
 
     @Test
-    public void find(@WiremockResolver.Wiremock WireMockServer server, @WiremockUriResolver.WiremockUri String uri) throws IOException {
+    public void find(WireMockRuntimeInfo wiremock) throws IOException {
 
-        server.stubFor(get(urlPathEqualTo("/broadcasters/")).willReturn(okForContentType("text/plain", IOUtils.toString(Objects.requireNonNull(getClass().getResourceAsStream("/broadcasters.properties")), StandardCharsets.UTF_8))));
-        server.stubFor(get(urlPathEqualTo("/broadcasters/mis")).willReturn(okForContentType("text/plain", IOUtils.toString(Objects.requireNonNull(getClass().getResourceAsStream("/broadcasters.mis.properties")), StandardCharsets.UTF_8))));
-        server.stubFor(get(urlPathEqualTo("/broadcasters/whats_on")).willReturn(okForContentType("text/plain", IOUtils.toString(Objects.requireNonNull(getClass().getResourceAsStream("/broadcasters.mis.properties")), StandardCharsets.UTF_8))));
+        stubFor(get(urlPathEqualTo("/broadcasters/")).willReturn(okForContentType("text/plain", IOUtils.toString(Objects.requireNonNull(getClass().getResourceAsStream("/broadcasters.properties")), StandardCharsets.UTF_8))));
+        stubFor(get(urlPathEqualTo("/broadcasters/mis")).willReturn(okForContentType("text/plain", IOUtils.toString(Objects.requireNonNull(getClass().getResourceAsStream("/broadcasters.mis.properties")), StandardCharsets.UTF_8))));
+        stubFor(get(urlPathEqualTo("/broadcasters/whats_on")).willReturn(okForContentType("text/plain", IOUtils.toString(Objects.requireNonNull(getClass().getResourceAsStream("/broadcasters.mis.properties")), StandardCharsets.UTF_8))));
 
 
-        BindincBroadcasterService bs = new BindincBroadcasterService(uri  + "/broadcasters/");
+        BindincBroadcasterService bs = new BindincBroadcasterService(wiremock.getHttpBaseUrl()  + "/broadcasters/");
 
         assertThat(bs.find("BNNVARA")).isNotNull();
 
         assertThat(bs.find("VER")).isEqualTo(new Broadcaster(""));
 
         assertThat(bs.find("OMROP FRYSLAN")).isEqualTo(new Broadcaster("ROFR", "Omrop Fryslân"));
-
     }
-
-
 
 
 }
