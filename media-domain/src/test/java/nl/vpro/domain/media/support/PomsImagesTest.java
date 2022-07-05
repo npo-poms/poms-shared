2a2e04926e165a6b887bcefbf4ec5a47d03298ef
Michiel Meeuwissen <git@meeuw.org>
2022-07-05 14:33:48 +0200

    Using 'of'

--- media-domain/src/test/java/nl/vpro/domain/media/support/PomsImagesTest.java
+++ media-domain/src/test/java/nl/vpro/domain/media/support/PomsImagesTest.java
@@ -41,7 +41,7 @@ class PomsImagesTest {
             "      \"lastModified\": 1639275163526,\n" +
             "      \"urn\": \"urn:vpro:media:image:122414908\"\n" +
             "    }", Image.class);
-        ImageMetadata imageMetadata = new ImageMetadataProvider.Wrapper<>(support).toImageMetadataWithSourceSet();
+        ImageMetadata imageMetadata = ImageMetadataProvider.of(support).toImageMetadataWithSourceSet();
         Jackson2TestUtil.roundTripAndSimilarAndEquals(imageMetadata, "{\n" +
             "  \"type\" : \"STILL\",\n" +
             "  \"title\" : \"2Doc:\",\n" +
