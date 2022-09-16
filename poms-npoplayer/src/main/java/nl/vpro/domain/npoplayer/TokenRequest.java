0b2d9d3857e6b6c238ae1c7ca7221683b29a61ad
Rico Jansen <r.jansen@vpro.nl>
2022-09-16 16:59:08 +0200

    API-556 jwt token support.

---
+++ poms-npoplayer/src/main/java/nl/vpro/domain/npoplayer/TokenRequest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2022 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.domain.npoplayer;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+import lombok.*;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * @author r.jansen
+ */
+@XmlRootElement
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlType(propOrder = {
+    "mid",
+    "age"
+})
+@JsonTypeName("tokenRequest")
+public class TokenRequest {
+    String mid;
+    String age;
+}
