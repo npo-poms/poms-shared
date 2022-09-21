169bf01a40d3dad602ab0b86f9000ff8b768cafa
Michiel Meeuwissen <git@meeuw.org>
2022-09-21 12:18:53 +0200

    javadoc.

--- poms-shared-domain/src/main/java/nl/vpro/domain/ChangeReport.java
+++ poms-shared-domain/src/main/java/nl/vpro/domain/ChangeReport.java
@@ -1,10 +1,14 @@
 package nl.vpro.domain;
 
+import java.util.function.BooleanSupplier;
+
 /**
+ * A small wrapper for a {@code boolean}, which can be used to keep track of whether
+ * some process results in changes, and things may need saving.
  * @author Michiel Meeuwissen
  * @since 5.10
  */
-public class ChangeReport {
+public class ChangeReport implements BooleanSupplier  {
 
     boolean hasChanges = false;
 
@@ -17,10 +21,19 @@ public class ChangeReport {
         return hasChanges;
     }
 
+    /**
+     * Marks the report has having changes, because of a change in given field.
+     * <p>
+     * Currently just calls {@link #change()},
+     */
     public void change(String field) {
-        // we may want to
+
         change();
     }
+
+    /**
+     * Marks the report has having changes.
+    */
     public void change() {
         hasChanges = true;
     }
@@ -30,7 +43,13 @@ public class ChangeReport {
         result.hasChanges = hasChanges || other.hasChanges;
         return result;
     }
+
     public void also(ChangeReport other ) {
         this.hasChanges = this.hasChanges | other.hasChanges;
+    }
+
+    @Override
+    public boolean getAsBoolean() {
+        return hasChanges;
     }
 }
