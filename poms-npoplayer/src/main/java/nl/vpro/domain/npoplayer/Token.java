0778ba27d0116615beeb18ffbd425206bbfee6b3
Rico Jansen <r.jansen@vpro.nl>
2022-09-21 15:33:08 +0200

    API-556 token/payload constructie volgens de code niet volgens de cods.

--- poms-npoplayer/src/main/java/nl/vpro/domain/npoplayer/Token.java
+++ poms-npoplayer/src/main/java/nl/vpro/domain/npoplayer/Token.java
@@ -1,7 +1,3 @@
-/*
- * Copyright (C) 2022 All rights reserved
- * VPRO The Netherlands
- */
 package nl.vpro.domain.npoplayer;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
@@ -11,25 +7,24 @@ import lombok.*;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
-/**
- * @author r.jansen
- */
 @XmlRootElement
 @Data
 @lombok.Builder(builderClassName = "Builder")
 @NoArgsConstructor
 @AllArgsConstructor
 @XmlType(propOrder = {
-    "prid",
-    "broadcaster",
-    "clientIp",
+    "iat",
+    "sub",
+    "iss",
+    "cip",
     "age"
 })
 @JsonTypeName("response")
-public class TokenPayload {
-    String prid;
-    String broadcaster;
-    String clientIp;
-    String age;
+public class Token {
+    Long iat;
+    String sub;
+    String iss;
+    String cip;
+    Integer age;
 }
 
