dbb199d63e321b60710473db238ed46558525277
Michiel Meeuwissen <git@meeuw.org>
2022-10-25 15:21:31 +0200

    NPA-615

--- media-domain/src/main/java/nl/vpro/domain/media/MediaPublisherHeaders.java
+++ media-domain/src/main/java/nl/vpro/domain/media/MediaPublisherHeaders.java
@@ -20,6 +20,7 @@ public interface MediaPublisherHeaders {
     String QUEUETIME_HEADER         = "queueTime";
     String TRANSACTION_UUID_HEADER  = "transactionUUID";
     String REASON_HEADER            = "reason";
+    String COLLECTED_REASONS_HEADER = "collectedReasons";
     String TRIGGERED_BY_HEADER         = "triggeredBy";
     String RECENTLY_MODIFIED_BY_HEADER = "recentlyModifiedBy";
     String LAST_MODIFIED_BY_HEADER     = "lastModifiedBy";
