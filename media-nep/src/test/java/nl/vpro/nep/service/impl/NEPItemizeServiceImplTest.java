d2960a02a51f0f48b065e5dd5ed4a7939162e3ff
Michiel Meeuwissen <git@meeuw.org>
2022-11-08 14:47:53 +0100

    Dropped ru.lanwen.

--- media-nep/src/test/java/nl/vpro/nep/service/impl/NEPItemizeServiceImplTest.java
+++ media-nep/src/test/java/nl/vpro/nep/service/impl/NEPItemizeServiceImplTest.java
@@ -1,15 +1,14 @@
 package nl.vpro.nep.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
-import ru.lanwen.wiremock.ext.WiremockResolver;
-import ru.lanwen.wiremock.ext.WiremockUriResolver;
 
 import java.util.Properties;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 
 import nl.vpro.nep.domain.ItemizerStatus;
 import nl.vpro.nep.domain.ItemizerStatusResponse;
@@ -24,52 +23,50 @@ import static org.assertj.core.api.Assertions.catchThrowableOfType;
  * @since 5.24
  */
 @Slf4j
-
-@ExtendWith({
-    WiremockResolver.class,
-    WiremockUriResolver.class
-})
+@WireMockTest
 public class NEPItemizeServiceImplTest {
 
     @Test
-    public void getJobStatus404(@WiremockResolver.Wiremock WireMockServer server, @WiremockUriResolver.WiremockUri String uri) {
+    public void getJobStatus404(WireMockRuntimeInfo wireMockRuntimeInfo) throws Exception {
         Properties properties = new Properties();
-        properties.setProperty("nep.itemizer-api.baseUrl", uri);
+        properties.setProperty("nep.itemizer-api.baseUrl", wireMockRuntimeInfo.getHttpBaseUrl());
         properties.getProperty("nep.itemizer-api.key", "bearer ");
-        NEPItemizeServiceImpl itemizer = new NEPItemizeServiceImpl(properties);
-        server.stubFor(
-            get(urlEqualTo("/api/itemizer/jobs/foobar/status"))
-                .willReturn(
-                    status(404).withBody("{\"success\":false,\"status\":\"error\",\"errors\":\"No job found for token [foobar] and provider [npo]\"}")
-                )
-        );
-        ItemizerStatusException statusException = catchThrowableOfType(() -> {
+        try (NEPItemizeServiceImpl itemizer = new NEPItemizeServiceImpl(properties)) {
+            WireMock.stubFor(
+                get(urlEqualTo("/api/itemizer/jobs/foobar/status"))
+                    .willReturn(
+                        status(404).withBody("{\"success\":false,\"status\":\"error\",\"errors\":\"No job found for token [foobar] and provider [npo]\"}")
+                    )
+            );
+            ItemizerStatusException statusException = catchThrowableOfType(() -> {
                 ItemizerStatusResponse job = itemizer.getLiveItemizerJobStatus("foobar");
             }, ItemizerStatusException.class);
-        assertThat(statusException).isInstanceOf(ItemizerStatusException.class);
-        assertThat(statusException.getStatusCode()).isEqualTo(404);
-        assertThat(statusException.getResponse()).isNotNull();
-        assertThat(statusException.getResponse().getStatus()).isEqualTo("error");
-        assertThat(statusException.getResponse().getErrors()).isEqualTo("No job found for token [foobar] and provider [npo]");
-        log.info("{}", statusException.toString());
+            assertThat(statusException).isInstanceOf(ItemizerStatusException.class);
+            assertThat(statusException.getStatusCode()).isEqualTo(404);
+            assertThat(statusException.getResponse()).isNotNull();
+            assertThat(statusException.getResponse().getStatus()).isEqualTo("error");
+            assertThat(statusException.getResponse().getErrors()).isEqualTo("No job found for token [foobar] and provider [npo]");
+            log.info("{}", statusException.toString());
+        }
     }
 
     @Test
-    public void getJobStatus(@WiremockResolver.Wiremock WireMockServer server, @WiremockUriResolver.WiremockUri String uri) {
+    public void getJobStatus(WireMockRuntimeInfo wireMockRuntimeInfo) throws Exception {
         Properties properties = new Properties();
-        properties.setProperty("nep.itemizer-api.baseUrl", uri);
+        properties.setProperty("nep.itemizer-api.baseUrl", wireMockRuntimeInfo.getHttpBaseUrl());
         properties.getProperty("nep.itemizer-api.key", "bearer ");
-        NEPItemizeServiceImpl itemizer = new NEPItemizeServiceImpl(properties);
-        server.stubFor(
-            get(urlEqualTo("/api/itemizer/jobs/2c829baf-c310-4f09-81c1-fd9a252ded0c/status"))
-                .willReturn(
-                    ok().withBody("{\"jobId\":\"2c829baf-c310-4f09-81c1-fd9a252ded0c\",\"status\":\"QUEUED\",\"statusMessage\":null}")
-                )
-        );
-        ItemizerStatusResponse job = itemizer.getLiveItemizerJobStatus("2c829baf-c310-4f09-81c1-fd9a252ded0c");
+        try (NEPItemizeServiceImpl itemizer = new NEPItemizeServiceImpl(properties)) {
+            WireMock.stubFor(
+                get(urlEqualTo("/api/itemizer/jobs/2c829baf-c310-4f09-81c1-fd9a252ded0c/status"))
+                    .willReturn(
+                        ok().withBody("{\"jobId\":\"2c829baf-c310-4f09-81c1-fd9a252ded0c\",\"status\":\"QUEUED\",\"statusMessage\":null}")
+                    )
+            );
+            ItemizerStatusResponse job = itemizer.getLiveItemizerJobStatus("2c829baf-c310-4f09-81c1-fd9a252ded0c");
 
-        assertThat(job).isEqualTo(ItemizerStatusResponse.builder().jobId("2c829baf-c310-4f09-81c1-fd9a252ded0c").status(ItemizerStatus.QUEUED).statusMessage(null).build());
-        log.info("{}", job);
+            assertThat(job).isEqualTo(ItemizerStatusResponse.builder().jobId("2c829baf-c310-4f09-81c1-fd9a252ded0c").status(ItemizerStatus.QUEUED).statusMessage(null).build());
+            log.info("{}", job);
+        }
     }
 
 }
