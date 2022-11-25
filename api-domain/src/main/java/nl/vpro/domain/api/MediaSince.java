e66d21d8b3129855a8307bffcce7d6d5e3753ec7
Michiel Meeuwissen <git@meeuw.org>
2022-11-25 13:41:20 +0100

    Added a bunch of utility methods.

--- api-domain/src/main/java/nl/vpro/domain/api/MediaSince.java
+++ api-domain/src/main/java/nl/vpro/domain/api/MediaSince.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.Instant;
+import java.util.Comparator;
+
+import static java.util.Comparator.naturalOrder;
 
 /**
  * Combines an {@link Instant} with a {@code mid}, together the defined the unique order the changes feed.
@@ -14,7 +17,7 @@ import java.time.Instant;
 @Data
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @lombok.Builder
-public class MediaSince {
+public class MediaSince implements Comparable<MediaSince> {
 
     private final Instant instant;
 
@@ -28,10 +31,51 @@ public class MediaSince {
         return mediaSince == null ? null : mediaSince.instant;
     }
 
+    /**
+     * @since 7.2
+     */
+    public static MediaSince of(Instant instant) {
+        return MediaSince.builder().instant(instant).build();
+    }
+
+    /**
+     * @since 7.2
+     */
+    public static MediaSince of(Instant instant, String mid) {
+        return MediaSince.builder().instant(instant).mid(mid).build();
+    }
+
     static String mid(String explicit, MediaSince mediaSince) {
         if (explicit != null) {
             return explicit;
         }
         return mediaSince == null ? null : mediaSince.mid;
+    }
+
+    /**
+     * @since 7.2
+     */
+    public String asQueryParam() {
+        String sinceString = instant.toString();
+        if (mid != null && sinceString != null) {
+            sinceString += "," + mid;
+        }
+        return sinceString;
+    }
+
+    /**
+     * @since 7.2
+     */
+    public static String asQueryParam(MediaSince since) {
+        return since == null ? null : since.asQueryParam();
+    }
+
+    /**
+     * @since 7.2
+     */
+    @Override
+    public int compareTo(MediaSince o) {
+        return Comparator.comparing(MediaSince::getInstant, Comparator.nullsFirst(naturalOrder()))
+            .thenComparing(MediaSince::getMid, Comparator.nullsFirst(naturalOrder())).compare(this, o);
     }
 }
