5691608132a1779f8c0c6d5d249e02d0cef27b1c
Michiel Meeuwissen <git@meeuw.org>
2023-01-06 16:45:03 +0100

    Added possibliity to merge reasons.

--- poms-shared-domain/src/main/java/nl/vpro/domain/PublicationReason.java
+++ poms-shared-domain/src/main/java/nl/vpro/domain/PublicationReason.java
@@ -17,6 +17,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import nl.vpro.jackson2.StringInstantToJsonTimestamp;
+import nl.vpro.util.DateUtils;
 import nl.vpro.xml.bind.InstantXmlAdapter;
 
 import static java.util.Comparator.*;
@@ -97,16 +98,31 @@ public class PublicationReason implements Serializable, Comparable<PublicationRe
         return "parent: " + reason;
     }
 
-    public static String toRecords(Collection<PublicationReason> reasons, long maxLength, String mid) {
+    public static String toRecords(List<PublicationReason> reasons, long maxLength, String mid, boolean mergeDuplicates) {
         StringBuilder builder = new StringBuilder();
+        PublicationReason prevReason = null;
         for (PublicationReason reason : reasons) {
-            String s = reason.toRecord();
-            if (builder.length() > 0){
+
+
+            boolean needsSplitter = builder.length() > 0;
+            if (prevReason != null) {
+                if (DateUtils.isAfter(prevReason.publishDate, reason.publishDate)) {
+                    log.warn("The list seems to be ordered wrong");
+                }
+                if (mergeDuplicates && Objects.equals(prevReason.getValue(), reason.getValue())) {
+                    builder.delete(builder.length() - prevReason.toRecord().length(), builder.length());
+                    needsSplitter = false;
+
+                }
+            }
+            if (needsSplitter){
                 builder.append(RECORD_SPLITTER);
             }
+            String s = reason.toRecord();
             builder.append(s);
-            if (builder.length() > maxLength) {
-                log.error("Reason header for {} is ridiculously long ({} reasons). Will be truncated", mid, reasons.size());
+            prevReason = reason;
+            if (builder.length() >= maxLength) {
+                log.error("Reason header for {} is ridiculously long ({} reasons, {} chars). Will be truncated", mid, reasons.size(), builder.length());
                 break;
             }
         }
--- poms-shared-domain/src/test/java/nl/vpro/domain/PublicationReasonTest.java
+++ poms-shared-domain/src/test/java/nl/vpro/domain/PublicationReasonTest.java
@@ -1,6 +1,7 @@
 package nl.vpro.domain;
 
 import java.time.Instant;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,9 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import nl.vpro.jackson2.Jackson2Mapper;
 import nl.vpro.test.util.jackson2.Jackson2TestUtil;
+
+import static java.time.Instant.ofEpochMilli;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PublicationReasonTest {
 
@@ -34,6 +38,45 @@ class PublicationReasonTest {
             "  }\n" +
             "}");
     }
+    static List<PublicationReason> reasons = List.of(
+                new PublicationReason("a", ofEpochMilli(1)),
+                new PublicationReason("a", ofEpochMilli(2)),
+                new PublicationReason("b", ofEpochMilli(3)),
+                new PublicationReason("b", ofEpochMilli(4)),
+                new PublicationReason("a", ofEpochMilli(5)),
+                new PublicationReason("c", ofEpochMilli(6))
+    );
 
+    @Test
+    public void toRecordsWithMerge() {
+        String records = PublicationReason.toRecords(reasons,
+            1000,
+            "mid_123",
+            true
+        );
+        assertThat(records).isEqualTo("a␟2␞b␟4␞a␟5␞c␟6");
+    }
+
+
+    @Test
+    public void toRecords() {
+        String records = PublicationReason.toRecords(reasons,
+            1000,
+            "mid_123",
+            false
+        );
+        assertThat(records).isEqualTo("a␟1␞a␟2␞b␟3␞b␟4␞a␟5␞c␟6");
+    }
+
+
+    @Test
+    public void toRecordsWithMax() {
+        String records = PublicationReason.toRecords(reasons,
+            8,
+            "mid_123",
+            false
+        );
+        assertThat(records).isEqualTo("a␟1␞a␟2␞b␟3");
+    }
 
 }
