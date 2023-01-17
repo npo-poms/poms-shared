96ac0387937187d19d808b8b178af615f15025db
Michiel Meeuwissen <git@meeuw.org>
2023-01-17 12:54:07 +0100

    I think this would fix failing test case (integration tests)

--- media-domain/src/main/java/nl/vpro/domain/media/Segment.java
+++ media-domain/src/main/java/nl/vpro/domain/media/Segment.java
@@ -176,6 +176,15 @@ public class Segment extends MediaObject implements Comparable<Segment>, Child<P
     }
 
     /**
+     * If a segment is member of a merged {@link Program} then its workflow will become {@link Workflow#MERGED}, to indicate that. But this will in all cases be considered a delete.
+     * <p>
+     * E.g. if such on object is requested from the API it should result 404.
+     */
+    public boolean isDeleted() {
+        return super.isDeleted() || isMerged();
+    }
+
+    /**
      * Returns the parent {@link Program} of this segment. Not that this does not work directly after a simple unmarshall of
      * an individual segment because the full program object simply is not available then.
      * <p>
