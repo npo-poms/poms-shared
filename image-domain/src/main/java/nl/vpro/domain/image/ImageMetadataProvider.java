1a15b7ced852919e57b6c6b6018be20da9b2d814
Michiel Meeuwissen <git@meeuw.org>
2023-01-20 11:58:23 +0100

    Trying compatibility.

---
+++ image-domain/src/main/java/nl/vpro/domain/image/ImageMetadataProvider.java
@@ -0,0 +1,25 @@
+package nl.vpro.domain.image;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+@Deprecated
+public interface ImageMetadataProvider extends ImageMetadataSupplier {
+
+    @NonNull
+    @Deprecated
+    default ImageMetadata toImageMetadataWithSourceSet() {
+        return getImageMetadataWithSourceSet();
+    }
+
+    /**
+     * This has to be implemented,
+     * <p>
+     * Normally when using you should call {@link #getImageMetadataWithSourceSet() , which will also add  (extra)  {@link ImageMetadata#getSourceSet()} via {@link ImageSourceService}.
+     */
+    @NonNull
+    @Deprecated
+    default ImageMetadata toImageMetadata() {
+        return getImageMetadata();
+    }
+
+}
