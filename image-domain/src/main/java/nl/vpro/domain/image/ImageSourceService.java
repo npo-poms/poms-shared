68ae68bcaf5e8afbcbbc1a8cadd6bf61bf03094e
Michiel Meeuwissen <git@meeuw.org>
2023-01-21 11:15:49 +0100

    MGNL-20021

--- image-domain/src/main/java/nl/vpro/domain/image/ImageSourceService.java
+++ image-domain/src/main/java/nl/vpro/domain/image/ImageSourceService.java
@@ -1,11 +1,14 @@
 package nl.vpro.domain.image;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.*;
 
 /**
  * Wrapper for the {@link ImageSourceCreator} {@link ServiceLoader}.
  *
  */
+@Slf4j
 public class ImageSourceService {
 
     public static final ImageSourceService INSTANCE = new ImageSourceService();
@@ -19,9 +22,12 @@ public class ImageSourceService {
         final SortedMap<ImageSource.Key, ImageSource> map = new TreeMap<>();
         services.forEach(creator -> {
             for (ImageSource.Key key : ImageSource.Key.values()) {
-                creator.createFor(metadataProvider, key).ifPresent(imageSource -> {
-                    map.put(imageSource.getKey(), imageSource);
-                });
+                Optional<ImageSource> image = creator.createFor(metadataProvider, key);
+                if (image.isPresent()) {
+                    map.put(image.get().getKey(), image.get());
+                } else {
+                    log.debug("No image could be created for {} by {}", key, creator);
+                };
             }
         });
         return Collections.unmodifiableMap(map);
