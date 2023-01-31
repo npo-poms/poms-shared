36b19f7b1a0d56567eb91f90d2969ec25b22e292
Michiel Meeuwissen <git@meeuw.org>
2023-01-31 22:10:21 +0100

    This may be handy.

--- image-domain/src/main/java/nl/vpro/domain/image/Picture.java
+++ image-domain/src/main/java/nl/vpro/domain/image/Picture.java
@@ -2,6 +2,11 @@ package nl.vpro.domain.image;
 
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import nl.vpro.jackson2.Jackson2Mapper;
+
 public interface Picture {
 
     Map<String, String> getSources();
@@ -11,5 +16,10 @@ public interface Picture {
     String getAlternative();
 
     String getStyle();
+
+    @JsonIgnore
+    default JsonNode getJson() {
+        return Jackson2Mapper.getModelInstance().valueToTree(this);
+    }
 
 }
