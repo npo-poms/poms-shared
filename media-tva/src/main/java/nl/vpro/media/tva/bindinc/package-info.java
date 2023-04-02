8b9c1fb30691190c7faa7da2ddc78deee906488d
Michiel Meeuwissen <git@meeuw.org>
2023-04-02 20:03:42 +0200

    Added support for byte[]

--- media-tva/src/main/java/nl/vpro/media/tva/bindinc/package-info.java
+++ media-tva/src/main/java/nl/vpro/media/tva/bindinc/package-info.java
@@ -1,6 +1,5 @@
 /**
- * Contains various utility to use the TVA XSLT and related saxon plugins (as developed for intergration from poms with mis/whatson/pd) also on XML's as they shipped by bindinc, which
- * principally the same 'TV anytime' format.
+ * Contains various utility to use the TVA XSLT and related saxon plugins (as developed for integration from poms with mis/whatson/pd) also on XMLs as  shipped by Bindinc, with principally the same 'TV anytime' format.
  *
  * @author Michiel Meeuwissen
  * @since 5.20.1
