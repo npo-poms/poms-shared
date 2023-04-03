16bc79a62212430174a5bf58d9d711ad3ecb35ec
Michiel Meeuwissen <git@meeuw.org>
2023-04-03 16:20:49 +0200

    Using openapi generator is not feasible. This does it just with java http client.

--- media-sourcingservice/src/main/java/nl/vpro/sourcingservice/SourcingService.java
+++ media-sourcingservice/src/main/java/nl/vpro/sourcingservice/SourcingService.java
@@ -2,10 +2,8 @@ package nl.vpro.sourcingservice;
 
 import java.io.InputStream;
 
-import nl.vpro.sourcingservice.invoker.ApiException;
-
 public interface SourcingService {
 
-    void upload(String mid, InputStream inputStream) throws ApiException;
+    void upload(String mid, Long fileSize, InputStream inputStream);
 
 }
--- media-sourcingservice/src/main/java/nl/vpro/sourcingservice/SourcingServiceImpl.java
+++ media-sourcingservice/src/main/java/nl/vpro/sourcingservice/SourcingServiceImpl.java
@@ -1,44 +1,88 @@
 package nl.vpro.sourcingservice;
 
+import io.github.yskszk63.jnhttpmultipartformdatabodypublisher.MultipartFormDataBodyPublisher;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 
-import java.io.*;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.net.http.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.apache.commons.io.IOUtils;
-
-import nl.vpro.sourcingservice.api.IngestApi;
-import nl.vpro.sourcingservice.invoker.ApiClient;
-import nl.vpro.sourcingservice.invoker.ApiException;
 
 
 @Log4j2
 public class SourcingServiceImpl implements SourcingService {
 
     private final static long CHUNK_SIZE = 10_000_000L;
-    private final IngestApi ingestApi;
 
 
+    private final String baseUrl;
+    private final String token;
+    HttpClient client = HttpClient.newHttpClient();
 
 
-    public SourcingServiceImpl(ApiClient apiClient) {
-        this.ingestApi = new IngestApi(apiClient);
+    public SourcingServiceImpl(String baseUrl, String token) {
+        this.baseUrl = baseUrl;
+        this.token = token;
     }
 
 
     @Override
-    public void upload(String mid, InputStream inputStream) throws ApiException {
-
-        try {
-            File tempFile = File.createTempFile(mid, ".transfer");
-            try (FileOutputStream outputStream = new FileOutputStream(tempFile)) {
-                long test = IOUtils.copyLarge(inputStream, outputStream, 0, CHUNK_SIZE);
-                Object o = ingestApi.fe2878922c15ec40d930bec65724f0c7(mid, "start", "" + test, tempFile);
+    @SneakyThrows
+    public void upload(String mid, Long fileSize, InputStream inputStream)  {
+        {
+            MultipartFormDataBodyPublisher body = new MultipartFormDataBodyPublisher()
+                .add("upload_phase", "start")
+                .add("email", "michiel.meeuwissen@gmail.com")
+                .add("file_size", "" + fileSize);
+            HttpResponse<String> start = client.send(multipart(mid, body), HttpResponse.BodyHandlers.ofString());
+            if (start.statusCode() > 299) {
+                throw new IllegalArgumentException(start.body());
             }
-
-
-
-        } catch (IOException e) {
-            log.warn(e.getMessage(), e);
+            log.info("start: {} {}", start.statusCode(), start.body());
         }
+
+        while(true) {
+                Path tempFile = Files.createTempFile(mid, ".transfer");
+                try (OutputStream outputStream = Files.newOutputStream(tempFile)) {
+                    long test = IOUtils.copyLarge(inputStream, outputStream, 0, CHUNK_SIZE);
+                    MultipartFormDataBodyPublisher body = new MultipartFormDataBodyPublisher()
+                            .add("upload_phase", "transfer")
+                            .addFile("file_chunk", tempFile)
+                            .add("file_size", "" + fileSize);
+                    HttpRequest transferRequest = multipart(mid, body);
+                    HttpResponse<String> transfer = client.send(transferRequest, HttpResponse.BodyHandlers.ofString());
+                    log.info("transfer: {} {}", transfer.statusCode(), transfer.body());
+                    if (test < CHUNK_SIZE) {
+                        break;
+                    }
+                }
+
+            }
+        MultipartFormDataBodyPublisher body = new MultipartFormDataBodyPublisher()
+                            .add("upload_phase", "finish");
+
+        HttpResponse<String> finish = client.send(multipart(mid, body), HttpResponse.BodyHandlers.ofString());
+
+        log.info("finish: {} {}", finish.statusCode(), finish.body());
+
+
+    }
+
+    protected HttpRequest multipart(String mid, MultipartFormDataBodyPublisher body) {
+        return request("ingest/" + mid + "/multipart")
+            .header("Content-Type", body.contentType())
+            .POST(body).build();
+    }
+
+    protected HttpRequest.Builder request(String path) {
+        return HttpRequest.newBuilder()
+            .header("Authorization", "Bearer " + token)
+            .uri(URI.create(baseUrl + "api/" + path));
+
     }
 }
