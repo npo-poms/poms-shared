c9e053747ded1d3e4dcfed49888e2dd4a402f0fc
Michiel Meeuwissen <git@meeuw.org>
2023-04-17 11:02:14 +0200

    guava tool doesn't work as desired.

--- media-sourcingservice/src/main/java/nl/vpro/sourcingservice/SourcingServiceImpl.java
+++ media-sourcingservice/src/main/java/nl/vpro/sourcingservice/SourcingServiceImpl.java
@@ -11,25 +11,28 @@ import java.net.http.*;
 import org.springframework.beans.factory.annotation.Value;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.google.common.io.ByteStreams;
 
 import nl.vpro.domain.media.update.UploadResponse;
 import nl.vpro.domain.user.User;
 import nl.vpro.domain.user.UserService;
 import nl.vpro.jackson2.Jackson2Mapper;
 import nl.vpro.logging.simple.SimpleLogger;
+import nl.vpro.util.FileSizeFormatter;
+import nl.vpro.util.InputStreamChunk;
 
 
 @Log4j2
 public class SourcingServiceImpl implements SourcingService {
 
-    private final static long CHUNK_SIZE = 10_000_000L;
+
 
 
     private final String baseUrl;
     private final String callbackBaseUrl;
     private final String token;
     private final UserService<?> userService;
+    private final int chunkSize;
+
     HttpClient client = HttpClient.newHttpClient();
 
 
@@ -37,11 +40,13 @@ public class SourcingServiceImpl implements SourcingService {
         @Value("${sourcingservice.baseUrl}") String baseUrl,
         @Value("${sourcingservice.token}") String token,
         @Value("${sourcingservice.callbackBaseUrl}") String callbackBaseUrl,
+        @Value("${sourcingservice.chunkSize:10000000}") int chunkSize,
         UserService<?> userService) {
         this.baseUrl = baseUrl;
         this.token = token;
         this.callbackBaseUrl = callbackBaseUrl;
         this.userService = userService;
+        this.chunkSize = chunkSize;
     }
 
 
@@ -58,23 +63,26 @@ public class SourcingServiceImpl implements SourcingService {
             if (start.statusCode() > 299) {
                 throw new IllegalArgumentException(start.body());
             }
-            logger.info("start: {} {}", start.statusCode(), start.body());
+            logger.info("start: {} {} {}", FileSizeFormatter.DEFAULT.format(fileSize), start.statusCode(), start.body());
         }
         long uploaded = 0;
         while(uploaded < fileSize) {
+            InputStreamChunk chunkStream = new InputStreamChunk(chunkSize, inputStream);
             MultipartFormDataBodyPublisher body = new MultipartFormDataBodyPublisher()
                 .add("upload_phase", "transfer")
                 .addStream("file_chunk", "part" , () -> {
-                    InputStream limit = ByteStreams.limit(inputStream, CHUNK_SIZE);
-                    return limit;
+                    return chunkStream;
+
                 })
                 .add("file_size", String.valueOf(fileSize));
             HttpRequest transferRequest = multipart(mid, body);
             HttpResponse<String> transfer = client.send(
                 transferRequest, HttpResponse.BodyHandlers.ofString());
-            logger.info("transfer: {} {}", transfer.statusCode(), transfer.body());
-            uploaded += CHUNK_SIZE;
+            uploaded += chunkStream.getCount();
+            logger.info("transfer: {},  {} {}", FileSizeFormatter.DEFAULT.format(chunkStream.getCount()), transfer.statusCode(), transfer.body());
+            uploaded += chunkStream.getCount();
         }
+        inputStream.close();
         MultipartFormDataBodyPublisher body = new MultipartFormDataBodyPublisher()
             .add("upload_phase", "finish");
 
