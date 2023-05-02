6a5078a4287c9534d71a0c38df1ffa9086f18fbb
Michiel Meeuwissen <git@meeuw.org>
2023-05-02 15:21:32 +0200

    In preparation of MSE-5450

---
+++ media-domain/src/main/java/nl/vpro/domain/media/MediaObjectAccess.java
@@ -0,0 +1,98 @@
+package nl.vpro.domain.media;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import com.google.common.annotations.Beta;
+
+import nl.vpro.domain.media.support.*;
+
+/**
+ *
+ * Makes some package local method accessible.
+ * <p>
+ * This should normally only be done by media backend processes.
+ *
+ * @author Michiel Meeuwissen
+ * @since 5.1
+ */
+@Slf4j
+public class MediaObjectAccess {
+
+    public static void setRepubDate(MediaObject object, Instant repubDate) {
+        object.setRepubDate(repubDate);
+    }
+
+    public static void setSubtitlesWorkflow(MediaObject object, AvailableSubtitlesWorkflow subtitlesWorkflow) {
+        object.setSubtitlesWorkflow(subtitlesWorkflow);
+    }
+
+
+    public static void setWorkflow(MediaObject object, Workflow workflow) {
+        setWorkflow(object, workflow, null);
+    }
+
+
+    public static void setWorkflow(@NonNull MediaObject object, Workflow workflow, String reason) {
+        PublishableObjectAccess.setWorkflow(object, workflow);
+        MediaObjects.appendReason(object, reason);
+        object.setRepubDestinations(null);
+    }
+
+    public static AvailableSubtitlesWorkflow getSubtitlesWorkflow(MediaObject object) {
+        return object.getSubtitlesWorkflow();
+    }
+
+    public static void setStreamingPlatformStatus(MediaObject object, StreamingStatusImpl status) {
+        if (status != object.getStreamingPlatformStatus()) {
+            log.info("Set streaming platform status for {} from {} to {}", object, object.getStreamingPlatformStatus(), status);
+            object.setStreamingPlatformStatus(status);
+        }
+    }
+    public static StreamingStatusImpl getModifiableStreamingPlatformStatus(MediaObject object) {
+        return object.getModifiableStreamingPlatformStatus();
+    }
+
+    public static void setAuthorizedDuration(MediaObject o, Duration duration) {
+        if (o != null) {
+            AuthorizedDuration authorized = AuthorizedDuration.authorized(duration);
+            o.setDuration(authorized);
+        } else {
+            log.warn("Can't set duration {} because not mediaobject given", duration);
+        }
+
+    }
+    public static void setMid(MediaObject o, String mid) {
+        o.mid = mid;
+    }
+
+    public static void setPdAuthorityImported(Program p, boolean pdAuthorityImported) {
+        p.setPdAuthorityImported(pdAuthorityImported);;
+    }
+
+    public static Boolean isPdAuthorityImported(Program p) {
+        return p.getPdAuthorityImported();
+    }
+
+    public static String getCurrentCorrelationId(MediaObject m) {
+        return m.correlationId;
+    }
+
+
+    @Beta
+    public  static void setCorrelationForSegmentDefault(boolean defaultCorrelation) {
+        Segment.defaultCorrelation = defaultCorrelation;
+    }
+
+    @Beta
+    public  static boolean getCorrelationForSegmentDefault() {
+        return Segment.defaultCorrelation;
+    }
+
+
+
+}
