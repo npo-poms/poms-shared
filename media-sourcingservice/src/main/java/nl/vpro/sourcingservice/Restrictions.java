3669f14609d7a256624a18b45992402f95a2eb9b
Michiel Meeuwissen <git@meeuw.org>
2023-06-05 15:16:28 +0200

    javadoc.

--- media-sourcingservice/src/main/java/nl/vpro/sourcingservice/Restrictions.java
+++ media-sourcingservice/src/main/java/nl/vpro/sourcingservice/Restrictions.java
@@ -6,8 +6,14 @@ import nl.vpro.domain.media.AgeRating;
 import nl.vpro.domain.media.GeoRestriction;
 
 @Data
-public
-class Restrictions {
+public class Restrictions {
+
     GeoRestriction geoRestriction;
+
+    /**
+     * The age restriction determines at which hours of the day an object may be broadcast.
+     * <p>
+     * Modelled as a NICAM {@link AgeRating}, but it is not really the same.
+     */
     AgeRating ageRating;
 }
