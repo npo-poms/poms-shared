c4c2500394e96a78d32d1395c2d80113c2a797fd
Michiel Meeuwissen <git@meeuw.org>
2023-06-08 08:58:31 +0200

    Syncing identifiers for deployment, all conforming to openshift container names now.

---
+++ poms-shared/src/test/java/nl/vpro/poms/shared/DeploymentTest.java
@@ -0,0 +1,30 @@
+package nl.vpro.poms.shared;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.util.List;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import nl.vpro.util.Env;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Log4j2
+class DeploymentTest {
+
+	@ParameterizedTest
+    @EnumSource(Deployment.class)
+	void getBaseUrl(Deployment deployment) {
+        for (Env env : List.of(Env.PROD, Env.ACC, Env.TEST, Env.LOCALHOST)) {
+            String baseUrl = deployment.getBaseUrl(env);
+            log.info("{} {} {}", deployment, env, baseUrl);
+            if (baseUrl != null) {
+                assertThat(baseUrl.isEmpty()).isFalse();
+                assertThat(baseUrl).endsWith("/");
+            }
+        }
+    }
+
+}
