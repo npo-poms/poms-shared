25b321bb543d0e445db73ca19940c6cdde957e8a
Michiel Meeuwissen <git@meeuw.org>
2023-06-16 16:15:30 +0200

    NPA-635

--- api-domain/src/main/java/nl/vpro/domain/api/profile/ProfileDefinition.java
+++ api-domain/src/main/java/nl/vpro/domain/api/profile/ProfileDefinition.java
@@ -4,17 +4,16 @@
  */
 package nl.vpro.domain.api.profile;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.Instant;
 
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.*;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import nl.vpro.domain.Xmlns;
-import nl.vpro.domain.constraint.AbstractFilter;
-import nl.vpro.domain.constraint.DelegatingDisplayablePredicate;
-import nl.vpro.domain.constraint.DisplayablePredicate;
-import nl.vpro.xml.bind.InstantXmlAdapter;
+import nl.vpro.domain.constraint.*;
 
 /**
  * @author Roelof Jan Koekoek
@@ -25,17 +24,15 @@ import nl.vpro.xml.bind.InstantXmlAdapter;
 public class ProfileDefinition<T> implements DelegatingDisplayablePredicate<T>, Comparable<ProfileDefinition<T>> {
 
     @XmlTransient
+    @Getter
+    @Setter
     private Profile profile;
-
-    @XmlAttribute(name = "since")
-    @XmlJavaTypeAdapter(value = InstantXmlAdapter.class)
-    @XmlSchemaType(name = "dateTime")
-    private Instant sinceDate;
 
     @XmlElements({
         @XmlElement(name = "filter", namespace = Xmlns.MEDIA_CONSTRAINT_NAMESPACE, type = nl.vpro.domain.constraint.media.Filter.class),
         @XmlElement(name = "filter", namespace = Xmlns.PAGE_CONSTRAINT_NAMESPACE, type = nl.vpro.domain.constraint.page.Filter.class)
     })
+    @Getter
     private AbstractFilter<T> filter;
 
     public ProfileDefinition() {
@@ -43,11 +40,6 @@ public class ProfileDefinition<T> implements DelegatingDisplayablePredicate<T>, 
 
     public ProfileDefinition(AbstractFilter<T> filter) {
         this.filter = filter;
-    }
-
-    public ProfileDefinition(AbstractFilter<T> filter, Instant since) {
-        this.filter = filter;
-        this.sinceDate = since;
     }
 
     public static <S> ProfileDefinition<S> of(AbstractFilter<S> filter) {
@@ -58,29 +50,10 @@ public class ProfileDefinition<T> implements DelegatingDisplayablePredicate<T>, 
         return filter != null && filter.getConstraint() != null;
     }
 
-    public Profile getProfile() {
-        return profile;
-    }
-
-    public void setProfile(Profile profile) {
-        this.profile = profile;
-    }
-
     public String getName() {
         return profile == null ? null : profile.getName();
     }
 
-    public Instant getSince() {
-        return sinceDate == null ? Instant.EPOCH : sinceDate;
-    }
-
-    public void setSince(Instant since) {
-        this.sinceDate = since;
-    }
-
-    public AbstractFilter<T> getFilter() {
-        return filter;
-    }
 
 
     @Override
@@ -89,20 +62,20 @@ public class ProfileDefinition<T> implements DelegatingDisplayablePredicate<T>, 
 
     }
 
-
+    @Deprecated
     public Instant getTimeStamp() {
         return profile == null ? null : profile.getTimestamp();
     }
 
     @Override
     public String toString() {
-        return "ProfileDefinition{name='" + getName() + "', since=" + sinceDate + "}";
+        return "ProfileDefinition{name='" + getName() + "'}";
     }
 
     @Override
     public int compareTo(ProfileDefinition<T> o) {
         // newest first
-        return o.getSince().compareTo(getSince());
+        return o.getName().compareTo(o.getName());
     }
 
     @Override
@@ -118,17 +91,12 @@ public class ProfileDefinition<T> implements DelegatingDisplayablePredicate<T>, 
             return false;
         }
 
-        if(sinceDate != null ? !sinceDate.equals(that.sinceDate) : that.sinceDate != null) {
-            return false;
-        }
-
         return getTimeStamp() != null ? getTimeStamp().equals(that.getTimeStamp()) : that.getTimeStamp() == null;
     }
 
     @Override
     public int hashCode() {
         int result = getName() != null ? getName().hashCode() : 0;
-        result = 31 * result + (sinceDate != null ? sinceDate.hashCode() : 0);
         return result;
     }
 
