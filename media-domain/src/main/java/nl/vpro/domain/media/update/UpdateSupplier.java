5d3d161b371444de3f14c180e354c5853802937a
Michiel Meeuwissen <git@meeuw.org>
2023-07-04 14:55:40 +0200

    MSE-5484

--- media-domain/src/main/java/nl/vpro/domain/media/update/UpdateSupplier.java
+++ media-domain/src/main/java/nl/vpro/domain/media/update/UpdateSupplier.java
@@ -1,12 +1,28 @@
 package nl.vpro.domain.media.update;
 
 
+import com.google.common.annotations.Beta;
+
 /**
+ * An update supplier is an authoritative system - i.e. it is not a broadcaster - that supplies updates to POMS.
+ * <p>
+ * This is a limited number of system, for each of which the POMS backend may have specific configuration on how to exactly handle incoming updates.
+ *
  * @since 7.7
  */
 public enum UpdateSupplier {
 
-    RCRS;
+    /**
+     * See MSE-5484
+     */
+    RCRS,
+
+
+    /**
+     * I think we would like to do something similar for Sourcing Service
+     */
+    @Beta
+    SOURCING_SERVICE;
 
 
 }
