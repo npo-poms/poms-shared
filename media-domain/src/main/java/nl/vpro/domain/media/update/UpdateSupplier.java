70b9096d2023acdeec8cfcc64214dc64017e9d34
Michiel Meeuwissen <git@meeuw.org>
2023-07-04 21:16:15 +0200

    javadoc.

--- media-domain/src/main/java/nl/vpro/domain/media/update/UpdateSupplier.java
+++ media-domain/src/main/java/nl/vpro/domain/media/update/UpdateSupplier.java
@@ -6,7 +6,7 @@ import com.google.common.annotations.Beta;
 /**
  * An update supplier is an authoritative system - i.e. it is not a broadcaster - that supplies updates to POMS.
  * <p>
- * This is a limited number of system, for each of which the POMS backend may have specific configuration on how to exactly handle incoming updates.
+ * This is a limited number of systems, for each of which the POMS backend may have specific configuration on how to exactly handle incoming updates.
  *
  * @since 7.7
  */
