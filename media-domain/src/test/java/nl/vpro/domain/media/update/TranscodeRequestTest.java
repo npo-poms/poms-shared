f22924d8249a46b16f93652cb948139208db2c79
Michiel Meeuwissen <git@meeuw.org>
2023-08-31 15:36:32 +0200

    Test toString

--- media-domain/src/test/java/nl/vpro/domain/media/update/TranscodeRequestTest.java
+++ media-domain/src/test/java/nl/vpro/domain/media/update/TranscodeRequestTest.java
@@ -5,21 +5,23 @@ import org.junit.jupiter.api.Test;
 import nl.vpro.domain.media.Encryption;
 import nl.vpro.test.util.jaxb.JAXBTestUtil;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * @author Michiel Meeuwissen
  * @since 5.6
  */
 public class TranscodeRequestTest {
 
-
-    @Test
-    public void xml() {
-        TranscodeRequest request = TranscodeRequest.builder()
+    TranscodeRequest request = TranscodeRequest.builder()
             .mid("MID_123")
             .encryption(Encryption.DRM)
             .priority(TranscodeRequest.Priority.NORMAL)
             .fileName("vpro/test.m4v")
             .build();
+    @Test
+    public void xml() {
+
         JAXBTestUtil.roundTripAndSimilar(request, """
             <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
             <transcode mid='MID_123' xmlns="urn:vpro:media:update:2009" xmlns:shared="urn:vpro:shared:2009" xmlns:media="urn:vpro:media:2009">
@@ -27,6 +29,13 @@ public class TranscodeRequestTest {
                 <encryption>DRM</encryption>
                 <priority>NORMAL</priority>
             </transcode>""");
+    }
+
+    @Test
+    public void testToString() {
+
+        assertThat(request.toString()).isEqualTo("TranscodeRequest(mid=MID_123, fileName=vpro/test.m4v, encryption=DRM, priority=NORMAL)");
+
     }
 
 }
