d8d042603d7be995ff534c66575b56bff5d5dc01
Michiel Meeuwissen <git@meeuw.org>
2023-09-20 13:31:45 +0200

    javadoc.

--- media-domain/src/main/java/nl/vpro/validation/HasGenre.java
+++ media-domain/src/main/java/nl/vpro/validation/HasGenre.java
@@ -5,9 +5,14 @@ import java.lang.annotation.*;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
+import nl.vpro.domain.media.MediaIdentifiable;
+
 import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * Checks whether a {@link MediaIdentifiable} as at least one genre if that is required according to {@link MediaIdentifiable#getMediaType() media type}.{@link nl.vpro.domain.media.MediaType#requiresGenre() #requiresGenre()
+ */
 @Target({  TYPE_USE })
 @Retention(RUNTIME)
 @Constraint(validatedBy = HasGenreValidator.class)
