c172d73ef1e99dac358aec3d02cf85a9973cf8bf
Michiel Meeuwissen <git@meeuw.org>
2023-09-25 17:02:58 +0200

    javadoc.

--- poms-npoplayer/src/main/java/nl/vpro/rs/npoplayer/NPOPlayerRestService.java
+++ poms-npoplayer/src/main/java/nl/vpro/rs/npoplayer/NPOPlayerRestService.java
@@ -10,7 +10,11 @@ import javax.ws.rs.core.MediaType;
 import nl.vpro.domain.npoplayer.*;
 
 /**
- * This is a wrapper around the NPO Player 8 and 9 API.
+ * This is a rest service definition which can be used to server for the NPO Player 8 and 9.
+ * <p>
+ * The player 8 stuff is derpecated and will soon e removed.
+ * This service is e.g. implemented in <a href="https://rs.vpro.nl/v3/docs/api/#/npoplayer">vproapi</a>
+ *
  * @author r.jansen
  * @since 5.10
  */
@@ -21,6 +25,7 @@ public interface NPOPlayerRestService {
 
     /**
      * player 8.
+     * @deprecated This is player 8
      */
     @POST
     @Path("/request")
@@ -30,6 +35,8 @@ public interface NPOPlayerRestService {
     /**
      * See <a href="https://docs.npoplayer.nl/implementation/create-a-jwt/">documentation of npo player 9</a>
      * player 9
+     *<p>
+     * Just performs the server side generation of the token. Implementation could user {@link nl.vpro.npoplayer.NpoPlayer9}.
      */
     @POST
     @Path("/token")
