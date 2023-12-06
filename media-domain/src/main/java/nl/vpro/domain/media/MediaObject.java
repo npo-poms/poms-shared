a27f5a8cd3c6cd3e2dd9ed8bc34416744338a90f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2023-12-06 22:26:58 +0100

    More test case failures.

--- media-domain/src/main/java/nl/vpro/domain/media/MediaObject.java
+++ media-domain/src/main/java/nl/vpro/domain/media/MediaObject.java
@@ -355,7 +355,7 @@ public abstract class MediaObject extends PublishableObject<MediaObject>
     @Valid
     protected Set<@NotNull Description> descriptions;
 
-    @ManyToMany(cascade = {DETACH, MERGE, PERSIST, REFRESH}) // todo since the genre table only contains 1 field, namely the id, which is already in the mediaobject_genre table, this odd.
+    @ManyToMany(cascade = {DETACH, MERGE, PERSIST, REFRESH}) // todo since the genre table only contains 1 field, namely the id, which is already in the mediaobject_genre table, this is odd.
     @SortNatural
     @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
     @Valid
