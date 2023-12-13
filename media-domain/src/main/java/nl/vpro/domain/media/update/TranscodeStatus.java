55cd211ff0c0724362e194c3a10c752145453fe0
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2023-12-13 22:23:20 +0100

    removed odd swspes.

--- media-domain/src/main/java/nl/vpro/domain/media/update/TranscodeStatus.java
+++ media-domain/src/main/java/nl/vpro/domain/media/update/TranscodeStatus.java
@@ -63,12 +63,12 @@ public class TranscodeStatus {
     @XmlType(name = "transcodeStatusEnum")
     @Getter
     public enum Status  {
-        RUNNING("De ​workflow ​is ​gestart ​en ​in ​verwerking", false),
-        COMPLETED("De ​workflow ​is ​succesvol ​afgerond ​en ​de ​streams ​zijn ​afgemeld bij ​POMS", true),
-        FAILED("De ​workflow ​kon ​niet ​worden ​voltooid", true),
-        TIMED_OUT("De ​workflow ​was ​niet ​binnen ​een ​redelijke ​tijd ​voltooid ​en ​is afgebroken", true),
-        TERMINATED("De ​workflow ​is ​handmatig ​afgebroken ​door ​een ​NEP ​medewerker", true),
-        PAUSED("De ​workflow ​is ​gepauzeerd ​door ​een ​NEP ​medewerker", false);
+        RUNNING("De workflow is gestart en in verwerking", false),
+        COMPLETED("De workflow is succesvol afgerond en de streams zijn afgemeld bij POMS", true),
+        FAILED("De workflow kon niet worden voltooid", true),
+        TIMED_OUT("De workflow was niet binnen een redelijke tijd voltooid en is afgebroken", true),
+        TERMINATED("De workflow is handmatig afgebroken door een NEP medewerker", true),
+        PAUSED("De workflow is gepauzeerd door een NEP medewerker", false);
         private final String description;
         private final boolean endStatus;
 
