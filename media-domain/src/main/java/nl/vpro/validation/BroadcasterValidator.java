f3ba5e3493e05ca2d78667a90d4a35072ec3768c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2023-12-13 09:18:31 +0100

    Added some proper broadcaster validation.

---
+++ media-domain/src/main/java/nl/vpro/validation/BroadcasterValidator.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2011 Licensed under the Apache License, Version 2.0
+ * VPRO The Netherlands
+ */
+package nl.vpro.validation;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import nl.vpro.domain.user.*;
+
+/**
+ * @since 7.10
+ */
+@Slf4j
+public class BroadcasterValidator implements ConstraintValidator<BroadcasterValidation, Object> {
+
+    private static boolean warned = false;
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext constraintValidatorContext) {
+
+        final BroadcasterService broadcasterService =  ServiceLocator.getBroadcasterService();
+        if (broadcasterService == null || broadcasterService.findAll().isEmpty()) {
+            if (! warned) {
+                log.warn("No broadcaster service found, so no validation");
+            }
+            warned = true;
+            return true;
+        }
+        warned = false;
+        if (value instanceof Broadcaster b) {
+            value  = b.getId();
+        }
+        if (value instanceof CharSequence cs) {
+            return broadcasterService.find(cs.toString()) != null;
+        }
+        throw new IllegalArgumentException("Cannot validate " + value);
+    }
+}
