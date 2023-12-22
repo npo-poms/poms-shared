12fb48cfca3fac7a264e710a227891bb28120d8c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2023-12-22 14:41:54 +0100

    javadoc.

--- user-domain/src/main/java/nl/vpro/domain/user/Portal.java
+++ user-domain/src/main/java/nl/vpro/domain/user/Portal.java
@@ -14,6 +14,11 @@ import javax.xml.bind.annotation.XmlType;
 
 import nl.vpro.domain.Xmlns;
 
+/**
+ * A portal is a website, which may or may not be associated to a one specific {@link Broadcaster}. Some broadcasters maintain multiple portals.
+ * <p>
+ * Content may be associated to a portal. Users may also be associated to a portal. A user may edit content if they shared either a broadcaster or a portal (or a {@link ThirdParty})
+ */
 @Entity
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "portalType", namespace = Xmlns.MEDIA_NAMESPACE)
