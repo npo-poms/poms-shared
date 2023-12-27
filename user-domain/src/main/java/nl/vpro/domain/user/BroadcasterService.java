b197946feaf676f1892bc9daf83d512f84725421
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2023-12-27 09:34:25 +0100

    Javadoc.

--- user-domain/src/main/java/nl/vpro/domain/user/BroadcasterService.java
+++ user-domain/src/main/java/nl/vpro/domain/user/BroadcasterService.java
@@ -57,6 +57,8 @@ public interface BroadcasterService extends OrganizationService<Broadcaster> {
     }
 
     /**
+     * Just try to find the broadcaster using all available ids.
+     * First using @{link #find(String)}, then {@link #findForWhatsOnId(String)}, then {@link #findForMisId(String)}, then {@link #findForNeboId(String)
      * @since 5.13
      */
     default Optional<Broadcaster> findForIds(String value) {
