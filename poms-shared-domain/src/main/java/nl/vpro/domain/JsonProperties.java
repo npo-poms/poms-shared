3a195f83967e09d63a9c6346a5503f9881603ef7
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-02-22 15:37:57 +0100

    Didn't compile

--- poms-shared-domain/src/main/java/nl/vpro/domain/JsonProperties.java
+++ poms-shared-domain/src/main/java/nl/vpro/domain/JsonProperties.java
@@ -1,17 +1,16 @@
 package nl.vpro.domain;
 
-import javax.xml.bind.annotation.XmlEnumValue;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
- * An enum can be made to extend this, which indicates that an extra method will be present {@link #getXmlValue()} which
- * will be the {@link XmlEnumValue} of the enum value.
+ * An enum can be made to extend this, which indicates that an extra method will be present {@link #getJsonPropertyValue()}} which
+ * will be the {@link JsonProperty} of the enum value.
  * <p>
  * Normally this would be {@link Enum#name()}}, but sometimes this is overriden, via the said annotation, and you need programmatic access to it.
  *
  * @author Michiel Meeuwissen
  * @since 8.0
+ * @see XmlValued
  */
 public interface JsonProperties {
 
@@ -31,7 +30,6 @@ public interface JsonProperties {
 
     /**
      *
-     * @since 8.0
      */
     static <E extends Enum<E> & JsonProperties> E valueOfJson(E[] values, String value) {
         for (E v : values) {
@@ -39,7 +37,7 @@ public interface JsonProperties {
                 return v;
             }
         }
-        throw new IllegalArgumentException("No constant with xml value " + value);
+        throw new IllegalArgumentException("No constant with json value " + value);
     }
 
 }
