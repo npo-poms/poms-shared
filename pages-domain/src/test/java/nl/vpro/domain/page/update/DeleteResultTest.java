5cbf04b6e355d857e9dce41bf160d78c72838be4
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-03-05 14:28:05 +0100

    Tst case failure.

--- pages-domain/src/test/java/nl/vpro/domain/page/update/DeleteResultTest.java
+++ pages-domain/src/test/java/nl/vpro/domain/page/update/DeleteResultTest.java
@@ -49,7 +49,7 @@ public class DeleteResultTest {
             .build();
 
         JAXBTestUtil.roundTripAndSimilar(result, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-            "<pageUpdate:deleteresult xmlns:pageUpdate=\"urn:vpro:pages:update:2013\" count=\"100\" notallowedCount=\"0\"  success=\"true\" duration=\"P0DT0H0M0.123S\" xmlns:shared=\"urn:vpro:shared:2009\" xmlns:page=\"urn:vpro:pages:2013\"/>");
+            "<pageUpdate:deleteResult xmlns:pageUpdate=\"urn:vpro:pages:update:2013\" count=\"100\" notallowedCount=\"0\"  success=\"true\" duration=\"P0DT0H0M0.123S\" xmlns:shared=\"urn:vpro:shared:2009\" xmlns:page=\"urn:vpro:pages:2013\"/>");
     }
 
     @Test
@@ -60,7 +60,7 @@ public class DeleteResultTest {
             .build();
 
         JAXBTestUtil.roundTripAndSimilar(result, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-            "<pageUpdate:deleteresult xmlns:pageUpdate=\"urn:vpro:pages:update:2013\" count=\"100\" notallowedCount=\"0\"  success=\"true\" duration=\"P0DT0H0M0.123S\" xmlns:shared=\"urn:vpro:shared:2009\" xmlns:page=\"urn:vpro:pages:2013\"/>");
+            "<pageUpdate:deleteResult xmlns:pageUpdate=\"urn:vpro:pages:update:2013\" count=\"100\" notallowedCount=\"0\"  success=\"true\" duration=\"P0DT0H0M0.123S\" xmlns:shared=\"urn:vpro:shared:2009\" xmlns:page=\"urn:vpro:pages:2013\"/>");
     }
 
     @Test
