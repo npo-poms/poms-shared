adc34fd7d28fe411d38e780f30382c0be8cc53ed
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-03-12 11:33:21 +0100

    Json unmarshalling not ok.

--- poms-npoplayer/src/main/java/nl/vpro/npoplayer9/TokenRequest.java
+++ poms-npoplayer/src/main/java/nl/vpro/npoplayer9/TokenRequest.java
@@ -4,9 +4,9 @@
  */
 package nl.vpro.npoplayer9;
 
-import lombok.*;
+import lombok.Data;
 
-import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.*;
 
 /**
  * Token request for NPO Player 9 endpoint. Just does jwt signing
@@ -17,4 +17,9 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 @JsonTypeName("tokenRequest")
 public class TokenRequest {
     private final String mid;
+
+    @JsonCreator
+    public TokenRequest(@JsonProperty("mid") String mid) {
+        this.mid = mid;
+    }
 }
