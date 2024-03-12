7440398aa807ba3a2b908491a8e7b5f41d3bbca9
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-03-12 14:29:51 +0100

    Made it resistance to empty strings.

--- media-domain/src/main/java/nl/vpro/domain/media/Channel.java
+++ media-domain/src/main/java/nl/vpro/domain/media/Channel.java
@@ -2924,12 +2924,10 @@ public enum Channel implements Displayable, XmlValued {
         final String[] values = channels.split("\\s*,\\s*");
         List<Channel> result = new ArrayList<>();
         for(String value : values) {
-            if (! value.isBlank()) {
-                if (Character.isDigit(value.charAt(0))) {
-                    value = "_" + value;
-                }
-                result.add(Channel.valueOf(value));
+            if(Character.isDigit(value.charAt(0))) {
+                value = "_" + value;
             }
+            result.add(Channel.valueOf(value));
         }
         return result.toArray(new Channel[0]);
     }
