c9ade0768eee158417f187e409fe1f6f14435132
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-05-15 10:48:30 +0200

    Roll back cchanged related related to hibernate search upgrade. They are moved to 7.12 branch now.

--- media-domain/src/main/java/nl/vpro/domain/media/search/MediaSortField.java
+++ media-domain/src/main/java/nl/vpro/domain/media/search/MediaSortField.java
@@ -1,168 +1,58 @@
 package nl.vpro.domain.media.search;
 
-import java.util.Optional;
-import java.util.Set;
-import java.util.function.Predicate;
-
-import nl.vpro.domain.media.*;
-import nl.vpro.domain.media.support.Image;
 import nl.vpro.domain.media.support.OwnerType;
-
-import static nl.vpro.domain.media.search.SortField.Type.INSTANT;
 
 /**
  * @author Michiel Meeuwissen
  * @since 5.0
  */
 public enum MediaSortField implements SortField {
-
-
-
-    sortTitle(Type.STRING, null, "titles.main"),
-    sortTitle_NPO(Type.STRING, null, "titles_NPO.main"),
+    sortTitle(Type.STRING),
+    sortTitle_NPO(Type.STRING),
 
 
     mid(Type.STRING),
+    type(Type.STRING),
+    mediaType(Type.STRING),
+
+    sortDate(Type.LONG),
+    lastModified(Type.LONG),
+    creationDate(Type.LONG, "creationInstant", "creationDate"),
+    publishStop(Type.LONG),
+    publishStart(Type.LONG),
+    lastPublished(Type.LONG),
 
 
-    /**
-     * @deprecated use {@link #mediaType} instead
-     */
-    @Deprecated
-    type(Type.STRING, null),
-    mediaType(Type.STRING) {
-        @Override
-        public String[] derivedFrom(Class<? extends MediaObject> clazz) {
-            return new String[] {"type"};
-        }
-        @Override
-        public Optional<String> nulls() {
-            return Optional.of(MediaType.MEDIA.name());
-        }
-    },
+    lastModifiedBy(Type.STRING),
+    createdBy(Type.STRING),
 
-    sortDate(INSTANT, "sortInstant", null) {
-        @Override
-        public String[] derivedFrom(Class<? extends MediaObject> clazz) {
-            if (clazz.equals(Program.class)) {
-                return new String[]{"scheduleEvents", "creationInstant"};
-            } else if (clazz.equals(Segment.class)) {
-                return new String[]{"parent"};
-            } else {
-                return super.derivedFrom(clazz);
-            }
-        }
-        @Override
-        public Set<Class<? extends MediaObject>> forClasses() {
-            return Set.of(Program.class, Segment.class, Group.class);
+    locations(Type.LONG, "locations", "locationCount"),
 
-        }
-    },
-    lastModified(INSTANT),
-    creationDate(INSTANT, "creationInstant", "creationDate"),
-    publishStop(INSTANT) {
-        @Override
-        public Optional<String> nulls() {
-            return Optional.of(MAX_INSTANT);
-        }
+    memberofCount(Type.LONG),
+    episodeofCount(Type.LONG),
+    scheduleEventsCount(Type.LONG),
 
-    },
-    publishStart(INSTANT) {
-        @Override
-        public Optional<String> nulls() {
-            return Optional.of(MIN_INSTANT);
-        }
-    },
-    lastPublished(INSTANT),
+    firstScheduleEvent(Type.LONG),
+    firstScheduleEventNoRerun(Type.LONG),
+    lastScheduleEvent(Type.LONG),
+    lastScheduleEventNoRerun(Type.LONG)
 
 
-    lastModifiedBy(Type.STRING, null, null),
-    createdBy(Type.STRING, null, null),
-
-    /**
-     * Sort on location count
-     */
-    locations(Type.COUNT, "locations", "locationCount"),
-
-    /**
-     * @since 7.12
-     */
-    publishedLocations(Type.COUNT, "locations", "locationPublishedCount") {
-        @Override
-        public Predicate<Object> predicate() {
-            return i -> ((Location) i).isPublishable();
-        }
-    },
-
-    memberofCount(Type.COUNT, "memberOf", null),
-    episodeofCount(Type.COUNT, "episodeOf", null) {
-        @Override
-        public Set<Class<? extends MediaObject>> forClasses() {
-            return Set.of(Program.class);
-        }
-    },
-    scheduleEventsCount(Type.COUNT, "scheduleEvents", null) {
-        @Override
-        public Set<Class<? extends MediaObject>> forClasses() {
-            return Set.of(Program.class);
-        }
-    },
-    imagesCount(Type.COUNT, "images", "imagesCount"),
-
-    /**
-     * @since 7.12
-     */
-    publishedImagesCount(Type.COUNT, "images", "imagesPublishedCount") {
-        @Override
-        public Predicate<Object> predicate() {
-            return i -> ((Image) i).isPublishable();
-        }
-    },
-     /**
-     * @since 7.12
-     */
-     imagesWithoutCreditsCount(Type.COUNT, "images", "imagesWithoutCreditsCount") {
-         private boolean missingCredits(Image img) {
-             return img.getCredits() == null || img.getCredits().trim().isEmpty() || img.getLicense() == null || img.getSource() == null || img.getSource().trim().isEmpty();
-         }
-         @Override
-         public Predicate<Object> predicate() {
-             return i -> {
-                Image img = (Image) i;
-                return img.isPublishable() && missingCredits(img);
-
-             };
-         }
-     },
-
-    firstScheduleEvent(INSTANT, null ),
-    firstScheduleEventNoRerun(INSTANT, null),
-    lastScheduleEvent(INSTANT, null),
-    lastScheduleEventNoRerun(INSTANT, null)
     ;
-
-
-
-
     private final Type t;
-    private final String property;
+    private final String field;
     private final String sortField;
-
 
     MediaSortField(Type type) {
         t = type;
-        property = name();
+        field = name();
         sortField = name();
     }
-    MediaSortField(Type type, String property) {
-        this(type, property, null);
-
-    }
 
 
-    MediaSortField(Type type, String property, String sortField) {
+    MediaSortField(Type type, String field, String sortField) {
         this.t = type;
-        this.property = property;
+        this.field = field == null ? name() : field;
         this.sortField = sortField == null ? name() : sortField;
     }
     @Override
@@ -171,24 +61,14 @@ public enum MediaSortField implements SortField {
     }
 
     @Override
-    public String property() {
-        return property;
+    public String field() {
+        return field;
     }
+
 
     @Override
     public String sortField() {
         return sortField;
-    }
-
-    public String[] derivedFrom(Class<? extends MediaObject> clazz) {
-        return new String[] {};
-    }
-    public Set<Class<? extends MediaObject>> forClasses() {
-        return Set.of(MediaObject.class);
-    }
-
-    public Predicate<Object> predicate() {
-        return null;
     }
 
 
--- media-domain/src/main/java/nl/vpro/domain/media/search/SortField.java
+++ media-domain/src/main/java/nl/vpro/domain/media/search/SortField.java
@@ -1,45 +1,26 @@
 package nl.vpro.domain.media.search;
 
-import java.util.Optional;
-
 /**
  * @author Michiel Meeuwissen
  * @since 5.0
  */
 public interface SortField {
 
-    String NULL_INSTANT = "1970-01-01T00:00:00Z";
-    String MIN_INSTANT = "1900-01-01T00:00:00Z";
-    String MAX_INSTANT = "2500-01-01T00:00:00Z";
-
 
     String name();
 
     Type type();
 
-    default Optional<String> nulls() {
-        return switch(type()) {
-            case INSTANT -> Optional.of(NULL_INSTANT);
-            case COUNT -> Optional.of("0");
-            case STRING -> Optional.empty();
-        };
+    default String field() {
+        return name();
     }
 
-    /**
-     * The associated property or <code>null</code> if there is none (this field will require manual mapping)
-     *
-     */
-    default String property() {
-        return null;
-    }
-
-    default String sortField() {
+     default String sortField() {
         return name();
     }
 
     enum Type {
         STRING,
-        COUNT,
-        INSTANT
+        LONG
     }
 }
