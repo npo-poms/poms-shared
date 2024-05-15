b8caa594cbc47432aab69d9389f81c11b392683c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-05-15 09:58:30 +0200

    Fixed issues with undeleting available subtitles.

--- media-domain/src/test/java/nl/vpro/domain/media/AvailableSubtitleTest.java
+++ media-domain/src/test/java/nl/vpro/domain/media/AvailableSubtitleTest.java
@@ -1,14 +1,21 @@
 package nl.vpro.domain.media;
 
+import net.jqwik.api.Arbitraries;
+import net.jqwik.api.Arbitrary;
+
 import java.util.Locale;
 
 import org.junit.jupiter.api.Test;
+import org.meeuw.theories.BasicObjectTheory;
 
 import nl.vpro.domain.subtitles.SubtitlesType;
 import nl.vpro.domain.subtitles.SubtitlesWorkflow;
+import nl.vpro.i18n.Locales;
 import nl.vpro.test.util.jaxb.JAXBTestUtil;
 
-public class AvailableSubtitleTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AvailableSubtitleTest implements BasicObjectTheory<AvailableSubtitles> {
 
     @Test
     public void test() {
@@ -22,4 +29,44 @@ public class AvailableSubtitleTest {
                 """);
     }
 
+    @Test
+    public void equalsAndHashCode() {
+        var en1 = AvailableSubtitles.builder()
+                .language(Locale.ENGLISH)
+                .type(SubtitlesType.TRANSLATION)
+                .workflow(SubtitlesWorkflow.MISSING)
+                .build();
+        var en2 = AvailableSubtitles.builder()
+                .language(Locale.ENGLISH)
+                .type(SubtitlesType.TRANSLATION)
+                .workflow(SubtitlesWorkflow.FOR_REPUBLICATION)
+                .build();
+
+        assertThat(en1).isEqualTo(en2);
+    }
+
+    @Override
+    public Arbitrary<? extends AvailableSubtitles> datapoints() {
+        return Arbitraries.of(
+            AvailableSubtitles.builder()
+                .language(Locale.ENGLISH)
+                .type(SubtitlesType.TRANSLATION)
+                .workflow(SubtitlesWorkflow.MISSING)
+                .build(),
+            AvailableSubtitles.builder()
+                .language(Locale.ENGLISH)
+                .type(SubtitlesType.TRANSLATION)
+                .workflow(SubtitlesWorkflow.FOR_REPUBLICATION)
+                .build(),
+            AvailableSubtitles.builder()
+                .language(Locales.NETHERLANDISH)
+                .type(SubtitlesType.CAPTION)
+                .build(),
+            AvailableSubtitles.builder()
+                .language(Locale.JAPANESE)
+                .type(SubtitlesType.CAPTION)
+                .build()
+        );
+
+    }
 }
