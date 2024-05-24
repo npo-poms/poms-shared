db57309eb6fc3fcc6934990f71d061813a8412ea
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-05-24 08:51:01 +0200

    Testing.

--- media-domain/src/main/java/nl/vpro/domain/media/MediaObject.java
+++ media-domain/src/main/java/nl/vpro/domain/media/MediaObject.java
@@ -160,6 +160,7 @@ import static nl.vpro.domain.media.support.Workflow.PUBLICATIONS;
     "publishStop",
     "urn",
     "embeddable",
+    "adoptQualityFromPlus",
     /* xml elements */
     "episodeOf",
     "crids",
