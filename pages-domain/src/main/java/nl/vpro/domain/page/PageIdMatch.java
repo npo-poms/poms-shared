a948ac832e95ec05d7d529f4af4f66951f9a2533
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-06-21 16:02:51 +0200

    This is an obvious performance improvement.

--- pages-domain/src/main/java/nl/vpro/domain/page/PageIdMatch.java
+++ pages-domain/src/main/java/nl/vpro/domain/page/PageIdMatch.java
@@ -8,17 +8,31 @@ import lombok.Getter;
  */
 
 @Getter
-public enum PageIdMatch {
+public enum PageIdMatch  {
 
     URL(true, false),
     CRID(false, true),
-    BOTH(true, true);
+    BOTH(true, true),
+
+    /**
+     * Indicates that effective page id matching should be determined using {@link #forRef(String)}
+     */
+    AUTOMATIC(true, true)
+    ;
     private final boolean matchUrl;
     private final boolean matchCrid;
 
     PageIdMatch(boolean matchUrl, boolean matchCrid) {
         this.matchUrl = matchUrl;
         this.matchCrid = matchCrid;
+    }
+
+    public static PageIdMatch forRef(String pageRef) {
+        if (pageRef.startsWith("crid")) {
+            return CRID;
+        } else {
+            return URL;
+        }
     }
 
 }
