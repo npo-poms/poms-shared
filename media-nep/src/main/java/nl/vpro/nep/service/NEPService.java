ecd899f41bee63a64519aca7163465bdd13c4ef1
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-06-25 14:04:18 +0200

    javadoc.

--- media-nep/src/main/java/nl/vpro/nep/service/NEPService.java
+++ media-nep/src/main/java/nl/vpro/nep/service/NEPService.java
@@ -1,6 +1,15 @@
 package nl.vpro.nep.service;
 
+import lombok.SneakyThrows;
+
+import java.time.Duration;
+import java.util.function.Consumer;
+
 /**
+ *  Wraps all NEP services ({@link NEPGatekeeperService}, {@link NEPDownloadService}, {@link NEPUploadService}, {@link NEPItemizeService}, {@link NEPSAMService}, {@link NEPPlayerTokenService}) in one service.
+ * These things are often related, and it is handy to be able to wire just one service.
+ *
+ * @see nl.vpro.nep.service.impl.NEPServiceImpl
  * @author Michiel Meeuwissen
  * @since 5.6
  */
@@ -11,4 +20,25 @@ public interface NEPService extends
     NEPItemizeService,
     NEPSAMService,
     NEPPlayerTokenService {
+
+
+
+    enum RateRapType {
+        ITEMIZE,
+
+    }
+
+    interface RateRapHandling extends Consumer<Duration> {
+    }
+
+    public static RateRapHandling DELAY = new RateRapHandling() {
+        @SneakyThrows
+        @Override
+        public void accept(Duration duration) {
+            Thread.sleep(duration.toMillis());
+        }
+    };
+
+    ThreadLocal<RateRapHandling> handling  = ThreadLocal.withInitial(() -> DELAY);
+
 }
