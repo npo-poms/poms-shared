5efb82034649f82cf921686d4495737359f8ed02
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-09-27 14:39:05 +0200

    Testing availability.

--- media-berlijn/src/main/java/nl/vpro/berlijn/domain/availability/AvailabilityPeriod.java
+++ media-berlijn/src/main/java/nl/vpro/berlijn/domain/availability/AvailabilityPeriod.java
@@ -6,12 +6,16 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.google.common.collect.Range;
 
 import nl.vpro.util.Ranges;
+
+/**
+ * @param transmissionId guci
+ */
 @JsonIgnoreProperties({
     // used to exist?
     "geoip",
     "encryption"
 })
-
+@lombok.Builder
 public record AvailabilityPeriod(
     String transmissionId,
     String onDemandTransmissionId,
@@ -25,5 +29,10 @@ public record AvailabilityPeriod(
 
     public Range<Instant> range() {
         return Ranges.closedOpen(start(), stop());
+    }
+
+
+    public String guci() {
+        return transmissionId;
     }
 }
