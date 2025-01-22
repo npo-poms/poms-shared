a9be90ef63a7cd0c76c909a5f76ed294d53864ef
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-01-22 14:36:12 +0100

    Missing depdnency.

--- media-domain/src/test/java/nl/vpro/domain/media/ProgramTest.java
+++ media-domain/src/test/java/nl/vpro/domain/media/ProgramTest.java
@@ -8,7 +8,10 @@ import java.util.SortedSet;
 
 import org.junit.jupiter.api.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import nl.vpro.domain.media.exceptions.CircularReferenceException;
+import nl.vpro.jackson2.Jackson2Mapper;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -67,5 +70,53 @@ public class ProgramTest {
 
             program.createEpisodeOf(group, 1, null);
         }).isInstanceOf(CircularReferenceException.class);
+    }
+
+    @Test
+    public void moveEvent() throws JsonProcessingException {
+        Program p = new Program();
+        p.setMid("POW_05759681");
+        ScheduleEvent s = Jackson2Mapper.getLenientInstance().readValue(
+            """
+                      {
+                        "channel": "NED1",
+                        "start": 1733763600000,
+                        "guideDay": 1733698800000,
+                        "duration": 900000,
+                        "midRef": "POW_05759681",
+                        "poProgID": "POW_05759681",
+                        "textSubtitles": "Teletekst ondertitels",
+                        "rerun": false,
+                        "imi": "imi:1612093401",
+                        "urnRef": "crid://media-press.tv/394635384",
+                        "eventStart": 1733763600000
+                      }\
+                """, ScheduleEvent.class);
+        p.addScheduleEvent(s);
+        Program p2 = new Program();
+        p2.setMid("POW_05759681");
+
+        ScheduleEvent s2 = Jackson2Mapper.getLenientInstance().readValue(
+            """
+                      {
+                        "channel": "BVNT",
+                        "start": 1733763600000,
+                        "guideDay": 1733698800000,
+                        "duration": 900000,
+                        "midRef": "POW_05759681",
+                        "poProgID": "POW_05759681",
+                        "textSubtitles": "Teletekst ondertitels",
+                        "rerun": false,
+                        "imi": "imi:1612093401",
+                        "urnRef": "crid://media-press.tv/394635384",
+                        "eventStart": 1733763600000
+                      }\
+                """, ScheduleEvent.class);
+        s2.setParent(p2);
+
+        s2.setParent(p);
+        assertThat(p.getScheduleEvents()).hasSize(2);
+
+
     }
 }
