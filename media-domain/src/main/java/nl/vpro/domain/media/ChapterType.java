be76232547190f78e18b46b12a9eb0b823590d2a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-03-07 15:39:54 +0100

    reference to issue.

--- media-domain/src/main/java/nl/vpro/domain/media/ChapterType.java
+++ media-domain/src/main/java/nl/vpro/domain/media/ChapterType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import nl.vpro.i18n.Displayable;
 
 /**
+ * See <a href="https://publiekeomroep.atlassian.net/browse/P0MS-236">jira</a>
  * @since 8.6
  */
 public enum ChapterType implements Displayable {
