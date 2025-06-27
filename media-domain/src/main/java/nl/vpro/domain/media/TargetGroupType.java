5acf8aeedaba162ce662a08b1a614be77fadb100
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-06-27 11:04:17 +0200

    @sicne tags only.

--- media-domain/src/main/java/nl/vpro/domain/media/TargetGroupType.java
+++ media-domain/src/main/java/nl/vpro/domain/media/TargetGroupType.java
@@ -13,18 +13,33 @@ public enum TargetGroupType implements Displayable {
     KIDS_6("Kinderen tot 6 jaar (Zappelin)"),
     KIDS_12("Kinderen 6-12 (Zapp)"),
 
+    /**
+     * @since 8.10
+     */
     @XmlEnumValue("3-6")
     _3_6("Kindprofiel 3-6"),
 
+    /**
+     * @since 8.10
+     */
     @XmlEnumValue("-9")
     _9("Kindprofiel tot 9"),
 
+    /**
+     * @since 8.10
+     */
     @XmlEnumValue("-12")
     _12("Kindprofiel tot 12"),
 
+    /**
+     * @since 8.10
+     */
     @XmlEnumValue("-14")
     _14("Kindprofiel tot 14"),
 
+    /**
+     * @since 8.10
+     */
     @XmlEnumValue("-16")
     _16("Kindprofiel tot 16"),
 
