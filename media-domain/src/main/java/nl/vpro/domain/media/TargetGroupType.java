f3e4df622464d54676f2144fb878b6d2c1d75abd
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-06-27 10:11:16 +0200

    P0MS-254

--- media-domain/src/main/java/nl/vpro/domain/media/TargetGroupType.java
+++ media-domain/src/main/java/nl/vpro/domain/media/TargetGroupType.java
@@ -2,8 +2,7 @@ package nl.vpro.domain.media;
 
 import lombok.Getter;
 
-import jakarta.xml.bind.annotation.XmlEnum;
-import jakarta.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.*;
 
 import nl.vpro.i18n.Displayable;
 
@@ -13,6 +12,22 @@ import nl.vpro.i18n.Displayable;
 public enum TargetGroupType implements Displayable {
     KIDS_6("Kinderen tot 6 jaar (Zappelin)"),
     KIDS_12("Kinderen 6-12 (Zapp)"),
+
+    @XmlEnumValue("3-6")
+    _3_6("Kindprofiel 3-6"),
+
+    @XmlEnumValue("-9")
+    _9("Kindprofiel tot 9"),
+
+    @XmlEnumValue("-12")
+    _12("Kindprofiel tot 12"),
+
+    @XmlEnumValue("-14")
+    _14("Kindprofiel tot 14"),
+
+    @XmlEnumValue("-16")
+    _16("Kindprofiel tot 16"),
+
     YOUNG_ADULTS("Jongeren (NPO3)"),
     ADULTS("Volwassenen"),
     ADULTS_WITH_KIDS_6( "Volwassenen met kinderen 0-6 jaar"),
