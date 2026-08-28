f282aba4de70b9fddc0a73fe663cd66b05694a42
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-08-28 13:56:47 +0200

    surpressing output

--- subtitles-domain/src/test/java/nl/vpro/domain/subtitles/WEBVTTandSRTTest.java
+++ subtitles-domain/src/test/java/nl/vpro/domain/subtitles/WEBVTTandSRTTest.java
@@ -1,12 +1,13 @@
 package nl.vpro.domain.subtitles;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 
+import nl.vpro.logging.Log4j2OutputStream;
 import nl.vpro.util.CountedIterator;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,17 +16,14 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Michiel Meeuwissen
  * @since ...
  */
-@Slf4j
+@Log4j2
 public class WEBVTTandSRTTest {
 
 
     @Test
     public void test() {
-
         Stream<Cue> bla = WEBVTTandSRT.parseSRT("bla", getClass().getClassLoader().getResourceAsStream("VPWON_1272504.srt"));
-
         assertThat(bla).hasSize(181);
-
     }
 
 
@@ -33,9 +31,7 @@ public class WEBVTTandSRTTest {
     public void MSE4363() throws IOException {
         Subtitles subtitles = Subtitles.builder().value(getClass().getResourceAsStream("/WO_NTR_15099292.srt")).format(SubtitlesFormat.SRT).build();
         CountedIterator<StandaloneCue> parsed = SubtitlesUtil.standaloneIterator(subtitles, true, true);
-        SubtitlesUtil.stream(parsed, SubtitlesFormat.WEBVTT, System.out);
-
-
+        SubtitlesUtil.stream(parsed, SubtitlesFormat.WEBVTT, Log4j2OutputStream.debug(log));
     }
 
 }
